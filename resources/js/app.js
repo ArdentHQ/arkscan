ab88028f75dab9019a736def7a158aedcfac8e7c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-17 03:05:05 +0100

    feat: avatars (#16)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import "alpinejs";
+import { picasso } from "@vechain/picasso";
+
+window.createAvatar = (seed) =>
+    picasso(seed).replace('width="100" height="100"', "");
---
+++ resources/views/components/avatar.blade.php
@@ -0,0 +1,1 @@
+<x-ark-avatar :identifier="$identifier" class="rounded-full {{ $size ?? 'w-6 h-6 md:w-11 md:h-11' }}" />
