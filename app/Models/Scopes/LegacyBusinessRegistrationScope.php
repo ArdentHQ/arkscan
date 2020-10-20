3aab9dbd4eee15486223c8fc8198eb2cd851cb00
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-20 08:55:23 +0300

    fat: implement transaction type scopes (#55)

---
+++ app/Models/Scopes/LegacyBusinessRegistrationScope.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Scopes;
+
+use App\Enums\MagistrateTransactionTypeEnum;
+use App\Enums\TransactionTypeGroupEnum;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+final class LegacyBusinessRegistrationScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('type_group', TransactionTypeGroupEnum::MAGISTRATE);
+        $builder->where('type', MagistrateTransactionTypeEnum::BUSINESS_REGISTRATION);
+    }
+}
