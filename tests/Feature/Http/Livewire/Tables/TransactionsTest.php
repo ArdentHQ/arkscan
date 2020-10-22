1215662fcb7564908e659d06d17391616d949bff
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-22 08:20:02 +0300

    test: add coverage for block/transaction/wallet tables (#87)

---
+++ tests/Feature/Http/Livewire/Tables/TransactionsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Livewire\Tables\Transactions;
+use App\Models\Transaction;
+use App\ViewModels\ViewModelFactory;
+use Livewire\Livewire;
+
+use function Tests\configureExplorerDatabase;
+
+beforeEach(fn () => configureExplorerDatabase());
+
+it('should list the first page of records', function () {
+    Transaction::factory(60)->create();
+
+    $component = Livewire::test(Transactions::class, [
+        'transactions' => Transaction::latestByTimestamp(),
+    ]);
+
+    foreach (ViewModelFactory::paginate(Transaction::latestByTimestamp()->paginate())->items() as $transaction) {
+        $component->assertSee($transaction->id());
+        $component->assertSee($transaction->timestamp());
+        $component->assertSee($transaction->sender());
+        $component->assertSee($transaction->recipient());
+        $component->assertSee($transaction->fee());
+        $component->assertSee($transaction->amount());
+    }
+});
---
+++ tests/Feature/Http/Livewire/Tables/WalletsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Livewire\Tables\Wallets;
+use App\Models\Wallet;
+use App\ViewModels\ViewModelFactory;
+
+use Livewire\Livewire;
+use function Tests\configureExplorerDatabase;
+
+beforeEach(fn () => configureExplorerDatabase());
+
+it('should list the first page of records', function () {
+    Wallet::factory(60)->create();
+
+    $component = Livewire::test(Wallets::class, [
+        'wallets' => Wallet::wealthy(),
+    ]);
+
+    foreach (ViewModelFactory::paginate(Wallet::wealthy()->paginate())->items() as $wallet) {
+        $component->assertSee($wallet->address());
+        $component->assertSee($wallet->balance());
+    }
+});
