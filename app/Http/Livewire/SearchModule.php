eb6fe27efe168a098acdde0c437e79a3bfb967f5
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-22 11:24:10 +0300

    feat: implement simple unique identifier search (#91)

--- app/Http/Livewire/SearchModule.php
+++ app/Http/Livewire/SearchModule.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace App\Http\Livewire;
 
 use App\Http\Livewire\Concerns\ManagesSearch;
+use App\Models\Block;
+use App\Models\Transaction;
+use App\Models\Wallet;
+use App\Services\Search\BlockSearch;
+use App\Services\Search\TransactionSearch;
+use App\Services\Search\WalletSearch;
 use Livewire\Component;
 
 final class SearchModule extends Component
@@ -30,7 +36,61 @@ final class SearchModule extends Component
         if ($this->isAdvanced) {
             $this->emit('searchTriggered', $data);
         } else {
+            if ($this->searchWallet()) {
+                return;
+            }
+
+            if ($this->searchTransaction()) {
+                return;
+            }
+
+            if ($this->searchBlock()) {
+                return;
+            }
+
             $this->redirectRoute('search', ['state' => $data]);
         }
+    }
+
+    private function searchWallet(): bool
+    {
+        /** @var Wallet|null */
+        $wallet = (new WalletSearch())->search(['term' => $this->state['term']])->first();
+
+        if (is_null($wallet)) {
+            return false;
+        }
+
+        $this->redirectRoute('wallet', $wallet->address);
+
+        return true;
+    }
+
+    private function searchTransaction(): bool
+    {
+        /** @var Transaction|null */
+        $transaction = (new TransactionSearch())->search(['term' => $this->state['term']])->first();
+
+        if (is_null($transaction)) {
+            return false;
+        }
+
+        $this->redirectRoute('transaction', $transaction->id);
+
+        return true;
+    }
+
+    private function searchBlock(): bool
+    {
+        /** @var Block|null */
+        $block = (new BlockSearch())->search(['term' => $this->state['term']])->first();
+
+        if (is_null($block)) {
+            return false;
+        }
+
+        $this->redirectRoute('block', $block->id);
+
+        return true;
     }
 }
--- tests/Feature/Http/Livewire/SearchModuleTest.php
+++ tests/Feature/Http/Livewire/SearchModuleTest.php
@@ -3,33 +3,52 @@
 declare(strict_types=1);
 
 use App\Http\Livewire\SearchModule;
+use App\Models\Block;
+use App\Models\Transaction;
+use App\Models\Wallet;
 use Livewire\Livewire;
-
 use function Tests\configureExplorerDatabase;
 
 beforeEach(fn () => configureExplorerDatabase());
 
-it('should search by type', function () {
+it('should search for a wallet and redirect', function () {
+    $wallet = Wallet::factory()->create();
+
     Livewire::test(SearchModule::class)
-        ->call('performSearch');
+        ->set('state.term', $wallet->address)
+        ->set('state.type', 'wallet')
+        ->call('performSearch')
+        ->assertRedirect(route('wallet', $wallet->address));
 });
 
-it('should search by amount range', function () {
+it('should search for a transaction and redirect', function () {
+    $transaction = Transaction::factory()->create();
+
     Livewire::test(SearchModule::class)
-        ->call('performSearch');
+        ->set('state.term', $transaction->id)
+        ->set('state.type', 'transaction')
+        ->call('performSearch')
+        ->assertRedirect(route('transaction', $transaction->id));
 });
 
-it('should search by fee range', function () {
+it('should search for a block and redirect', function () {
+    $block = Block::factory()->create();
+
     Livewire::test(SearchModule::class)
-        ->call('performSearch');
+        ->set('state.term', $block->id)
+        ->set('state.type', 'block')
+        ->call('performSearch')
+        ->assertRedirect(route('block', $block->id));
 });
 
-it('should search by date range', function () {
+it('should redirect to the advanced search page if there are no results', function () {
     Livewire::test(SearchModule::class)
-        ->call('performSearch');
-});
-
-it('should search by vendor field', function () {
-    Livewire::test(SearchModule::class)
-        ->call('performSearch');
+        ->set('state.term', 'unknown')
+        ->set('state.type', 'block')
+        ->call('performSearch')
+        ->assertRedirect(route('search', [
+            'state[term]'            => 'unknown',
+            'state[transactionType]' => 'transfer',
+            'state[type]'            => 'block',
+        ]));
 });
