f91add3718aeb92626b78510a12c1856309c4e88
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-23 05:21:58 +0300

    test: transaction filter applying through event (#111)

--- tests/Feature/Http/Livewire/TransactionTableTest.php
+++ tests/Feature/Http/Livewire/TransactionTableTest.php
@@ -78,3 +78,50 @@ it('should apply filters', function () {
         $component->assertSee($transaction->amount());
     }
 });
+
+it('should apply filters through an event', function () {
+    $block = Block::factory()->create();
+    $wallet = Wallet::factory()->create([
+        'public_key' => 'public_key',
+        'address'    => 'address',
+    ]);
+
+    $component = Livewire::test(TransactionTable::class);
+
+    $notExpected = Transaction::factory(10)->create([
+        'id'                => (string) Uuid::uuid4(),
+        'block_id'          => $block->id,
+        'type'              => TransactionTypeGroupEnum::CORE,
+        'type_group'        => CoreTransactionTypeEnum::TRANSFER,
+        'sender_public_key' => $wallet->public_key,
+        'recipient_id'      => $wallet->address,
+        'timestamp'         => 112982056,
+        'fee'               => 1e8,
+        'amount'            => 1e8,
+    ]);
+
+    foreach (ViewModelFactory::collection($notExpected) as $transaction) {
+        $component->assertDontSee($transaction->id());
+        $component->assertDontSee($transaction->timestamp());
+        $component->assertDontSee($transaction->sender());
+        $component->assertDontSee($transaction->recipient());
+        $component->assertDontSee($transaction->fee());
+        $component->assertDontSee($transaction->amount());
+    }
+
+    $expected = Transaction::factory(10)->create([
+        'type_group' => TransactionTypeGroupEnum::CORE,
+        'type'       => CoreTransactionTypeEnum::VOTE,
+    ]);
+
+    $component->emit('filterTransactionsByType', 'vote');
+
+    foreach (ViewModelFactory::collection($expected) as $transaction) {
+        $component->assertSee($transaction->id());
+        $component->assertSee($transaction->timestamp());
+        $component->assertSee($transaction->sender());
+        $component->assertSee($transaction->recipient());
+        $component->assertSee($transaction->fee());
+        $component->assertSee($transaction->amount());
+    }
+});
