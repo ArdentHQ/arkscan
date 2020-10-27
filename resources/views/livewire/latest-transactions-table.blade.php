a7e7e130e236eca74fe754c2e94ccbc4dc414b3d
Brian Faust <hello@basecode.sh>
2020-10-27 17:14:31 +0200

    chore: show mobile table for latest transactions

--- resources/views/livewire/latest-transactions-table.blade.php
+++ resources/views/livewire/latest-transactions-table.blade.php
@@ -1,7 +1,7 @@
 <div id="transaction-list" class="w-full" wire:poll.8s>
     <x-transactions.table-desktop :transactions="$transactions" />
 
-    {{-- <x-transactions.list-mobile :transactions="$transactions" /> --}}
+    <x-transactions.list-mobile :transactions="$transactions" />
 
     <div class="pt-4 mt-8 border-t border-theme-secondary-300 dark:border-theme-secondary-800 md:mt-0 md:border-dashed">
         <a href="{{ route('transactions') }}" class="w-full button-secondary">@lang('actions.view_all')</a>
