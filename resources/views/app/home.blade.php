dd05b7be7485a593e04264eeb13d484cf50fbead
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-27 09:12:30 +0200

    fix: hide price chart if network can not be exchanged (#197)

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -9,39 +9,7 @@
         <x-general.search.header />
 
         @if(Settings::usesCharts())
-            <div class="content-container">
-                <div class="flex-col hidden w-full divide-y sm:flex divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-800">
-                    <div class="flex flex-col w-full pt-16 space-x-0 lg:flex-row lg:space-x-10">
-                        @if(Settings::usesPriceChart())
-                            <x-charts.price :data="$prices" identifier="price" colours-scheme="#339A51" />
-                        @endif
-
-                        @if(Settings::usesFeeChart())
-                            <x-charts.price :data="$fees" identifier="fees" colours-scheme="#FFAE10" />
-                        @endif
-                    </div>
-
-                    <div class="grid w-full grid-flow-row grid-cols-2 gap-6 pt-8 mt-5 mb-16 {{ Network::canbeExchanged() ?  'xl:grid-cols-4' : 'xl:grid-cols-3'}} gap-y-12 xl:gap-y-4">
-                        @if(Network::canBeExchanged())
-                            <x-details-box :title="trans('pages.home.network-details.price')" icon="app-price">
-                                {{ $aggregates['price'] }}
-                            </x-details-box>
-                        @endif
-
-                        <x-details-box :title="trans('pages.home.network-details.lifetime_transactions_volume')" icon="app-volume">
-                            {{ $aggregates['volume'] }}
-                        </x-details-box>
-
-                        <x-details-box :title="trans('pages.home.network-details.lifetime_transactions')" icon="app-transactions-amount">
-                            {{ $aggregates['transactionsCount'] }}
-                        </x-details-box>
-
-                        <x-details-box :title="trans('pages.home.network-details.total_votes')" :extra-value="$aggregates['votesPercentage']" icon="app-votes">
-                            {{ $aggregates['votesCount'] }}
-                        </x-details-box>
-                    </div>
-                </div>
-            </div>
+            <x-home.charts />
         @endif
 
         <x-home.content />
--- resources/views/livewire/navbar-settings.blade.php
+++ resources/views/livewire/navbar-settings.blade.php
@@ -34,15 +34,15 @@
                         <option value="USD">USD</option>
                     </select>
                 </x-navbar.setting-option>
-            @endif
 
-            <x-navbar.setting-option :title="trans('forms.settings.price_chart.title')" :subtitle="trans('forms.settings.price_chart.description')">
-                <x-ark-toggle
-                    name="state.priceChart"
-                    :default="$this->state['priceChart'] ? 'true' : 'false'"
-                    hide-label
-                />
-            </x-navbar.setting-option>
+                <x-navbar.setting-option :title="trans('forms.settings.price_chart.title')" :subtitle="trans('forms.settings.price_chart.description')">
+                    <x-ark-toggle
+                        name="state.priceChart"
+                        :default="$this->state['priceChart'] ? 'true' : 'false'"
+                        hide-label
+                    />
+                </x-navbar.setting-option>
+            @endif
 
             <x-navbar.setting-option :title="trans('forms.settings.fee_chart.title')" :subtitle="trans('forms.settings.fee_chart.description')">
                 <x-ark-toggle
