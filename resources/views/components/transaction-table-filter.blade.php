83600563c580e05198409c787d94f0f3cbd1b059
Jolan <jolanbeer@gmail.com>
2020-10-27 11:03:12 +0100

    refactor: transactions filtering dropdown design fixes (#178)

--- resources/views/components/transaction-table-filter.blade.php
+++ resources/views/components/transaction-table-filter.blade.php
@@ -4,35 +4,36 @@
     transactionTypeFilterLabel: 'All',
 }" x-cloak>
     <x-ark-dropdown
-        dropdown-classes="left-0 w-64 mt-3 overflow-y-scroll h-128"
+        dropdown-classes="left-0 w-64 mt-3 dark:bg-theme-secondary-900 w-full"
         button-class="items-end w-64 h-10 pb-0 pr-0 dropdown-button"
         dropdown-property="filterOpen"
         :init-alpine="false"
     >
         @slot('button')
-            <div class="flex items-center justify-end w-full space-x-2 font-semibold flex-inline text-theme-secondary-700">
+            <div class="flex items-center justify-end w-full space-x-2 font-semibold flex-inline">
                 <div>
-                    @lang('general.transaction.type'): <span x-text="transactionTypeFilterLabel"></span>
+                    <span class="text-theme-secondary-500">@lang('general.transaction.type'): </span>
+                    <span class="text-theme-secondary-700"x-text="transactionTypeFilterLabel"></span>
                 </div>
 
                 <span
                     :class="{ 'rotate-180 bg-theme-primary-600 text-theme-secondary-100': filterOpen }"
-                    class="flex items-center justify-center w-4 h-4 transition duration-150 ease-in-out rounded-full bg-theme-primary-100 text-theme-primary-600"
+                    class="flex items-center justify-center w-4 h-4 transition duration-150 ease-in-out rounded-full bg-theme-primary-100 dark:bg-theme-secondary-800 text-theme-primary-600 dark:text-theme-secondary-200"
                 >
-                    @svg('chevron-up', 'h-3 w-2')
+                    @svg('chevron-down', 'h-3 w-2')
                 </span>
             </div>
         @endslot
 
-        <div class="items-center justify-center block py-3">
+        <div class="items-center justify-center block py-3 overflow-y-scroll h-128 dropdown-scrolling">
             <div
-                class="cursor-pointer dropdown-entry text-theme-secondary-900"
+                class="cursor-pointer dropdown-entry text-theme-secondary-900 dark:text-theme-secondary-200"
                 @click="window.livewire.emit('filterTransactionsByType', 'all'); transactionTypeFilter = 'all'; transactionTypeFilterLabel = '@lang('forms.search.transaction_types.all')'"
             >
                 @lang('forms.search.transaction_types.all')
             </div>
 
-            <div class="w-full border-b border-theme-secondary-300"></div>
+            <hr class="mx-8 mt-4 border-b border-dashed border-theme-secondary-300">
 
             @foreach([
                 'core' => [
@@ -76,11 +77,11 @@
                     'productEntityUpdate',
                 ],
             ] as $typeGroup => $types)
-                <span class="flex items-center w-full px-8 pt-8 text-sm font-bold leading-5 text-left text-theme-secondary-500">{{ ucfirst($typeGroup) }}</span>
+                <span class="flex items-center w-full px-8 pt-8 text-sm font-bold leading-5 text-left text-theme-secondary-500">{{ strtoupper($typeGroup) }}</span>
 
                 @foreach ($types as $type)
                     <div
-                        class="cursor-pointer dropdown-entry text-theme-secondary-900"
+                        class="cursor-pointer dropdown-entry text-theme-secondary-900 dark:text-theme-secondary-200"
                         @click="window.livewire.emit('filterTransactionsByType', '{{ $type }}'); transactionTypeFilter = '{{ $type }}'; transactionTypeFilterLabel = '@lang('forms.search.transaction_types.'.$type)'"
                     >
                         @lang('forms.search.transaction_types.'.$type)
@@ -88,7 +89,7 @@
                 @endforeach
 
                 @if (! $loop->last)
-                    <div class="w-full border-b border-theme-secondary-300"></div>
+                    <hr class="mx-8 mt-3 border-b border-dashed border-theme-secondary-300">
                 @endif
             @endforeach
         </div>
