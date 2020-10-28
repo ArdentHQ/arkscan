dfb565249ebb4d203666c8ccdf1b0afea2b56676
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-28 10:25:28 +0000

    refactor: add mobile footer (#206)

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,7 @@
-<div class="hidden bg-theme-secondary-900 lg:block">
-    <div class="flex flex-col content-container">
-        <x-ark-footer-bar-desktop />
+<div class="border-t bg-theme-secondary-900 border-theme-secondary-800">
+    <div class="flex-col hidden px-8 mx-auto lg:flex max-w-7xl">
+        <x-ark-footer-bar-desktop class="" />
     </div>
+
+    <x-ark-footer-bar-mobile class="px-8 pb-8 lg:hidden" />
 </div>
