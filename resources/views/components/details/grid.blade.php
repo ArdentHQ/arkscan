79d6ed8d3f27a51781ac1d8ac6b11704e83afeca
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-29 16:10:34 +0200

    fix: transaction grid top padding (#260)

--- resources/views/components/details/grid.blade.php
+++ resources/views/components/details/grid.blade.php
@@ -1,6 +1,6 @@
 <div class="content-container">
     <div class="flex flex-col w-full divide-y divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-800">
-        <div class="grid w-full grid-flow-row grid-cols-1 gap-6 pt-8 mt-5 mb-16 md:grid-cols-2 gap-y-12 xl:gap-y-4">
+        <div class="grid w-full grid-flow-row grid-cols-1 gap-6 pt-8 mb-16 md:grid-cols-2 gap-y-12 xl:gap-y-4">
             {{ $slot }}
         </div>
     </div>
