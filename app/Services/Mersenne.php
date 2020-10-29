f8c2b868b1c2b22655085cb8def948d190fc0125
Brian Faust <hello@basecode.sh>
2020-10-29 12:32:58 +0200

    fix: twister class name

--- app/Services/Mersenne.php
+++ app/Services/Mersenne.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use mersenne_twister\mersenne_twister;
+use mersenne_twister\twister;
 
 final class Mersenne
 {
-    private mersenne_twister $twister;
+    private twister $twister;
 
     public function __construct(int $seed)
     {
         ini_set('precision', '16');
 
-        $this->twister = new mersenne_twister($seed);
+        $this->twister = new twister($seed);
     }
 
     public function random(): float
