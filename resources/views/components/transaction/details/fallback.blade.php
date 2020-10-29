7ff43e4a1a7ed2f16b12bb180c155d2ac3eb6783
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-29 16:28:48 +0200

    fix: add copy button for transaction block ID (#261)

--- resources/views/components/transaction/details/fallback.blade.php
+++ resources/views/components/transaction/details/fallback.blade.php
@@ -9,9 +9,12 @@
 </x-details.generic>
 
 <x-details.generic :title="trans('general.transaction.block_id')" icon="app-block-id" without-border>
-    <a href="{{ route('block', $transaction->blockId()) }}" class="font-semibold link">
-        <x-truncate-middle :value="$transaction->blockId()" :length="32" />
-    </a>
+    <span class="flex items-center">
+        <a href="{{ route('block', $transaction->blockId()) }}" class="font-semibold link">
+            <x-truncate-middle :value="$transaction->blockId()" :length="32" />
+        </a>
+        <x-ark-clipboard :value="$transaction->blockId()" class="flex items-center w-auto h-auto ml-2 text-theme-secondary-600" no-styling />
+    </span>
 </x-details.generic>
 
 <x-details.generic :title="trans('general.transaction.nonce')" icon="app-nonce" without-border>
--- resources/views/components/transaction/details/multi-signature.blade.php
+++ resources/views/components/transaction/details/multi-signature.blade.php
@@ -11,9 +11,12 @@
     icon="app-volume" />
 
 <x-details.generic :title="trans('general.transaction.block_id')" icon="app-block-id">
-    <a href="{{ route('block', $transaction->blockId()) }}" class="font-semibold link">
-        <x-truncate-middle :value="$transaction->blockId()" :length="32" />
-    </a>
+    <span class="flex items-center">
+        <a href="{{ route('block', $transaction->blockId()) }}" class="font-semibold link">
+            <x-truncate-middle :value="$transaction->blockId()" :length="32" />
+        </a>
+        <x-ark-clipboard :value="$transaction->blockId()" class="flex items-center w-auto h-auto text-theme-secondary-600" no-styling />
+    </span>
 </x-details.generic>
 
 <x-details.generic :title="trans('general.transaction.timestamp')" icon="app-timestamp" without-border>
