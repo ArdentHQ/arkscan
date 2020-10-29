c5bbf97932512b7c10571e8f8457ffdbbfa64fee
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-29 12:03:20 +0000

    refactor: update wallet transaction filter design (#243)

--- resources/views/app/wallet.blade.php
+++ resources/views/app/wallet.blade.php
@@ -22,26 +22,7 @@
             <x-wallet.registrations :wallet="$wallet" />
         @endif
 
-        <div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
-            <div class="py-16 content-container md:px-8">
-                <div x-data="{
-                    dropdownOpen: false,
-                    direction: 'all',
-                    transactionTypeFilter: 'all',
-                    transactionTypeFilterLabel: 'All',
-                }" x-cloak class="w-full">
-                    <div class="w-full mb-8">
-                        <div class="relative flex items-end justify-between">
-                            <h2 class="text-3xl sm:text-4xl">@lang('pages.wallet.transaction_history')</h2>
-
-                            <x-transaction-table-filter />
-                        </div>
-                    </div>
-
-                    <livewire:wallet-transaction-table :address="$wallet->address()" :public-key="$wallet->publicKey()" />
-                </div>
-            </div>
-        </div>
+        <x-wallet.transactions :wallet="$wallet" />
     @endsection
 
 @endcomponent
---
+++ resources/views/components/wallet/transactions.blade.php
@@ -0,0 +1,20 @@
+<div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
+    <div class="py-16 content-container md:px-8">
+        <div x-data="{
+            dropdownOpen: false,
+            direction: 'all',
+            transactionTypeFilter: 'all',
+            transactionTypeFilterLabel: 'All',
+        }" x-cloak class="w-full">
+            <div class="w-full md:mb-8">
+                <div class="relative flex flex-col justify-between md:items-end md:flex-row md:justify-start">
+                    <h2 class="mb-8 text-3xl md:mb-0 sm:text-4xl">@lang('pages.wallet.transaction_history')</h2>
+
+                    <x-transaction-table-filter />
+                </div>
+            </div>
+
+            <livewire:wallet-transaction-table :address="$wallet->address()" :public-key="$wallet->publicKey()" />
+        </div>
+    </div>
+</div>
