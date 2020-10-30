791445922724eecae6a048a705c43363fa54609e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-30 01:54:11 +0000

    refactor: transaction show plus & minus amount (#272)

--- resources/views/components/general/amount-fiat-tooltip.blade.php
+++ resources/views/components/general/amount-fiat-tooltip.blade.php
@@ -1,9 +1,9 @@
-{{-- @TODO: do we really need +/- symbols and color coding? Seems overkill --}}
-
 @if(isset($isSent))
     <span class="p-2 font-bold border rounded text-theme-danger-400 border-theme-danger-100 dark:text-theme-danger-600 dark:border-theme-danger-600">
+        -
 @elseif(isset($isReceived))
     <span class="p-2 font-bold border rounded text-theme-success-400 border-theme-success-100 dark:text-theme-success-600 dark:border-theme-success-600">
+        +
 @else
     <span>
 @endif
