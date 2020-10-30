8a53e2b047b415994bf86b08013da4dcbb866bee
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-30 06:54:43 +0200

    fix: avatar scaling of delegate heading (#291)

--- resources/views/components/headings/avatar-with-icon.blade.php
+++ resources/views/components/headings/avatar-with-icon.blade.php
@@ -3,7 +3,11 @@
         @svg($icon, 'w-5 h-5')
     </div>
 
-    <div class="circled-icon text-theme-secondary-400 border-theme-secondary-400">
-        <x-general.avatar :identifier="$model->address()" size="w-5 h-5" />
+    <div class="circled-icon text-theme-secondary-400 border-theme-secondary-700">
+        <div class="w-6 h-6 overflow-hidden rounded-full md:w-8 md:h-8">
+            <div class="object-cover w-full h-full">
+                {!! Avatar::make($model->address()) !!}
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/components/headings/avatar.blade.php
+++ resources/views/components/headings/avatar.blade.php
@@ -1,3 +1,7 @@
 <div class="circled-icon text-theme-secondary-400 border-theme-danger-400">
-    <x-general.avatar :identifier="$model->address()" size="w-8 h-8" />
+    <div class="w-6 h-6 overflow-hidden rounded-full md:w-8 md:h-8">
+        <div class="object-cover w-full h-full">
+            {!! Avatar::make($model->address()) !!}
+        </div>
+    </div>
 </div>
