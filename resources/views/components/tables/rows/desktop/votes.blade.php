92c7ba48e576ca79a935fa53cd78c833be14d778
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-30 16:19:33 +0000

    refactor: vote percentage style (#327)

--- resources/views/components/tables/rows/desktop/votes.blade.php
+++ resources/views/components/tables/rows/desktop/votes.blade.php
@@ -1,1 +1,5 @@
-<x-currency>{{ $model->votes() }}</x-currency> <span><x-percentage>{{ $model->votesPercentage() }}</x-percentage></span>
+<x-currency>{{ $model->votes() }}</x-currency>
+
+<span class="vote-percentage">
+    <x-percentage>{{ $model->votesPercentage() }}</x-percentage>
+</span>
