f63f824436d388293758b0c844ab8e63edc010e3
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-30 14:47:12 +0200

    fix: grid text sizes and colors (#314)

--- resources/views/components/details/address.blade.php
+++ resources/views/components/details/address.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-center justify-between pb-4 border-b border-theme-secondary-300 dark:border-theme-secondary-800">
     <div class="flex flex-col space-y-2">
         <span class="text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700">{{ $title }}</span>
-        <span class="text-lg font-semibold text-theme-secondary-700">
+        <span class="font-semibold text-theme-secondary-700">
             <div wire:key="{{ $transaction->id() }}">
                 @if($model->isDelegate())
                     <x-general.identity-delegate :model="$model" />
--- resources/views/components/grid/generic.blade.php
+++ resources/views/components/grid/generic.blade.php
@@ -8,13 +8,13 @@
         @if((string) $slot === "")
             <span class="font-semibold text-theme-secondary-500 dark:text-theme-secondary-200"">@lang('generic.not_specified')</span>
         @else
-            <span class="text-lg font-semibold text-theme-secondary-700 dark:text-theme-secondary-200"">{{ $slot }}</span>
+            <span class="font-semibold text-theme-secondary-700 dark:text-theme-secondary-200"">{{ $slot }}</span>
         @endif
     </div>
 
     <div class="flex items-center justify-center w-12 h-12">
         <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
-            @svg($icon, 'h-5 w-5 text-theme-secondary-900')
+            @svg($icon, 'h-5 w-5 text-theme-secondary-900 dark:text-theme-secondary-700')
         </div>
     </div>
 </div>
