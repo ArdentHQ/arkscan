9937b4697f29eefd2ed665125ad0bf1484d15a82
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-30 13:45:11 +0200

    fix: icon and avatar overlap for wallet headings (#310)

--- resources/views/components/headings/avatar-with-icon.blade.php
+++ resources/views/components/headings/avatar-with-icon.blade.php
@@ -3,7 +3,9 @@
         @svg($icon, 'w-5 h-5')
     </div>
 
-    <div class="circled-icon text-theme-secondary-400 border-theme-secondary-700">
-        <x-general.avatar-small :identifier="$model->address()" />
+    <div class="-mt-1 border-4 border-black rounded-full dark:border-theme-secondary-900">
+        <div class="bg-black circled-icon text-theme-secondary-400 border-theme-secondary-700 dark:bg-theme-secondary-900">
+            <x-general.avatar-small :identifier="$model->address()" />
+        </div>
     </div>
 </div>
