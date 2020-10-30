a96f9fec1b2ebe83107397bc1c423ac09653d5e2
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-30 05:42:02 +0200

    fix: use correct entity icons (#285)

--- resources/views/components/grid/entity.blade.php
+++ resources/views/components/grid/entity.blade.php
@@ -1,4 +1,4 @@
-<x-grid.generic :title="ucfirst($model->iconType())" icon="app-volume">
+<x-grid.generic :title="ucfirst($model->iconType())" icon="app-{{ $model->iconType() }}">
     <a href="{{ route('wallet', $model->recipient()->address()) }}" class="link">
         {{ $model->entityName() }}
     </a>
