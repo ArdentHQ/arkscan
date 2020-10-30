40fca6659870f8c8fca96a94426abf8a6fcf57fa
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-30 16:04:58 +0200

    fix: hide amount for vote combinations (#319)

---
+++ resources/views/components/transaction/header/vote-combination.blade.php
@@ -0,0 +1,22 @@
+<x-general.entity-header-item
+    :title="trans('pages.transaction.transaction_type')"
+    icon="app-transactions.transfer"
+    :text="$transaction->typeLabel()"
+/>
+<x-general.entity-header-item
+    :title="trans('pages.transaction.fee')"
+    icon="app-fee"
+>
+    <x-slot name="text">
+        <x-currency>{{ $transaction->fee() }}</x-currency>
+    </x-slot>
+</x-general.entity-header-item>
+
+<x-general.entity-header-item
+    :title="trans('pages.transaction.confirmations')"
+    icon="app-confirmations"
+>
+    <x-slot name="text">
+        <x-number>{{ $transaction->confirmations() }}</x-number>
+    </x-slot>
+</x-general.entity-header-item>
