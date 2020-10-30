990aa2df885aac1d6f5dbb143d399d7c85c1092f
Brian Faust <faustbrian@users.noreply.github.com>
2020-10-30 06:12:14 +0200

    fix: transaction direction icons (#288)

--- resources/views/components/tables/rows/desktop/sender-with-direction.blade.php
+++ resources/views/components/tables/rows/desktop/sender-with-direction.blade.php
@@ -3,9 +3,9 @@
         <div class="flex items-center justify-center w-12 h-12 -mr-2">
             <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
                 @if($model->isSent($wallet->address()))
-                    @svg('app-arrow-down', 'h-5 w-5')
-                @else
                     @svg('app-arrow-up', 'h-5 w-5')
+                @else
+                    @svg('app-arrow-down', 'h-5 w-5')
                 @endif
             </div>
         </div>
