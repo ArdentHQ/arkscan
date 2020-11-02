0468a70a18b3ad8137292d91806b2cd6deab9207
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2020-11-02 17:05:56 +0100

    fix: multi-payment recipient component (#383)

--- resources/views/components/tables/mobile/payments.blade.php
+++ resources/views/components/tables/mobile/payments.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-8 divide-y table-list-mobile">
     @foreach ($payments as $payment)
         <div class="space-y-3 table-list-mobile-row">
-            <x-tables.rows.mobile.recipient :model="$payment" />
+            <x-tables.rows.mobile.multipayment-recipient :model="$payment" />
 
             <x-tables.rows.mobile.amount :model="$payment" />
         </div>
