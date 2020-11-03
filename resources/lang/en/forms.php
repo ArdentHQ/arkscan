f17c1d02ea07e5cb9ef2d82113121eb285ece8ba
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-03 04:57:25 +0200

    fix: use Multivote label for vote combination (#395)

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -61,7 +61,7 @@ return [
             'timelock'                      => 'Timelock',
             'transfer'                      => 'Transfer',
             'vote'                          => 'Vote',
-            'voteCombination'               => 'Vote Combination',
+            'voteCombination'               => 'Multivote',
         ],
 
     ],
