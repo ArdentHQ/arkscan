80983deb05208e2153045ba6c2766353423cb033
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-03 14:04:38 +0200

    fix: run command instead of job for productivity calculations (#421)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,13 +12,13 @@ use App\Console\Commands\CacheLastBlocks;
 use App\Console\Commands\CacheMultiSignatureAddresses;
 use App\Console\Commands\CacheNetworkStatistics;
 use App\Console\Commands\CachePastRoundPerformance;
+use App\Console\Commands\CacheProductivity;
 use App\Console\Commands\CacheRealTimeStatistics;
 use App\Console\Commands\CacheResignationIds;
 use App\Console\Commands\CacheUsernames;
 use App\Console\Commands\CacheVoterCount;
 use App\Console\Commands\CacheVotes;
 use App\Facades\Network;
-use App\Jobs\CacheProductivityByPublicKey;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Spatie\ShortSchedule\ShortSchedule;
@@ -65,7 +65,7 @@ final class Kernel extends ConsoleKernel
 
         $schedule->command(CachePastRoundPerformance::class)->everyMinute();
 
-        $schedule->command(CacheProductivityByPublicKey::class)->everyMinute();
+        $schedule->command(CacheProductivity::class)->everyMinute();
 
         $schedule->command(CacheResignationIds::class)->everyMinute();
     }
