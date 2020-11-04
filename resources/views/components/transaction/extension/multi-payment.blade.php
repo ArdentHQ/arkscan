a74213486edec74bff28580d36e3dcdfc762d849
Jolan <jolanbeer@gmail.com>
2020-11-04 13:06:45 +0100

    fix: indent between content and title on multipayment tx page (#454)

--- resources/views/components/transaction/extension/multi-payment.blade.php
+++ resources/views/components/transaction/extension/multi-payment.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
     <div class="py-16 content-container md:px-8">
         <div class="w-full">
-            <div class="relative flex items-end justify-between mb-8">
+            <div class="relative flex items-end justify-between mb-5">
                 <h2 class="text-xl sm:text-2xl">@lang('pages.transaction.recipient_list')</h2>
             </div>
 
