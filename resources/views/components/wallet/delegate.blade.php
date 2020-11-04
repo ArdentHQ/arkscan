b482ef3b6cc0d54d01b9e92aa7e349165d5e62a9
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2020-11-04 16:05:23 +0100

    fix: delegate details dividers (#460)

--- resources/views/components/wallet/delegate.blade.php
+++ resources/views/components/wallet/delegate.blade.php
@@ -4,7 +4,7 @@
             <h2 class="text-xl sm:text-2xl">@lang('pages.wallet.delegate.title', [$wallet->username()])</h2>
         </div>
         <div class="flex flex-wrap w-full sm:flex-no-wrap sm:flex-row md:flex-wrap sm:justify-between sm:divide-x md:divide-x-0 divide-theme-secondary-300 dark:divide-theme-secondary-800">
-            <div class="grid w-full grid-flow-row grid-cols-1 gap-6 pb-8 mb-8 border-b border-dashed sm:pt-8 sm:pt-0 md:pt-8 sm:pb-0 md:pb-8 sm:mb-0 md:mb-8 sm:border-b-0 md:border-b border-theme-secondary-300 dark:border-theme-secondary-800 md:grid-cols-2 lg:grid-cols-4 gap-y-10 boxes-details">
+            <div class="grid w-full grid-flow-row grid-cols-1 gap-6 pb-8 mb-8 border-b border-dashed sm:pt-8 sm:pt-0 md:pt-8 sm:pb-0 md:pb-8 sm:mb-0 md:mb-8 sm:border-b-0 md:border-b border-theme-secondary-300 dark:border-theme-secondary-800 md:grid-cols-2 lg:grid-cols-4 gap-y-10 delegate-details">
                 <x-details-box :title="trans('pages.wallet.delegate.rank')" icon="app-rank" icon-wrapper-class="bg-theme-danger-100 dark:bg-theme-danger-400" icon-text-class="text-theme-danger-400 dark:text-theme-secondary-200">
                     @if ($wallet->rank() > Network::delegateCount())
                         <x-number>{{ $wallet->rank() }}</x-number>
