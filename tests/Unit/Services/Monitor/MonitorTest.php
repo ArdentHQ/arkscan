6c9400d9544546c7b106d95c54cdc4b90381eee4
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-05 03:39:51 +0200

    test: remove skipped monitor tests (#470)

--- tests/Unit/Services/Monitor/MonitorTest.php
+++ tests/Unit/Services/Monitor/MonitorTest.php
@@ -2,25 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Models\Round;
-use App\Models\Wallet;
 use App\Services\Monitor\Monitor;
-
-use function Tests\configureExplorerDatabase;
-
-it('should get the active delegates for the given round', function () {
-    configureExplorerDatabase();
-
-    Wallet::factory(51)->create()->each(function ($wallet) {
-        Round::factory()->create([
-            'round'      => '112168',
-            'public_key' => $wallet->public_key,
-        ]);
-    });
-
-    expect(Monitor::activeDelegates(112168))->toHaveCount(51);
-})->skip();
 
 it('should calculate the forging information', function () {
     expect(Monitor::roundNumber())->toBeInt();
+});
+
+it('should calculate the height range for the given round', function () {
+    expect(Monitor::heightRangeByRound(50))->toBe([2499, 2549]);
 });
