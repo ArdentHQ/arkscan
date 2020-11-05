8d0645048e9a9d9cb1e51b0f741b4b323545a272
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-05 10:50:18 +0200

    fix: vote combination recipient styling (#485)

--- resources/views/components/transactions/recipient.blade.php
+++ resources/views/components/transactions/recipient.blade.php
@@ -4,6 +4,18 @@
     <div>
         @if ($transaction->isTransfer() || $transaction->isUnknown())
             <x-general.identity :model="$transaction->recipient()" />
+        @elseif ($transaction->isVoteCombination())
+            <x-general.identity :model="$transaction->voted()">
+                <x-slot name="icon">
+                    <x-transactions.icon :icon-type="$iconType" />
+                </x-slot>
+
+                <x-slot name="prefix">
+                    <span class="pr-2 mr-2 font-semibold border-r text-theme-secondary-900 dark:text-theme-secondary-200 border-theme-secondary-300 dark:border-theme-secondary-800">
+                        @lang('general.transaction.vote-combination')
+                    </span>
+                </x-slot>
+            </x-general.identity>
         @elseif ($transaction->isVote())
             <x-general.identity :model="$transaction->voted()">
                 <x-slot name="icon">
