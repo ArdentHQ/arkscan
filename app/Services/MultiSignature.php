17ec2863a1d354818fd9c21d1cab902b056bc884
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-05 15:51:02 +0200

    refactor: use `shell_exec` to for multi-signature address derivation (#491)

--- app/Services/MultiSignature.php
+++ app/Services/MultiSignature.php
@@ -5,23 +5,17 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Facades\Network;
-use Symfony\Component\Process\Process;
 
 final class MultiSignature
 {
     public static function address(int $min, array $publicKeys): string
     {
-        $command = sprintf(
+        return trim(shell_exec(sprintf(
             "%s %s %s '%s'",
             config('explorer.nodejs'),
             base_path('musig.js'),
             Network::alias(),
             json_encode(['min' => $min, 'publicKeys' => $publicKeys])
-        );
-
-        $process = Process::fromShellCommandline($command);
-        $process->run();
-
-        return trim($process->getOutput());
+        )) ?? '');
     }
 }
