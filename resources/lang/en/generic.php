158132f80f4aba2b5bef70878ad859db0a20af6e
Jolan <jolanbeer@gmail.com>
2020-11-06 05:53:09 +0100

    refactor: display N/A if block was missed and TBA if still has to be forged (#504)

--- resources/lang/en/generic.php
+++ resources/lang/en/generic.php
@@ -5,4 +5,6 @@ declare(strict_types=1);
 return [
     'not_specified' => 'Not Specified',
     'at'            => 'at',
+    'not-available' => 'N/A',
+    'to-be-done'    => 'TBD',
 ];
--- resources/views/components/tables/rows/desktop/wallet-last-block.blade.php
+++ resources/views/components/tables/rows/desktop/wallet-last-block.blade.php
@@ -1,7 +1,9 @@
-@if($model->lastBlock())
+@if($model->justMissed())
+    <span>@lang('generic.not-available')</span>
+@elseif(! $model->isDone())
+    <span>@lang('generic.to-be-done')</span>
+@else
     <a href="{{ route('block', $model->lastBlock()['id']) }}" class="font-semibold link">
         <x-truncate-middle :value="$model->lastBlock()['id']" />
     </a>
-@else
-    n/a
 @endif
