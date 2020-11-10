f43f7171ff63e0cb35078eb8dfb46884201b0f0c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-10 14:04:44 +0000

    refactor: resize wallet header buttons for mobile (#553)

--- resources/views/components/page-headers/wallet/actions/qr-code.blade.php
+++ resources/views/components/page-headers/wallet/actions/qr-code.blade.php
@@ -1,5 +1,9 @@
 <livewire:wallet-qr-code :address="$wallet->address()" />
 
-<button @click="livewire.emit('toggleQrCode')" type="button" class="flex items-center justify-center flex-1 w-16 px-3 rounded cursor-pointer bg-theme-primary-600 hover:bg-theme-primary-700 transition-default lg:flex-none h-11">
+<button
+    @click="livewire.emit('toggleQrCode')"
+    type="button"
+    class="flex items-center justify-center flex-1 w-16 rounded cursor-pointer bg-theme-primary-600 hover:bg-theme-primary-700 transition-default h-11 lg:flex-none lg:px-3"
+>
     <x-ark-icon name="app-qr-code" size="md" />
 </button>
