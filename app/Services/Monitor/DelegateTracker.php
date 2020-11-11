1634bbc1ccf1cce54b028fb44d8eb80a4dcc44ca
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-11 12:34:28 +0200

    refactor: extract delegate shuffling into an action (#569)

--- app/Services/Monitor/DelegateTracker.php
+++ app/Services/Monitor/DelegateTracker.php
@@ -7,6 +7,7 @@ namespace App\Services\Monitor;
 use App\Facades\Network;
 use App\Models\Block;
 use App\Models\Scopes\OrderByHeightScope;
+use App\Services\Monitor\Actions\ShuffleDelegates;
 use Illuminate\Support\Collection;
 
 /**
@@ -374,7 +375,7 @@ final class DelegateTracker
         //     'INPUT_DIFFS' => array_diff($activeDelegates->toArray(), static::EXPECTED['input']),
         // ]);
 
-        $activeDelegates = static::getActiveDelegates($activeDelegates->toArray(), $height);
+        $activeDelegates = ShuffleDelegates::execute($activeDelegates->toArray(), $height);
 
         // dd([
         //     'OUTPUT_EQUAL' => $activeDelegates === static::EXPECTED['output'],
@@ -430,55 +431,5 @@ final class DelegateTracker
             })
             ->sortBy('order')
             ->toArray();
-    }
-
-    private static function getActiveDelegates(array $delegates, int $height): array
-    {
-        $seedSource  = (string) RoundCalculator::calculate($height)['round'];
-        $currentSeed = hex2bin(hash('sha256', $seedSource));
-        $delCount    = count($delegates);
-
-        // $seeds = [];
-        for ($i = 0; $i < $delCount; $i++) {
-            // $elements = [];
-
-            for ($x = 0; $x < 4 && $i < $delCount; $i++, $x++) {
-                $newIndex             = intval(unpack('C*', $currentSeed)[$x + 1]) % $delCount;
-                $b                    = $delegates[$newIndex];
-                $delegates[$newIndex] = $delegates[$i];
-                $delegates[$i]        = $b;
-
-                // $elements[] = [
-                //     'i'        => $i,
-                //     'x'        => $x,
-                //     'newIndex' => $newIndex,
-                // ];
-            }
-
-            // $seeds[bin2hex($currentSeed)] = $elements;
-
-            $currentSeed = hex2bin(hash('sha256', $currentSeed));
-        }
-
-        // dump([
-        //     'SEEDS_EQUAL' => array_keys($seeds) === array_keys(static::EXPECTED['seeds']),
-        //     'SEEDS_DIFFS' => array_diff(array_keys($seeds), array_keys(static::EXPECTED['seeds'])),
-        // ]);
-
-        // foreach ($seeds as $hash => $actual) {
-        //     $expected = static::EXPECTED['seeds'][$hash];
-
-        //     for ($i = 0; $i < count($expected); $i++) {
-        //         dump([
-        //             'i'        => $expected[$i]['i'] === $actual[$i]['i'],
-        //             'x'        => $expected[$i]['x'] === $actual[$i]['x'],
-        //             'newIndex' => $expected[$i]['newIndex'] === $actual[$i]['newIndex'],
-        //             'expected' => $expected[$i],
-        //             'actual'   => $actual[$i],
-        //         ]);
-        //     }
-        // }
-
-        return $delegates;
     }
 }
