a73009fd4463c54f5f25aeb80bfff722c6d04023
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-18 12:04:01 +0000

    fix: firefox advanced search sizes (#625)

--- resources/views/components/search/block.blade.php
+++ resources/views/components/search/block.blade.php
@@ -15,124 +15,144 @@
 
 <x-general.search.advanced-option :title="trans('forms.search.height_range')">
     <div class="flex items-center space-x-2">
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.heightFrom"
-            wire:key="state_height_from"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.heightFrom"
+                wire:key="state_height_from"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
 
         <span>-</span>
 
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.heightTo"
-            wire:key="state_height_to"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.heightTo"
+                wire:key="state_height_to"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
     </div>
 </x-general.search.advanced-option>
 
 <x-general.search.advanced-option :title="trans('forms.search.total_amount_range')">
     <div class="flex items-center space-x-2">
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.totalAmountFrom"
-            wire:key="state_total_amount_from"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.totalAmountFrom"
+                wire:key="state_total_amount_from"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
 
         <span>-</span>
 
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.totalAmountTo"
-            wire:key="state_total_amount_to"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.totalAmountTo"
+                wire:key="state_total_amount_to"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
     </div>
 </x-general.search.advanced-option>
 
 <x-general.search.advanced-option :title="trans('forms.search.total_fee_range')">
     <div class="flex items-center space-x-2">
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.totalFeeFrom"
-            wire:key="state_total_fee_from"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.totalFeeFrom"
+                wire:key="state_total_fee_from"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
 
         <span>-</span>
 
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.totalFeeTo"
-            wire:key="state_total_fee_to"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.totalFeeTo"
+                wire:key="state_total_fee_to"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
     </div>
 </x-general.search.advanced-option>
 
 <x-general.search.advanced-option :title="trans('forms.search.reward_range')">
     <div class="flex items-center space-x-2">
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.rewardFrom"
-            wire:key="state_reward_from"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.rewardFrom"
+                wire:key="state_reward_from"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
 
         <span>-</span>
 
-        <input
-            type="number"
-            min="0"
-            placeholder="0.00"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.rewardTo"
-            wire:key="state_reward_to"
-            wire:keydown.enter="performSearch"
-        />
+        <div class="flex-1">
+            <input
+                type="number"
+                min="0"
+                placeholder="0.00"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.rewardTo"
+                wire:key="state_reward_to"
+                wire:keydown.enter="performSearch"
+            />
+        </div>
     </div>
 </x-general.search.advanced-option>
 
 <x-general.search.advanced-option :title="trans('forms.search.date_range')">
     <div class="flex items-center space-x-2">
-        <x-date-picker
-            placeholder="DD.MM.YYYY"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.dateFrom"
-            wire:key="state_date_from"
-        />
+        <div class="flex-1">
+            <x-date-picker
+                placeholder="DD.MM.YYYY"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.dateFrom"
+                wire:key="state_date_from"
+            />
+        </div>
 
         <span>-</span>
 
-        <x-date-picker
-            placeholder="DD.MM.YYYY"
-            class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
-            wire:model.defer="state.dateTo"
-            wire:key="state_date_to"
-        />
+        <div class="flex-1">
+            <x-date-picker
+                placeholder="DD.MM.YYYY"
+                class="w-full dark:text-theme-secondary-200 dark:bg-theme-secondary-900"
+                wire:model.defer="state.dateTo"
+                wire:key="state_date_to"
+            />
+        </div>
     </div>
 </x-general.search.advanced-option>
