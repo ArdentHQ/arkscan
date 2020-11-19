9cbf1aca4670ffd97cb42e8afac21b50461dbe5c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-19 10:45:39 +0000

    feat: poll active delegates table (#630)

--- resources/views/livewire/delegate-table.blade.php
+++ resources/views/livewire/delegate-table.blade.php
@@ -1,26 +1,40 @@
 <div id="delegate-list" class="w-full">
-    <x-loading.visible>
+    <span x-show="status !== 'active'">
+        <x-loading.visible>
+            <span x-show="status === 'active'">
+                <x-tables.desktop.skeleton.delegates.active />
+                <x-tables.mobile.skeleton.delegates.active />
+            </span>
+
+            <span x-show="status === 'standby'">
+                <x-tables.desktop.skeleton.delegates.standby />
+                <x-tables.mobile.skeleton.delegates.standby />
+            </span>
+
+            <span x-show="status === 'resigned'">
+                <x-tables.desktop.skeleton.delegates.resigned />
+                <x-tables.mobile.skeleton.delegates.resigned />
+            </span>
+        </x-loading.visible>
+    </span>
+
+    @if($this->state['status'] === 'active')
+        <div wire:poll.{{ Network::blockTime() }}s wire:key="poll_active_delegates_skeleton">
+            @if (count($delegates) && $state['status'] === 'active')
+                <x-tables.desktop.delegates.active :delegates="$delegates" />
+                <x-tables.mobile.delegates.active :delegates="$delegates" />
+            @else
+                <x-loading.hidden>
+                    <x-tables.desktop.delegates.active :delegates="$delegates" />
+                    <x-tables.mobile.delegates.active :delegates="$delegates" />
+                </x-loading.hidden>
+            @endif
+        </div>
+    @elseif (! count($delegates) || $state['status'] !== 'active')
         <span x-show="status === 'active'">
             <x-tables.desktop.skeleton.delegates.active />
             <x-tables.mobile.skeleton.delegates.active />
         </span>
-
-        <span x-show="status === 'standby'">
-            <x-tables.desktop.skeleton.delegates.standby />
-            <x-tables.mobile.skeleton.delegates.standby />
-        </span>
-
-        <span x-show="status === 'resigned'">
-            <x-tables.desktop.skeleton.delegates.resigned />
-            <x-tables.mobile.skeleton.delegates.resigned />
-        </span>
-    </x-loading.visible>
-
-    @if($this->state['status'] === 'active')
-        <x-loading.hidden>
-            <x-tables.desktop.delegates.active :delegates="$delegates" />
-            <x-tables.mobile.delegates.active :delegates="$delegates" />
-        </x-loading.hidden>
     @endif
 
     @if($this->state['status'] === 'standby')
