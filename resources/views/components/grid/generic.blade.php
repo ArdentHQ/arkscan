b718d26aeb9900ef9b5fd579d8c0c115f43ed69b
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2020-11-26 12:31:49 +0100

    fix: use break-words over break-all (#658)

--- resources/views/components/grid/generic.blade.php
+++ resources/views/components/grid/generic.blade.php
@@ -2,9 +2,9 @@
     <div class="flex flex-col flex-1 min-w-0 min-h-0 space-y-2">
         <span class="text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700">{{ $title }}</span>
         @if((string) $slot === "")
-            <span class="font-semibold break-all text-theme-secondary-500 dark:text-theme-secondary-200">@lang('generic.not_specified')</span>
+            <span class="font-semibold break-words text-theme-secondary-500 dark:text-theme-secondary-200">@lang('generic.not_specified')</span>
         @else
-            <span class="font-semibold break-all text-theme-secondary-700 dark:text-theme-secondary-200">{{ $slot }}</span>
+            <span class="font-semibold break-words text-theme-secondary-700 dark:text-theme-secondary-200">{{ $slot }}</span>
         @endif
     </div>
 
