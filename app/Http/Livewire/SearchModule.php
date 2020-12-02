6595dc2959ff4af969488d8deecf61f84330296d
Brian Faust <faustbrian@users.noreply.github.com>
2020-12-02 14:26:43 +0200

    fix: strip out `0x` from search term (#664)

--- app/Http/Livewire/SearchModule.php
+++ app/Http/Livewire/SearchModule.php
@@ -92,6 +92,10 @@ final class SearchModule extends Component
     {
         $data = $this->validateSearchQuery();
 
+        if (array_key_exists('term', $data) && ! is_null($data['term'])) {
+            $data['term'] = preg_replace('/(0x[0-9A-Z]+)/', '', $data['term']);
+        }
+
         if ($this->searchWallet($data)) {
             return;
         }
