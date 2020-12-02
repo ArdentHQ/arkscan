4dab7b1034ac23eab56957980674043040165d84
Brian Faust <faustbrian@users.noreply.github.com>
2020-12-02 14:14:43 +0200

    refactor: use string max lengths that match possible values (#665)

--- app/Http/Livewire/Concerns/ManagesSearch.php
+++ app/Http/Livewire/Concerns/ManagesSearch.php
@@ -15,7 +15,7 @@ trait ManagesSearch
         return $this->validate([
             // Generic
             'state'             => 'array',
-            'state.term'        => ['nullable', 'string', 'max:255'],
+            'state.term'        => ['nullable', 'string', 'max:66'],
             'state.type'        => ['nullable', Rule::in(['block', 'transaction', 'wallet'])],
             'state.dateFrom'    => ['nullable', 'date'],
             'state.dateTo'      => ['nullable', 'date'],
@@ -28,7 +28,7 @@ trait ManagesSearch
             'state.totalFeeTo'         => ['nullable', 'numeric', 'min:0'],
             'state.rewardFrom'         => ['nullable', 'numeric', 'min:0'],
             'state.rewardTo'           => ['nullable', 'numeric', 'min:0'],
-            'state.generatorPublicKey' => ['nullable', 'string', 'max:255'],
+            'state.generatorPublicKey' => ['nullable', 'string', 'max:66'],
             // Transactions
             'state.transactionType' => ['nullable', Rule::in(array_keys(trans('forms.search.transaction_types')))],
             'state.amountFrom'      => ['nullable', 'numeric', 'min:0'],
@@ -37,8 +37,8 @@ trait ManagesSearch
             'state.feeTo'           => ['nullable', 'numeric', 'min:0'],
             'state.smartBridge'     => ['nullable', 'string', 'max:255'],
             // Wallets
-            'state.username'    => ['nullable', 'string', 'max:255'],
-            'state.vote'        => ['nullable', 'string', 'max:255'],
+            'state.username'    => ['nullable', 'string', 'min:1', 'max:20'],
+            'state.vote'        => ['nullable', 'string', 'max:66'],
             'state.balanceFrom' => ['nullable', 'numeric', 'min:0'],
             'state.balanceTo'   => ['nullable', 'numeric', 'min:0'],
         ])['state'];
