b317eba41a7f9dc91a98f3765f4d562055cb6b87
Jolan <mc-somango@live.fr>
2020-12-03 11:57:22 +0100

    refactor: link the entity registrations to the registration tx (#669)

--- resources/views/components/grid/entity.blade.php
+++ resources/views/components/grid/entity.blade.php
@@ -1,5 +1,5 @@
 <x-grid.generic :title="ucfirst($model->iconType())" icon="app-transactions.{{ $model->iconType() }}">
-    <a href="{{ route('wallet', $model->recipient()->address()) }}" class="link">
+    <a href="{{ route('transaction', $model->id()) }}" class="link">
         {{ $model->entityName() }}
     </a>
 </x-grid.generic>
