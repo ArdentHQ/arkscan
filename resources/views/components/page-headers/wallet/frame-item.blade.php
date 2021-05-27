d1c4333610810422bb513a00bda30d408ada56a1
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-05-27 17:15:58 +0200

    fix: wallet header balance and button alignment (#744)

--- resources/views/components/page-headers/wallet/frame-item.blade.php
+++ resources/views/components/page-headers/wallet/frame-item.blade.php
@@ -1,11 +1,11 @@
-<div class="flex flex-1 justify-between font-semibold border-theme-secondary-800 lg:border-l lg:ml-8 lg:pl-4">
+<div class="flex flex-1 font-semibold border-theme-secondary-800 lg:border-l lg:ml-8 lg:pl-4">
     <div class="hidden items-center md:flex">
         <div class="circled-icon text-theme-secondary-700 border-theme-secondary-800 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
             <x-ark-icon :name="$icon" />
         </div>
     </div>
 
-    <div class="flex flex-col flex-1 justify-between space-y-2 md:pl-4">
+    <div class="flex flex-col flex-1 justify-center space-y-2 md:pl-4">
         <div class="text-sm leading-tight text-theme-secondary-600 dark:text-theme-secondary-700 {{ $titleClass ?? '' }}">
             {{ $title }}
         </div>
