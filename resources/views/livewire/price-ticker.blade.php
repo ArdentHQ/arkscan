484f5fa720ce44074e0d9b0d6181f3b342621139
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-28 04:40:29 -0500

    refactor: add opacity after updating currency (#751)

--- resources/views/livewire/price-ticker.blade.php
+++ resources/views/livewire/price-ticker.blade.php
@@ -1,3 +1,9 @@
-<div wire:poll.60s class="uppercase">
+<div
+    wire:poll.60s
+    class="uppercase"
+    :class="{ 'opacity-50': busy }"
+    x-data="{ to: '{{ $to }}', busy: false }"
+    x-init="livewire.on('currencyChanged', () => busy = true);"
+>
     {{ $from }}/{{ $to }}: {{ $price }}
 </div>
