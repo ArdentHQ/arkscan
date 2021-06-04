fa0c3de7594c27c8677d8b45b9fe708ec5d51e09
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-06-04 16:54:06 +0200

    fix: inactive tab semibold (#783)

--- resources/views/components/tabs/tab.blade.php
+++ resources/views/components/tabs/tab.blade.php
@@ -19,10 +19,10 @@
     {{ $attributes }}
 >
     <span
-        class="block pt-4 pb-3 w-full h-full whitespace-nowrap border-b-4"
+        class="block pt-4 pb-3 w-full h-full font-semibold whitespace-nowrap border-b-4"
         :class="{
             'border-transparent dark:text-theme-secondary-500 ': selected !== '{{ $name }}',
-            'text-theme-secondary-900 border-theme-primary-600 dark:text-theme-secondary-200 font-semibold': selected === '{{ $name }}',
+            'text-theme-secondary-900 border-theme-primary-600 dark:text-theme-secondary-200': selected === '{{ $name }}',
         }"
     >{{ $slot }}</span>
 </button>
