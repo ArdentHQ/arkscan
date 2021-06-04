736cfe12e57d40727451a8949f3ac2b13a41b937
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-06-04 16:21:47 +0200

    fix: failing tests (#785)

--- app/Providers/ExplorerServiceProvider.php
+++ app/Providers/ExplorerServiceProvider.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use ArkEcosystem\Crypto\Configuration\Network as NetworkConfiguration;
 use App\Contracts\Network;
 use App\Services\Blockchain\NetworkFactory;
+use ArkEcosystem\Crypto\Configuration\Network as NetworkConfiguration;
 use Illuminate\Support\ServiceProvider;
 
 final class ExplorerServiceProvider extends ServiceProvider
@@ -18,15 +18,13 @@ final class ExplorerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $network = NetworkFactory::make(config('explorer.network'));
-
         $this->app->singleton(
             Network::class,
-            fn () => $network
+            fn ($app) => NetworkFactory::make($app['config']['explorer']['network'])
         );
 
         // Used for crypto calculations, e.g. multisig address derivation
-        NetworkConfiguration::set($network->config());
+        NetworkConfiguration::set(NetworkFactory::make($this->app['config']['explorer']['network'])->config());
     }
 
     /**
