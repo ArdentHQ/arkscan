bf68dc7fc5e527d68c5cad5229ecdab1a852c14e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-06-07 11:14:40 +0100

    refactor: fix filter margin (#794)

--- resources/views/livewire/transaction-table.blade.php
+++ resources/views/livewire/transaction-table.blade.php
@@ -4,7 +4,7 @@
             <div class="flex relative flex-col justify-between md:items-end md:flex-row">
                 <h2 class="mb-8 md:mb-0">@lang('pages.transactions.title')</h2>
 
-                <div class="-my-3 -mr-8">
+                <div class="-my-3 md:-mr-8">
                     <x-transaction-table-filter />
                 </div>
             </div>
