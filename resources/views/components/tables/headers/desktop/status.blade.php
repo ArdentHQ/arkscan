edae1941f6bdfcf4a86f010020b4b6a3d1d1ca45
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-08 15:08:41 +0000

    fix: alignment on delegate table status column (#810)

--- resources/views/components/tables/headers/desktop/status.blade.php
+++ resources/views/components/tables/headers/desktop/status.blade.php
@@ -16,7 +16,7 @@
     :class="$class . ' text-left'"
 >
     @isset ($slot)
-        <div class="inline-flex items-center space-x-2">
+        <div class="inline-flex justify-end items-center space-x-2 w-full md:justify-start">
             <div>@lang($name)</div>
 
             {{ $slot }}
