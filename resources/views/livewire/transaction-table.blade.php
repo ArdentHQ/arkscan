b37d5a91e5ac2ffc06285808c0951bfbae426a17
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-30 04:58:44 -0500

    fix: add bottom margin to transactions table filter menu (#868)

--- resources/views/livewire/transaction-table.blade.php
+++ resources/views/livewire/transaction-table.blade.php
@@ -1,10 +1,10 @@
 <div>
     @isset($showTitle)
-        <div class="mb-3 w-full">
+        <div class="w-full md:mb-5">
             <div class="flex relative flex-col justify-between md:flex-row md:items-end">
                 <h1 class="mb-8 md:mb-0">@lang('pages.transactions.title')</h1>
 
-                <div class="-my-3 md:-mr-8">
+                <div class="-my-3 mb-5 md:mb-0 md:-mr-8">
                     <x-transaction-table-filter />
                 </div>
             </div>
