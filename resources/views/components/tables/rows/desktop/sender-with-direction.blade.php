f0f1c0579e1b62a5f9cf20fe33e707b1eb94bec1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-21 14:51:25 +0100

    refactor: update arrow direction colour (#922)

--- resources/views/components/tables/rows/desktop/sender-with-direction.blade.php
+++ resources/views/components/tables/rows/desktop/sender-with-direction.blade.php
@@ -1,13 +1,15 @@
 <div class="flex items-center space-x-4">
     <div class="flex justify-center items-center sender-direction-wrapper">
         <div class="flex">
-            <div class="arrow-direction text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
-                @if($model->isSent($wallet->address()))
+            @if($model->isSent($wallet->address()))
+                <div class="arrow-direction text-theme-danger-400 border-theme-danger-100 dark:border-theme-danger-400">
                     <x-ark-icon name="sent" />
-                @else
+                </div>
+            @else
+                <div class="arrow-direction text-theme-success-600 border-theme-success-200 dark:border-theme-success-600">
                     <x-ark-icon name="received" />
-                @endif
-            </div>
+                </div>
+            @endif
 
             <div class="table-avatar">
                 <div class="dark:text-theme-secondary-600 dark:border-theme-secondary-600">
