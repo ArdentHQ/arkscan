27a2ad42a8f10b7ae9f79a66a279380fc048cfdb
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-09-27 05:26:08 -0500

    fix: ensure it reloads the page when use back button on safari (#942)

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,3 +21,14 @@ window.RichSelect = RichSelect;
 window.PriceChart = PriceChart;
 window.Navbar = Navbar;
 window.CustomChart = CustomChart;
+
+/**
+ * If browser back button was used, flush cache
+ * This ensures that user will always see an accurate, up-to-date view based on their state
+ * https://stackoverflow.com/questions/8788802/prevent-safari-loading-from-cache-when-back-button-is-clicked
+ */
+window.onpageshow = function (event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+};
