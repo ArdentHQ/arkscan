491c9dfbcbe5e5dc8fedc5d10792fbc1f15cce02
Maurizio <leMaur@users.noreply.github.com>
2022-01-27 12:14:37 +0100

    fix: price graph height on the homepage (#1033)

--- resources/views/livewire/price-stats.blade.php
+++ resources/views/livewire/price-stats.blade.php
@@ -1,10 +1,10 @@
 <div
     wire:poll.60s
-    class="hidden flex-grow justify-end lg:flex"
+    class="hidden flex-grow justify-end h-full lg:flex"
 >
     <div
         wire:key="{{ Settings::currency() }}-{{ $isPositive ? 'positive' : 'negative' }}-{{ $usePlaceholder ? 'placeholder' : 'live' }}"
-        class="ml-6"
+        class="ml-6 h-full"
         x-data="PriceChart(
             {{ $historical->values()->toJson() }},
             {{ $historical->keys()->toJson() }},
@@ -15,12 +15,12 @@
         )"
         x-init="init"
     >
-        <div class="block" wire:ignore>
+        <div class="block h-full" wire:ignore>
             <canvas
                 x-ref="chart"
                 class="w-full h-full"
                 width="{{ ExplorerNumberFormatter::isFiat(Settings::currency()) ? 210 : 120 }}"
-                height="40"
+                height="44"
             ></canvas>
         </div>
     </div>
