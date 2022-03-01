8f1c787361320425ce794e6767c60795677b4b72
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-03-01 10:33:12 +0000

    fix: legacy multisignature details (#1041)

--- tests/Unit/ViewModels/TransactionViewModelTest.php
+++ tests/Unit/ViewModels/TransactionViewModelTest.php
@@ -1115,3 +1115,84 @@ it('should determine that the transaction is not any kind of registration', func
 
     expect($subject->isRegistration())->toBeFalse();
 });
+
+it('should get the address of legacy multi signature transactions', function () {
+    $this->subject = new TransactionViewModel(Transaction::factory()->multiSignature()->create([
+        'sender_public_key' => $this->sender->public_key,
+        'asset'             => [
+            'multiSignatureLegacy' => [
+                'min'       => 3,
+                'lifetime'  => 24,
+                'keysgroup' => [
+                    '+02fb3def2593a00c5b84620addf28ff21bac452bd71a37d4d8e24f301683a81b56',
+                    '+02bc9f661fcc8abca65fe9aff4614036867b7fdcc5730085ccc5cb854664d0194b',
+                    '+03c44c6b6cc9893ae21ca606712fd0f6f03c41ce81c4f6ce5a640f4b0b82ec1ce0',
+                    '+020300039e973baf5e46b945777cfae330d6392cdb039b1cebc5c3382d421166c3',
+                    '+03b050073621b9b5caec9461d44d6bcf21a858c47dd88230ce723e25c1bc75c219',
+                ],
+            ],
+        ],
+    ]));
+
+    expect($this->subject->multiSignatureAddress())->toBe($this->sender->address);
+});
+
+it('should get the participant count for legacy multi signature transactions', function () {
+    $this->subject = new TransactionViewModel(Transaction::factory()->multiSignature()->create([
+        'asset' => [
+            'multiSignatureLegacy' => [
+                'min'       => 3,
+                'lifetime'  => 24,
+                'keysgroup' => [
+                    '+02fb3def2593a00c5b84620addf28ff21bac452bd71a37d4d8e24f301683a81b56',
+                    '+02bc9f661fcc8abca65fe9aff4614036867b7fdcc5730085ccc5cb854664d0194b',
+                    '+03c44c6b6cc9893ae21ca606712fd0f6f03c41ce81c4f6ce5a640f4b0b82ec1ce0',
+                    '+020300039e973baf5e46b945777cfae330d6392cdb039b1cebc5c3382d421166c3',
+                    '+03b050073621b9b5caec9461d44d6bcf21a858c47dd88230ce723e25c1bc75c219',
+                ],
+            ],
+        ],
+    ]));
+
+    expect($this->subject->multiSignatureParticipantCount())->toBe(5);
+});
+
+it('should get the participants for legacy multi signature transactions', function () {
+    $this->subject = new TransactionViewModel(Transaction::factory()->multiSignature()->create([
+        'asset' => [
+            'multiSignatureLegacy' => [
+                'min'       => 3,
+                'lifetime'  => 24,
+                'keysgroup' => [
+                    '+02fb3def2593a00c5b84620addf28ff21bac452bd71a37d4d8e24f301683a81b56',
+                    '+02bc9f661fcc8abca65fe9aff4614036867b7fdcc5730085ccc5cb854664d0194b',
+                    '+03c44c6b6cc9893ae21ca606712fd0f6f03c41ce81c4f6ce5a640f4b0b82ec1ce0',
+                    '+020300039e973baf5e46b945777cfae330d6392cdb039b1cebc5c3382d421166c3',
+                    '+03b050073621b9b5caec9461d44d6bcf21a858c47dd88230ce723e25c1bc75c219',
+                ],
+            ],
+        ],
+    ]));
+
+    expect($this->subject->participants())->toHaveCount(5);
+});
+
+it('should get the minimum for legacy multi signature transactions', function () {
+    $this->subject = new TransactionViewModel(Transaction::factory()->multiSignature()->create([
+        'asset' => [
+            'multiSignatureLegacy' => [
+                'min'       => 3,
+                'lifetime'  => 24,
+                'keysgroup' => [
+                    '+02fb3def2593a00c5b84620addf28ff21bac452bd71a37d4d8e24f301683a81b56',
+                    '+02bc9f661fcc8abca65fe9aff4614036867b7fdcc5730085ccc5cb854664d0194b',
+                    '+03c44c6b6cc9893ae21ca606712fd0f6f03c41ce81c4f6ce5a640f4b0b82ec1ce0',
+                    '+020300039e973baf5e46b945777cfae330d6392cdb039b1cebc5c3382d421166c3',
+                    '+03b050073621b9b5caec9461d44d6bcf21a858c47dd88230ce723e25c1bc75c219',
+                ],
+            ],
+        ],
+    ]));
+
+    expect($this->subject->multiSignatureMinimum())->toBe(3);
+});
