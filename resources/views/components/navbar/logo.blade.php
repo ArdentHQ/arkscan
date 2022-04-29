b43385005638fb037dd20a2801bf4deb582ee283
ItsANameToo <itsanametoo@protonmail.com>
2022-04-29 11:25:41 +0200

    wip

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -2,8 +2,8 @@
     <img src="/images/logo.svg" class="h-10 lg:h-11" />
 
     <span class="hidden ml-4 sm:flex sm:items-center sm:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
-        <span class="font-bold">ARK</span>
-
-        @lang('general.scan')
+        <span class="font-bold">
+            {{ Network::explorerTitle() }}
+        </span>
     </span>
 </span>
