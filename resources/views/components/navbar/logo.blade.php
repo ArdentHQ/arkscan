3bd8df0291d899606e529446f4c945f60e629f9a
Josip Crnković <jcrnkovic95@gmail.com>
2022-05-02 14:53:52 +0200

    make uppercase

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -3,7 +3,7 @@
 
     <span class="hidden ml-4 sm:flex sm:items-center sm:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
         <span>
-            <span class="font-bold">{{ Network::currency() }}</span><span>Scan</span>
+            <span class="font-bold">{{ Network::currency() }}</span><span class="uppercase">{{ trans('generic.scan') }}</span>
         </span>
     </span>
 </span>
