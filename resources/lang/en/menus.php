729ecb01043252ba881ba012be869a7df5ad7703
crnkovic <crnkovic@users.noreply.github.com>
2022-05-02 09:55:28 +0000

    style: resolve style guide violations

--- resources/lang/en/menus.php
+++ resources/lang/en/menus.php
@@ -3,19 +3,19 @@
 declare(strict_types=1);
 
 return [
-    'delegates'  => 'Delegates',
-    'home'       => 'Home',
-    'wallets'    => 'Wallets',
-    'statistics' => 'Statistics',
+    'delegates'      => 'Delegates',
+    'home'           => 'Home',
+    'wallets'        => 'Wallets',
+    'statistics'     => 'Statistics',
     'network-select' => [
         'main' => [
-            'url' => 'https://live.arkscan.io',
-            'label' => 'Main Network',
+            'url'          => 'https://live.arkscan.io',
+            'label'        => 'Main Network',
             'mobile-label' => 'Mainnet',
         ],
         'test' => [
-            'url' => 'https://test.arkscan.io',
-            'label' => 'Test Network',
+            'url'          => 'https://test.arkscan.io',
+            'label'        => 'Test Network',
             'mobile-label' => 'Testnet',
         ],
     ],
