b221750c3178fdb444ae31617f5f113134e065ee
Josip Crnković <jcrnkovic95@gmail.com>
2022-05-02 14:47:44 +0200

    Update logo.blade.php

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -2,8 +2,8 @@
     <img src="/images/logo.svg" class="h-10 lg:h-11" />
 
     <span class="hidden ml-4 sm:flex sm:items-center sm:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
-        <span class="font-bold">
-            {{ Network::explorerTitle() }}
+        <span>
+            <span class="font-bold">{{ Network::currency() }}</span><span>Scan</span>
         </span>
     </span>
 </span>
