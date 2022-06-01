96151cec690d749675951e40818e270f0a5c9258
Adrian Kerchev <akerchev@gmail.com>
2022-06-01 06:49:21 -0400

    ARKscan docker changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-sudo chown -R explorer:www-data /var/www/explorer
+sudo chown -R arkscan:www-data /var/www/arkscan
 touch database/database.sqlite
 #--- run installs
 composer install --ignore-platform-reqs
 yarn install
 #--- fire up services
 sudo supervisord &
-sudo chmod -R 775 /var/www/explorer/database
-sudo chmod -R 775 /var/www/explorer/storage
-sudo chmod -R 775 /var/www/explorer/bootstrap/cache
+sudo chmod -R 775 /var/www/arkscan/database
+sudo chmod -R 775 /var/www/arkscan/storage
+sudo chmod -R 775 /var/www/arkscan/bootstrap/cache
 yarn cache clean
 rm -rf ~/.composer/
 #--- laravel and cache setup
@@ -17,21 +17,21 @@ php artisan key:generate --force
 php artisan migrate:fresh --force
 php artisan storage:link
 #
-php artisan explorer:cache-network-aggregates
-php artisan explorer:cache-fees
-php artisan explorer:cache-transactions
-php artisan explorer:cache-prices
-php artisan explorer:cache-currencies-data
-php artisan explorer:cache-currencies-history --no-delay
-php artisan explorer:cache-delegate-aggregates
-php artisan explorer:cache-delegate-performance
-php artisan explorer:cache-delegate-productivity
-php artisan explorer:cache-delegate-resignation-ids
-php artisan explorer:cache-delegate-usernames
-php artisan explorer:cache-delegate-wallets
-php artisan explorer:cache-delegates-with-voters
-php artisan explorer:cache-delegate-voter-counts
-php artisan explorer:cache-multi-signature-addresses
+php artisan arkscan:cache-network-aggregates
+php artisan arkscan:cache-fees
+php artisan arkscan:cache-transactions
+php artisan arkscan:cache-prices
+php artisan arkscan:cache-currencies-data
+php artisan arkscan:cache-currencies-history --no-delay
+php artisan arkscan:cache-delegate-aggregates
+php artisan arkscan:cache-delegate-performance
+php artisan arkscan:cache-delegate-productivity
+php artisan arkscan:cache-delegate-resignation-ids
+php artisan arkscan:cache-delegate-usernames
+php artisan arkscan:cache-delegate-wallets
+php artisan arkscan:cache-delegates-with-voters
+php artisan arkscan:cache-delegate-voter-counts
+php artisan arkscan:cache-multi-signature-addresses
 
 #--- run system scheduler
 sudo crond
