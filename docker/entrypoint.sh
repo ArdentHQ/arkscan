c7e77d43514ee203c70689723ed9b5bc50c5f1b3
Adrian Kerchev <akerchev@gmail.com>
2022-06-01 07:16:25 -0400

    entrypoint update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,21 +17,21 @@ php artisan key:generate --force
 php artisan migrate:fresh --force
 php artisan storage:link
 #
-php artisan arkscan:cache-network-aggregates
-php artisan arkscan:cache-fees
-php artisan arkscan:cache-transactions
-php artisan arkscan:cache-prices
-php artisan arkscan:cache-currencies-data
-php artisan arkscan:cache-currencies-history --no-delay
-php artisan arkscan:cache-delegate-aggregates
-php artisan arkscan:cache-delegate-performance
-php artisan arkscan:cache-delegate-productivity
-php artisan arkscan:cache-delegate-resignation-ids
-php artisan arkscan:cache-delegate-usernames
-php artisan arkscan:cache-delegate-wallets
-php artisan arkscan:cache-delegates-with-voters
-php artisan arkscan:cache-delegate-voter-counts
-php artisan arkscan:cache-multi-signature-addresses
+php artisan explorer:cache-network-aggregates
+php artisan explorer:cache-fees
+php artisan explorer:cache-transactions
+php artisan explorer:cache-prices
+php artisan explorer:cache-currencies-data
+php artisan explorer:cache-currencies-history --no-delay
+php artisan explorer:cache-delegate-aggregates
+php artisan explorer:cache-delegate-performance
+php artisan explorer:cache-delegate-productivity
+php artisan explorer:cache-delegate-resignation-ids
+php artisan explorer:cache-delegate-usernames
+php artisan explorer:cache-delegate-wallets
+php artisan explorer:cache-delegates-with-voters
+php artisan explorer:cache-delegate-voter-counts
+php artisan explorer:cache-multi-signature-addresses
 
 #--- run system scheduler
 sudo crond
