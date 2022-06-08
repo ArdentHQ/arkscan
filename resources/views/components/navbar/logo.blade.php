4679005aeb503d3af4d3e041585a1d644bc43dda
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-06-08 15:44:53 +0100

    refactor: allow navbar name override (#37)

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -3,7 +3,13 @@
 
     <span class="hidden ml-4 sm:flex sm:items-center sm:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
         <span>
-            <span class="font-bold">{{ Network::currency() }}</span><span class="uppercase">{{ trans('generic.scan') }}</span>
+            <span class="font-bold">
+                {{ config('app.navbar_name', Network::currency()) }}
+            </span>
+
+            <span class="uppercase">
+                {{ trans('generic.scan') }}
+            </span>
         </span>
     </span>
 </span>
