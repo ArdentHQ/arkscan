b576d7ae80a6f31d6d14490a0209997dc8efedf8
Josip Crnković <crnkovic@users.noreply.github.com>
2022-06-13 13:00:51 +0200

    refactor: ignore additional view exceptions (#38)

---
+++ tests/Unit/SentryTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Exceptions\Sentry;
+use Facade\Ignition\Exceptions\ViewException as FacadeViewException;
+use Illuminate\View\ViewException;
+use Sentry\Event;
+use Sentry\EventHint;
+
+it('ignores if event hint is null', function () {
+    $event = Event::createEvent();
+    $hint  = null;
+
+    $result = Sentry::before($event, $hint);
+
+    expect($result)->toBe($event);
+});
+
+it('ignores if exception is not set', function () {
+    $event = Event::createEvent();
+    $hint  = EventHint::fromArray([
+        //
+    ]);
+
+    $result = Sentry::before($event, $hint);
+
+    expect($result)->toBe($event);
+});
+
+it('does not report laravel view exceptions that contain specific messages', function () {
+    $event = Event::createEvent();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new ViewException('unclosed'),
+    ]));
+
+    expect($result)->toBeNull();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new ViewException('filemtime(): stat failed for'),
+    ]));
+
+    expect($result)->toBeNull();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new ViewException('undefined array key'),
+    ]));
+
+    expect($result)->toBe($event);
+});
+
+it('does not report spatie view exceptions that contain specific messages', function () {
+    $event = Event::createEvent();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new FacadeViewException('unclosed'),
+    ]));
+
+    expect($result)->toBeNull();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new FacadeViewException('filemtime(): stat failed for'),
+    ]));
+
+    expect($result)->toBeNull();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new FacadeViewException('undefined array key'),
+    ]));
+
+    expect($result)->toBe($event);
+});
+
+it('reports all other exceptions even if they contain specific messages', function () {
+    $event = Event::createEvent();
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new RuntimeException('unclosed'),
+    ]));
+
+    expect($result)->toBe($event);
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new RuntimeException('filemtime(): stat failed for'),
+    ]));
+
+    expect($result)->toBe($event);
+
+    $result = Sentry::before($event, EventHint::fromArray([
+        'exception' => new RuntimeException('undefined array key'),
+    ]));
+
+    expect($result)->toBe($event);
+});
