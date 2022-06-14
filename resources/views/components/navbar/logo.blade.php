0969f25a2d91d3e9797c9c8f5ef4d9541ec15599
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-06-15 00:55:55 +0100

    fix: navbar name spacing

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -2,7 +2,7 @@
     <img src="/images/logo.svg" class="h-10 lg:h-11" />
 
     <span class="hidden ml-4 sm:flex sm:items-center sm:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
-        <span>
+        <span class="inline-flex">
             <span class="font-bold">
                 {{ config('app.navbar_name', Network::currency()) }}
             </span>
