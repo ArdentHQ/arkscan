870f2cc6bd1d5bb8fba77dde6e19b31a133c19b3
alexbarnsley <alexbarnsley@users.noreply.github.com>
2022-08-10 09:09:37 +0000

    style: resolve style guide violations

--- tests/Feature/Http/Livewire/__stubs/SearchModalExceptionStub.php
+++ tests/Feature/Http/Livewire/__stubs/SearchModalExceptionStub.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Http\Livewire\__stubs;
 
+use App\Http\Livewire\Concerns\HandlesSearchModal;
 use App\Http\Livewire\Concerns\ManagesSearch;
 use ARKEcosystem\Foundation\UserInterface\Http\Livewire\Concerns\HasModal;
-use App\Http\Livewire\Concerns\HandlesSearchModal;
 use Livewire\Component;
 
 /**
