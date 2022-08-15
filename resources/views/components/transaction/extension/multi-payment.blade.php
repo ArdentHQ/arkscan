528285fdde21dff2fd136af6744a5b91a9672b3d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-15 13:48:37 +0100

    refactor: update padding & container usage (#65)

--- resources/views/components/transaction/extension/multi-payment.blade.php
+++ resources/views/components/transaction/extension/multi-payment.blade.php
@@ -1,12 +1,10 @@
-<div class="bg-white border-t border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-secondary-900">
-    <x-ark-container>
-        <div class="w-full">
-            <div class="flex relative justify-between items-end">
-                <h3>@lang('pages.transaction.recipient_list')</h3>
-            </div>
-
-
-            <x-tables.payments :payments="$transaction->payments()" />
+<x-ark-container class="border-t border-theme-secondary-300 dark:border-theme-secondary-800">
+    <div class="w-full">
+        <div class="flex relative justify-between items-end">
+            <h3>@lang('pages.transaction.recipient_list')</h3>
         </div>
-    </x-ark-container>
-</div>
+
+
+        <x-tables.payments :payments="$transaction->payments()" />
+    </div>
+</x-ark-container>
