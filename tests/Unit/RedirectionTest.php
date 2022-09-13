9618bb619887dd51624b2a72ad7b5198395e1e48
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-09-13 17:55:17 -0500

    Fix tests

--- tests/Unit/RedirectionTest.php
+++ tests/Unit/RedirectionTest.php
@@ -6,12 +6,23 @@ use App\Models\Block;
 use App\Models\Transaction;
 use App\Models\Wallet;
 
-it('redirects to the correct route', function ($model, $oldUrl, $newUrl): void {
-    $modelId = $model::factory()->create()->id;
+it('redirects to the correct route for transactions', function (): void {
+    $transactionId = Transaction::factory()->create()->id;
 
-    $this->get(sprintf('%s/%s', $oldUrl, $modelId))->assertRedirect(sprintf('%s/%s', $newUrl, $modelId));
-})->with([
-    [Transaction::class, 'transaction', 'transactions'],
-    [Wallet::class, 'wallet', 'wallets'],
-    [Block::class, 'block', 'blocks'],
-]);
+    $this->get(sprintf('transaction/%s', $transactionId))
+        ->assertRedirect(sprintf('transactions/%s', $transactionId));
+});
+
+it('redirects to the correct route for wallets', function (): void {
+    $walletAddress = Wallet::factory()->create()->address;
+
+    $this->get(sprintf('wallet/%s', $walletAddress))
+        ->assertRedirect(sprintf('wallets/%s', $walletAddress));
+});
+
+it('redirects to the correct route for blocks', function (): void {
+    $blockId = Block::factory()->create()->id;
+
+    $this->get(sprintf('block/%s', $blockId))
+        ->assertRedirect(sprintf('blocks/%s', $blockId));
+});
