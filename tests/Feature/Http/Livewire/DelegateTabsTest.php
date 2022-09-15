c3ac216e0d951003044fd7f9e1c3f2d358a14975
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-09-15 13:50:57 +0100

    update delegate tabs tests

--- tests/Feature/Http/Livewire/DelegateTabsTest.php
+++ tests/Feature/Http/Livewire/DelegateTabsTest.php
@@ -3,9 +3,73 @@
 declare(strict_types=1);
 
 use App\Http\Livewire\DelegateTabs;
+use App\Models\Wallet;
 use Livewire\Livewire;
 
-// @TODO: make assertions about data visibility
 it('should render without errors', function () {
-    $component = Livewire::test(DelegateTabs::class);
+    Livewire::test(DelegateTabs::class)
+        ->assertSeeHtml(trans('pages.delegates.monitor'));
+});
+
+it('should get active delegates', function () {
+    $instance = Livewire::test(DelegateTabs::class)
+        ->instance();
+
+    expect($instance->activeQuery()->count())->toBe(0);
+
+    Wallet::factory(51)
+        ->activeDelegate()
+        ->create();
+
+    Wallet::factory(15)
+        ->standbyDelegate(false)
+        ->create();
+
+    Wallet::factory(9)
+        ->standbyDelegate(true)
+        ->create();
+
+    expect($instance->activeQuery()->count())->toBe(51);
+});
+
+it('should get standby delegates', function () {
+    $instance = Livewire::test(DelegateTabs::class)
+        ->instance();
+
+    expect($instance->activeQuery()->count())->toBe(0);
+
+    Wallet::factory(51)
+        ->activeDelegate()
+        ->create();
+
+    Wallet::factory(15)
+        ->standbyDelegate(false)
+        ->create();
+
+    Wallet::factory(9)
+        ->standbyDelegate(true)
+        ->create();
+
+    expect($instance->standbyQuery()->count())->toBe(15);
+});
+
+it('should get resigned delegates', function () {
+    $instance = Livewire::test(DelegateTabs::class)
+        ->instance();
+
+    expect($instance->activeQuery()->count())->toBe(0);
+
+    Wallet::factory(51)
+        ->activeDelegate()
+        ->create();
+
+    Wallet::factory(15)
+        ->standbyDelegate(false)
+        ->create();
+
+    Wallet::factory(9)
+        ->standbyDelegate(true)
+        ->create();
+
+    expect($instance->resignedQuery()->count())->toBe(9);
 });
