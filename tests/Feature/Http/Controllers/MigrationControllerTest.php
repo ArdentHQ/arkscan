6fcd24b51afbf18f6fa271e7f8494f2ed68071d2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-01-04 19:25:05 +0000

    tests

--- tests/Feature/Http/Controllers/MigrationControllerTest.php
+++ tests/Feature/Http/Controllers/MigrationControllerTest.php
@@ -22,6 +22,6 @@ it('should render the page without any errors', function () {
         ->get(route('migration'))
         ->assertOk()
         ->assertSee('DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj')
-        ->assertSee('98.765 ARK')
-        ->assertSee('813.58 ARK');
+        ->assertSee('98.765 DARK')
+        ->assertSee('813.58 DARK');
 });
