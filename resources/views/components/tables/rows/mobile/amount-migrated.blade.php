4c1bc64f71fec2edead92e864e479b926e1b97d5
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-01-05 12:05:21 +0100

    add amount migrated to mobile

---
+++ resources/views/components/tables/rows/mobile/amount-migrated.blade.php
@@ -0,0 +1,18 @@
+@props([
+    'model',
+])
+
+<div class="items-center">
+    @lang('labels.amount')
+
+    <span class="flex px-1.5 py-1 font-semibold whitespace-nowrap rounded border-2 fiat-tooltip-sent text-theme-hint-600 border-theme-hint-100 dark:border-theme-hint-400">
+        <span class="fiat-hint-migration" data-tippy-content="@lang ('pages.wallet.migration')">
+            <x-ark-icon name="hint-small" size="xs" />
+        </span>
+
+        <span @if(Network::canBeExchanged()) data-tippy-content="{{ $model->amountFiatExcludingItSelf() }}" @endif>
+            -
+            <x-currency :currency="Network::currency()">{{ $model->amountExcludingItSelf() }}</x-currency>
+        </span>
+    </span>
+</div>
