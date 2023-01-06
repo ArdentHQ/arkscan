76e3f51a269b2cf807051e8d4a659dddbeb94ef3
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-01-06 01:14:26 +0000

    use dynamic truncate

--- resources/views/livewire/migration/stats.blade.php
+++ resources/views/livewire/migration/stats.blade.php
@@ -1,23 +1,23 @@
 <div class="flex flex-col px-8 m-auto space-y-5 md:px-10 xl:flex-row xl:space-y-0 xl:space-x-5 xl:max-w-7xl">
-    <div class="flex md:space-x-4 xl:w-164">
-        <div class="flex flex-col flex-1 justify-between space-y-5 font-semibold sm:flex-row sm:space-y-0 sm:rounded-xl sm:bg-theme-hint-800">
-            <div class="flex py-4 px-8 rounded-xl sm:pr-0 sm:bg-transparent sm:rounded-none md:space-x-4 bg-theme-hint-800">
+    <div class="flex md:space-x-4 xl:w-164 min-w-0">
+        <div class="flex flex-col flex-1 justify-between space-y-5 font-semibold sm:flex-row sm:space-x-4 sm:space-y-0 sm:rounded-xl sm:bg-theme-hint-800 w-full">
+            <div class="flex flex-1 py-4 px-8 rounded-xl sm:pr-0 sm:bg-transparent sm:rounded-none md:space-x-4 bg-theme-hint-800 min-w-0">
                 <x-ark-icon
                     name="app-transactions.migration"
                     size="w-11 h-11"
                     class="hidden md:block migration-icon-dark"
                 />
 
-                <div class="flex flex-col justify-between space-y-2">
+                <div class="flex flex-col justify-between space-y-2 min-w-0">
                     <span class="text-sm leading-none text-theme-hint-300">
                         @lang('pages.migration.stats.migration_wallet')
                     </span>
 
                     <div class="flex items-center space-x-1 leading-tight text-white">
-                        <a href="{{ route('wallet', config('explorer.migration_address')) }}">
-                            <x-truncate-middle length="20">
+                        <a href="{{ route('wallet', config('explorer.migration_address')) }}" class="min-w-0">
+                            <x-truncate-dynamic>
                                 {{ config('explorer.migration_address') }}
-                            </x-truncate-middle>
+                            </x-truncate-dynamic>
                         </a>
 
                         <x-ark-icon
@@ -29,8 +29,8 @@
                 </div>
             </div>
 
-            <div class="flex justify-between py-4 px-8 space-x-6 rounded-xl divide-x sm:pl-0 sm:bg-transparent sm:rounded-none divide-theme-hint-700 bg-theme-hint-800">
-                <div class="flex flex-col justify-between space-y-2 sm:text-right">
+            <div class="flex justify-between py-4 px-8 space-x-6 rounded-xl divide-x sm:pl-0 sm:bg-transparent sm:rounded-none divide-theme-hint-700 bg-theme-hint-800 w-62">
+                <div class="flex flex-col justify-between space-y-2 sm:text-right whitespace-nowrap">
                     <span class="text-sm leading-none text-theme-hint-300">
                         @lang('pages.migration.stats.amount_migrated')
                     </span>
