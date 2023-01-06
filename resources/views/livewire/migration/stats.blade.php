a2124da70f42a3dae97d71cea2d1d00fbe251829
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-01-06 01:16:07 +0000

    style: resolve style guide violations

--- resources/views/livewire/migration/stats.blade.php
+++ resources/views/livewire/migration/stats.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col px-8 m-auto space-y-5 md:px-10 xl:flex-row xl:space-y-0 xl:space-x-5 xl:max-w-7xl">
-    <div class="flex md:space-x-4 xl:w-164 min-w-0">
-        <div class="flex flex-col flex-1 justify-between space-y-5 font-semibold sm:flex-row sm:space-x-4 sm:space-y-0 sm:rounded-xl sm:bg-theme-hint-800 w-full">
-            <div class="flex flex-1 py-4 px-8 rounded-xl sm:pr-0 sm:bg-transparent sm:rounded-none md:space-x-4 bg-theme-hint-800 min-w-0">
+    <div class="flex min-w-0 md:space-x-4 xl:w-164">
+        <div class="flex flex-col flex-1 justify-between space-y-5 w-full font-semibold sm:flex-row sm:space-y-0 sm:space-x-4 sm:rounded-xl sm:bg-theme-hint-800">
+            <div class="flex flex-1 py-4 px-8 min-w-0 rounded-xl sm:pr-0 sm:bg-transparent sm:rounded-none md:space-x-4 bg-theme-hint-800">
                 <x-ark-icon
                     name="app-transactions.migration"
                     size="w-11 h-11"
@@ -30,7 +30,7 @@
             </div>
 
             <div class="flex justify-between py-4 px-8 space-x-6 rounded-xl divide-x sm:pl-0 sm:bg-transparent sm:rounded-none divide-theme-hint-700 bg-theme-hint-800 w-62">
-                <div class="flex flex-col justify-between space-y-2 sm:text-right whitespace-nowrap">
+                <div class="flex flex-col justify-between space-y-2 whitespace-nowrap sm:text-right">
                     <span class="text-sm leading-none text-theme-hint-300">
                         @lang('pages.migration.stats.amount_migrated')
                     </span>
