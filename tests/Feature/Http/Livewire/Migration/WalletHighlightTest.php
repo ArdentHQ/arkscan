3fb5fab64132f15123e2f56926d275bdab1b5ba1
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-01-09 14:53:57 +0000

    style: resolve style guide violations

--- tests/Feature/Http/Livewire/Migration/WalletHighlightTest.php
+++ tests/Feature/Http/Livewire/Migration/WalletHighlightTest.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 use App\Http\Livewire\Migration\WalletHighlight;
 use App\Models\Transaction;
 use App\Models\Wallet;
+use App\Services\BigNumber;
 use App\Services\Cache\NetworkCache;
 use Illuminate\Support\Facades\Config;
 use Livewire\Livewire;
-use App\Services\BigNumber;
 
 it('should render the component correctly', function () {
     Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
