5fd9725ed7c7bb02a5fe5ee54d9fb9fff2fedd52
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-01-19 20:28:45 +0000

    tests

--- tests/Unit/Models/TransactionTest.php
+++ tests/Unit/Models/TransactionTest.php
@@ -37,8 +37,11 @@ it('should belong to a recipient', function () {
 it('should get migrated transactions', function () {
     Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
 
-    $migratedTransactions = Transaction::factory(5)->transfer()->create([
+    Transaction::factory(5)->transfer()->create([
         'recipient_id' => 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj',
+        'fee'          => '10000000', // 0.1
+        'amount'       => '200000000', // 2
+        'vendor_field' => '0xRKeoIZ9Kh2g4HslgeHr5B9yblHbnwWYgfeFgO36n',
     ]);
 
     expect(Transaction::migrated()->count())->toBe(5);
@@ -47,8 +50,11 @@ it('should get migrated transactions', function () {
 it('should exclude migrated transactions which are not transfers', function () {
     Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
 
-    $migratedTransactions = Transaction::factory(5)->vote()->create([
+    Transaction::factory(5)->vote()->create([
         'recipient_id' => 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj',
+        'fee'          => '10000000', // 0.1
+        'amount'       => '200000000', // 2
+        'vendor_field' => '0xRKeoIZ9Kh2g4HslgeHr5B9yblHbnwWYgfeFgO36n',
     ]);
 
     expect(Transaction::migrated()->count())->toBe(0);
@@ -57,9 +63,55 @@ it('should exclude migrated transactions which are not transfers', function () {
 it('should exclude migrated transactions which are not core group type', function () {
     Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
 
-    $migratedTransactions = Transaction::factory(5)->legacyBusinessRegistration()->create([
+    Transaction::factory(5)->legacyBusinessRegistration()->create([
         'recipient_id' => 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj',
+        'fee'          => '10000000', // 0.1
+        'amount'       => '200000000', // 2
+        'vendor_field' => '0xRKeoIZ9Kh2g4HslgeHr5B9yblHbnwWYgfeFgO36n',
     ]);
 
     expect(Transaction::migrated()->count())->toBe(0);
 });
+
+it('should exclude migrated transactions which are below the minimum amount', function () {
+    Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
+
+    Transaction::factory(5)->transfer()->create([
+        'recipient_id' => 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj',
+        'fee'          => '5000000', // 0.05
+        'amount'       => '20000000', // 0.2
+        'vendor_field' => '0xRKeoIZ9Kh2g4HslgeHr5B9yblHbnwWYgfeFgO36n',
+    ]);
+
+    expect(Transaction::migrated()->count())->toBe(0);
+});
+
+it('should exclude migrated transactions which are below the minimum fee', function () {
+    Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
+
+    Transaction::factory(5)->transfer()->create([
+        'recipient_id' => 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj',
+        'fee'          => '1000000', // 0.01
+        'amount'       => '200000000', // 2
+        'vendor_field' => '0xRKeoIZ9Kh2g4HslgeHr5B9yblHbnwWYgfeFgO36n',
+    ]);
+
+    expect(Transaction::migrated()->count())->toBe(0);
+});
+
+it('should exclude migrated transactions which has an invalid vendorfield value', function ($vendorField) {
+    Config::set('explorer.migration_address', 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj');
+
+    Transaction::factory(5)->transfer()->create([
+        'recipient_id' => 'DENGkAwEfRvhhHKZYdEfQ1P3MEoRvPkHYj',
+        'fee'          => '1000000', // 0.01
+        'amount'       => '200000000', // 2
+        'vendor_field' => $vendorField,
+    ]);
+
+    expect(Transaction::migrated()->count())->toBe(0);
+})->with([
+    'empty'     => '',
+    'too short' => '0x0000',
+    'too long'  => '0xRKeoIZ9Kh2g4HslgeHr5B9yblHbnwWYgfeFgO36n0',
+]);
