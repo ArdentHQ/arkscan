f084b4750e74caf719efacbf45559bd55e0983ea
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2023-02-13 08:03:34 +0100

    chore: update PHP dependencies (#169)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ final class Kernel extends HttpKernel
      *
      * These middleware are run during every request to your application.
      *
-     * @var array
+     * @var array<int, class-string|string>
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
@@ -53,7 +53,7 @@ final class Kernel extends HttpKernel
     /**
      * The application's route middleware groups.
      *
-     * @var array
+     * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
         'web' => [
@@ -79,7 +79,7 @@ final class Kernel extends HttpKernel
      *
      * These middleware may be assigned to groups or used individually.
      *
-     * @var array
+     * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
         'auth'               => Authenticate::class,
