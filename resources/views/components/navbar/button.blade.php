5b47b89b367a8e4e31b2c21608dfa68798994746
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-09 22:29:15 +0100

    update navbar button padding

--- resources/views/components/navbar/button.blade.php
+++ resources/views/components/navbar/button.blade.php
@@ -3,7 +3,7 @@
 ])
 <button {{ $attributes->merge([
     'type' => 'button',
-    'class' => 'items-center justify-center flex py-3 px-5 rounded text-theme-secondary-600 hover:text-theme-primary-700 hover:bg-theme-primary-100 focus:outline-none transition-default dark:text-theme-secondary-600 dark:hover:text-theme-secondary-100 dark:hover:bg-theme-secondary-800 ' . $marginClass
+    'class' => 'items-center justify-center flex p-2.5 rounded text-theme-secondary-600 hover:text-theme-primary-700 hover:bg-theme-primary-100 focus:outline-none transition-default dark:text-theme-secondary-600 dark:hover:text-theme-secondary-100 dark:hover:bg-theme-secondary-800 ' . $marginClass
 ]) }}>
     {{ $slot }}
 </button>
