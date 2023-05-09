4f1c648352eb789640792921730b82ee050683db
ItsANameToo <itsanametoo@protonmail.com>
2023-05-09 16:58:42 +0200

    wip

--- resources/views/app/compatible-wallets.blade.php
+++ resources/views/app/compatible-wallets.blade.php
@@ -10,20 +10,20 @@
 
             <div class="flex flex-col mt-6 w-full rounded-xl border min-[960px]:flex-row border-theme-secondary-300 dark:border-theme-secondary-800">
                 <div class="flex flex-col flex-1 justify-between py-8 px-8 space-y-6">
-                    <span class="items-center rounded font-semibold px-2 py-1 flex space-x-2 text-theme-secondary-900 dark:text-white dark:from-theme-primary-400 dark:via-theme-primary-400 dark:to-theme-primary-400 bg-gradient-to-r from-[#E5F0F8] via-[#FFF8EB] to-[#E5F0F8]">
+                    <span class="text-sm items-center rounded font-semibold px-2 py-1 flex space-x-2 text-theme-secondary-900 dark:text-white dark:from-theme-navy-600 dark:via-theme-navy-600 dark:to-theme-navy-600 bg-gradient-to-r from-[#E5F0F8] via-[#FFF8EB] to-[#E5F0F8]">
                         <x-ark-icon name="circle.info" />
                         <span>@lang('pages.compatible-wallets.arkvault.disclaimer')</span>
                     </span>
-                    <div class="">
-                        <h2 class="text-2xl font-semibold text-theme-secondary-900">@lang('general.arkvault') <span class="text-theme-secondary-500 dark:text-theme-secondary-200">(@lang('pages.compatible-wallets.arkvault.web_wallet'))</span></h2>
+                    <div>
+                        <h2 class="text-2xl font-semibold text-theme-secondary-900">@lang('general.arkvault') <span class="text-theme-secondary-500 dark:text-theme-secondary-700">(@lang('pages.compatible-wallets.arkvault.web_wallet'))</span></h2>
                         <p class="mt-2 leading-7 dark:text-theme-secondary-400">@lang('pages.compatible-wallets.arkvault.description')</p>
                     </div>
-                    <div class="flex flex-col justify-between py-3 pr-3 pl-6 rounded-xl sm:flex-row bg-theme-primary-50">
-                        <div class="flex flex-1 items-center py-1 bg-no-repeat bg-right sm:bg-[url('/images/wallets/arrows.svg')] min-[960px]:bg-none lg:bg-[url('/images/wallets/arrows.svg')] mr-2">
-                            <div><x-ark-icon name="app.wallets-arkvault" size="none" class="w-10 h-10" /></div>
+                    <div class="flex flex-col justify-between py-3 pr-3 pl-6 rounded-xl sm:flex-row bg-theme-primary-50 dark:bg-theme-dark-blue-800">
+                        <div class="flex flex-1 items-center py-1 bg-no-repeat bg-right sm:dark:bg-[url('/images/wallets/arrows-dark.svg')] sm:bg-[url('/images/wallets/arrows.svg')] min-[960px]:bg-none lg:dark:bg-[url('/images/wallets/arrows-dark.svg')] lg:bg-[url('/images/wallets/arrows.svg')] mr-2">
+                            <div><x-ark-icon name="app.wallets-arkvault" size="none" class="w-10 h-10 text-theme-navy-600 dark:text-white" /></div>
                             <div class="flex flex-col ml-3">
-                                <span class="text-lg font-semibold text-theme-secondary-900">@lang('general.arkvault')</span>
-                                <span class="text-sm font-semibold text-theme-secondary-700">@lang('pages.compatible-wallets.arkvault.subtitle')</span>
+                                <span class="text-lg font-semibold text-theme-secondary-900 dark:text-white">@lang('general.arkvault')</span>
+                                <span class="text-sm font-semibold text-theme-secondary-700 dark:text-theme-dark-blue-400">@lang('pages.compatible-wallets.arkvault.subtitle')</span>
                             </div>
                         </div>
                         <div class="flex items-center">
@@ -32,7 +32,8 @@
                     </div>
                 </div>
                 <div class="flex flex-1 py-2 pr-3">
-                    <img src="{{ mix('images/wallets/arkvault.svg') }}" />
+                    <img src="{{ mix('images/wallets/arkvault.svg') }}" class="dark:hidden" />
+                    <img src="{{ mix('images/wallets/arkvault-dark.svg') }}" class="hidden dark:block" />
                 </div>
             </div>
 
