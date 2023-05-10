32d099f35bc0d61dbe523daaba2d3d4c83c5c9fd
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-05-10 09:30:40 +0000

    style: resolve style guide violations

--- resources/views/app/compatible-wallets.blade.php
+++ resources/views/app/compatible-wallets.blade.php
@@ -4,18 +4,18 @@
     @section('content')
         <x-ark-container>
             <div>
-                <h1 class="text-2xl font-semibold text-theme-secondary-900 xl:mb-1.5">
+                <h1 class="text-2xl font-semibold xl:mb-1.5 text-theme-secondary-900">
                     @lang('pages.compatible-wallets.title')
                 </h1>
-                <span class="font-semibold text-xs">
+                <span class="text-xs font-semibold">
                     @lang('pages.compatible-wallets.subtitle')
                 </span>
             </div>
 
-            <hr class="-mx-8 my-6 h-1 sm:hidden text-theme-secondary-200 bg-theme-secondary-200 dark:text-theme-secondary-800 dark:bg-theme-secondary-800" />
+            <hr class="my-6 -mx-8 h-1 sm:hidden text-theme-secondary-200 bg-theme-secondary-200 dark:text-theme-secondary-800 dark:bg-theme-secondary-800" />
 
-            <div class="flex flex-col sm:mt-6 w-full rounded-xl sm:border min-[960px]:flex-row border-theme-secondary-300 dark:border-theme-secondary-800">
-                <div class="flex flex-col flex-1 justify-center pb-8 sm:py-8 sm:px-8 space-y-6">
+            <div class="flex flex-col w-full rounded-xl sm:mt-6 sm:border min-[960px]:flex-row border-theme-secondary-300 dark:border-theme-secondary-800">
+                <div class="flex flex-col flex-1 justify-center pb-8 space-y-6 sm:py-8 sm:px-8">
                     <span class="text-sm items-center rounded font-semibold px-2 py-1 flex space-x-2 text-theme-secondary-900 dark:text-white dark:from-theme-navy-600 dark:via-theme-navy-600 dark:to-theme-navy-600 bg-gradient-to-r from-[#E5F0F8] via-[#FFF8EB] to-[#E5F0F8]">
                         <x-ark-icon name="circle.info" class="shrink-0" />
                         <span>
@@ -58,15 +58,15 @@
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-1 pt-2 sm:py-2 pr-3 grow">
+                <div class="flex flex-1 pt-2 pr-3 sm:py-2 grow">
                     <img src="{{ mix('images/wallets/arkvault.svg') }}" class="dark:hidden" />
                     <img src="{{ mix('images/wallets/arkvault-dark.svg') }}" class="hidden dark:block" />
                 </div>
             </div>
 
-            <hr class="-mx-8 my-6 h-1 sm:hidden text-theme-secondary-200 bg-theme-secondary-200 dark:text-theme-secondary-800 dark:bg-theme-secondary-800" />
+            <hr class="my-6 -mx-8 h-1 sm:hidden text-theme-secondary-200 bg-theme-secondary-200 dark:text-theme-secondary-800 dark:bg-theme-secondary-800" />
 
-            <div class="grid grid-cols-1 gap-3 sm:mt-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:mt-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 @foreach (trans('pages.compatible-wallets.wallets') as $wallet)
                     <div class="flex flex-col bg-white rounded-xl border border-theme-secondary-300 dark:bg-theme-secondary-900 dark:border-theme-secondary-800">
                         <div class="flex justify-center items-center mx-2 mt-2 rounded-xl aspect-square">
@@ -82,7 +82,7 @@
             </div>
 
             <div class="text-center sm:text-start flex-col sm:flex-row mt-6 w-full flex py-6 sm:py-2 px-6 justify-between items-center dark:from-theme-secondary-800 dark:via-theme-secondary-800 dark:to-theme-secondary-800 bg-gradient-to-r from-[#E5F0F8] via-[#FFF8EB] to-[#E5F0F8] rounded-xl">
-                <span class="sm:text-lg font-semibold dark:text-white text-theme-primary-900">
+                <span class="font-semibold sm:text-lg dark:text-white text-theme-primary-900">
                     @lang('pages.compatible-wallets.get_listed')
                 </span>
                 <button type="button" class="mt-3 w-full sm:mt-0 sm:w-auto button-primary">
