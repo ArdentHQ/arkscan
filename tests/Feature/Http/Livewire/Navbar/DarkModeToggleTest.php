a691e82a71b9b5c93c34e5a6ea715f3bdaa38bb2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-10 16:48:55 +0100

    tests

--- tests/Feature/Http/Livewire/Navbar/DarkModeToggleTest.php
+++ tests/Feature/Http/Livewire/Navbar/DarkModeToggleTest.php
@@ -1,22 +1,22 @@
 <?php
 
 use App\Facades\Settings;
-use App\Http\Livewire\Navbar\Toggle;
+use App\Http\Livewire\Navbar\DarkModeToggle;
 use Illuminate\Support\Facades\Cookie;
 use Livewire\Livewire;
 
 beforeEach(function () {
-    $this->component = Livewire::test(Toggle::class, [
+    $this->component = Livewire::test(DarkModeToggle::class, [
         'activeIcon'   => 'underline-moon',
         'inactiveIcon' => 'underline-sun',
-        'setting'      => 'priceChart',
+        'setting'      => 'darkTheme',
     ]);
 });
 
 it('should render', function () {
     $this->component->assertSet('activeIcon', 'underline-moon')
         ->assertSet('inactiveIcon', 'underline-sun')
-        ->assertSet('setting', 'priceChart')
+        ->assertSet('setting', 'darkTheme')
         ->assertSet('activeValue', true)
         ->assertSet('inactiveValue', false)
         ->assertSet('value', false)
@@ -26,32 +26,38 @@ it('should render', function () {
 it('should get icon', function () {
     $instance = $this->component->instance();
 
-    expect($instance->icon())->toBe('underline-moon');
+    expect($instance->icon())->toBe('underline-sun');
 
     $instance->toggle();
 
-    expect($instance->icon())->toBe('underline-sun');
+    expect($instance->icon())->toBe('underline-moon');
 });
 
 it('should toggle and save settings', function () {
     Cookie::shouldReceive('queue')
         ->with('settings', json_encode([
             ...Settings::all(),
-            'priceChart' => false,
+            'darkTheme' => true,
         ]), 60 * 24 * 365 * 5)
         ->once();
 
     Cookie::shouldReceive('queue')
         ->with('settings', json_encode([
             ...Settings::all(),
-            'priceChart' => true,
+            'darkTheme' => false,
         ]), 60 * 24 * 365 * 5)
         ->once();
 
     $this->component->assertSet('activeIcon', 'underline-moon')
         ->assertSet('inactiveIcon', 'underline-sun')
-        ->assertSet('setting', 'priceChart')
-        ->assertSet('currentValue', true)
+        ->assertSet('setting', 'darkTheme')
+        ->assertSet('currentValue', false)
         ->call('toggle')
-        ->call('toggle');
+        ->assertDispatchedBrowserEvent('setThemeMode', [
+            'theme' => 'dark',
+        ])
+        ->call('toggle')
+        ->assertDispatchedBrowserEvent('setThemeMode', [
+            'theme' => 'light',
+        ]);
 });
