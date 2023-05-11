f5834e854ac8e9f4d92cd23c57ef6fff00d5c2b6
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2023-05-11 10:05:30 +0200

    chore: restructure compatible-wallets components (#196)

---
+++ resources/views/components/compatible-wallets/list-wallet.blade.php
@@ -0,0 +1,8 @@
+<div class="text-center sm:text-start flex-col sm:flex-row mt-6 w-full flex py-6 sm:py-2 px-6 justify-between items-center dark:from-theme-secondary-800 dark:via-theme-secondary-800 dark:to-theme-secondary-800 bg-gradient-to-r from-[#E5F0F8] via-[#FFF8EB] to-[#E5F0F8] rounded-xl">
+    <span class="font-semibold sm:text-lg dark:text-white text-theme-primary-900">
+        @lang('pages.compatible-wallets.get_listed')
+    </span>
+    <button type="button" class="mt-3 w-full sm:mt-0 sm:w-auto button-primary">
+        @lang('actions.submit_wallet')
+    </button>
+</div>
