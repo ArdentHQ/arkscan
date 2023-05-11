462a5da0d12b04bd0739184bc3e653ca20a5e843
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-12 00:03:26 +0100

    add network toggle to navbar & add toggles to mobile dropdown

--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -100,7 +100,11 @@
                 </div>
             </div>
 
-            <template x-if="open">
+            <div
+                x-show="open"
+                x-transition.opacity
+                x-cloak
+            >
                 <div class="border-t-2 shadow-xl md:hidden border-theme-secondary-200 dark:border-theme-secondary-800">
                     <div class="pt-2 pb-4 bg-white rounded-b-lg dark:bg-theme-secondary-800">
                         @foreach ($navigation as $navItem)
@@ -143,15 +147,39 @@
                             @endif
                         @endforeach
 
+                        <div class="bg-theme-secondary-100 dark:bg-black py-4">
+                            <div class="mx-6 space-y-3 divide-y divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-800">
+                                <x-navbar.mobile.setting-item title="Select Theme">
+                                    <livewire:navbar.dark-mode-toggle
+                                        active-icon="underline-moon"
+                                        inactive-icon="underline-sun"
+                                        setting="darkTheme"
+                                        mobile
+                                    />
+                                </x-navbar.mobile.setting-item>
 
-                        @if(Network::canBeExchanged())
-                            <div class="flex py-3 px-8 mt-2 -mb-4 font-semibold md:hidden dark:text-white bg-theme-secondary-100 text-theme-secondary-900 dark:bg-theme-secondary-800">
-                                <livewire:price-ticker />
+                                <x-navbar.mobile.setting-item
+                                    title="Select Network"
+                                    class="pt-3"
+                                >
+                                    <livewire:navbar.network-toggle
+                                        active-icon="app-ark-testnet"
+                                        inactive-icon="networks.ark"
+                                        setting="testnet"
+                                        mobile
+                                    />
+                                </x-navbar.mobile.setting-item>
+
+                                @if(Network::canBeExchanged())
+                                    <div class="flex font-semibold pt-3 dark:text-theme-secondary-500">
+                                        <livewire:price-ticker />
+                                    </div>
+                                @endif
                             </div>
-                        @endif
+                        </div>
                     </div>
                 </div>
-            </template>
+            </div>
         </nav>
     </div>
 </header>
