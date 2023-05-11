c2403decc96bd2c67db68e616e35bad3647493e8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-12 00:09:54 +0100

    fix gap between header & subtitle

--- resources/views/components/compatible-wallets/header.blade.php
+++ resources/views/components/compatible-wallets/header.blade.php
@@ -1,7 +1,8 @@
-<div>
+<div class="flex flex-col">
     <h1 class="text-2xl font-semibold xl:mb-1.5 text-theme-secondary-900">
         @lang('pages.compatible-wallets.title')
     </h1>
+
     <span class="text-xs font-semibold">
         @lang('pages.compatible-wallets.subtitle')
     </span>
