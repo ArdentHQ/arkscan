63c6f5ab0bf2388fd318bfb19f14d3f7a7dcacca
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-05-11 14:52:35 +0000

    style: resolve style guide violations

--- resources/views/components/tables/rows/mobile/encapsulated/address.blade.php
+++ resources/views/components/tables/rows/mobile/encapsulated/address.blade.php
@@ -3,7 +3,7 @@
         @lang('labels.address')
     </span>
 
-    <span class="flex items-center justify-center space-x-2 ">
+    <span class="flex justify-center items-center space-x-2">
         <x-general.identity :model="$model" without-username without-reverse />
         <x-ark-clipboard :value="$model->address()" class="mr-3 transition text-theme-primary-400 dark:text-theme-secondary-600 hover:text-theme-primary-700" no-styling />
     </span>
