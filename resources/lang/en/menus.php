934d2a70a45f05d09ccf0eff088b22147557d476
ItsANameToo <itsanametoo@protonmail.com>
2023-05-12 14:56:18 +0200

    wip

--- resources/lang/en/menus.php
+++ resources/lang/en/menus.php
@@ -9,6 +9,7 @@ return [
     'developers'     => 'Developers',
 
     'delegates'      => 'Delegates',
+    'wallets'        => 'Wallets',
     'top_accounts'   => 'Top Accounts',
     'blocks'         => 'Blocks',
     'transactions'   => 'Transactions',
