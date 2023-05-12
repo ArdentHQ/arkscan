3a40c8e25a1fb82ffc0a8db680b75f538d2c0cda
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-05-12 11:25:50 +0000

    style: resolve style guide violations

--- resources/views/components/compatible-wallets/list-wallet.blade.php
+++ resources/views/components/compatible-wallets/list-wallet.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col justify-between items-center py-6 px-6 mt-6 w-full text-center rounded-xl sm:flex-row sm:py-2 sm:text-start bg-theme-primary-100 dark:bg-theme-secondary-800">
+<div class="flex flex-col justify-between items-center py-6 px-6 mt-6 w-full text-center rounded-xl sm:flex-row sm:py-2 bg-theme-primary-100 sm:text-start dark:bg-theme-secondary-800">
     <span class="font-semibold sm:text-lg dark:text-white text-theme-primary-900">
         @lang('pages.compatible-wallets.get_listed')
     </span>
