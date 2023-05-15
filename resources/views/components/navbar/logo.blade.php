e771a23a61f2b88260b729da0da4ad29e782031d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-15 11:20:18 +0100

    refactor: increase navbar branding text size

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -2,7 +2,7 @@
     <img src="/images/logo.svg" class="h-8" />
 
     <span class="hidden ml-4 md:flex md:items-center text-theme-secondary-900 dark:text-theme-secondary-200">
-        <span class="inline-flex">
+        <span class="inline-flex text-lg">
             <span class="font-bold">
                 @if(config('app.navbar_name'))
                     {{ config('app.navbar_name') }}
