8f205f21f2cc5b667110a090acdf7b46afdfb424
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-16 16:58:42 +0100

    test

---
+++ tests/Feature/Http/Livewire/Modals/SubmitWalletTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use App\Http\Livewire\Modals\SubmitWallet;
+use App\Mail\WalletFormSubmitted;
+use Illuminate\Support\Facades\Mail;
+use Livewire\Livewire;
+
+it('should render', function () {
+    Livewire::test(SubmitWallet::class)
+        ->assertSee(trans('actions.submit_wallet'))
+        ->assertDontSee(trans('pages.compatible-wallets.submit-modal.title'));
+});
+
+it('should open modal', function () {
+    Livewire::test(SubmitWallet::class)
+        ->assertSee(trans('actions.submit_wallet'))
+        ->assertDontSee(trans('pages.compatible-wallets.submit-modal.title'))
+        ->call('openModal')
+        ->assertSee(trans('pages.compatible-wallets.submit-modal.title'));
+});
+
+it('should not send mail if not valid data', function () {
+    Mail::fake();
+
+    Livewire::test(SubmitWallet::class)
+        ->call('openModal')
+        ->assertSet('name', null)
+        ->assertSet('website', null)
+        ->assertSet('message', null)
+        ->call('submit')
+        ->assertHasErrors([
+            'name' => 'required',
+            'website' => 'required',
+            'message' => 'required',
+        ]);
+
+    Mail::assertNothingQueued();
+});
+
+it('should submit', function () {
+    Mail::fake();
+
+    Livewire::test(SubmitWallet::class)
+        ->call('openModal')
+        ->set('name', 'Potato Wallet')
+        ->set('website', 'https://potato.wallet')
+        ->set('message', 'Exciting new universal wallet')
+        ->call('submit')
+        ->assertHasNoErrors()
+        ->assertSet('name', null)
+        ->assertSet('website', null)
+        ->assertSet('message', null);
+
+    Mail::assertQueued(WalletFormSubmitted::class);
+});
+
+it('should throttle submissions', function () {
+    Mail::fake();
+
+    $this->freezeTime();
+
+    Livewire::test(SubmitWallet::class)
+        ->call('openModal')
+        ->set('name', 'Potato Wallet')
+        ->set('website', 'https://potato.wallet')
+        ->set('message', 'Exciting new universal wallet')
+        ->call('submit')
+        ->assertNotEmitted('toastMessage', [trans('pages.compatible-wallets.submit-modal.throttle_error', ['time' => '1 hour']), 'warning'])
+        ->set('name', 'Potato Wallet')
+        ->set('website', 'https://potato.wallet')
+        ->set('message', 'Exciting new universal wallet')
+        ->call('submit')
+        ->assertNotEmitted('toastMessage', [trans('pages.compatible-wallets.submit-modal.throttle_error', ['time' => '1 hour']), 'warning'])
+        ->set('name', 'Potato Wallet')
+        ->set('website', 'https://potato.wallet')
+        ->set('message', 'Exciting new universal wallet')
+        ->call('submit')
+        ->assertNotEmitted('toastMessage', [trans('pages.compatible-wallets.submit-modal.throttle_error', ['time' => '1 hour']), 'warning'])
+        ->set('name', 'Potato Wallet')
+        ->set('website', 'https://potato.wallet')
+        ->set('message', 'Exciting new universal wallet')
+        ->call('submit')
+        ->assertEmitted('toastMessage', [trans('pages.compatible-wallets.submit-modal.throttle_error', ['time' => '1 hour']), 'warning']);
+
+    Mail::assertQueued(WalletFormSubmitted::class, 3);
+});
