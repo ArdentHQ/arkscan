36ce208f3f5358f64feb8366611055ed24437c0a
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-05-23 17:56:25 -0600

    remove unused factory

--- app/Models/Exchange.php
+++ app/Models/Exchange.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class Exchange extends Model
 {
-    use HasFactory;
 }
