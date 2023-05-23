d5bf36b81dd22284a18f843db7ce67a8e5d36bbb
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-23 22:35:21 +0100

    feat: submit exchange modal

--- resources/lang/en/mails.php
+++ resources/lang/en/mails.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 return [
     'subjects' => [
         'wallet_submitted' => 'A Wallet has been submitted',
+        'exchange_submitted' => 'An Exchange has been submitted',
     ],
 ];
