c504b4bc99f21017950f243085e8328a8a4b3859
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-05-23 18:32:35 -0600

    Revert "remove unused factory"
    This reverts commit 36ce208f3f5358f64feb8366611055ed24437c0a.

--- app/Models/Exchange.php
+++ app/Models/Exchange.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class Exchange extends Model
 {
+    use HasFactory;
 }
