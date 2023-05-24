4b27733507bb75ef77222dbbfe73db5c12fa5180
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-24 10:48:42 +0100

    fixes to dropdown

--- resources/views/components/exchanges/dropdown.blade.php
+++ resources/views/components/exchanges/dropdown.blade.php
@@ -3,7 +3,12 @@
     'title',
 ])
 
-<x-general.dropdown.dropdown class="flex-1 lg:flex-none">
+<x-general.dropdown.dropdown
+    class="flex-1 lg:flex-none"
+    dropdown-class="w-full px-6 md:px-10 md-lg:px-0 md-lg:w-50"
+    dropdown-wrapper-class="w-full"
+    activeButtonClass=""
+>
     <x-slot name="button" class="w-full font-semibold rounded border border-theme-secondary-300 text-theme-secondary-900 md-lg:w-50 dark:border-theme-secondary-800 dark:text-theme-secondary-200">
         <div class="flex justify-between items-center py-3 px-4 w-full">
             <div class="flex items-center space-x-2">
@@ -17,18 +22,18 @@
 
             <span
                 class="transition-default"
-                :class="{ 'rotate-180': isOpen }"
+                :class="{ 'rotate-180': dropdownOpen }"
             >
                 <x-ark-icon
                     name="arrows.chevron-down-small"
-                    size="w-2.5 h-2.5"
+                    size="w-3 h-3"
                     class="text-theme-secondary-700 dark:text-theme-secondary-200"
                 />
             </span>
         </div>
     </x-slot>
 
-    <x-slot name="content" class="w-full">
+    <x-slot name="content">
         {{ $slot }}
     </x-slot>
 </x-general.dropdown.dropdown>
--- resources/views/components/general/dropdown/dropdown.blade.php
+++ resources/views/components/general/dropdown/dropdown.blade.php
@@ -1,8 +1,11 @@
 @props([
     'button',
     'content',
+    'dropdownWrapperClass' => null,
     'dropdownClass' => null,
     'disabled' => false,
+    'disabledButtonClass' => 'text-theme-secondary-500 dark:text-theme-secondary-700 bg-theme-secondary-200 dark:bg-theme-secondary-800',
+    'activeButtonClass' => 'bg-theme-secondary-200 dark:bg-theme-secondary-800 md:bg-white md:dark:text-theme-secondary-600 md:hover:text-theme-secondary-900 md:hover:bg-theme-secondary-200 md:dark:bg-theme-secondary-900 dark:hover:bg-theme-secondary-800 text-theme-secondary-700 dark:text-theme-secondary-200',
 ])
 
 <div
@@ -10,19 +13,20 @@
     x-data="{ dropdownOpen: false }"
 >
     <x-ark-dropdown
+        :wrapper-class="Arr::toCssClasses(['inline-block', $dropdownWrapperClass])"
         :init-alpine="false"
-        wrapper-class="inline-block"
         with-placement="bottom"
-        dropdown-classes="min-w-40 transition-opacity"
+        :dropdown-classes="Arr::toCssClasses(['min-w-40 transition-opacity', $dropdownClass])"
         dropdown-content-classes="bg-white dark:bg-theme-secondary-800 rounded-xl shadow-toggle-dropdown py-2"
         :disabled="$disabled"
         z-index="z-20"
+        button-class="w-full"
     >
         <x-slot name="button">
             <div {{ $button->attributes->class([
                 'inline-flex items-center transition-default',
-                'text-theme-secondary-500 dark:text-theme-secondary-700 bg-theme-secondary-200 dark:bg-theme-secondary-800' => $disabled,
-                'bg-theme-secondary-200 dark:bg-theme-secondary-800 md:bg-white md:dark:text-theme-secondary-600 md:hover:text-theme-secondary-900 md:hover:bg-theme-secondary-200 md:dark:bg-theme-secondary-900 dark:hover:bg-theme-secondary-800 text-theme-secondary-700 dark:text-theme-secondary-200' => ! $disabled,
+                $disabledButtonClass => $disabled,
+                $activeButtonClass => ! $disabled,
             ]) }}>
                 {{ $button }}
             </div>
