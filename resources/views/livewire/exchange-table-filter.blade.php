851660ab5dfdf9cf765496947a486a469cafa815
alfonsobries <alfonsobries@users.noreply.github.com>
2023-05-24 17:17:19 +0000

    style: resolve style guide violations

--- resources/views/livewire/exchange-table-filter.blade.php
+++ resources/views/livewire/exchange-table-filter.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col w-full space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3 md-lg:w-auto">
+<div class="flex flex-col space-y-2 w-full sm:flex-row sm:space-y-0 sm:space-x-3 md-lg:w-auto">
     <x-exchanges.filter-dropdown :selected="$selectedType" :options="$types" icon="app-stack" param="type" />
 
     <x-exchanges.filter-dropdown :selected="$selectedPair" :options="$pairs" icon="app-pair" param="pair" />
