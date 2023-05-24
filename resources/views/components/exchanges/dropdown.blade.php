c881f5513179a1d88a58e963b084c7e3252cd494
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-24 10:18:27 +0100

    use div for dropdown button

--- resources/views/components/exchanges/dropdown.blade.php
+++ resources/views/components/exchanges/dropdown.blade.php
@@ -5,7 +5,7 @@
 
 <x-general.dropdown.dropdown class="flex-1 lg:flex-none">
     <x-slot name="button" class="w-full font-semibold rounded border border-theme-secondary-300 text-theme-secondary-900 md-lg:w-50 dark:border-theme-secondary-800 dark:text-theme-secondary-200">
-        <button class="flex justify-between items-center py-3 px-4 w-full">
+        <div class="flex justify-between items-center py-3 px-4 w-full">
             <div class="flex items-center space-x-2">
                 <x-ark-icon
                     :name="$icon"
@@ -25,7 +25,7 @@
                     class="text-theme-secondary-700 dark:text-theme-secondary-200"
                 />
             </span>
-        </button>
+        </div>
     </x-slot>
 
     <x-slot name="content" class="w-full">
