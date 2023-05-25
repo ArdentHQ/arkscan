6d2767da3d698e69f612bddfb254d9a8c79d4cd8
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-05-25 10:02:20 -0600

    add job for fetch exchange details

---
+++ app/Jobs/FetchExchangeDetails.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Contracts\MarketDataProvider;
+use App\Models\Exchange;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\RateLimited;
+use Illuminate\Queue\SerializesModels;
+
+class FetchExchangeDetails implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * The maximum attempts for this job.
+     *
+     * @var int
+     */
+    public $tries = 3;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(public Exchange $exchange)
+    {
+        //
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array<int, object>
+     */
+    public function middleware(): array
+    {
+        return [new RateLimited('coingecko_api_rate')];
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $result = app(MarketDataProvider::class)->exchangeDetails($this->exchange);
+
+        // No information for ark in any usd compatible exchange
+        if ($result === null) {
+            $this->exchange->price  = null;
+            $this->exchange->volume = null;
+        } else {
+            $this->exchange->price  = $result['price'];
+            $this->exchange->volume = $result['volume'];
+        }
+
+        $this->exchange->save();
+    }
+
+    /**
+     * Calculate the number of seconds to wait before retrying the job.
+     *
+     * @return int
+     */
+    public function retryAfter()
+    {
+        return 60; // 60 seconds = 1 minute
+    }
+}
---
+++ tests/Unit/Jobs/FetchExchangeDetailsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Contracts\MarketDataProvider;
+use App\Jobs\FetchExchangeDetails;
+use App\Models\Exchange;
+use Illuminate\Support\Facades\Artisan;
+
+beforeEach(function () {
+    Artisan::call('migrate:fresh');
+});
+
+it('should fetch exchange details and update the exchange model', function () {
+    $this->mock(MarketDataProvider::class)->shouldReceive('exchangeDetails')->andReturn([
+        'price'  => 10.5,
+        'volume' => 1000,
+    ]);
+
+    $exchange = Exchange::factory()->create([
+        'price'  => null,
+        'volume' => null,
+    ]);
+
+    FetchExchangeDetails::dispatch($exchange);
+
+    $exchange->refresh();
+
+    expect($exchange->price)->toBe('10.5');
+    expect($exchange->volume)->toBe('1000');
+});
+
+it('should clear exchange details when no information is available for the exchange', function () {
+    $this->mock(MarketDataProvider::class)->shouldReceive('exchangeDetails')->andReturn(null);
+
+    $exchange = Exchange::factory()->create([
+        'price'  => '10.5',
+        'volume' => '1000',
+    ]);
+
+    FetchExchangeDetails::dispatch($exchange);
+
+    $exchange->refresh();
+
+    expect($exchange->price)->toBeNull();
+    expect($exchange->volume)->toBeNull();
+});
