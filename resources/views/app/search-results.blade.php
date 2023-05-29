f22f2211e1a17f4a8577519cfb4d8919ffd508c4
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-05-29 12:29:08 -0600

    remove some deprecated stuff

--- resources/views/app/search-results.blade.php
+++
@@ -1,7 +0,0 @@
-@component('layouts.app')
-    <x-metadata page="search" :detail="['searchTerm' => request()->get('term')]" />
-
-    @section('content')
-        <livewire:search-page />
-    @endsection
-@endcomponent
