7635ba8b11425ae920224a5c5eb5ebff57da03b9
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-05-30 15:24:51 -0600

    feat: fetch volume price from coingecko (usd) (#237)

---
+++ app/Jobs/FetchExchangeDetails.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Contracts\MarketDataProvider;
+use App\Exceptions\CoinGeckoThrottledException;
+use App\Models\Exchange;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\RateLimited;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
+
+final class FetchExchangeDetails implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(public Exchange $exchange)
+    {
+        //
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array<int, object>
+     */
+    public function middleware(): array
+    {
+        return [new RateLimited('coingecko_api_rate')];
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        try {
+            $result = app(MarketDataProvider::class)->exchangeDetails($this->exchange);
+        } catch (CoinGeckoThrottledException $e) {
+            // Release back to the queue
+            $this->release(60); // 60 seconds = 1 minute
+
+            return;
+        }
+
+        if ($result === null) {
+            return;
+        }
+
+        $this->exchange->price  = Arr::get($result, 'price');
+        $this->exchange->volume = Arr::get($result, 'volume');
+        $this->exchange->save();
+    }
+
+    /**
+     * Calculate the number of seconds to wait before retrying the job.
+     *
+     * @return int
+     */
+    public function retryAfter()
+    {
+        return 60; // 60 seconds = 1 minute
+    }
+}
