f03a52e3e0c0245756fe5d9498fa9ad6cbde5c58
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-05-31 15:18:26 +0100

    fix type/icons for mobile txs

--- resources/views/components/search/results/transaction-types/multi-payment.blade.php
+++ resources/views/components/search/results/transaction-types/multi-payment.blade.php
@@ -1,7 +1,11 @@
 @props(['transaction'])
 
 <div class="flex items-center space-x-2 text-xs">
-    <div class="text-theme-secondary-500 dark:text-theme-secondary-700">
+    <div class="flex items-center text-theme-secondary-500 dark:text-theme-secondary-700">
+        <div class="md:hidden mr-2">
+            @lang('general.search.type')
+        </div>
+
         <x-ark-icon name="app-transactions.{{ $transaction->iconType() }}" />
     </div>
 
