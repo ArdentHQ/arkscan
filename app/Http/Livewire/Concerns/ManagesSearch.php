3853b41e0e9c8b56917025d816a449e81eb8d130
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-05-31 12:50:21 +0000

    style: resolve style guide violations

--- app/Http/Livewire/Concerns/ManagesSearch.php
+++ app/Http/Livewire/Concerns/ManagesSearch.php
@@ -9,11 +9,11 @@ use App\Services\Search\TransactionSearch;
 use App\Services\Search\WalletSearch;
 use App\ViewModels\ViewModelFactory;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Pagination\LengthAwarePaginator as PaginationLengthAwarePaginator;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Routing\Redirector;
 
 trait ManagesSearch
 {
