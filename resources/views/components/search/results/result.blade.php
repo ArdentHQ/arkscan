229a4254cca89c928d18c0d6961ab3106dc8a8c9
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-05-31 13:32:39 +0000

    style: resolve style guide violations

--- resources/views/components/search/results/result.blade.php
+++ resources/views/components/search/results/result.blade.php
@@ -1,7 +1,7 @@
 @props(['model'])
 
 <a
-    class="block p-3 md:-mx-3 space-y-2 min-w-0 cursor-pointer group transition-default rounded-[10px] dark:hover:bg-black hover:bg-theme-secondary-100"
+    class="block p-3 space-y-2 min-w-0 cursor-pointer md:-mx-3 group transition-default rounded-[10px] dark:hover:bg-black hover:bg-theme-secondary-100"
     href="{{ $model->url() }}"
 >
     {{ $slot }}
