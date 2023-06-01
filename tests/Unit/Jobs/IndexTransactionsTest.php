3953c9ed60ad475ba0e5392012e60a478451f70c
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-06-01 13:15:42 -0600

    fix tests

--- tests/Unit/Jobs/IndexTransactionsTest.php
+++ tests/Unit/Jobs/IndexTransactionsTest.php
@@ -8,10 +8,18 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Http;
 use Laravel\Scout\Events\ModelsImported;
+use Meilisearch\Client as MeilisearchClient;
+use Meilisearch\Endpoints\Indexes;
 
 beforeEach(function () {
     // Default value, overriden in phpunit.xml for the tests
     Config::set('scout.driver', 'meilisearch');
+
+    // Mock the Meilisearch client and indexes
+    $mock    = $this->mock(MeilisearchClient::class);
+    $indexes = $this->mock(Indexes::class);
+    $mock->shouldReceive('index')->andReturn($indexes);
+    $indexes->shouldReceive('addDocuments');
 });
 
 it('should index new Transactions', function () {
