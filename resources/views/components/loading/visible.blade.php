15a56b73923415db7814a433d2baa312c38aa215
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-01 23:35:10 +0100

    refactor: update table skeleton

--- resources/views/components/loading/visible.blade.php
+++ resources/views/components/loading/visible.blade.php
@@ -1,3 +1,23 @@
-<div class="w-full" wire:loading>
+@props([
+    'displayType' => null,
+])
+
+@php
+    if ($displayType !== null) {
+        $displayType = [
+            'block' => 'block',
+            'inline-block' => 'inline-block',
+        ][$displayType] ?? 'inline-block';
+    }
+@endphp
+
+<div
+    class="w-full"
+    @if ($displayType)
+        wire:loading.{{ $displayType }}
+    @else
+        wire:loading
+    @endif
+>
     {{ $slot }}
 </div>
