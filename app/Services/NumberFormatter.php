e02658ed13386857748cb6b49ec5c253dfffbb2a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-01 13:28:51 +0100

    workaround for rounding

--- app/Services/NumberFormatter.php
+++ app/Services/NumberFormatter.php
@@ -63,7 +63,8 @@ final class NumberFormatter
         return rtrim(BetterNumberFormatter::new()
             ->withLocale('international')
             ->withFractionDigits($decimals ?? 4)
-            ->formatCurrency((float) $value, 'USD'), '0.');
+            // Workaround to fix 5 rounding down (e.g. 1.00005 > 1 instead of 1.0001)
+            ->formatCurrency(floatval(number_format((float) $value, $decimals ?? 4, '.', '')), 'USD'), '0.');
     }
 
     /**
