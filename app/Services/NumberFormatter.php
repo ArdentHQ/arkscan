007825aea03d905970988bf093bdd40a1c254899
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-02 10:29:31 +0100

    always show min 2 dps

--- app/Services/NumberFormatter.php
+++ app/Services/NumberFormatter.php
@@ -60,11 +60,12 @@ final class NumberFormatter
      */
     public static function usdWithDecimals($value, ?int $decimals = 4): string
     {
-        return self::trimZeros(BetterNumberFormatter::new()
+        return BetterNumberFormatter::new()
             ->withLocale('en-US')
             ->withFractionDigits($decimals ?? 4)
+            ->withMinFractionDigits(2)
             // Workaround to fix 5 rounding down (e.g. 1.00005 > 1 instead of 1.0001)
-            ->formatCurrency(floatval(number_format((float) $value, $decimals ?? 4, '.', '')), 'USD'));
+            ->formatCurrency(floatval(number_format((float) $value, $decimals ?? 4, '.', '')), 'USD');
     }
 
     /**
@@ -123,10 +124,5 @@ final class NumberFormatter
         }
 
         return self::CRYPTO_DECIMALS;
-    }
-
-    private static function trimZeros(string $value): string
-    {
-        return rtrim(rtrim($value, '0'), '.');
     }
 }
--- tests/Unit/Services/NumberFormatterTest.php
+++ tests/Unit/Services/NumberFormatterTest.php
@@ -93,17 +93,18 @@ it('should use two decimals for a fiat currency', function () {
 });
 
 it('should use up to decimals and trim zero', function () {
-    expect(NumberFormatter::usdWithDecimals(1))->toBe('$1');
-    expect(NumberFormatter::usdWithDecimals(1.1))->toBe('$1.1');
-    expect(NumberFormatter::usdWithDecimals(1.5))->toBe('$1.5');
+    expect(NumberFormatter::usdWithDecimals(1))->toBe('$1.00');
+    expect(NumberFormatter::usdWithDecimals(1.1))->toBe('$1.10');
+    expect(NumberFormatter::usdWithDecimals(1.5))->toBe('$1.50');
     expect(NumberFormatter::usdWithDecimals(1.05))->toBe('$1.05');
     expect(NumberFormatter::usdWithDecimals(1.005))->toBe('$1.005');
     expect(NumberFormatter::usdWithDecimals(1.0005))->toBe('$1.0005');
     expect(NumberFormatter::usdWithDecimals(1.00005))->toBe('$1.0001');
-    expect(NumberFormatter::usdWithDecimals(1.00004))->toBe('$1');
+    expect(NumberFormatter::usdWithDecimals(1.00004))->toBe('$1.00');
     expect(NumberFormatter::usdWithDecimals(1.00006))->toBe('$1.0001');
-    expect(NumberFormatter::usdWithDecimals(1.00000))->toBe('$1');
+    expect(NumberFormatter::usdWithDecimals(1.00000))->toBe('$1.00');
     expect(NumberFormatter::usdWithDecimals(1.1200001))->toBe('$1.12');
-    expect(NumberFormatter::usdWithDecimals(1.1200001, 1))->toBe('$1.1');
-    expect(NumberFormatter::usdWithDecimals(29510))->toBe('$29,510');
+    expect(NumberFormatter::usdWithDecimals(1.1200001, 1))->toBe('$1.10');
+    expect(NumberFormatter::usdWithDecimals(29510))->toBe('$29,510.00');
+    expect(NumberFormatter::usdWithDecimals(29510.1))->toBe('$29,510.10');
 });
