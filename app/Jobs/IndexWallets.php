4764474d66a966a4f819359b27a95951abb71c68
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-06-07 16:59:53 -0600

    consider new wallets the ones that has new transactions

--- app/Jobs/IndexWallets.php
+++ app/Jobs/IndexWallets.php
@@ -17,6 +17,6 @@ final class IndexWallets extends IndexModel
 
     protected function elementsToIndexQuery(int $latestIndexedTimestamp): Builder
     {
-        return Wallet::getSearchableQuery()->where(DB::raw('(SELECT MIN(transactions.timestamp) FROM transactions WHERE transactions.recipient_id = wallets.address)'), '>', $latestIndexedTimestamp);
+        return Wallet::getSearchableQuery()->where(DB::raw('(SELECT MAX(transactions.timestamp) FROM transactions WHERE transactions.recipient_id = wallets.address)'), '>', $latestIndexedTimestamp);
     }
 }
