bf45b141439797b68e7e8524c6d5e9ffd250f091
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-07 11:06:44 +0100

    update colours of productivity based on status

--- resources/views/components/wallet/overview/delegate/productivity.blade.php
+++ resources/views/components/wallet/overview/delegate/productivity.blade.php
@@ -1,6 +1,6 @@
 @props(['wallet'])
 
-@php ($productivity = $wallet->productivity())
+@php ($productivity = $wallet->isActive() ? $wallet->productivity() : 0)
 
 <x-wallet.overview.item-entry
     :title="trans('pages.wallet.delegate.productivity_title')"
@@ -16,16 +16,16 @@
         ])>
             <div>
                 <x-percentage>
-                    {{ $productivity > 0 ? $productivity : 0 }}
+                    {{ $productivity }}
                 </x-percentage>
             </div>
 
             <div @class([
                 'w-4 h-4 rounded-full border',
-                'border-theme-secondary-200 bg-theme-secondary-400 dark:border-theme-secondary-800 dark:bg-theme-secondary-700' => $productivity <= 0,
-                'border-theme-danger-200 bg-theme-danger-700 dark:border-theme-danger-700 dark:bg-theme-danger-400' => $productivity < 50,
-                'border-theme-warning-200 bg-theme-warning-700 dark:border-theme-warning-800 dark:bg-theme-warning-700' => $productivity > 50 && $productivity <= 90,
-                'border-theme-success-200 bg-theme-success-700 dark:border-theme-success-800 dark:bg-theme-success-700' => $productivity > 90,
+                'border-theme-secondary-200 bg-theme-secondary-400 dark:border-theme-secondary-800 dark:bg-theme-secondary-700' => ! $wallet->isActive() || ($wallet->isActive() && $productivity <= 0),
+                'border-theme-danger-200 bg-theme-danger-700 dark:border-theme-danger-700 dark:bg-theme-danger-400' => $wallet->isActive() && $productivity < 50,
+                'border-theme-warning-200 bg-theme-warning-700 dark:border-theme-warning-800 dark:bg-theme-warning-700' => $wallet->isActive() && $productivity > 50 && $productivity <= 90,
+                'border-theme-success-200 bg-theme-success-700 dark:border-theme-success-800 dark:bg-theme-success-700' => $wallet->isActive() && $productivity > 90,
             ])></div>
         </div>
     </x-slot>
