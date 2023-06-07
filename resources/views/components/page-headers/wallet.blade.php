272798d86c45177bf43517a5a6c224d2b1fb60fb
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-06-07 10:07:10 +0000

    style: resolve style guide violations

--- resources/views/components/page-headers/wallet.blade.php
+++ resources/views/components/page-headers/wallet.blade.php
@@ -2,15 +2,15 @@
 
 <div
     x-data="{}"
-    class="pt-8 pb-6 px-6 md:mx-auto md:max-w-7xl md:px-10 flex flex-col"
+    class="flex flex-col px-6 pt-8 pb-6 md:px-10 md:mx-auto md:max-w-7xl"
 >
-    <div class="flex overflow-hidden flex-col space-y-4 font-semibold sm:flex-row sm:items-end sm:justify-between md:items-center sm:space-y-0 md:rounded-lg md:border md:border-theme-secondary-300 md:dark:border-theme-secondary-800">
-        <div class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:items-center md:space-x-3">
+    <div class="flex overflow-hidden flex-col space-y-4 font-semibold sm:flex-row sm:justify-between sm:items-end sm:space-y-0 md:items-center md:rounded-lg md:border md:border-theme-secondary-300 md:dark:border-theme-secondary-800">
+        <div class="flex flex-col space-y-2 md:flex-row md:items-center md:space-y-0 md:space-x-3">
             <div class="md:px-4 md:py-[14.5px] md:bg-theme-secondary-200 md:dark:bg-black text-sm md:text-lg dark:text-theme-secondary-500 !leading-[17px] md:!leading-[21px]">
                 @lang('general.address')
             </div>
 
-            <div class="text-theme-secondary-900 dark:text-theme-secondary-200 leading-5 md:leading-[21px]">
+            <div class="leading-5 text-theme-secondary-900 md:leading-[21px] dark:text-theme-secondary-200">
                 <x-truncate-dynamic>{{ $wallet->address() }}</x-truncate-dynamic>
             </div>
         </div>
