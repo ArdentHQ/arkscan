239b7d54d34c95f560eb5865da1cf4b1334be99f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-08 13:13:33 +0100

    update padding on mobile

--- resources/views/components/wallet/transactions.blade.php
+++ resources/views/components/wallet/transactions.blade.php
@@ -1,6 +1,6 @@
 <x-ark-container
     class="border-t-4 border-theme-secondary-200 dark:border-black md:border-0"
-    container-class="md:pt-0"
+    container-class="pt-6 md:pt-0"
 >
     <div
         x-data="{
