7274dc07d77af5447e5afc31614cac653ffb575d
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-06-14 11:09:29 +0000

    style: resolve style guide violations

--- resources/views/components/tables/rows/mobile/encapsulated/fee.blade.php
+++ resources/views/components/tables/rows/mobile/encapsulated/fee.blade.php
@@ -1,7 +1,7 @@
 @props(['model'])
 
 <div {{ $attributes->class('space-y-2 sm:flex sm:flex-col sm:justify-center') }}>
-    <div class="text-sm font-semibold dark:text-theme-secondary-500 leading-[17px]">
+    <div class="text-sm font-semibold leading-[17px] dark:text-theme-secondary-500">
         @lang('tables.transactions.fee', ['currency' => Network::currency()])
     </div>
 
