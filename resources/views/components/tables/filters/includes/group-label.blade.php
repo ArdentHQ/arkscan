a1090b987f6dc92db6b517c71a48e65710752f39
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-06-14 16:21:41 +0000

    style: resolve style guide violations

--- resources/views/components/tables/filters/includes/group-label.blade.php
+++ resources/views/components/tables/filters/includes/group-label.blade.php
@@ -1,5 +1,5 @@
 @props(['text'])
 
-<div class="font-semibold mt-3 mb-1 px-4 leading-5 dark:text-theme-secondary-200">
+<div class="px-4 mt-3 mb-1 font-semibold leading-5 dark:text-theme-secondary-200">
     {{ $text }}
 </div>
