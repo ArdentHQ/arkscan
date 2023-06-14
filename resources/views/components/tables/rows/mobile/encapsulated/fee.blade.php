55cf0b7bfc0dbc56dee1a856d72b992542b58189
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-14 12:09:04 +0100

    line height

--- resources/views/components/tables/rows/mobile/encapsulated/fee.blade.php
+++ resources/views/components/tables/rows/mobile/encapsulated/fee.blade.php
@@ -1,7 +1,7 @@
 @props(['model'])
 
 <div {{ $attributes->class('space-y-2 sm:flex sm:flex-col sm:justify-center') }}>
-    <div class="text-sm font-semibold dark:text-theme-secondary-500">
+    <div class="text-sm font-semibold dark:text-theme-secondary-500 leading-[17px]">
         @lang('tables.transactions.fee', ['currency' => Network::currency()])
     </div>
 
