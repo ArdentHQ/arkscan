688bc75da33d30d334d18e5c0383fa21cf9bb1c2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-14 13:35:08 +0100

    refactor: update productivity values (#302)

--- resources/views/components/wallet/overview/delegate/productivity.blade.php
+++ resources/views/components/wallet/overview/delegate/productivity.blade.php
@@ -1,6 +1,11 @@
 @props(['wallet'])
 
-@php ($productivity = $wallet->isActive() ? $wallet->productivity() : 0)
+@php
+    $productivity = $wallet->isActive() ? $wallet->productivity() : 0;
+    $isLow = $wallet->isActive() && $productivity < config('arkscan.productivity.danger');
+    $isMedium = ! $isLow && $wallet->isActive() && $productivity >= config('arkscan.productivity.danger') && $productivity < config('arkscan.productivity.warning');
+    $isHigh = ! $isMedium && $wallet->isActive() && $productivity >= config('arkscan.productivity.warning');
+@endphp
 
 <x-wallet.overview.item-entry
     :title="trans('pages.wallet.delegate.productivity_title')"
@@ -10,9 +15,9 @@
         <div @class([
             'flex items-center space-x-2',
             'text-theme-secondary-500 dark:text-theme-secondary-700' => ! $wallet->isActive(),
-            'text-theme-danger-700 dark:text-theme-danger-400' => $wallet->isActive() && $productivity < 50,
-            'text-theme-warning-700' => $wallet->isActive() && $productivity > 50 && $productivity <= 90,
-            'text-theme-success-700' => $wallet->isActive() && $productivity > 90,
+            'text-theme-danger-700 dark:text-theme-danger-400' => $isLow,
+            'text-theme-warning-700' => $isMedium,
+            'text-theme-success-700' => $isHigh,
         ])>
             <div>
                 <x-percentage>
@@ -23,9 +28,9 @@
             <div @class([
                 'w-4 h-4 rounded-full border',
                 'border-theme-secondary-200 bg-theme-secondary-400 dark:border-theme-secondary-800 dark:bg-theme-secondary-700' => ! $wallet->isActive(),
-                'border-theme-danger-200 bg-theme-danger-700 dark:border-theme-danger-700 dark:bg-theme-danger-400' => $wallet->isActive() && $productivity < 50,
-                'border-theme-warning-200 bg-theme-warning-700 dark:border-theme-warning-800 dark:bg-theme-warning-700' => $wallet->isActive() && $productivity > 50 && $productivity <= 90,
-                'border-theme-success-200 bg-theme-success-700 dark:border-theme-success-800 dark:bg-theme-success-700' => $wallet->isActive() && $productivity > 90,
+                'border-theme-danger-200 bg-theme-danger-700 dark:border-theme-danger-700 dark:bg-theme-danger-400' => $isLow,
+                'border-theme-warning-200 bg-theme-warning-700 dark:border-theme-warning-800 dark:bg-theme-warning-700' => $isMedium,
+                'border-theme-success-200 bg-theme-success-700 dark:border-theme-success-800 dark:bg-theme-success-700' => $isHigh,
             ])></div>
         </div>
     </x-slot>
