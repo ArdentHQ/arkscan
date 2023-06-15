9f8674637260436346b108aadec1e26a21156a1c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-15 22:26:25 +0100

    move table pagination stuff to trait

---
+++ app/Http/Livewire/Concerns/HasTablePagination.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Livewire\Concerns;
+
+use ARKEcosystem\Foundation\UserInterface\Http\Livewire\Concerns\HasPagination;
+
+trait HasTablePagination
+{
+    use HasPagination;
+
+    public int $perPage;
+
+    public function bootHasTablePagination(): void
+    {
+        $this->perPage = $this->getDefault();
+    }
+
+    /**
+     * @var mixed
+     */
+    public function getQueryString(): array
+    {
+        return [
+            'perPage' => ['except' => $this->getDefault()],
+        ];
+    }
+
+    public function setPerPage(int $perPage): void
+    {
+        if (! in_array($perPage, trans('pagination.per_page_options'), true)) {
+            return;
+        }
+
+        $this->perPage = $perPage;
+    }
+
+    private function getDefault(): int
+    {
+        if (defined(static::class.'::PER_PAGE')) {
+            $const = constant(static::class.'::PER_PAGE');
+            if (is_int($const)) {
+                return $const;
+            }
+        }
+
+        return 10;
+    }
+}
--- app/Http/Livewire/TopAccountsTable.php
+++ app/Http/Livewire/TopAccountsTable.php
@@ -4,39 +4,23 @@ declare(strict_types=1);
 
 namespace App\Http\Livewire;
 
+use App\Http\Livewire\Concerns\HasTablePagination;
 use App\Models\Scopes\OrderByBalanceScope;
 use App\Models\Wallet;
 use App\ViewModels\ViewModelFactory;
-use ARKEcosystem\Foundation\UserInterface\Http\Livewire\Concerns\HasPagination;
 use Illuminate\View\View;
 use Livewire\Component;
 
 final class TopAccountsTable extends Component
 {
-    use HasPagination;
+    use HasTablePagination;
 
-    public int $perPage = 25;
-
-    /**
-     * @var mixed
-     */
-    protected $queryString = [
-        'perPage' => ['except' => 25],
-    ];
+    const PER_PAGE = 25;
 
     public function render(): View
     {
         return view('livewire.top-accounts-table', [
             'wallets' => ViewModelFactory::paginate(Wallet::withScope(OrderByBalanceScope::class)->paginate($this->perPage)),
         ]);
-    }
-
-    public function setPerPage(int $perPage): void
-    {
-        if (! in_array($perPage, trans('pagination.per_page_options'), true)) {
-            return;
-        }
-
-        $this->perPage = $perPage;
     }
 }
