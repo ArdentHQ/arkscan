88d49d18c471e58ddfde1fe7827e1efdc8bbd0f8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-19 13:10:28 +0100

    update filter group light mode colour

--- resources/views/components/tables/filters/includes/group.blade.php
+++ resources/views/components/tables/filters/includes/group.blade.php
@@ -1,7 +1,7 @@
 @props(['label'])
 
 <div>
-    <div class="px-4 mt-3 mb-1 font-semibold leading-5 dark:text-theme-secondary-200">
+    <div class="px-4 mt-3 mb-1 font-semibold leading-5 text-theme-secondary-900 dark:text-theme-secondary-200">
         {{ $label }}
     </div>
 
