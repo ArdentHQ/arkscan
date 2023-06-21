7b0dcc9b5e21a9e688f12482e6b701878852e0af
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2023-06-21 12:29:28 +0200

    refactor: further dark mode adjustments (#326)

--- resources/views/components/tables/filters/includes/item.blade.php
+++ resources/views/components/tables/filters/includes/item.blade.php
@@ -3,7 +3,6 @@
 <div {{ $attributes->class([
     'table-filter-item flex items-center space-x-2 py-2 px-4 group cursor-pointer transition-default',
     'hover:bg-theme-secondary-100 dark:hover:bg-theme-secondary-800' => ! $isSelected,
-    'bg-theme-primary-50 dark:bg-black' => $isSelected,
 ]) }}>
     {{ $slot }}
 </div>
--- resources/views/components/wallet/overview/delegate/productivity.blade.php
+++ resources/views/components/wallet/overview/delegate/productivity.blade.php
@@ -17,7 +17,7 @@
             'text-theme-secondary-500 dark:text-theme-secondary-700' => ! $wallet->isActive(),
             'text-theme-danger-700 dark:text-theme-danger-400' => $isLow,
             'text-theme-warning-700' => $isMedium,
-            'text-theme-success-700' => $isHigh,
+            'text-theme-success-500' => $isHigh,
         ])>
             <div>
                 <x-percentage>
@@ -30,7 +30,7 @@
                 'border-theme-secondary-200 bg-theme-secondary-400 dark:border-theme-secondary-800 dark:bg-theme-secondary-700' => ! $wallet->isActive(),
                 'border-theme-danger-200 bg-theme-danger-700 dark:border-theme-danger-700 dark:bg-theme-danger-400' => $isLow,
                 'border-theme-warning-200 bg-theme-warning-700 dark:border-theme-warning-800 dark:bg-theme-warning-700' => $isMedium,
-                'border-theme-success-200 bg-theme-success-700 dark:border-theme-success-800 dark:bg-theme-success-700' => $isHigh,
+                'border-theme-success-200 bg-theme-success-700 dark:border-theme-success-600 dark:bg-theme-success-500' => $isHigh,
             ])></div>
         </div>
     </x-slot>
