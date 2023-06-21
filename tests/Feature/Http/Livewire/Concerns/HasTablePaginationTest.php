f100d0cb670c7c0d5e87588eb754340325ca50d8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-21 23:32:26 +0100

    test

---
+++ tests/Feature/Http/Livewire/Concerns/HasTablePaginationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Config;
+use Tests\Feature\Http\Livewire\__stubs\TablePaginationComponentStub;
+
+class TablePaginationComponentStubWithConstant extends TablePaginationComponentStub
+{
+    const PER_PAGE = 50;
+}
+
+it('should use PER_PAGE constant if exists', function () {
+    $instance = new TablePaginationComponentStubWithConstant();
+
+    expect(isset($instance->perPage))->toBeFalse();
+
+    $instance->bootHasTablePagination();
+
+    expect($instance->perPage)->toBe(TablePaginationComponentStubWithConstant::PER_PAGE);
+});
+
+it('should use config if constant does not exist', function () {
+    Config::set('arkscan.pagination.per_page', 100);
+
+    $instance = new TablePaginationComponentStub();
+
+    expect(isset($instance->perPage))->toBeFalse();
+
+    $instance->bootHasTablePagination();
+
+    expect($instance->perPage)->toBe(100);
+});
