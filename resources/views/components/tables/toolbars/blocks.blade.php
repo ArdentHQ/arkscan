7295d4850b3cd73a19a0df85e6c40df478c1a24a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-21 14:25:15 +0100

    coming soon tooltip

--- resources/views/components/tables/toolbars/blocks.blade.php
+++ resources/views/components/tables/toolbars/blocks.blade.php
@@ -2,7 +2,10 @@
 
 <x-tables.toolbars.toolbar :result-count="$blocks->total()">
     <div class="flex space-x-3">
-        <div class="flex-1 sm:flex-none">
+        <div
+            class="flex-1 sm:flex-none"
+            data-tippy-content="@lang('general.coming_soon')"
+        >
             <button
                 type="button"
                 class="flex justify-center items-center space-x-2 w-full sm:py-1.5 sm:px-4 button-secondary"
