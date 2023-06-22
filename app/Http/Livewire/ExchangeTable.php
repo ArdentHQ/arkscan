b00fb306ba8a3de2f0e7ee8bfaa12b757faf5576
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-22 14:55:53 +0100

    refactor: php sort volume descending default

--- app/Http/Livewire/ExchangeTable.php
+++ app/Http/Livewire/ExchangeTable.php
@@ -40,10 +40,10 @@ final class ExchangeTable extends Component
 
     public function getExchanges(): Collection
     {
-        return Exchange::orderBy('name')
-            ->filterByType($this->type)
+        return Exchange::filterByType($this->type)
             ->filterByPair($this->pair)
-            ->get();
+            ->get()
+            ->sort(fn ($a, $b) => $b->volume - $a->volume);
     }
 
     public function setFilter(string $param, string $value): void
