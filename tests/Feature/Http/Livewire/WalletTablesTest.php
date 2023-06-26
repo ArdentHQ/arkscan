a1673d2dbc56fded6622f0dd06bebb7a88270013
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-26 23:10:16 +0100

    test

--- tests/Feature/Http/Livewire/WalletTablesTest.php
+++ tests/Feature/Http/Livewire/WalletTablesTest.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Http\Livewire\WalletBlockTable;
 use App\Http\Livewire\WalletTables;
+use App\Http\Livewire\WalletTransactionTable;
+use App\Http\Livewire\WalletVoterTable;
 use App\Models\Wallet;
 use App\ViewModels\WalletViewModel;
 use Livewire\Livewire;
@@ -36,4 +39,48 @@ it('should change view with event', function () {
         ->assertSet('view', 'blocks')
         ->emit('showWalletView', 'transactions')
         ->assertSet('view', 'transactions');
+});
+
+it('should track querystring between tabs', function () {
+    $wallet = Wallet::factory()->activeDelegate()->create();
+
+    Livewire::test(WalletTables::class, [new WalletViewModel($wallet)])
+        ->set('view', 'transactions')
+
+        ->assertSet('tabQueryData', [
+            'transactions' => [
+                'page'          => 1,
+                'perPage'       => WalletTransactionTable::defaultPerPage(),
+                'outgoing'      => true,
+                'incoming'      => true,
+                'transfers'     => true,
+                'votes'         => true,
+                'multipayments' => true,
+                'others'        => true,
+            ],
+
+            'blocks' => [
+                'page'    => 1,
+                'perPage' => WalletBlockTable::defaultPerPage(),
+            ],
+
+            'voters' => [
+                'page'    => 1,
+                'perPage' => WalletVoterTable::defaultPerPage(),
+            ],
+        ])
+
+        ->set('tabQueryData.transactions.outgoing', false)
+        ->assertSet('outgoing', false)
+        ->set('tabQueryData.transactions.page', 2)
+
+        ->set('view', 'blocks')
+        ->assertSet('tabQueryData.blocks.page', 1)
+        ->assertSet('page', 1)
+        ->set('tabQueryData.blocks.page', 3)
+        ->assertSet('page', 3)
+
+        ->set('view', 'transactions')
+        ->assertSet('tabQueryData.transactions.page', 2)
+        ->assertSet('page', 2);
 });
