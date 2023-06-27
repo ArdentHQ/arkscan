b2611cdcf07f741a5bd77fbabf00777632591fd4
ItsANameToo <itsanametoo@protonmail.com>
2023-06-27 11:57:01 +0200

    fix: handle dark mode spamming by disabling input

--- resources/views/livewire/navbar/toggle.blade.php
+++ resources/views/livewire/navbar/toggle.blade.php
@@ -1,5 +1,6 @@
 <button
     wire:click="toggle"
+    wire:loading.attr="disabled"
     class="flex justify-center items-center w-8 h-8 rounded border text-theme-secondary-700 border-theme-secondary-300 transition-default dark:text-theme-secondary-600 dark:border-theme-secondary-800 dark:hover:bg-theme-secondary-800 hover:bg-theme-secondary-200"
 >
     <x-ark-icon
