ed333f29ee00909c889cc6f8dda9e01c7f3e449b
ItsANameToo <itsanametoo@protonmail.com>
2023-06-27 12:20:44 +0200

    refactor: table hover types

--- resources/views/components/tables/rows/desktop/encapsulated/transaction-type.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/transaction-type.blade.php
@@ -1,5 +1,5 @@
 @props(['model'])
 
-<div class="text-xs font-semibold rounded border border-transparent dark:bg-transparent px-[3px] py-[2px] bg-theme-secondary-200 leading-[15px] dark:border-theme-secondary-800 dark:text-theme-secondary-500">
+<div class="encapsulated-transaction-type text-xs font-semibold rounded border border-transparent dark:bg-transparent px-[3px] py-[2px] bg-theme-secondary-200 leading-[15px] dark:border-theme-secondary-800 dark:text-theme-secondary-500">
     <x-general.encapsulated.transaction-type :transaction="$model" />
 </div>
