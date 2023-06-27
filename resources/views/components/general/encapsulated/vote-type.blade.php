b9b7a7ef972c367153be54d0a7ac814e6c395b6f
ItsANameToo <itsanametoo@protonmail.com>
2023-06-27 14:04:29 +0200

    feat: add tooltips to votes, unvotes and vote swaps

---
+++ resources/views/components/general/encapsulated/vote-type.blade.php
@@ -0,0 +1,25 @@
+
+@props(['transaction'])
+
+@if($transaction->isVoteCombination())
+    <span
+        data-tippy-html-content="{{ trans('general.transaction.vote_swap_delegate', ['delegate_vote' => $transaction->voted()->username(), 'delegate_unvote' => $transaction->unvoted()->username()]) }}"
+        class="cursor-pointer"
+    >
+        @lang('general.transaction.types.'.$transaction->typeName())
+    </span>
+@elseif($transaction->isVote())
+    <span
+        data-tippy-html-content="{{ trans('general.transaction.vote_delegate', ['delegate' => $transaction->voted()->username()]) }}"
+        class="cursor-pointer"
+    >
+        @lang('general.transaction.types.'.$transaction->typeName())
+    </span>
+@else
+    <span
+        data-tippy-html-content="{{ trans('general.transaction.unvote_delegate', ['delegate' => $transaction->unvoted()->username()]) }}"
+        class="cursor-pointer"
+    >
+        @lang('general.transaction.types.'.$transaction->typeName())
+    </span>
+@endif
