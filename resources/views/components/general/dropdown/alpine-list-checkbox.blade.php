c1f53b465600ae663a999154b343ee039cf02062
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-06-28 23:41:38 +0000

    style: resolve style guide violations

--- resources/views/components/general/dropdown/alpine-list-checkbox.blade.php
+++ resources/views/components/general/dropdown/alpine-list-checkbox.blade.php
@@ -7,7 +7,7 @@
     name="{{ $id }}"
     x-ref="{{ $id }}"
     x-model="{{ $variableName }}.{{ $id }}"
-    class="border-l-4 pl-5 pr-6 py-3 hover:text-theme-secondary-900 hover:bg-theme-secondary-200 dark:hover:bg-theme-secondary-900 dark:text-theme-secondary-200 transition-default cursor-pointer leading-5"
+    class="py-3 pr-6 pl-5 leading-5 border-l-4 cursor-pointer transition-default dark:hover:bg-theme-secondary-900 dark:text-theme-secondary-200 hover:text-theme-secondary-900 hover:bg-theme-secondary-200"
 
     ::class="{
         'border-theme-primary-600 bg-theme-primary-50 dark:bg-theme-secondary-900 text-theme-primary-600 dark:text-white font-semibold': {{ $variableName }}.{{ $id }} === true,
