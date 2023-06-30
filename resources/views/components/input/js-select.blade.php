3bbf0a13787a77894f7c2237545c91322a31a70b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-30 09:38:27 +0100

    fix mobile dropdown margins

--- resources/views/components/input/js-select.blade.php
+++ resources/views/components/input/js-select.blade.php
@@ -64,7 +64,8 @@
 
     <x-general.dropdown.dropdown
         dropdown-wrapper-class="flex relative flex-col w-full"
-        dropdown-class="rounded dark:bg-theme-secondary-800"
+        dropdown-class="rounded"
+        dropdown-padding="py-2 mx-6 sm:mx-0"
         :width="$dropdownWidth"
         :close-on-click="! $multiple"
         :init-alpine="false"
