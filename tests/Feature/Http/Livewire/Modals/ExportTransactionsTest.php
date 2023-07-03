169f121c217df1e8816e5c1f90e1f67c72a0624e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-03 17:23:30 +0100

    fix tests

--- tests/Feature/Http/Livewire/Modals/ExportTransactionsTest.php
+++ tests/Feature/Http/Livewire/Modals/ExportTransactionsTest.php
@@ -3,16 +3,24 @@
 declare(strict_types=1);
 
 use App\Http\Livewire\Modals\ExportTransactions;
+use App\Models\Wallet;
+use App\ViewModels\WalletViewModel;
 use Livewire\Livewire;
 
 it('should render', function () {
-    Livewire::test(ExportTransactions::class)
+    $wallet = Wallet::factory()->create();
+
+    Livewire::test(ExportTransactions::class, [new WalletViewModel($wallet)])
+        ->assertSet('address', $wallet->address)
         ->assertSee(trans('actions.export'))
         ->assertDontSee(trans('pages.wallet.export-transactions-modal.title'));
 });
 
 it('should open modal', function () {
-    Livewire::test(ExportTransactions::class)
+    $wallet = Wallet::factory()->create();
+
+    Livewire::test(ExportTransactions::class, [new WalletViewModel($wallet)])
+        ->assertSet('address', $wallet->address)
         ->assertSee(trans('actions.export'))
         ->assertDontSee(trans('pages.wallet.export-transactions-modal.title'))
         ->call('openModal')
@@ -20,7 +28,10 @@ it('should open modal', function () {
 });
 
 it('should close modal', function () {
-    Livewire::test(ExportTransactions::class)
+    $wallet = Wallet::factory()->create();
+
+    Livewire::test(ExportTransactions::class, [new WalletViewModel($wallet)])
+        ->assertSet('address', $wallet->address)
         ->assertSee(trans('actions.export'))
         ->call('openModal')
         ->assertSee(trans('pages.wallet.export-transactions-modal.title'))
