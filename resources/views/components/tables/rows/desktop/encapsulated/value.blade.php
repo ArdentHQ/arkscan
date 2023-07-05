988bac59548d0245e85c23e8bca704b63cf70ea7
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-05 21:53:15 +0100

    refactor: update table block value font style

--- resources/views/components/tables/rows/desktop/encapsulated/value.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/value.blade.php
@@ -1,5 +1,5 @@
 @props(['model'])
 
-<span class="text-theme-secondary-900 dark:text-theme-secondary-200">
+<span class="text-sm font-semibold text-theme-secondary-900 dark:text-theme-secondary-200">
     {{ $model->rewardFiat() }}
 </span>
