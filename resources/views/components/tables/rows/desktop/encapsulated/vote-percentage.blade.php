b42711f4e0037a2473f0b6d9fad0d6a8e3fb7fd3
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-07-06 07:56:09 +0000

    style: resolve style guide violations

--- resources/views/components/tables/rows/desktop/encapsulated/vote-percentage.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/vote-percentage.blade.php
@@ -1,5 +1,5 @@
 @props(['model'])
 
-<div class="text-sm leading-[17px] font-semibold text-theme-secondary-900 dark:text-theme-secondary-200">
+<div class="text-sm font-semibold leading-[17px] text-theme-secondary-900 dark:text-theme-secondary-200">
     <x-percentage>{{ $model->votePercentage() }}</x-percentage>
 </div>
