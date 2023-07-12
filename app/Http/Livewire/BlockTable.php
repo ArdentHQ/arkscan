f59d595ddeaa91c5b1eb0354060f5f0d9df515d4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-12 02:46:20 +0100

    refactor: improve blocks overview pagination

--- app/Http/Livewire/BlockTable.php
+++ app/Http/Livewire/BlockTable.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Livewire;
 
 use App\Models\Block;
-use App\Models\Scopes\OrderByHeightScope;
+use App\Models\Scopes\OrderByTimestampScope;
 use App\ViewModels\ViewModelFactory;
 use ARKEcosystem\Foundation\UserInterface\Http\Livewire\Concerns\HasPagination;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -15,13 +16,31 @@ final class BlockTable extends Component
 {
     use HasPagination;
 
+    const PER_PAGE = 15;
+
     /** @var mixed */
     protected $listeners = ['currencyChanged' => '$refresh'];
 
     public function render(): View
     {
+        $lastBlock = Block::withScope(OrderByTimestampScope::class)->first();
+
+        $lastBlockHeight = $lastBlock->height;
+        $heightTo   = $lastBlockHeight->minus(self::PER_PAGE * ($this->page - 1))->toNumber();
+        $heightFrom = $lastBlockHeight->minus(self::PER_PAGE)->toNumber();
+
+        $blocks = Block::withScope(OrderByTimestampScope::class)
+            ->where('height', '<=', $heightTo)
+            ->where('height', '>', $heightFrom)
+            ->get();
+
+        $blocks = new LengthAwarePaginator($blocks, $lastBlock->height->toNumber(), self::PER_PAGE, $this->page, [
+            'path'     => route('blocks'),
+            'pageName' => 'page',
+        ]);
+
         return view('livewire.block-table', [
-            'blocks' => ViewModelFactory::paginate(Block::withScope(OrderByHeightScope::class)->paginate()),
+            'blocks' => ViewModelFactory::paginate($blocks),
         ]);
     }
 }
