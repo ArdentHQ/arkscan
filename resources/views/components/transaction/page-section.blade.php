35e5f06f5670390f697f08979daf5f193ec17315
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-13 14:40:22 +0100

    dark mode

--- resources/views/components/transaction/page-section.blade.php
+++ resources/views/components/transaction/page-section.blade.php
@@ -3,21 +3,21 @@
     'items',
 ])
 
-<div {{ $attributes->class('flex sm:space-x-3 px-6 md:px-10 md:mx-auto md:max-w-7xl group') }}>
+<div {{ $attributes->class('flex sm:space-x-3 px-6 md:px-10 md:mx-auto md:max-w-7xl group dark:text-theme-secondary-200') }}>
     <div class="hidden flex-col ml-3 sm:flex w-[1.625rem]">
-        <div class="hidden -mt-2 w-full border-l-2 sm:block group-first:sm:block border-theme-secondary-300 h-[9px]"></div>
+        <div class="hidden -mt-2 w-full border-l-2 sm:block group-first:sm:block border-theme-secondary-300 dark:border-theme-dark-700 h-[9px]"></div>
 
-        <div class="hidden w-full rounded-bl-xl border-b-2 border-l-2 sm:block border-theme-secondary-300 min-h-[12px]"></div>
+        <div class="hidden w-full rounded-bl-xl border-b-2 border-l-2 sm:block border-theme-secondary-300 dark:border-theme-dark-700 min-h-[12px]"></div>
 
-        <div class="hidden flex-1 w-full border-l-2 sm:block group-last:hidden border-theme-secondary-300 min-h-[12px]"></div>
+        <div class="hidden flex-1 w-full border-l-2 sm:block group-last:hidden border-theme-secondary-300 dark:border-theme-dark-700 min-h-[12px]"></div>
     </div>
 
     <div class="flex flex-col flex-1 space-y-3 font-semibold sm:pb-4 sm:space-y-2">
-        <div class="py-2 px-3 border-l-2 sm:py-0 sm:px-0 sm:bg-transparent sm:border-0 bg-theme-secondary-100 border-theme-primary-400">
+        <div class="py-2 px-3 border-l-2 sm:py-0 sm:px-0 sm:bg-transparent dark:sm:bg-transparent sm:border-0 bg-theme-secondary-100 dark:bg-theme-dark-950 border-theme-primary-400 dark:border-theme-dark-blue-400">
             {{ $title }}
         </div>
 
-        <div class="flex space-x-4 text-sm sm:py-4 sm:px-6 sm:text-base sm:leading-5 sm:rounded-xl sm:border leading-[17px] sm:border-theme-secondary-300">
+        <div class="flex space-x-4 text-sm sm:py-4 sm:px-6 sm:text-base sm:leading-5 sm:rounded-xl sm:border leading-[17px] sm:border-theme-secondary-300 dark:border-theme-dark-700">
             @if (is_array($items))
                 <div class="flex flex-col space-y-3 whitespace-nowrap w-[87px]">
                     @foreach (array_keys($items) as $dataTitle)
@@ -25,14 +25,14 @@
                     @endforeach
                 </div>
 
-                <div class="flex flex-col flex-1 space-y-3 text-right sm:text-left">
+                <div class="flex flex-col flex-1 space-y-3 text-right sm:text-left text-theme-secondary-900 dark:text-theme-dark-50">
                     @foreach ($items as $dataContent)
                         @if (empty($dataContent))
-                            <span class="text-theme-secondary-900 font-base">
+                            <span class="font-base">
                                 @lang('general.na')
                             </span>
                         @elseif (! is_array($dataContent))
-                            <span class="text-theme-secondary-900">
+                            <span>
                                 {{ $dataContent }}
                             </span>
                         @elseif (array_key_exists('component', $dataContent))
