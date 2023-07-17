b48ab0a0c584609a3692545dce22dd2b61f2fcde
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-17 17:38:35 +0100

    scrub

--- resources/views/components/transaction/section-detail/addressing-section.blade.php
+++ resources/views/components/transaction/section-detail/addressing-section.blade.php
@@ -1,44 +1,5 @@
 @props(['transaction'])
 
-@php
-    $items = [
-        trans('pages.transaction.header.from') => [
-            'component' => '<x-transaction.section-detail.address
-                :address="$address"
-                class="inline-block"
-            />',
-
-            'data' => [
-                'address' => $transaction->sender()->address(),
-            ],
-        ],
-    ];
-
-    if ($transaction->isTransfer()) {
-        $items[trans('pages.transaction.header.to')] = [
-            'component' => '<x-transaction.section-detail.address
-                :address="$address"
-                class="inline-block"
-            />',
-
-            'data' => [
-                'address' => $transaction->recipient()->address(),
-            ],
-        ];
-    } elseif ($transaction->isMultiPayment()) {
-        $items[trans('pages.transaction.header.to')] = [
-            'component' => '<x-transaction.section-detail.recipients
-                :transaction="$transaction"
-                class="inline-block"
-            />',
-
-            'data' => [
-                'transaction' => $transaction,
-            ],
-        ];
-    }
-@endphp
-
 <x-transaction.page-section :title="trans('pages.transaction.addressing')">
     <x-transaction.section-detail.row
         :title="trans('pages.transaction.header.from')"
