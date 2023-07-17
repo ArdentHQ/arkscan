d33dd9227c0772cbbd2bced06612c22bda5cf85b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-17 17:25:36 +0100

    scrub

--- resources/views/components/transaction/section-detail/recipients.blade.php
+++ resources/views/components/transaction/section-detail/recipients.blade.php
@@ -1,6 +1,4 @@
-@props([
-    'transaction',
-])
+@props(['transaction'])
 
 <span class="inline-flex items-center">
     @lang('pages.transaction.value.multiple_x', ['count' => $transaction->recipientsCount()])
