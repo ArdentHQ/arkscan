9de7d4fbeaa66ae34c84c49b838816b86e6b2afd
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-07-18 23:29:28 +0000

    style: resolve style guide violations

--- resources/views/components/transaction/section-detail/confirmations-section.blade.php
+++ resources/views/components/transaction/section-detail/confirmations-section.blade.php
@@ -3,9 +3,9 @@
 <x-transaction.page-section
     :title="trans('pages.transaction.confirmations')"
     border-class="border-theme-success-200 dark:border-theme-success-500"
-    wrapper-container-class="mx-2 sm:mx-0 bg-theme-success-100 dark:bg-theme-success-900 py-2 rounded-lg border"
+    wrapper-container-class="py-2 mx-2 rounded-lg border sm:mx-0 bg-theme-success-100 dark:bg-theme-success-900"
 >
-    <div class="flex space-x-2 sm:space-x-3 items-center divide-x divide-theme-success-200 dark:divide-theme-success-800">
+    <div class="flex items-center space-x-2 divide-x sm:space-x-3 divide-theme-success-200 dark:divide-theme-success-800">
         <div class="flex items-center space-x-2 text-theme-success-700 dark:text-theme-success-500">
             <x-ark-icon
                 name="double-check-mark"
