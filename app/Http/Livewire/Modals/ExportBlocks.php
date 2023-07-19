c97a5c003d99d45d71fd577168354a94ba7f7cd8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-19 18:39:41 +0100

    refactor: defer export button loading

--- app/Http/Livewire/Modals/ExportBlocks.php
+++ app/Http/Livewire/Modals/ExportBlocks.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Livewire\Modals;
 
+use App\Facades\Wallets;
+use App\Http\Livewire\Concerns\DeferLoading;
 use App\ViewModels\WalletViewModel;
 use ARKEcosystem\Foundation\UserInterface\Http\Livewire\Concerns\HasModal;
 use Illuminate\Contracts\View\View;
@@ -11,6 +13,7 @@ use Livewire\Component;
 
 final class ExportBlocks extends Component
 {
+    use DeferLoading;
     use HasModal;
 
     public ?string $publicKey;
@@ -24,13 +27,19 @@ final class ExportBlocks extends Component
         $this->publicKey = $wallet->publicKey();
 
         if ($wallet->isDelegate()) {
-            $this->username        = $wallet->username();
-            $this->hasForgedBlocks = $wallet->blocksCount() > 0;
+            $this->username = $wallet->username();
         }
     }
 
     public function render(): View
     {
         return view('livewire.modals.export-blocks');
+    }
+
+    public function setIsReady(): void
+    {
+        $wallet = Wallets::findByPublicKey($this->publicKey);
+
+        $this->hasForgedBlocks = $wallet->blocks()->count() > 0;
     }
 }
