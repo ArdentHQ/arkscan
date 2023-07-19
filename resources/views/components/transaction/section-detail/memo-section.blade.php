ff34212a46bd0501523654e6d177a0afcfe13bed
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-19 10:57:10 +0100

    update font sizes

--- resources/views/components/transaction/section-detail/memo-section.blade.php
+++ resources/views/components/transaction/section-detail/memo-section.blade.php
@@ -7,11 +7,11 @@
     wrapper-class="font-normal leading-7"
 >
     @if (! $vendorField)
-        <span class="text-sm font-semibold sm:font-normal text-theme-secondary-500 dark:text-theme-dark-500">
+        <span class="text-sm sm:text-base font-semibold sm:font-normal text-theme-secondary-500 dark:text-theme-dark-500">
             @lang('general.na')
         </span>
     @else
-        <span class="text-theme-secondary-900 dark:text-theme-dark-50">
+        <span class="text-theme-secondary-900 dark:text-theme-dark-50 text-base">
             {{ $vendorField }}
         </span>
     @endif
