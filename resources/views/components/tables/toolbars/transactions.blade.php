1c922802735406e20db16f1d47d5ced4d99d7288
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-21 10:32:03 +0100

    refactor: filter dropdown for mobile (#437)

--- resources/views/components/tables/toolbars/transactions.blade.php
+++ resources/views/components/tables/toolbars/transactions.blade.php
@@ -9,6 +9,8 @@
 
         <div class="flex-1">
             <x-tables.filters.transactions />
+
+            <x-tables.filters.transactions mobile />
         </div>
     </div>
 </x-general.encapsulated.table-header>
