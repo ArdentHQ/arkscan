1577cff7df727ec42154f353d1faf46e4497e218
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-21 10:09:44 +0100

    refactor: filter dropdown

--- resources/views/components/general/dropdown/filter.blade.php
+++ resources/views/components/general/dropdown/filter.blade.php
@@ -1,10 +1,18 @@
+@props([
+    'mobile' => false,
+])
+
 <x-general.dropdown.dropdown
-    placement="right-start"
+    :placement="$mobile ? 'bottom' : 'right-start'"
     :placement-fallbacks="['bottom', 'bottom-end', 'left-start']"
     dropdown-class="px-6 w-full md:px-8 table-filter md:w-[284px]"
     :close-on-click="false"
     class=""
-    dropdown-wrapper-class="w-full"
+    :dropdown-wrapper-class="Arr::toCssClasses([
+        'w-full',
+        'md:hidden' => $mobile,
+        'hidden md:block' => ! $mobile,
+    ])"
     dropdown-background="bg-white dark:bg-theme-secondary-900 dark:border dark:border-theme-secondary-800"
     dropdown-padding="py-1"
     button-class="flex flex-1 justify-center items-center w-full rounded sm:flex-none sm:py-1.5 sm:px-4 md:p-2 button-secondary"
