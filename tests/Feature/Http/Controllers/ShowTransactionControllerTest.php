da4567c45acda37cc6f5677e2939701c6eceb931
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-24 15:08:46 +0100

    tests

--- tests/Feature/Http/Controllers/ShowTransactionControllerTest.php
+++ tests/Feature/Http/Controllers/ShowTransactionControllerTest.php
@@ -3,11 +3,85 @@
 declare(strict_types=1);
 
 use App\Models\Transaction;
+use App\Models\Wallet;
 
-it('should render the page without any errors', function () {
+it('should render the page without any errors', function ($type) {
     $this->withoutExceptionHandling();
 
+    $transaction = Transaction::factory()->{$type}()->create();
+
     $this
-        ->get(route('transaction', Transaction::factory()->transfer()->create()))
-        ->assertOk();
+        ->get(route('transaction', $transaction))
+        ->assertOk()
+        ->assertSee($transaction->id);
+})->with([
+    'transfer',
+    'secondSignature',
+    'delegateRegistration',
+    'multiSignature',
+    'ipfs',
+    'delegateResignation',
+    'timelock',
+    'timelockClaim',
+    'timelockRefund',
+    'entityRegistration',
+    'entityResignation',
+    'entityUpdate',
+    'businessEntityRegistration',
+    'businessEntityResignation',
+    'businessEntityUpdate',
+    'productEntityRegistration',
+    'productEntityResignation',
+    'productEntityUpdate',
+    'pluginEntityRegistration',
+    'pluginEntityResignation',
+    'pluginEntityUpdate',
+    'moduleEntityRegistration',
+    'moduleEntityResignation',
+    'moduleEntityUpdate',
+    'delegateEntityRegistration',
+    'delegateEntityResignation',
+    'delegateEntityUpdate',
+    'legacyBusinessRegistration',
+    'legacyBusinessResignation',
+    'legacyBusinessUpdate',
+    'legacyBridgechainRegistration',
+    'legacyBridgechainResignation',
+    'legacyBridgechainUpdate',
+]);
+
+it('should render the page for a vote/unvote transaction without any errors', function ($type) {
+    $this->withoutExceptionHandling();
+
+    $delegate = Wallet::factory()->activeDelegate()->create();
+    $transaction = Transaction::factory()->{$type}()->create([
+        'asset' => [
+            'votes' => ['+'.$delegate->public_key],
+        ],
+    ]);
+
+    $this
+        ->get(route('transaction', $transaction))
+        ->assertOk()
+        ->assertSee($transaction->id);
+})->with([
+    'vote',
+    'unvote',
+]);
+
+it('should render the page for a vote combination transaction without any errors', function () {
+    $this->withoutExceptionHandling();
+
+    $oldDelegate = Wallet::factory()->activeDelegate()->create();
+    $newDelegate = Wallet::factory()->activeDelegate()->create();
+    $transaction = Transaction::factory()->voteCombination()->create([
+        'asset' => [
+            'votes' => ['-'.$oldDelegate->public_key, '+'.$newDelegate->public_key],
+        ],
+    ]);
+
+    $this
+        ->get(route('transaction', $transaction))
+        ->assertOk()
+        ->assertSee($transaction->id);
 });
