a6cf6de2d4532c913e524146f6b8814abd63a03e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-25 15:36:26 +0100

    show loader regardless of ready for mobile

--- resources/views/components/general/pagination/lazy-loading.blade.php
+++ resources/views/components/general/pagination/lazy-loading.blade.php
@@ -1,19 +1,23 @@
-@if ($this->isReady && ! $this->isOnLastPage())
-    <div
-        wire:poll.100ms.visible="nextPage"
-        wire:loading.remove
-    ></div>
-
-    <div class="md:hidden">
+<div>
+    @if ($this->isReady && ! $this->isOnLastPage())
         <div
-            class="items-center justify-center mt-3"
-            wire:loading.flex
-        >
-            <x-ark-loader-icon
-                class="w-8 h-8"
-                path-class="fill-theme-primary-600 dark:fill-theme-dark-blue-400"
-                circle-class="stroke-theme-primary-100 dark:stroke-theme-dark-700"
-            />
+            wire:poll.100ms.visible="nextPage"
+            wire:loading.remove
+        ></div>
+    @endif
+
+    @if (! $this->isOnLastPage())
+        <div class="md:hidden">
+            <div
+                class="items-center justify-center mt-3"
+                wire:loading.flex
+            >
+                <x-ark-loader-icon
+                    class="w-8 h-8"
+                    path-class="fill-theme-primary-600 dark:fill-theme-dark-blue-400"
+                    circle-class="stroke-theme-primary-100 dark:stroke-theme-dark-700"
+                />
+            </div>
         </div>
-    </div>
-@endif
+    @endif
+</div>
