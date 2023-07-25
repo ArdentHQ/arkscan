f2a84a418285924250f53ed0189f7f406af528fd
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-25 15:25:44 +0100

    fix double spinners

--- resources/views/components/general/pagination/lazy-loading.blade.php
+++ resources/views/components/general/pagination/lazy-loading.blade.php
@@ -4,14 +4,16 @@
         wire:loading.remove
     ></div>
 
-    <div
-        class="md:hidden items-center justify-center mt-3"
-        wire:loading.flex
-    >
-        <x-ark-loader-icon
-            class="w-8 h-8"
-            path-class="fill-theme-primary-600 dark:fill-theme-dark-blue-400"
-            circle-class="stroke-theme-primary-100 dark:stroke-theme-dark-700"
-        />
+    <div class="md:hidden">
+        <div
+            class="items-center justify-center mt-3"
+            wire:loading.flex
+        >
+            <x-ark-loader-icon
+                class="w-8 h-8"
+                path-class="fill-theme-primary-600 dark:fill-theme-dark-blue-400"
+                circle-class="stroke-theme-primary-100 dark:stroke-theme-dark-700"
+            />
+        </div>
     </div>
 @endif
