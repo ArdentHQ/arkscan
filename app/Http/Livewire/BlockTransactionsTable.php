aae90a1817d2055578c027a2d5a307308978ceac
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-07-26 01:33:34 +0100

    tests

--- app/Http/Livewire/BlockTransactionsTable.php
+++ app/Http/Livewire/BlockTransactionsTable.php
@@ -7,6 +7,7 @@ namespace App\Http\Livewire;
 use App\Http\Livewire\Concerns\DeferLoading;
 use App\Http\Livewire\Concerns\HasLazyLoadingPagination;
 use App\Models\Block;
+use App\Models\Scopes\OrderByTimestampScope;
 use App\ViewModels\BlockViewModel;
 use App\ViewModels\ViewModelFactory;
 use Illuminate\Contracts\View\View;
@@ -50,6 +51,7 @@ final class BlockTransactionsTable extends Component
 
         return $this->getBlock()
             ->transactions()
+            ->withScope(OrderByTimestampScope::class)
             ->limit($this->perPage * $this->page)
             ->get();
     }
--- tests/Feature/Http/Livewire/BlockTransactionsTableTest.php
+++ tests/Feature/Http/Livewire/BlockTransactionsTableTest.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\BlockTransactionsTable;
 use App\Models\Block;
 use App\Models\Transaction;
 use App\Services\NumberFormatter;
+use App\Services\Timestamp;
 use App\ViewModels\BlockViewModel;
 use App\ViewModels\ViewModelFactory;
 use Livewire\Livewire;
@@ -24,5 +25,54 @@ it('should list the first transactions for the giving block id', function () {
         $component->assertSee($transaction->recipient()->address());
         $component->assertSee(NumberFormatter::networkCurrency($transaction->amount()));
         $component->assertSee(NumberFormatter::networkCurrency($transaction->fee()));
+    }
+});
+
+it('should load the next batch of transactions', function () {
+    $block = Block::factory()->create([
+        'number_of_transactions' => 54,
+    ]);
+    $thirdPageTransactions = Transaction::factory(4)->transfer()->create([
+        'block_id'  => $block->id,
+        'timestamp' => Timestamp::now()->sub(2, 'days')->timestamp,
+    ]);
+    $secondPageTransactions = Transaction::factory(25)->transfer()->create([
+        'block_id'  => $block->id,
+        'timestamp' => Timestamp::now()->sub(1, 'day')->timestamp,
+    ]);
+    $visibleTransactions = Transaction::factory(25)->transfer()->create([
+        'block_id'  => $block->id,
+        'timestamp' => Timestamp::now()->timestamp,
+    ]);
+
+    $component = Livewire::test(BlockTransactionsTable::class, ['block' => new BlockViewModel($block)])
+        ->call('setIsReady')
+        ->assertCount('lazyLoadedData', 25);
+
+    foreach ($visibleTransactions as $transaction) {
+        $component->assertSee($transaction->id);
+    }
+
+    foreach ($secondPageTransactions as $transaction) {
+        $component->assertDontSee($transaction->id);
+    }
+
+    foreach ($thirdPageTransactions as $transaction) {
+        $component->assertDontSee($transaction->id);
+    }
+
+    $component->call('nextPage')
+        ->assertCount('lazyLoadedData', 50);
+
+    foreach ($visibleTransactions as $transaction) {
+        $component->assertSee($transaction->id);
+    }
+
+    foreach ($secondPageTransactions as $transaction) {
+        $component->assertSee($transaction->id);
+    }
+
+    foreach ($thirdPageTransactions as $transaction) {
+        $component->assertDontSee($transaction->id);
     }
 });
