a7e3f4dc3960f73fea27603be727cb4ffb7785d9
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-08-01 21:14:01 +0000

    style: resolve style guide violations

--- resources/views/components/page-headers/delegates.blade.php
+++ resources/views/components/page-headers/delegates.blade.php
@@ -2,17 +2,17 @@
 
 <div class="flex flex-col px-6 pt-8 pb-6 space-y-6 font-semibold md:px-10 md:mx-auto md:max-w-7xl">
     <div class="flex flex-col space-y-1.5">
-        <h1 class="text-lg md:text-2xl mb-0 leading-5.25 md:leading-[1.8125rem]">
+        <h1 class="mb-0 text-lg md:text-2xl leading-5.25 md:leading-[1.8125rem]">
             @lang('pages.delegates.title')
         </h1>
 
-        <span class="text-xs text-theme-secondary-500 dark:text-theme-dark-500 leading-3.75">
+        <span class="text-xs text-theme-secondary-500 leading-3.75 dark:text-theme-dark-500">
             @lang('pages.delegates.subtitle')
         </span>
     </div>
 
-    <div class="flex flex-col space-y-2 xl:flex-row sm:space-y-3 xl:space-y-0 xl:space-x-3">
-        <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-2 md:space-x-3 flex-1">
+    <div class="flex flex-col space-y-2 sm:space-y-3 xl:flex-row xl:space-y-0 xl:space-x-3">
+        <div class="flex flex-col flex-1 space-y-2 sm:flex-row sm:space-y-0 sm:space-x-2 md:space-x-3">
             <x-page-headers.delegates.missed-blocks :statistics="$statistics" />
             <x-page-headers.delegates.voting />
         </div>
