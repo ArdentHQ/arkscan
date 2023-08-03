724dec9eef29bbb0f44b9ec209bf545b8d4c4958
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-08-03 23:05:09 +0100

    delegate tabs tests

---
+++ tests/Feature/Http/Livewire/Delegates/TabsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Livewire\Delegates\Tabs;
+use App\Models\Wallet;
+use Livewire\Livewire;
+
+it('should render', function () {
+    Livewire::test(Tabs::class)
+        ->assertSee('Delegates');
+});
+
+it('should set initial data', function () {
+    Livewire::test(Tabs::class)
+        ->assertSet('tabQueryData', [
+            'delegates' => [
+                'page'    => 1,
+                'perPage' => 51,
+            ],
+        ]);
+});
+
+it('should get querystring data', function () {
+    $instance = Livewire::test(Tabs::class)
+        ->instance();
+
+    expect($instance->queryString())->toBe([
+        'view'    => ['except' => 'delegates'],
+        'page'    => ['except' => 1],
+        'perPage' => ['except' => 51],
+    ]);
+});
+
+it('should change querystring if different view', function () {
+    $instance = Livewire::test(Tabs::class)
+        ->set('view', 'testing')
+        ->instance();
+
+    expect($instance->queryString())->toBe([
+        'view'    => ['except' => 'delegates'],
+        'page'    => ['except' => 1],
+        'perPage' => ['except' => 25],
+    ]);
+});
