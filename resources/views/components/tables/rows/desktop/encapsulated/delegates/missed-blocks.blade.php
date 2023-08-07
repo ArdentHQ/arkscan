1d8629797ac0b3893fc5d90c340875692287042b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-08-07 14:26:29 +0100

    use generic badge component

--- resources/views/components/tables/rows/desktop/encapsulated/delegates/missed-blocks.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/delegates/missed-blocks.blade.php
@@ -16,12 +16,13 @@
     }
 @endphp
 
-{{-- TODO: use <x-general.badge when PR #465 is merged --}}
-<div {{ $attributes->class([
-    'text-xs font-semibold rounded border px-[3px] py-[2px] leading-[15px] dark:bg-transparent min-w-[30px] text-center',
-    'bg-theme-success-100 border-theme-success-100 text-theme-success-700 dark:border-theme-success-700 dark:text-theme-success-500' => $state === 'success',
-    'bg-theme-orange-light border-theme-orange-light text-theme-orange-dark dark:border-theme-orange-dark dark:text-theme-warning-400' => $state === 'warning',
-    'bg-theme-danger-100 border-theme-danger-100 text-theme-danger-700 dark:border-[#AA6868] dark:text-[#F39B9B]' => $state === 'danger',
-]) }}>
+<x-general.badge
+    class="min-w-[30px] text-center"
+    :colors="Arr::toCssClasses([
+        'bg-theme-success-100 border-theme-success-100 text-theme-success-700 dark:border-theme-success-700 dark:text-theme-success-500' => $state === 'success',
+        'bg-theme-orange-light border-theme-orange-light text-theme-orange-dark dark:border-theme-orange-dark dark:text-theme-warning-400' => $state === 'warning',
+        'bg-theme-danger-100 border-theme-danger-100 text-theme-danger-700 dark:border-[#AA6868] dark:text-[#F39B9B]' => $state === 'danger',
+    ])"
+>
     {{ $missedBlocks }}
-</div>
+</x-general.badge>
