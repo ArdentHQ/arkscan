fea6a5e86bf4eefaec1d8dedb6b0b07fd21ba1c9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-08-09 17:34:06 +0100

    border separator

--- resources/views/app/delegates.blade.php
+++ resources/views/app/delegates.blade.php
@@ -4,8 +4,8 @@
     @section('content')
         <livewire:delegates.header-stats />
 
-        <x-ark-container>
+        <div class="px-6 pb-8 pt-6 border-t-4 sm:border-0 border-theme-secondary-200 dark:border-theme-dark-950 md:px-10 md:pt-0 md:pb-6 md:mx-auto md:max-w-7xl">
             <livewire:delegates.tabs />
-        </x-ark-container>
+        </div>
     @endsection
 @endcomponent
