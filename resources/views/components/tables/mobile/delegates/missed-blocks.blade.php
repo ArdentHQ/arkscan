20377bf84a6922335d254cd649495b09b749cfd1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-08-21 16:42:21 +0100

    align delegate name on sm devices

--- resources/views/components/tables/mobile/delegates/missed-blocks.blade.php
+++ resources/views/components/tables/mobile/delegates/missed-blocks.blade.php
@@ -16,19 +16,19 @@
                 <div class="flex flex-1 justify-between">
                     <x-tables.headers.mobile.encapsulated.block-height
                         :model="$block"
-                        class="font-semibold sm:flex-1 text-theme-secondary-900 dark:text-theme-dark-50"
+                        class="sm:flex-1 font-semibold text-theme-secondary-900 dark:text-theme-dark-50"
                         without-link
                     />
 
                     <x-tables.rows.mobile.encapsulated.delegates.address
                         :model="$block->delegate()"
-                        class="hidden sm:block"
+                        class="hidden sm:block sm:flex-1"
                         without-label
                     />
 
                     <x-tables.rows.mobile.encapsulated.age
                         :model="$block"
-                        class="sm:flex-1 sm:text-right leading-4.25"
+                        class="sm:text-right leading-4.25 sm:min-w-[110px]"
                     />
                 </div>
             </x-slot>
@@ -38,11 +38,18 @@
                 class="sm:hidden"
             />
 
-            <x-tables.rows.mobile.encapsulated.delegates.number-of-voters :model="$block->delegate()" />
+            <x-tables.rows.mobile.encapsulated.delegates.number-of-voters
+                :model="$block->delegate()"
+                class="sm:flex-1"
+            />
 
-            <x-tables.rows.mobile.encapsulated.delegates.votes :model="$block->delegate()" />
+            <div class="sm:flex-1">
+                <x-tables.rows.mobile.encapsulated.delegates.votes :model="$block->delegate()" />
+            </div>
 
-            <x-tables.rows.mobile.encapsulated.delegates.votes-percentage :model="$block->delegate()" />
+            <div class="sm:flex sm:justify-end sm:min-w-[110px]">
+                <x-tables.rows.mobile.encapsulated.delegates.votes-percentage :model="$block->delegate()" />
+            </div>
         </x-tables.rows.mobile>
     @endforeach
 </x-tables.mobile.includes.encapsulated>
