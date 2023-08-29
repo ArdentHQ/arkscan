333e4cb820661a284d1f557d361a8c1981e94339
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-08-29 22:38:41 +0100

    cache stats for 5 minutes

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -8,10 +8,13 @@ use App\Services\BigNumber;
 use App\Services\Timestamp;
 use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 final class TransactionsController
 {
+    const STATS_TTL = 300;
+
     public function __invoke(): View
     {
         $data = $this->transactionData();
@@ -26,20 +29,22 @@ final class TransactionsController
 
     private function transactionData(): array
     {
-        $data = (array) DB::connection('explorer')
-            ->table('transactions')
-            ->selectRaw('COUNT(*) as transaction_count')
-            ->selectRaw('SUM(amount) as volume')
-            ->selectRaw('SUM(fee) as total_fees')
-            ->selectRaw('AVG(fee) as average_fee')
-            ->where('timestamp', '>', Timestamp::fromUnix(Carbon::now()->subDays(1)->unix())->unix())
-            ->first();
+        return Cache::remember('transactions:stats', self::STATS_TTL, function () {
+            $data = (array) DB::connection('explorer')
+                ->table('transactions')
+                ->selectRaw('COUNT(*) as transaction_count')
+                ->selectRaw('SUM(amount) as volume')
+                ->selectRaw('SUM(fee) as total_fees')
+                ->selectRaw('AVG(fee) as average_fee')
+                ->where('timestamp', '>', Timestamp::fromUnix(Carbon::now()->subDays(1)->unix())->unix())
+                ->first();
 
-        return [
-            'transaction_count' => $data['transaction_count'],
-            'volume'            => $data['volume'] ?? 0,
-            'total_fees'        => $data['total_fees'] ?? 0,
-            'average_fee'       => $data['average_fee'] ?? 0,
-        ];
+            return [
+                'transaction_count' => $data['transaction_count'],
+                'volume'            => $data['volume'] ?? 0,
+                'total_fees'        => $data['total_fees'] ?? 0,
+                'average_fee'       => $data['average_fee'] ?? 0,
+            ];
+        });
     }
 }
