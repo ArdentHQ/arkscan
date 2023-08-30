60e28311eac6861f23ef9141503d485fbab8c169
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-08-30 14:41:12 +0000

    style: resolve style guide violations

--- app/Models/ForgingStats.php
+++ app/Models/ForgingStats.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Models\Concerns\SearchesCaseInsensitive;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Builder;
 
 /**
  * @property int $missed_height
