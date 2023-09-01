43f3d7d51f8c17910958794f0e0539991b8d6cfb
ItsANameToo <itsanametoo@protonmail.com>
2023-09-01 15:14:24 +0200

    chore: rename variable

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,37 +1,43 @@
-const mix = require('laravel-mix');
-const focusVisible = require('postcss-focus-visible');
-const path = require('path');
+const mix = require("laravel-mix");
+const focusVisible = require("postcss-focus-visible");
+const path = require("path");
 
 mix.webpackConfig({
-        resolve: {
-            alias: {
-                '@ui': path.resolve(__dirname, 'vendor/arkecosystem/foundation/resources/assets/')
-            }
+    resolve: {
+        alias: {
+            "@ui": path.resolve(
+                __dirname,
+                "vendor/arkecosystem/foundation/resources/assets/"
+            ),
         },
-        // @see: https://laravel-mix.com/docs/6.0/upgrade#unused-library-extraction
-        optimization: {
-            providedExports: false,
-            sideEffects: false,
-            usedExports: false
-        }
-    })
+    },
+    // @see: https://laravel-mix.com/docs/6.0/upgrade#unused-library-extraction
+    optimization: {
+        providedExports: false,
+        sideEffects: false,
+        usedExports: false,
+    },
+})
     // Options
     .options({
         processCssUrls: false,
     })
     // App
-    .js('resources/js/app.js', 'public/js')
-    .js('vendor/arkecosystem/foundation/resources/assets/js/clipboard.js', 'public/js/clipboard.js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-        focusVisible()
+    .js("resources/js/app.js", "public/js")
+    .js(
+        "vendor/arkecosystem/foundation/resources/assets/js/clipboard.js",
+        "public/js/clipboard.js"
+    )
+    .postCss("resources/css/app.css", "public/css", [
+        require("postcss-import"),
+        require("tailwindcss"),
+        focusVisible(),
     ])
-    .copyDirectory('resources/images', 'public/images')
+    .copyDirectory("resources/images", "public/images")
     // Extract node_modules
-    .extract(['alpinejs', 'chart.js']);
+    .extract(["alpinejs", "chart.js"]);
 
-if (process.env.MIX_DISABLE_NOTIFICATIONS === 'true') {
+if (process.env.MIX_NOTIFICATIONS_DISABLED === "true") {
     mix.disableSuccessNotifications();
 }
 
