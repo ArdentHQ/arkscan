f207b16920b102905bacdad5bc191e8162503c90
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-11 15:39:56 +0100

    refactor: consistent headings & fix mobile hidden header (#534)

--- resources/views/app/compatible-wallets.blade.php
+++ resources/views/app/compatible-wallets.blade.php
@@ -2,14 +2,12 @@
     <x-metadata page="compatible-wallets" />
 
     @section('content')
-        <x-ark-container>
-            <x-information-pages.header
-                :title="trans('pages.compatible-wallets.title')"
-                :subtitle="trans('pages.compatible-wallets.subtitle')"
-            />
+        <x-page-headers.generic
+            :title="trans('pages.compatible-wallets.title')"
+            :subtitle="trans('pages.compatible-wallets.subtitle')"
+        />
 
-            <x-compatible-wallets.section-divider />
-
+        <div class="px-6 pt-6 pb-8 border-t-4 md:px-10 md:pt-0 md:pb-6 md:mx-auto md:max-w-7xl md:border-0 border-theme-secondary-200 dark:border-theme-dark-950">
             <x-compatible-wallets.arkvault />
 
             <x-compatible-wallets.section-divider />
@@ -19,6 +17,6 @@
             <x-information-pages.cta :text="trans('pages.compatible-wallets.get_listed')">
                 <livewire:modals.submit-wallet />
             </x-information-pages.cta>
-        </x-ark-container>
+        </div>
     @endsection
 @endcomponent
--- resources/views/app/exchanges.blade.php
+++ resources/views/app/exchanges.blade.php
@@ -2,17 +2,16 @@
     <x-metadata page="exchanges" />
 
     @section('content')
-        <x-ark-container container-class="flex flex-col">
-            <x-information-pages.header
-                :title="trans('pages.exchanges.title')"
-                :subtitle="trans('pages.exchanges.subtitle')"
-            >
-                <livewire:exchange-table-filter />
-            </x-information-pages.header>
+        <x-page-headers.generic
+            :title="trans('pages.exchanges.title')"
+            :subtitle="trans('pages.exchanges.subtitle')"
+            class="sm:space-y-4 md:justify-between md-lg:flex-row md-lg:items-center md-lg:space-y-0"
+        >
+            <livewire:exchange-table-filter />
+        </x-page-headers.generic>
 
-            <div class="mt-6">
-                <livewire:exchange-table />
-            </div>
+        <div class="flex flex-col px-6 pb-8 md:px-10 md:pb-6 md:mx-auto md:max-w-7xl">
+            <livewire:exchange-table />
 
             <x-information-pages.cta
                 :text="trans('pages.exchanges.get_listed')"
@@ -21,6 +20,6 @@
             >
                 <livewire:modals.submit-exchange />
             </x-information-pages.cta>
-        </x-ark-container>
+        </div>
     @endsection
 @endcomponent
