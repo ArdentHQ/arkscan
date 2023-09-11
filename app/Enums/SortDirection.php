a4f2147e2db1b404fef0c04a8f6cdaafed4c9800
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-11 19:01:08 +0100

    sorting base trait & enum

---
+++ app/Enums/SortDirection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum SortDirection: string
+{
+    case ASC = 'asc';
+
+    case DESC = 'desc';
+}
---
+++ app/Http/Livewire/Concerns/HasTableSorting.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Livewire\Concerns;
+
+use App\Enums\SortDirection;
+
+trait HasTableSorting
+{
+    public string $sortKey;
+
+    public SortDirection $sortDirection;
+
+    public function bootHasTableSorting(): void
+    {
+        $this->sortKey = static::defaultSortKey();
+        $this->sortDirection = static::defaultSortDirection();
+    }
+
+    public function sortBy(string $key): void
+    {
+        if ($this->sortKey === $key) {
+            if ($this->sortDirection === SortDirection::ASC) {
+                $this->sortDirection = SortDirection::DESC;
+            } else {
+                $this->sortDirection = SortDirection::ASC;
+            }
+        } else {
+            $this->sortKey = $key;
+            $this->sortDirection = SortDirection::ASC;
+        }
+    }
+
+    public static function defaultSortKey(): string
+    {
+        return constant(static::class.'::INITIAL_SORT_KEY');
+    }
+
+    public static function defaultSortDirection(): SortDirection
+    {
+        return constant(static::class.'::INITIAL_SORT_DIRECTION');
+    }
+}
