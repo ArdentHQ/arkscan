d7ade1b3442d0153b14b8a020c113cd100e88f5c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-13 02:49:47 +0100

    use caret for icons

--- resources/views/components/tables/headers/desktop/includes/livewire-sort-icon.blade.php
+++ resources/views/components/tables/headers/desktop/includes/livewire-sort-icon.blade.php
@@ -3,7 +3,7 @@
 @if ($id !== null)
     <div {{ $attributes->class('flex flex-col transition-default text-theme-secondary-500 dark:text-theme-dark-500 group-hover/header:text-theme-secondary-900 group-hover/header:dark:text-theme-dark-50') }}>
         <x-ark-icon
-            name="arrows.chevron-up-small"
+            name="arrows.caret-up"
             size="w-2 h-2"
             :class="Arr::toCssClasses([
                 'text-theme-primary-600 dark:text-theme-dark-blue-400' => $this->sortKey === $id && $this->sortDirection === SortDirection::ASC,
@@ -11,7 +11,7 @@
         />
 
         <x-ark-icon
-            name="arrows.chevron-down-small"
+            name="arrows.caret-down"
             size="w-2 h-2"
             :class="Arr::toCssClasses([
                 'text-theme-primary-600 dark:text-theme-dark-blue-400' => $this->sortKey === $id && $this->sortDirection === SortDirection::DESC,
