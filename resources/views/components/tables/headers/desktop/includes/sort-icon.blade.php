7add71dece2c2c5629adc2ddb614ab0953270ae6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-21 22:08:31 +0100

    refactor: update exchanges sort styling

--- resources/views/components/tables/headers/desktop/includes/sort-icon.blade.php
+++ resources/views/components/tables/headers/desktop/includes/sort-icon.blade.php
@@ -5,17 +5,32 @@
 
 @if ($id !== null)
     <div
-        class="transition-default group-hover/header:text-theme-secondary-700"
-        :class="{
-            'text-theme-secondary-500': sortBy === '{{ $id }}',
-            'text-transparent': sortBy !== '{{ $id }}',
-            'rotate-180': (sortBy === '{{ $id }}' && sortAsc === false) || (sortBy !== '{{ $id }}' && '{{ $initialDirection }}' === 'desc'),
-        }"
+        class="flex flex-col text-theme-secondary-500 dark:text-theme-dark-500 group-hover/header:text-theme-secondary-900 group-hover/header:dark:text-theme-dark-50"
         x-cloak
     >
-        <x-ark-icon
-            name="arrows.chevron-up-small"
-            size="w-2.5 h-2.5"
-        />
+        <div
+            class="transition-default"
+            :class="{
+                'text-theme-primary-600 dark:text-theme-dark-blue-400': sortBy === '{{ $id }}' && sortAsc,
+            }"
+        >
+            <x-ark-icon
+                name="arrows.caret-up"
+                size="w-2 h-2"
+            />
+        </div>
+
+        <div
+            class="transition-default"
+            :class="{
+                'text-theme-primary-600 dark:text-theme-dark-blue-400': sortBy === '{{ $id }}' && ! sortAsc,
+                'text-theme-secondary-500 dark:text-theme-dark-500': sortBy !== '{{ $id }}',
+            }"
+        >
+            <x-ark-icon
+                name="arrows.caret-down"
+                size="w-2 h-2"
+            />
+        </div>
     </div>
 @endif
