0661af80acce4fb9649f2d21df6279dc67c3a1af
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-26 13:47:16 +0100

    update blocks no results text

--- resources/lang/en/tables.php
+++ resources/lang/en/tables.php
@@ -38,7 +38,13 @@ return [
         'volume'       => 'Volume (:currency)',
         'total_reward' => 'Total Reward (:currency)',
         'value'        => 'Value (:currency)',
-        'no_results'   => 'This delegate has not yet validated a block.',
+        'no_results'   => 'There are currently no blocks.',
+    ],
+
+    'wallet' => [
+        'blocks' => [
+            'no_results' => 'This delegate has not yet validated a block.',
+        ],
     ],
 
     'wallets' => [
