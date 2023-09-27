3d223ddb9a333e0483a7296c52cd8231b5a4035e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-27 12:36:10 +0100

    fix spacing of statistics

--- resources/views/components/home/stat.blade.php
+++ resources/views/components/home/stat.blade.php
@@ -4,12 +4,12 @@
 ])
 
 <div {{ $attributes->class('space-y-2') }}>
-    <div class="text-sm font-semibold">
+    <div class="text-sm font-semibold dark:text-theme-dark-200 leading-4.25">
         {{ $title }}
     </div>
 
     <div @class([
-        'font-semibold',
+        'font-semibold leading-5',
         'text-theme-secondary-900 dark:text-theme-secondary-50' => ! $disabled,
         'text-theme-secondary-500 dark:text-theme-secondary-700' => $disabled,
     ])>
