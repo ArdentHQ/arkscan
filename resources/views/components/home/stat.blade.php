36f0f7e7159f51644968f152602ad0e43b2a7ca7
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-27 02:35:56 +0100

    handle n/a market cap

--- resources/views/components/home/stat.blade.php
+++ resources/views/components/home/stat.blade.php
@@ -1,11 +1,22 @@
-@props(['title'])
+@props([
+    'title',
+    'disabled' => false,
+])
 
 <div {{ $attributes->class('space-y-2') }}>
     <div class="text-sm font-semibold">
         {{ $title }}
     </div>
 
-    <div class="font-semibold text-theme-secondary-900 dark:text-theme-secondary-50">
-        {{ $slot }}
+    <div @class([
+        'font-semibold',
+        'text-theme-secondary-900 dark:text-theme-secondary-50' => ! $disabled,
+        'text-theme-secondary-500 dark:text-theme-secondary-700' => $disabled,
+    ])>
+        @unless ($disabled)
+            {{ $slot }}
+        @else
+            @lang('general.na')
+        @endunless
     </div>
 </div>
