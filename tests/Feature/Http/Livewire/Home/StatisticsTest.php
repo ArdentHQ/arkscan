2efe9f3bb3068b5c6b9a6fc8e53f6197bba99ea6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-09-27 02:37:50 +0100

    test

--- tests/Feature/Http/Livewire/Home/StatisticsTest.php
+++ tests/Feature/Http/Livewire/Home/StatisticsTest.php
@@ -3,14 +3,19 @@
 declare(strict_types=1);
 
 use App\Facades\Settings;
-use App\Http\Livewire\NetworkStatusBlock;
+use App\Http\Livewire\Home\Statistics;
 use App\Models\Block;
+use App\Models\Transaction;
 use App\Models\Wallet;
 use App\Services\Cache\NetworkStatusBlockCache;
+use App\Services\Timestamp;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Config;
 use Livewire\Livewire;
 
-it('should render with a height, supply and not available market cap', function () {
+it('should render with a height, volume, supply and not available market cap', function () {
+    Config::set('arkscan.networks.development.canBeExchanged', false);
+
     Block::factory()->create([
         'height'               => 5651290,
         'generator_public_key' => Wallet::factory()->create([
@@ -18,15 +23,16 @@ it('should render with a height, supply and not available market cap', function 
         ])->public_key,
     ]);
 
-    (new NetworkStatusBlockCache())->setPrice('USD', 'USD', 0.2907);
-
-    Livewire::test(NetworkStatusBlock::class)
-        ->assertSee('5,651,290')
-        ->assertSee('136,280,982 DARK')
-        ->assertSee('Not Available');
+    Livewire::test(Statistics::class)
+        ->assertSeeInOrder([
+            '136,280,982 DARK', // Supply
+            'N/A', // Market Cap
+            'N/A', // Volume
+            '5,651,290', // Height
+        ]);
 });
 
-it('should render with a height, supply and market cap', function () {
+it('should render with a height, volume, supply and market cap', function () {
     Config::set('arkscan.network', 'production');
 
     Block::factory()->create([
@@ -36,17 +42,28 @@ it('should render with a height, supply and market cap', function () {
         ])->public_key,
     ]);
 
+    $transaction = Transaction::factory()->transfer()->create([
+        'timestamp' => Timestamp::fromUnix(Carbon::now()->unix())->unix(),
+        'amount'    => 18204 * 1e8,
+        'fee'       => 0.99 * 1e8,
+    ]);
+
+    $transaction->sender->balance = 0;
+    $transaction->block->delegate->balance = 0;
+    $transaction->sender->save();
+    $transaction->block->delegate->save();
+
     (new NetworkStatusBlockCache())->setPrice('ARK', 'USD', 1.606);
     (new NetworkStatusBlockCache())->setHistoricalHourly('ARK', 'USD', collect());
 
-    Livewire::test(NetworkStatusBlock::class)
+    Livewire::test(Statistics::class)
         ->assertSee('5,651,290') // Height
+        ->assertSee('18,204') // Volume
         ->assertSee('136,280,982 ARK') // Supply
-        ->assertSee('$218,867,257') // Market cap
-        ->assertSee('1.61'); // Price
+        ->assertSee('$218,867,257'); // Market cap
 });
 
-it('should render with a height, supply and market cap for BTC', function () {
+it('should render with a height, volume, supply and market cap for BTC', function () {
     Config::set('arkscan.network', 'production');
 
     $settings             = Settings::all();
@@ -63,32 +80,23 @@ it('should render with a height, supply and market cap for BTC', function () {
         ])->public_key,
     ]);
 
+    $transaction = Transaction::factory()->transfer()->create([
+        'timestamp' => Timestamp::fromUnix(Carbon::now()->unix())->unix(),
+        'amount'    => 18204 * 1e8,
+        'fee'       => 0.99 * 1e8,
+    ]);
+
+    $transaction->sender->balance = 0;
+    $transaction->block->delegate->balance = 0;
+    $transaction->sender->save();
+    $transaction->block->delegate->save();
+
     (new NetworkStatusBlockCache())->setPrice('ARK', 'BTC', 0.00003132);
     (new NetworkStatusBlockCache())->setHistoricalHourly('ARK', 'BTC', collect());
 
-    Livewire::test(NetworkStatusBlock::class)
+    Livewire::test(Statistics::class)
         ->assertSee('5,651,290') // Height
+        ->assertSee('18,204') // Volume
         ->assertSee('136,280,982 ARK') // Supply
-        ->assertSee('4,268.32035624 BTC') // Market cap
-        ->assertSee('0.00003132'); // Price
-});
-
-it('should render the price change', function () {
-    Config::set('arkscan.networks.development.canBeExchanged', true);
-
-    (new NetworkStatusBlockCache())->setPriceChange('DARK', 'USD', 0.137);
-    (new NetworkStatusBlockCache())->setPrice('DARK', 'USD', 1);
-    (new NetworkStatusBlockCache())->setHistoricalHourly('DARK', 'USD', collect());
-
-    Livewire::test(NetworkStatusBlock::class)->assertSee('13.70%');
-});
-
-it('handle price change when price is zero', function () {
-    Config::set('arkscan.networks.development.canBeExchanged', true);
-
-    (new NetworkStatusBlockCache())->setPriceChange('DARK', 'USD', 0);
-    (new NetworkStatusBlockCache())->setPrice('DARK', 'USD', 1);
-    (new NetworkStatusBlockCache())->setHistoricalHourly('DARK', 'USD', collect());
-
-    Livewire::test(NetworkStatusBlock::class)->assertSee('0.00%');
+        ->assertSee('4,268.32035624 BTC'); // Market cap
 });
