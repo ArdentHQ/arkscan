2f4df748d79a28e68d49f9995a9dbf97c6b41808
ItsANameToo <itsanametoo@protonmail.com>
2023-10-05 13:03:02 +0200

    wip

--- resources/views/livewire/home/chart.blade.php
+++ resources/views/livewire/home/chart.blade.php
@@ -50,7 +50,7 @@
     </div>
 
     <div class="flex flex-1 justify-end min-w-0 md:items-end">
-        <div class="flex w-full md:hidden max-h-[39px] sm:max-w-[258px]">
+        <div class="flex w-full md:hidden max-h-[39px] max-w-[258px] sm:max-w-full">
             <livewire:price-stats />
         </div>
 
