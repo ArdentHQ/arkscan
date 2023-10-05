3de75c155af9a4e9a56cfab04a6603d59b68e87d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-10-05 12:33:20 +0100

    fix: wallet transactions mobile amount

--- resources/views/components/tables/mobile/wallet-transactions.blade.php
+++ resources/views/components/tables/mobile/wallet-transactions.blade.php
@@ -25,13 +25,12 @@
                 class="sm:flex-1"
             />
 
-            <div class="flex flex-col space-y-4 sm:flex-row sm:items-start sm:space-y-0 sm:w-1/2">
-                <x-tables.rows.mobile.encapsulated.amount
-                    :model="$transaction"
-                    :wallet="$wallet"
-                    class="sm:flex-1"
-                />
+            <x-tables.rows.mobile.encapsulated.amount
+                :model="$transaction"
+                :wallet="$wallet"
+            />
 
+            <div class="sm:flex sm:flex-1 sm:justify-end">
                 <x-tables.rows.mobile.encapsulated.fee :model="$transaction" />
             </div>
         </x-tables.rows.mobile>
