907f18820e1c8bd3e944c36fedca6e2d36b8234b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-10-05 14:47:27 +0100

    refactor: remove duplicate currency formatting (#583)

--- resources/views/livewire/home/chart.blade.php
+++ resources/views/livewire/home/chart.blade.php
@@ -11,12 +11,22 @@
             :title="trans('pages.home.statistics.currency_price', ['currency' => Network::currency()])"
             class="md:hidden"
         >
-            {{ $mainValueFiat }} {{ Settings::currency() }}
+            {{ $mainValueFiat }}
+
+            @if (ExplorerNumberFormatter::hasSymbol(Settings::currency()))
+                {{ Settings::currency() }}
+            @endif
         </x-home.stat>
 
         <p class="hidden items-center space-x-2 sm:space-x-3 md:inline-flex">
             <span class="text-sm font-semibold sm:text-3xl md:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
-                <span>{{ $mainValueFiat }} {{ Settings::currency() }}</span>
+                <span>
+                    {{ $mainValueFiat }}
+
+                    @if (ExplorerNumberFormatter::hasSymbol(Settings::currency()))
+                        {{ Settings::currency() }}
+                    @endif
+                </span>
             </span>
         </p>
 
