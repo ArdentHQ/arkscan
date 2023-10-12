b9dce87f5083dff72e1f145f3042f890002c052d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-10-12 15:19:34 +0100

    chore: add github link to navbar (#602)

--- resources/lang/en/urls.php
+++ resources/lang/en/urls.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'ipfs' => 'https://cloudflare-ipfs.com/ipfs/:hash',
-    'docs' => [
+    'ipfs'   => 'https://cloudflare-ipfs.com/ipfs/:hash',
+    'github' => 'https://github.com/ardenthq/arkscan',
+    'docs'   => [
         'validator' => 'https://arkvault.io/docs/transactions/validator',
         'arkscan'   => 'https://arkscan.io/docs',
         'api'       => 'https://ark.dev/docs/api',
