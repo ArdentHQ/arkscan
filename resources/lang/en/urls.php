3aaa39b3ee1c00143ba8a45c5c2a33151facb546
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-10-12 13:54:28 +0000

    style: resolve style guide violations

--- resources/lang/en/urls.php
+++ resources/lang/en/urls.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'ipfs' => 'https://cloudflare-ipfs.com/ipfs/:hash',
+    'ipfs'   => 'https://cloudflare-ipfs.com/ipfs/:hash',
     'github' => 'https://github.com/ardenthq/arkscan',
-    'docs' => [
+    'docs'   => [
         'validator' => 'https://arkvault.io/docs/transactions/validator',
         'arkscan'   => 'https://arkscan.io/docs',
         'api'       => 'https://ark.dev/docs/api',
