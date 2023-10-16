5e2ab75c2c270d3c629c4e0e3b229386a2ebbbbb
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-10-16 13:05:30 +0000

    style: resolve style guide violations

--- resources/views/components/tables/rows/desktop/encapsulated/delegates/number-of-voters.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/delegates/number-of-voters.blade.php
@@ -8,7 +8,7 @@
         <x-number>{{ $model->voterCount() }}</x-number>
 
         @unless($withoutBreakdown)
-            <div class="hidden mt-1 space-x-2 text-xs divide-x sm:flex lg:hidden divide divide-theme-secondary-300 text-theme-secondary-700 dark:text-theme-dark-200 dark:divide-theme-dark-700 leading-3.75">
+            <div class="hidden mt-1 space-x-2 text-xs divide-x sm:flex lg:hidden divide divide-theme-secondary-300 text-theme-secondary-700 leading-3.75 dark:text-theme-dark-200 dark:divide-theme-dark-700">
                 <div>{{ number_format($model->votes()) }}</div>
 
                 <div class="pl-2">
