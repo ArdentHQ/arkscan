45ec7e44fac032f9bfcb29a056f5c98a1d962bc4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-10-16 14:04:43 +0100

    update missed blocks

--- resources/views/components/tables/skeletons/encapsulated/mobile/delegates/missed-blocks.blade.php
+++ resources/views/components/tables/skeletons/encapsulated/mobile/delegates/missed-blocks.blade.php
@@ -2,10 +2,26 @@
     <x-skeleton :row-count="$rowCount">
         <x-tables.rows.mobile>
             <x-slot name="header">
-                <x-tables.rows.mobile.skeletons.text />
+                <div class="flex flex-1 justify-between">
+                <x-tables.rows.mobile.skeletons.text class="sm:flex-1" />
 
-                <x-tables.rows.mobile.skeletons.text />
+                <x-tables.rows.mobile.skeletons.text class="hidden sm:block sm:flex-1" />
+
+                <x-tables.rows.mobile.skeletons.text class="sm:text-right leading-4.25 sm:min-w-[110px]" />
+                </div>
             </x-slot>
+
+            <div class="hidden sm:flex sm:justify-between sm:items-center sm:w-full">
+                <x-tables.rows.mobile.skeletons.encapsulated.text class="sm:flex-1" />
+
+                <div class="sm:flex-1">
+                    <x-tables.rows.mobile.skeletons.encapsulated.text />
+                </div>
+
+                <div class="sm:flex sm:justify-end sm:min-w-[110px]">
+                    <x-tables.rows.mobile.skeletons.encapsulated.text />
+                </div>
+            </div>
         </x-tables.rows.mobile>
     </x-skeleton>
 </x-tables.mobile.includes.encapsulated>
