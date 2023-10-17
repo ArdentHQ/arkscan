2e2a4c185abb4893fd0cb0e0589a3d2d71eb20ae
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-10-17 14:50:53 +0100

    refactor: statistics header & basic info updates (#608)

--- tests/Feature/Http/Livewire/StatsHighlightsTest.php
+++ tests/Feature/Http/Livewire/StatsHighlightsTest.php
@@ -23,11 +23,11 @@ it('should render the component', function (): void {
     $currency = Network::currency();
 
     Livewire::test(StatsHighlights::class)
-        ->assertSee(trans('pages.statistics.highlights.current_supply'))
+        ->assertSee(trans('pages.statistics.highlights.total_supply'))
         ->assertSee('1,362,809,820 '.$currency)
         ->assertSee(trans('pages.statistics.highlights.voting', ['percent' => '74.08%']))
         ->assertSee('84,235,364 '.$currency)
-        ->assertSee(trans('pages.statistics.highlights.registered-delegates'))
+        ->assertSee(trans('pages.statistics.highlights.delegates'))
         ->assertSee('1,171')
         ->assertSee(trans('pages.statistics.highlights.wallets'))
         ->assertSee('10');
