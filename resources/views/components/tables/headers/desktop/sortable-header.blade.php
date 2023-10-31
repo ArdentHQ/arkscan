70dec59f8b4edd52a77d7ad7215b23f93f12eba9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-10-31 09:02:37 +0000

    refactor: refresh table sorting on favorite toggle (#644)

--- resources/views/components/tables/headers/desktop/sortable-header.blade.php
+++ resources/views/components/tables/headers/desktop/sortable-header.blade.php
@@ -11,6 +11,7 @@
     'initialSort' => 'asc',
     'livewireSort' => false,
     'sortIconAlignment' => 'right',
+    'hideSorting' => false,
 ])
 
 <x-ark-tables.header
@@ -20,57 +21,65 @@
     :last-on="$lastOn"
     :width="$width"
     :class="Arr::toCssClasses([
-        'group/header' => $sortingId !== null,
-        'cursor-pointer' => $sortingId !== null && (($livewireSort && $this->isReady) || ! $livewireSort),
-        'flex-row-reverse space-x-0' => $livewireSort && $sortingId !== null,
-        'disabled' => $livewireSort && $sortingId !== null && ! $this->isReady,
+        'group/header' => ! $hideSorting && $sortingId !== null,
+        'cursor-pointer' => ! $hideSorting && $sortingId !== null && (($livewireSort && $this->isReady) || ! $livewireSort),
+        'flex-row-reverse space-x-0' => ! $hideSorting && $livewireSort && $sortingId !== null,
+        'disabled' => ! $hideSorting && $livewireSort && $sortingId !== null && ! $this->isReady,
         $class,
     ])"
     :attributes="$attributes->merge([
         'x-ref' => $sortingId,
-        'x-on:click' => ! $livewireSort && $sortingId !== null ? 'sortByColumn' : null,
-        'data-initial-sort' => ! $livewireSort && $sortingId !== null ? $initialSort : null,
-        'wire:loading.class' => $livewireSort && $sortingId !== null ? 'disabled' : null,
-        'wire:loading.class.remove' => $livewireSort && $sortingId !== null ? 'cursor-pointer' : null,
+        'x-on:click' => ! $hideSorting && ! $livewireSort && $sortingId !== null ? 'sortByColumn' : null,
+        'data-initial-sort' => ! $hideSorting && ! $livewireSort && $sortingId !== null ? $initialSort : null,
+        'wire:loading.class' => ! $hideSorting && $livewireSort && $sortingId !== null ? 'disabled' : null,
+        'wire:loading.class.remove' => ! $hideSorting && $livewireSort && $sortingId !== null ? 'cursor-pointer' : null,
     ])"
 >
-    <button
-        type="button"
-        @class([
-            'py-3 -my-3',
-            'flex w-full items-center space-x-2' => $sortingId !== null,
-            'justify-end' => $sortIconAlignment === 'left' && $sortingId !== null,
-        ])
-
-        @if ($livewireSort && $sortingId !== null)
-            wire:click="sortBy('{{ $sortingId }}')"
-            wire:loading.attr="disabled"
-
-            @unless ($this->isReady)
-                disabled
-            @endunless
-        @endif
-    >
-        @if (! $livewireSort && $sortIconAlignment === 'left')
-            <x-tables.headers.desktop.includes.sort-icon
-                :id="$sortingId"
-                :initial-direction="$initialSort"
-            />
-        @endif
-
+    @if ($hideSorting)
         <span>@lang($name, $nameProperties)</span>
 
         @if ($slot->isNotEmpty())
             {{ $slot }}
         @endif
+    @else
+        <button
+            type="button"
+            @class([
+                'py-3 -my-3',
+                'flex w-full items-center space-x-2' => $sortingId !== null,
+                'justify-end' => $sortIconAlignment === 'left' && $sortingId !== null,
+            ])
 
-        @if ($livewireSort)
-            <x-tables.headers.desktop.includes.livewire-sort-icon :id="$sortingId" />
-        @elseif ($sortIconAlignment === 'right')
-            <x-tables.headers.desktop.includes.sort-icon
-                :id="$sortingId"
-                :initial-direction="$initialSort"
-            />
-        @endif
-    </button>
+            @if ($livewireSort && $sortingId !== null)
+                wire:click="sortBy('{{ $sortingId }}')"
+                wire:loading.attr="disabled"
+
+                @unless ($this->isReady)
+                    disabled
+                @endunless
+            @endif
+        >
+            @if (! $livewireSort && $sortIconAlignment === 'left')
+                <x-tables.headers.desktop.includes.sort-icon
+                    :id="$sortingId"
+                    :initial-direction="$initialSort"
+                />
+            @endif
+
+            <span>@lang($name, $nameProperties)</span>
+
+            @if ($slot->isNotEmpty())
+                {{ $slot }}
+            @endif
+
+            @if ($livewireSort)
+                <x-tables.headers.desktop.includes.livewire-sort-icon :id="$sortingId" />
+            @elseif ($sortIconAlignment === 'right')
+                <x-tables.headers.desktop.includes.sort-icon
+                    :id="$sortingId"
+                    :initial-direction="$initialSort"
+                />
+            @endif
+        </button>
+    @endif
 </x-ark-tables.header>
