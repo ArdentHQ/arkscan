b4127840fe01cad1b833efff04e08b9f335dcd7d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-01 16:09:17 +0000

    refactor: statistics semibold chart price (#653)

--- resources/views/livewire/stats/chart.blade.php
+++ resources/views/livewire/stats/chart.blade.php
@@ -11,7 +11,7 @@
                             @lang('pages.statistics.chart.current_price')
                         </div>
 
-                        <span class="text-lg font-bold leading-5.25 md:!leading-[29px] md:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
+                        <span class="text-lg font-semibold leading-5.25 md:!leading-[29px] md:text-2xl text-theme-secondary-900 dark:text-theme-secondary-200">
                             {{ $mainValueFiat }}
                         </span>
                     </div>
