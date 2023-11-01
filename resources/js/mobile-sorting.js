ecc251b41b2f5d2cc939849221503bc49e5a9f07
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-01 15:04:29 +0000

    move generic sorting method to include

--- resources/js/mobile-sorting.js
+++ resources/js/mobile-sorting.js
@@ -1,3 +1,5 @@
+import { sortRow } from "./includes/sorting";
+
 // Variation of https://codepen.io/ryangjchandler/pen/WNQQKeR
 const MobileSorting = (
     sortBy = "",
@@ -69,37 +71,14 @@ const MobileSorting = (
         },
 
         sortRows(row1, row2, sortBy, sortAscending, sortByRowIndex = true) {
-            const row1Value = this.getValue(row1, sortBy);
-            const row2Value = this.getValue(row2, sortBy);
-
-            const isRow1Numeric =
-                row1Value !== "" && !isNaN(row1Value) && row1Value !== null;
-            const isRow2Numeric =
-                row2Value !== "" && !isNaN(row2Value) && row2Value !== null;
-
-            if (isNaN(row1Value) && isNaN(row2Value)) {
-                return sortAscending
-                    ? row2Value.toString().localeCompare(row1Value)
-                    : row1Value.toString().localeCompare(row2Value);
-            }
-
-            if (isRow1Numeric && isRow2Numeric) {
-                return sortAscending
-                    ? row1Value - row2Value
-                    : row2Value - row1Value;
-            }
-
-            if (isRow1Numeric && !isRow2Numeric) {
-                return 0;
-            }
-
-            if (sortByRowIndex && row1Value === row2Value) {
-                return sortAscending
-                    ? row2.dataset["rowIndex"] - row1.dataset["rowIndex"]
-                    : row1.dataset["rowIndex"] - row2.dataset["rowIndex"];
-            }
-
-            return sortAscending ? 1 : -1;
+            return sortRow(
+                row1,
+                row2,
+                this.getValue(row1, sortBy),
+                this.getValue(row2, sortBy),
+                sortAscending,
+                sortByRowIndex
+            );
         },
     };
 };
