f32c2f973fdeb1cc73f1a0051eb4721cae974cc3
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-11-01 15:05:33 +0000

    style: resolve style guide violations

--- resources/js/includes/sorting.js
+++ resources/js/includes/sorting.js
@@ -1,4 +1,11 @@
-export const sortRow = (row1, row2, row1Value, row2Value, sortAscending, sortByRowIndex) => {
+export const sortRow = (
+    row1,
+    row2,
+    row1Value,
+    row2Value,
+    sortAscending,
+    sortByRowIndex
+) => {
     const isRow1Numeric =
         row1Value !== "" && !isNaN(row1Value) && row1Value !== null;
     const isRow2Numeric =
@@ -11,9 +18,7 @@ export const sortRow = (row1, row2, row1Value, row2Value, sortAscending, sortByR
     }
 
     if (isRow1Numeric && isRow2Numeric) {
-        return sortAscending
-            ? row1Value - row2Value
-            : row2Value - row1Value;
+        return sortAscending ? row1Value - row2Value : row2Value - row1Value;
     }
 
     if (isRow1Numeric && !isRow2Numeric) {
@@ -27,4 +32,4 @@ export const sortRow = (row1, row2, row1Value, row2Value, sortAscending, sortByR
     }
 
     return sortAscending ? 1 : -1;
-}
+};
