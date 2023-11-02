31e86d8984ef8780875996e1a9f7c6fa76f2fd4c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-02 14:59:46 +0000

    fix: delegate monitor mobile favorite styling & header toggle (#660)

--- resources/views/components/tables/rows/mobile/encapsulated/delegates/monitor/favorite.blade.php
+++ resources/views/components/tables/rows/mobile/encapsulated/delegates/monitor/favorite.blade.php
@@ -1,8 +1,12 @@
-@props(['model'])
+@props([
+    'model',
+    'onClick' => null,
+])
 
 <x-delegates.favorite-toggle
     :model="$model"
     :attributes="$attributes"
+    :on-click="$onClick"
 >
     @lang('tables.delegate-monitor.favorite')
 </x-delegates.favorite-toggle>
