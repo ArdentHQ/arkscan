8c94ba25a1931db9c6bfd030733d21db8daa2edf
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-11-13 17:00:35 +0000

    style: resolve style guide violations

--- resources/views/components/general/desktop-divider.blade.php
+++ resources/views/components/general/desktop-divider.blade.php
@@ -2,7 +2,7 @@
     'color' => 'text-theme-secondary-300 dark:text-theme-dark-700',
 ])
 
-<div class="hidden sm:flex flex-col px-6 md:px-10 md:mx-auto md:max-w-7xl">
+<div class="hidden flex-col px-6 sm:flex md:px-10 md:mx-auto md:max-w-7xl">
     <hr {{ $attributes->class([
         'h-px hidden md:block',
         $color,
