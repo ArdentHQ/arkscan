3cef958d14f30c4a8daa0ff4d1994574645388a0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-13 20:29:13 +0000

    transactions

--- resources/views/components/tables/encapsulated-table.blade.php
+++ resources/views/components/tables/encapsulated-table.blade.php
@@ -2,9 +2,25 @@
     'rounded' => true,
     'paginator' => null,
     'noResultsMessage' => null,
+    'withPagination' => false,
 ])
 
-@php ($paginatorIsEmpty = $paginator === null || ($paginator !== null && $paginator->total() < config('arkscan.pagination.per_page')) || (isset($this) && property_exists($this, 'isReady') && ! $this->isReady))
+@php
+    $paginatorIsEmpty = false;
+    if (! $withPagination) {
+        if ($paginator === null) {
+            $paginatorIsEmpty = true;
+        }
+
+        if ($paginator !== null && $paginator->total() < config('arkscan.pagination.per_page')) {
+            $paginatorIsEmpty = true;
+        }
+
+        if (isset($this) && property_exists($this, 'isReady') && ! $this->isReady) {
+            $paginatorIsEmpty = true;
+        }
+    }
+@endphp
 
 <div {{ $attributes->class([
     'border border-theme-secondary-300 dark:border-theme-secondary-800 overflow-hidden table-container px-6 table-encapsulated encapsulated-table-header-gradient',
