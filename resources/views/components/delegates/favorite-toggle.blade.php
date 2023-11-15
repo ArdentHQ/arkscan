bb4f1586cc4cea5bae4682fbf25bd8c04a36e129
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-15 18:22:41 +0000

    refactor: favorite hover flickering

--- resources/views/components/delegates/favorite-toggle.blade.php
+++ resources/views/components/delegates/favorite-toggle.blade.php
@@ -5,11 +5,15 @@
 
 <div {{ $attributes }}>
     <button
+        x-data="{
+            hasHover: false,
+        }"
         type="button"
         class="flex items-center space-x-2 font-semibold favorite-icon"
         :class="{
             'dark:text-theme-dark-300': ! isFavorite,
             'text-theme-primary-600 favorite-icon__selected': isFavorite,
+            'favorite-icon__hover': hasHover,
         }"
         @click="function () {
             if (this.isFavorite) {
@@ -23,6 +27,8 @@
                 ({{ $onClick }})(this.isFavorite);
             @endif
         }"
+        @mouseenter.self="hasHover = true"
+        @mouseleave.self="hasHover = false"
     >
         <x-ark-icon name="app-favorite-star" />
 
