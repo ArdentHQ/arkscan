381bffabcb57bacf09403f8fae800f375d878cb1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-16 15:09:37 +0000

    remove more

--- resources/views/components/general/circled-icon.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="transaction-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
-    <x-ark-icon :name="$icon" />
-</div>
--- resources/views/components/tables/rows/mobile/balance-percentage.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <span class="font-semibold">
-        @lang('labels.balance_percentage')
-    </span>
-
-    <span>
-        <x-percentage>{{ $model->balancePercentage() }}</x-percentage>
-    </span>
-</div>
