f3d239bbcb95fa3f2b40828d4115fd45cfedb08c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-16 15:06:17 +0000

    remove unused skeletons

--- resources/views/components/tables/skeletons/mobile.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="divide-y table-list-mobile">
-    <x-skeleton :row-count="$rowCount">
-        <div class="table-list-mobile-row">
-            @foreach($rows as $row)
-                <x-dynamic-component :component="$row['component']" />
-            @endforeach
-        </div>
-    </x-skeleton>
-</div>
