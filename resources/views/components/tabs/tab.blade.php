71115de1e4d7ef6ca72b77a5a83fc3ef3b6c433b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-21 22:28:21 +0000

    refactor: update exchanges chart tab styling

--- resources/views/components/tabs/tab.blade.php
+++ resources/views/components/tabs/tab.blade.php
@@ -2,28 +2,29 @@
     'name',
 ])
 
-<button
-    type="button"
-    class="relative px-2 ml-8 cursor-pointer explorer-tab transition-default dark:hover:text-theme-secondary-200 hover:text-theme-secondary-900"
-    @click="select('{{ $name }}')"
-    @keydown.enter="select('{{ $name }}')"
-    @keydown.space.prevent="select('{{ $name }}')"
-    role="tab"
-    id="tab-{{ $name }}"
-    aria-controls="panel-{{ $name }}"
-    wire:key="tab-{{ $name }}"
-    @keydown.arrow-left="selectPrevTab"
-    @keydown.arrow-right="selectNextTab"
-    :tabindex="selected === '{{ $name }}' ? 0 : -1"
-    :aria-selected="selected === '{{ $name }}'"
-    {{ $attributes }}
->
-    <span
-        class="block pt-3 pb-2.5 w-full h-full text-sm font-semibold whitespace-nowrap border-b-2 leading-4.25"
-        :class="{
-            'border-transparent dark:text-theme-secondary-500 ': selected !== '{{ $name }}',
-            'text-theme-secondary-900 border-theme-primary-600 dark:text-theme-secondary-200': selected === '{{ $name }}',
-        }"
-    >{{ $slot }}</span>
-</button>
-
+<div {{ $attributes->only('class') }}>
+    <button
+        type="button"
+        class="relative px-2 cursor-pointer transition-default dark:hover:text-theme-secondary-200 hover:text-theme-secondary-900"
+        @click="select('{{ $name }}')"
+        @keydown.enter="select('{{ $name }}')"
+        @keydown.space.prevent="select('{{ $name }}')"
+        role="tab"
+        id="tab-{{ $name }}"
+        aria-controls="panel-{{ $name }}"
+        wire:key="tab-{{ $name }}"
+        @keydown.arrow-left="selectPrevTab"
+        @keydown.arrow-right="selectNextTab"
+        :tabindex="selected === '{{ $name }}' ? 0 : -1"
+        :aria-selected="selected === '{{ $name }}'"
+        {{ $attributes->except('class') }}
+    >
+        <span
+            class="block pt-3 pb-2.5 w-full h-full text-sm font-semibold whitespace-nowrap border-b-2 leading-4.25"
+            :class="{
+                'border-transparent dark:text-theme-secondary-500 ': selected !== '{{ $name }}',
+                'text-theme-secondary-900 border-theme-primary-600 dark:text-theme-secondary-200': selected === '{{ $name }}',
+            }"
+        >{{ $slot }}</span>
+    </button>
+</div>
