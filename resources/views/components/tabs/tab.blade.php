b9f7ced3921ff64227f4ac50058a95245e2daa91
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-21 22:47:31 +0000

    update hover styling

--- resources/views/components/tabs/tab.blade.php
+++ resources/views/components/tabs/tab.blade.php
@@ -5,7 +5,11 @@
 <div {{ $attributes->only('class') }}>
     <button
         type="button"
-        class="relative px-2 cursor-pointer transition-default dark:hover:text-theme-secondary-200 hover:text-theme-secondary-900"
+        class="relative px-2 cursor-pointer transition-default dark:hover:text-theme-dark-50 hover:text-theme-secondary-900"
+        :class="{
+            'dark:text-theme-dark-200': selected !== '{{ $name }}',
+            'text-theme-secondary-900 dark:text-theme-dark-50': selected === '{{ $name }}',
+        }"
         @click="select('{{ $name }}')"
         @keydown.enter="select('{{ $name }}')"
         @keydown.space.prevent="select('{{ $name }}')"
@@ -22,8 +26,8 @@
         <span
             class="block pt-3 pb-2.5 w-full h-full text-sm font-semibold whitespace-nowrap border-b-2 leading-4.25"
             :class="{
-                'border-transparent dark:text-theme-secondary-500 ': selected !== '{{ $name }}',
-                'text-theme-secondary-900 border-theme-primary-600 dark:text-theme-secondary-200': selected === '{{ $name }}',
+                'border-transparent': selected !== '{{ $name }}',
+                'border-theme-primary-600': selected === '{{ $name }}',
             }"
         >{{ $slot }}</span>
     </button>
