13a75684975fea58a50a3b455a55a590e5502b6b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-22 11:19:08 +0000

    refactor: home exchanges button chevron

--- resources/views/components/home/exchanges-button.blade.php
+++ resources/views/components/home/exchanges-button.blade.php
@@ -11,7 +11,15 @@
                 $buttonClass,
             ])
         >
-            @lang('actions.exchanges')
+            <div class="inline-flex items-center space-x-2 lg:space-x-0 xl:space-x-2">
+                <span>@lang('actions.exchanges')</span>
+
+                <x-ark-icon
+                    name="arrows.chevron-right-small"
+                    size="xs"
+                    class="lg:hidden xl:block"
+                />
+            </div>
         </a>
     </div>
 @endif
