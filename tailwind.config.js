72b9ee1d0133b0bfceee89e3d5475276b18c49c6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-11-22 17:59:11 +0000

    remove line height from font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,23 @@ module.exports = {
         extend: {
             ...defaultConfig.theme.extend,
 
+            // Remove line-height from font sizes
+            fontSize: {
+                xs: '0.75rem',
+                sm: '0.875rem',
+                base: '1rem',
+                lg: '1.125rem',
+                xl: '1.25rem',
+                '2xl': '1.5rem',
+                '3xl': '1.875rem',
+                '4xl': '2.25rem',
+                '5xl': '3rem',
+                '6xl': '3.75rem',
+                '7xl': '4.5rem',
+                '8xl': '6rem',
+                '9xl': '8rem',
+            },
+
             screens: {
                 ...defaultConfig.theme.extend.screens,
 
