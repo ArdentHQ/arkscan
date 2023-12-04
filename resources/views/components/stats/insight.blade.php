e44322caf55eb1e985996ee7ce559e6280e50c5a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-12-04 13:51:21 +0000

    statistics changes

--- resources/views/components/stats/insight.blade.php
+++ resources/views/components/stats/insight.blade.php
@@ -31,7 +31,7 @@
                 wire:model="{{ $model }}"
                 wrapper-class="relative left-0 w-full xl:inline-block"
                 dropdown-class="left-0 mt-1 origin-top-left"
-                button-class="inline-block w-full text-left !px-3 !py-2 form-input transition-default dark:bg-theme-dark-900 dark:border-theme-dark-800 !text-sm font-semibold"
+                button-class="inline-block w-full text-left !px-3 !py-2 form-input transition-default dark:bg-theme-dark-900 dark:border-theme-dark-700 !text-sm font-semibold"
                 :initial-value="$selected"
                 :placeholder="$selected"
                 :options="$options"
