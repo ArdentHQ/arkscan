af90eff39fb1b5261742e8fc280d5cb778cff98d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-12-06 13:52:43 +0000

    add averages to component

--- app/Http/Livewire/Stats/Insights.php
+++ app/Http/Livewire/Stats/Insights.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Livewire\Stats;
 
 use App\Enums\StatsTransactionType;
+use App\Facades\Network;
 use App\Services\Cache\TransactionCache;
+use App\Services\NumberFormatter;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -13,17 +15,29 @@ final class Insights extends Component
 {
     public function render(): View
     {
+        $transactionCache = new TransactionCache();
+
         return view('livewire.stats.insights', [
-            'transactionDetails' => $this->transactionDetails(),
+            'transactionDetails' => $this->transactionDetails($transactionCache),
+            'transactionAverages' => $this->transactionAverages($transactionCache),
         ]);
     }
 
-    private function transactionDetails(): array
+    private function transactionDetails(TransactionCache $cache): array
     {
-        $transactionCache = new TransactionCache();
-
         return StatsTransactionType::all()
-            ->mapWithKeys(fn ($type) => [$type => $transactionCache->getHistoricalByType($type)])
+            ->mapWithKeys(fn ($type) => [$type => $cache->getHistoricalByType($type)])
             ->toArray();
+    }
+
+    private function transactionAverages(TransactionCache $cache): array
+    {
+        $data = $cache->getHistoricalAverages();
+
+        return [
+            'transactions' => $data['count'],
+            'transaction_volume' => NumberFormatter::currency($data['amount'], Network::currency()),
+            'transaction_fees' => NumberFormatter::currency($data['fee'], Network::currency()),
+        ];
     }
 }
