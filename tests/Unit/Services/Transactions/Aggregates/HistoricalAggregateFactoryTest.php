1641dc918e30563ed00a78b7de0d5d34183794b8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-12-06 00:58:08 +0000

    test aggregate types

--- tests/Unit/Services/Transactions/Aggregates/HistoricalAggregateFactoryTest.php
+++ tests/Unit/Services/Transactions/Aggregates/HistoricalAggregateFactoryTest.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
+use App\Enums\StatsTransactionType;
 use App\Services\Transactions\Aggregates\Historical\AllAggregate;
 use App\Services\Transactions\Aggregates\Historical\DayAggregate;
 use App\Services\Transactions\Aggregates\Historical\MonthAggregate;
 use App\Services\Transactions\Aggregates\Historical\QuarterAggregate;
 use App\Services\Transactions\Aggregates\Historical\WeekAggregate;
 use App\Services\Transactions\Aggregates\Historical\YearAggregate;
+use App\Services\Transactions\Aggregates\Type\DelegateRegistrationAggregate;
+use App\Services\Transactions\Aggregates\Type\DelegateResignationAggregate;
+use App\Services\Transactions\Aggregates\Type\TransferAggregate;
+use App\Services\Transactions\Aggregates\Type\MultipaymentAggregate;
+use App\Services\Transactions\Aggregates\Type\VoteAggregate;
+use App\Services\Transactions\Aggregates\Type\UnvoteAggregate;
+use App\Services\Transactions\Aggregates\Type\VoteCombinationAggregate;
 use App\Services\Transactions\Aggregates\HistoricalAggregateFactory;
 
 it('should create an instance that matches the period', function (string $type, string $class) {
@@ -23,4 +31,20 @@ it('should create an instance that matches the period', function (string $type, 
 
 it('should throw if an unknown period is used', function () {
     HistoricalAggregateFactory::period('unknown');
+})->throws(InvalidArgumentException::class);
+
+it('should create an instance that matches the type', function (string $type, string $class) {
+    expect(HistoricalAggregateFactory::type($type))->toBeInstanceOf($class);
+})->with([
+    [StatsTransactionType::TRANSFER, TransferAggregate::class],
+    [StatsTransactionType::MULTIPAYMENT, MultipaymentAggregate::class],
+    [StatsTransactionType::VOTE, VoteAggregate::class],
+    [StatsTransactionType::UNVOTE, UnvoteAggregate::class],
+    [StatsTransactionType::SWITCH_VOTE, VoteCombinationAggregate::class],
+    [StatsTransactionType::DELEGATE_REGISTRATION, DelegateRegistrationAggregate::class],
+    [StatsTransactionType::DELEGATE_RESIGNATION, DelegateResignationAggregate::class],
+]);
+
+it('should throw if an unknown type is used', function () {
+    HistoricalAggregateFactory::type('unknown');
 })->throws(InvalidArgumentException::class);
