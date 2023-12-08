a9b09822e1e01c918eb569652d31403bc8c55d1c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-12-08 00:41:06 +0000

    test

--- tests/Unit/Services/Blocks/Aggregates/LargestBlockAggregateTest.php
+++ tests/Unit/Services/Blocks/Aggregates/LargestBlockAggregateTest.php
@@ -4,12 +4,18 @@ use App\Models\Block;
 use App\Models\Transaction;
 use App\Services\Blocks\Aggregates\LargestBlockAggregate;
 
-it('should get largest transaction', function () {
+it('should get largest block', function () {
     $largestBlock = Block::factory()->create();
     $otherBlock = Block::factory()->create();
 
-    Transaction::factory(1)->transfer()->create([
+    Transaction::factory()->transfer()->create([
         'amount'   => 20000 * 1e8,
+        'fee'      => 10 * 1e8,
+        'block_id' => $otherBlock->id,
+    ]);
+
+    Transaction::factory(200)->transfer()->create([
+        'amount'   => 1000 * 1e8,
         'fee'      => 10 * 1e8,
         'block_id' => $largestBlock->id,
     ]);
@@ -18,6 +24,105 @@ it('should get largest transaction', function () {
         'amount'   => 60 * 1e8,
         'fee'      => 10 * 1e8,
         'block_id' => $otherBlock->id,
+    ]);
+
+    expect((new LargestBlockAggregate())->aggregate()->id)->toBe($largestBlock->id);
+});
+
+it('should get largest block for multipayments', function () {
+    $largestBlock = Block::factory()->create();
+    $otherBlock = Block::factory()->create();
+
+    Transaction::factory()->transfer()->create([
+        'block_id' => $otherBlock->id,
+        'amount' => 2000 * 1e8,
+        'fee'    => 10 * 1e8,
+    ]);
+    Transaction::factory()->multipayment()->create([
+        'block_id' => $largestBlock->id,
+        'amount' => 0,
+        'fee'    => 10 * 1e8,
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 5000 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+            ],
+        ],
+    ]);
+    Transaction::factory()->transfer()->create([
+        'block_id' => $otherBlock->id,
+        'amount' => 3000 * 1e8,
+        'fee'    => 10 * 1e8,
+    ]);
+
+    expect((new LargestBlockAggregate())->aggregate()->id)->toBe($largestBlock->id);
+});
+
+it('should get largest block for transfers and multipayments', function () {
+    $largestBlock = Block::factory()->create();
+    $otherBlock = Block::factory()->create();
+
+    Transaction::factory(2)->transfer()->create([
+        'block_id' => $otherBlock->id,
+        'amount' => 2000 * 1e8,
+        'fee'    => 10 * 1e8,
+    ]);
+    Transaction::factory()->multipayment()->create([
+        'block_id' => $largestBlock->id,
+        'amount' => 0,
+        'fee'    => 10 * 1e8,
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 5000 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+            ],
+        ],
+    ]);
+    Transaction::factory(3)->transfer()->create([
+        'block_id' => $otherBlock->id,
+        'amount' => 3000 * 1e8,
+        'fee'    => 10 * 1e8,
+    ]);
+    Transaction::factory()->transfer()->create([
+        'block_id' => $largestBlock->id,
+        'amount' => 7000 * 1e8,
+        'fee'    => 10 * 1e8,
     ]);
 
     expect((new LargestBlockAggregate())->aggregate()->id)->toBe($largestBlock->id);
--- tests/Unit/Services/Transactions/Aggregates/LargestTransactionAggregateTest.php
+++ tests/Unit/Services/Transactions/Aggregates/LargestTransactionAggregateTest.php
@@ -13,8 +13,49 @@ it('should get largest transaction', function () {
         'fee'    => 10 * 1e8,
     ]);
     Transaction::factory()->transfer()->create([
+        'amount' => 3000 * 1e8,
+        'fee'    => 10 * 1e8,
+    ]);
+
+    expect((new LargestTransactionAggregate())->aggregate()->id)->toBe($transaction->id);
+});
+
+it('should get largest transaction including multipayments', function () {
+    Transaction::factory()->transfer()->create([
         'amount' => 2000 * 1e8,
         'fee'    => 10 * 1e8,
+    ]);
+    Transaction::factory()->transfer()->create([
+        'amount' => 6000 * 1e8,
+        'fee'    => 10 * 1e8,
+    ]);
+    $transaction = Transaction::factory()->multipayment()->create([
+        'amount' => 0,
+        'fee'    => 10 * 1e8,
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 5000 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+                [
+                    'recipientId' => 'test-address',
+                    'amount' => 300 * 1e8,
+                ],
+            ],
+        ],
     ]);
 
     expect((new LargestTransactionAggregate())->aggregate()->id)->toBe($transaction->id);
