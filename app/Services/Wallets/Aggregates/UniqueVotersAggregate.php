8b3de244088240be4f50159f77a80eb9ef5b0923
ItsANameToo <itsanametoo@protonmail.com>
2023-12-12 13:32:49 +0100

    wip

--- app/Services/Wallets/Aggregates/UniqueVotersAggregate.php
+++ app/Services/Wallets/Aggregates/UniqueVotersAggregate.php
@@ -5,24 +5,30 @@ declare(strict_types=1);
 namespace App\Services\Wallets\Aggregates;
 
 use App\Models\Wallet;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 final class UniqueVotersAggregate
 {
     /**
-     * @return array{'public_key': string, 'voter_count': int}>
+     * @return Collection{'public_key': string, 'voter_count': int}>
      */
-    public function aggregate(bool $sortDescending = true): ?array
+    public function aggregate(bool $sortDescending = true): ?Collection
     {
         // phpstan-ignore-next-line
-        return Wallet::query()
+        $result = Wallet::query()
             ->select(DB::raw('attributes->>\'vote\' as public_key, COUNT(*) as voter_count'))
             ->where('balance', '>=', 1 * 1e8)
             ->whereRaw('attributes->>\'vote\' IS NOT NULL')
             ->groupByRaw('attributes->>\'vote\'')
             ->orderBy('voter_count', $sortDescending ? 'desc' : 'asc')
             ->limit(1)
-            ->first()
-            ->toArray();
+            ->first();
+
+        if ($result === null) {
+            return $result;
+        }
+
+        return collect($result);
     }
 }
