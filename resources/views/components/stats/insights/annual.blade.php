9aed251caa1072be3885b1d80fd244eb919a9593
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-12-13 15:49:44 +0000

    style: resolve style guide violations

--- resources/views/components/stats/insights/annual.blade.php
+++ resources/views/components/stats/insights/annual.blade.php
@@ -17,7 +17,7 @@
         <div class="flex flex-col md:hidden">
             @foreach ($years as $year)
                 <x-stats.insights.container :title="$year['year']" full-width>
-                    <div class="flex flex-1 flex-col justify-between pt-3 space-y-3 w-full sm:flex-row sm:space-y-0">
+                    <div class="flex flex-col flex-1 justify-between pt-3 space-y-3 w-full sm:flex-row sm:space-y-0">
                         <div class="flex flex-col space-y-2">
                             <span>@lang('pages.statistics.insights.annual.header.transaction')</span>
                             <span class="text-theme-secondary-900 dark:text-theme-dark-50">
@@ -33,7 +33,7 @@
                         </div>
                     </div>
 
-                    <div class="flex flex-1 flex-col justify-between pt-3 space-y-3 w-full sm:flex-row sm:space-y-0">
+                    <div class="flex flex-col flex-1 justify-between pt-3 space-y-3 w-full sm:flex-row sm:space-y-0">
                         <div class="flex flex-col space-y-2">
                             <span>@lang('pages.statistics.insights.annual.header.fees')</span>
                             <span class="text-theme-secondary-900 dark:text-theme-dark-50">
@@ -59,7 +59,7 @@
                     @foreach ($years as $year)
                         <div class="flex">
                             <div class="flex flex-1 text-theme-secondary-900 dark:text-theme-dark-50">{{ $year['year'] }}</div>
-                            <div class="flex flex-1 md-lg:flex-2 flex-col md-lg:flex-row space-y-3 md-lg:space-y-0">
+                            <div class="flex flex-col flex-1 space-y-3 md-lg:flex-2 md-lg:flex-row md-lg:space-y-0">
                                 <div class="flex flex-col flex-1 space-y-3">
                                     <div class="flex justify-between space-x-3">
                                         <span>@lang('pages.statistics.insights.annual.header.transaction'):</span>
@@ -98,17 +98,17 @@
         {{-- Desktop Large --}}
         <div class="hidden xl:block">
             <x-stats.insights.container full-width>
-                <table class="justify-between w-full border-spacing-y-3 border-separate">
+                <table class="justify-between w-full border-separate border-spacing-y-3">
                     <tbody>
                         @foreach ($years as $year)
                             <tr>
                                 <td>
-                                    <div class="text-theme-secondary-900 dark:text-theme-dark-50 pr-8">
+                                    <div class="pr-8 text-theme-secondary-900 dark:text-theme-dark-50">
                                         {{ $year['year'] }}
                                     </div>
                                 </td>
                                 <td>
-                                    <div class="flex justify-between space-x-3 px-8">
+                                    <div class="flex justify-between px-8 space-x-3">
                                         <span>@lang('pages.statistics.insights.annual.header.transaction'):</span>
                                         <span class="text-theme-secondary-900 dark:text-theme-dark-50">
                                             <x-number>{{ $year['transactions'] }}</x-number>
@@ -116,7 +116,7 @@
                                     </div>
                                 </td>
                                 <td>
-                                    <div class="flex justify-between space-x-3 px-8">
+                                    <div class="flex justify-between px-8 space-x-3">
                                         <span>@lang('pages.statistics.insights.annual.header.volume'):</span>
                                         <span class="text-theme-secondary-900 dark:text-theme-dark-50">
                                             {{ ExplorerNumberFormatter::currencyWithDecimals($year['volume'], Network::currency(), 2) }}
@@ -124,7 +124,7 @@
                                     </div>
                                 </td>
                                 <td>
-                                    <div class="flex justify-between space-x-3 px-8">
+                                    <div class="flex justify-between px-8 space-x-3">
                                         <span>@lang('pages.statistics.insights.annual.header.fees'):</span>
                                         <span class="text-theme-secondary-900 dark:text-theme-dark-50">
                                             {{ ExplorerNumberFormatter::currencyWithDecimals($year['fees'], Network::currency(), 2) }}
@@ -132,7 +132,7 @@
                                     </div>
                                 </td>
                                 <td>
-                                    <div class="flex justify-between space-x-3 pl-8">
+                                    <div class="flex justify-between pl-8 space-x-3">
                                         <span>@lang('pages.statistics.insights.annual.header.blocks'):</span>
                                         <span class="text-theme-secondary-900 dark:text-theme-dark-50">
                                             <x-number>{{ $year['blocks'] }}</x-number>
