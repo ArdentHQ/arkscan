47325e26d6ed2272d14a8c92cca523f7c87fcfa0
ItsANameToo <itsanametoo@protonmail.com>
2023-12-18 13:29:28 +0100

    fix: wallet amount value

--- resources/views/components/general/network-currency.blade.php
+++ resources/views/components/general/network-currency.blade.php
@@ -4,9 +4,5 @@
 ])
 
 <x-currency :currency="Network::currency()">
-    @if ($decimals === 0)
-        {{ number_format((float) ARKEcosystem\Foundation\NumberFormatter\ResolveScientificNotation::execute((float) $value), $decimals) }}
-    @else
-        {{ ExplorerNumberFormatter::unformattedRawValue($value, $decimals) }}
-    @endif
+    {{ $value }}
 </x-currency>
