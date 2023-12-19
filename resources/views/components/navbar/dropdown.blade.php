813661cfc525e87340d23c6859f3c4bf4efcde08
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-12-19 01:16:09 +0000

    move navbar dropdown to separate component

---
+++ resources/views/components/navbar/dropdown.blade.php
@@ -0,0 +1,23 @@
+@props(['button'])
+
+<x-general.dropdown.dropdown
+    active-button-class="space-x-1.5"
+    button-wrapper-class=""
+    button-class="justify-center p-2 space-x-1.5 h-8 text-sm font-semibold rounded md:px-3 md:bg-white md:border bg-theme-secondary-200 text-theme-secondary-700 md:hover:text-theme-secondary-700 md:border-theme-secondary-300 md:dark:border-theme-dark-700 md:dark:bg-theme-dark-900 md:dark:text-theme-dark-200 md:hover:text-theme-secondary-900 dark:bg-theme-dark-800 dark:hover:bg-theme-dark-700 dark:text-theme-dark-50 hover:bg-theme-secondary-200"
+>
+    <x-slot name="button">
+        {{ $button }}
+
+        <span
+            class="transition-default"
+            :class="{ 'rotate-180': dropdownOpen }"
+        >
+            <x-ark-icon
+                name="arrows.chevron-down-small"
+                size="w-2.5 h-2.5"
+            />
+        </span>
+    </x-slot>
+
+    {{ $slot }}
+</x-general.dropdown>
