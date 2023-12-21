eeeed1657c7da03f72b20dd1a022e968bf7b2700
alexbarnsley <alexbarnsley@users.noreply.github.com>
2023-12-21 23:55:45 +0000

    style: resolve style guide violations

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -6,7 +6,7 @@
 
         <x-general.mobile-divider class="mt-6" />
 
-        <div class="px-6 mt-6 pb-8 md:px-10 md:pb-6 md:mx-auto md:max-w-7xl">
+        <div class="px-6 pb-8 mt-6 md:px-10 md:pb-6 md:mx-auto md:max-w-7xl">
             <livewire:home.tables />
 
             <x-home.footer />
