d590ee0917a7811c1117cb50fdaa2743e5cc619c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-01-18 09:42:45 +0000

    refactor: statistics cache all loading state

--- resources/views/components/loading/text.blade.php
+++ resources/views/components/loading/text.blade.php
@@ -1,9 +1,10 @@
 @props([
     'width' => 'w-[70px]',
     'height' => 'h-[17px]',
+    'outerClass' => null,
 ])
 
-<div>
+<div @class($outerClass)>
     <div {{ $attributes->class([
         'rounded-sm-md animate-pulse bg-theme-secondary-300 dark:bg-theme-dark-800',
         $width,
