4fd2d999db373cc60a1ee62cf95d18da21d382b4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-01-23 14:23:09 +0000

    unique?

--- app/Events/NewEntity.php
+++ app/Events/NewEntity.php
@@ -6,11 +6,12 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBeUnique;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-abstract class NewEntity implements ShouldBroadcast
+abstract class NewEntity implements ShouldBroadcast, ShouldBeUnique
 {
     use Dispatchable;
     use InteractsWithSockets;
@@ -21,5 +22,10 @@ abstract class NewEntity implements ShouldBroadcast
     final public function broadcastOn()
     {
         return new Channel(static::CHANNEL);
+    }
+
+    final public function uniqueId(): string
+    {
+        return static::class.':'.static::CHANNEL;
     }
 }
