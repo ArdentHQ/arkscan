34c205e49fb3eb089704e43a616bc81a697d758f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-01-24 14:06:33 +0000

    chore: stanley

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -11,21 +11,25 @@ final class WebhooksController
 {
     public function __invoke(): void
     {
+        // @phpstan-ignore-next-line
         if (! request()->hasValidSignature()) {
             abort(401);
         }
 
-        match (request()->input('event')) {
-            'block.applied' => $this->handleBlockApplied(),
-        };
+        $event = request()->input('event');
+        if ($event === 'block.applied') {
+            $this->handleBlockApplied();
+        }
     }
 
-    private function handleBlockApplied()
+    private function handleBlockApplied(): void
     {
         $lock = Cache::lock('foo', config('arkscan.webhooks.block-applied.ttl', 8));
 
-        if ($lock->get()) {
-            NewBlock::dispatch();
+        if ($lock->get() === false) {
+            return;
         }
+
+        NewBlock::dispatch();
     }
 }
