587737515db86c2c9c50df4367c0a6637cc6f250
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-03-21 14:56:48 +0000

    move x-cloak to child element

--- resources/views/components/navbar/arkconnect.blade.php
+++ resources/views/components/navbar/arkconnect.blade.php
@@ -4,7 +4,6 @@
     :class="{
         'border-t bg-theme-secondary-200 border-theme-secondary-300 md:border-0 md:bg-transparent': isConnected,
     }"
-    x-cloak
 >
     <button
         x-show="! isConnected"
@@ -18,6 +17,7 @@
     <div
         x-show="isConnected"
         class="flex justify-between items-center space-x-2 w-full min-w-0 md:hidden"
+        x-cloak
     >
         <div class="flex items-center space-x-1 min-w-0 text-sm font-semibold">
             <span class="hidden whitespace-nowrap xs:block">
@@ -43,43 +43,45 @@
         </div>
     </div>
 
-    <div
-        x-show="isConnected"
-        x-init="{
-            dropdownOpen: false,
-        }"
-        class="hidden md:block"
-    >
-        <x-general.dropdown.dropdown
-            placement="bottom"
-            :placement-fallbacks="['bottom-end', 'left-start']"
-            dropdown-class="w-full table-filter md:w-[158px]"
-            dropdown-background="bg-white dark:bg-theme-dark-700"
-            dropdown-padding="py-1"
-            dropdown-wrapper-class="w-full"
-            active-button-class="flex justify-between items-center whitespace-nowrap bg-transparent rounded md:justify-start md:pr-2 md:pl-3 md:space-x-2 md:h-8 md:border border-theme-secondary-300 text-theme-secondary-700 transition-default dark:border-theme-dark-700 dark:text-theme-dark-200 dark:hover:bg-theme-dark-700 hover:text-theme-secondary-900 hover:bg-theme-secondary-200"
+    <div x-cloak>
+        <div
+            x-show="isConnected"
+            x-init="{
+                dropdownOpen: false,
+            }"
+            class="hidden md:block"
         >
-            <x-slot name="button">
-                <div
-                    x-text="truncateMiddle(await address())"
-                    class="text-sm font-semibold md:leading-3.75"
-                ></div>
+            <x-general.dropdown.dropdown
+                placement="bottom"
+                :placement-fallbacks="['bottom-end', 'left-start']"
+                dropdown-class="w-full table-filter md:w-[158px]"
+                dropdown-background="bg-white dark:bg-theme-dark-700"
+                dropdown-padding="py-1"
+                dropdown-wrapper-class="w-full"
+                active-button-class="flex justify-between items-center whitespace-nowrap bg-transparent rounded md:justify-start md:pr-2 md:pl-3 md:space-x-2 md:h-8 md:border border-theme-secondary-300 text-theme-secondary-700 transition-default dark:border-theme-dark-700 dark:text-theme-dark-200 dark:hover:bg-theme-dark-700 hover:text-theme-secondary-900 hover:bg-theme-secondary-200"
+            >
+                <x-slot name="button">
+                    <div
+                        x-text="truncateMiddle(await address())"
+                        class="text-sm font-semibold md:leading-3.75"
+                    ></div>
 
-                <div class="dark:text-theme-dark-300">
-                    <x-ark-icon
-                        name="ellipsis-vertical"
-                        size="sm"
-                    />
-                </div>
-            </x-slot>
+                    <div class="dark:text-theme-dark-300">
+                        <x-ark-icon
+                            name="ellipsis-vertical"
+                            size="sm"
+                        />
+                    </div>
+                </x-slot>
 
-            <x-general.dropdown.list-item x-on:click="copy">
-                @lang('general.navbar.arkconnect.copy_address')
-            </x-general.dropdown.list-item>
+                <x-general.dropdown.list-item x-on:click="copy">
+                    @lang('general.navbar.arkconnect.copy_address')
+                </x-general.dropdown.list-item>
 
-            <x-general.dropdown.list-item x-on:click="disconnect">
-                @lang('general.navbar.arkconnect.disconnect')
-            </x-general.dropdown.list-item>
-        </x-general.dropdown.dropdown>
+                <x-general.dropdown.list-item x-on:click="disconnect">
+                    @lang('general.navbar.arkconnect.disconnect')
+                </x-general.dropdown.list-item>
+            </x-general.dropdown.dropdown>
+        </div>
     </div>
 </div>
