274f390411970781488f65c5a4149a8316c327cc
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-03-26 13:14:43 +0000

    test

--- tests/Unit/Actions/CacheNetworkSupplyTest.php
+++ tests/Unit/Actions/CacheNetworkSupplyTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Actions\CacheNetworkSupply;
 use App\Models\Block;
+use App\Models\Wallet;
 use App\Services\Cache\NetworkCache;
 
 it('should execute the command', function () {
@@ -18,4 +19,14 @@ it('should execute the command with missing data', function () {
     CacheNetworkSupply::execute();
 
     expect((new NetworkCache())->getSupply())->toBe(0.0);
+});
+
+it('should sum all wallets', function () {
+    Wallet::factory(50)->create([
+        'balance' => 1000 * 1e8,
+    ]);
+
+    CacheNetworkSupply::execute();
+
+    expect((new NetworkCache())->getSupply())->toBe(50000 * 1e8);
 });
