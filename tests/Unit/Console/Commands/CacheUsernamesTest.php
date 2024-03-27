ae3a3e6e00ac2e853d1c5f9d176bdfe653310c48
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-03-27 17:48:36 +0000

    test and update factory

--- tests/Unit/Console/Commands/CacheUsernamesTest.php
+++ tests/Unit/Console/Commands/CacheUsernamesTest.php
@@ -3,13 +3,17 @@
 declare(strict_types=1);
 
 use App\Console\Commands\CacheUsernames;
+use App\Models\Transaction;
 use App\Models\Wallet;
+use App\Services\Cache\WalletCache;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
 
 it('should cache the username for the public key', function () {
-    $wallet = Wallet::factory()->create();
+    $wallet = Wallet::factory()->create([
+        'attributes->username' => 'testuser',
+    ]);
 
     expect(Cache::tags('wallet')->has(md5("username_by_address/$wallet->address")))->toBeFalse();
     expect(Cache::tags('wallet')->has(md5("username_by_public_key/$wallet->public_key")))->toBeFalse();
@@ -41,7 +45,7 @@ it('should cache the known wallet name if defined', function () {
     ]);
 
     $regularWallet = Wallet::factory()->create([
-        'attributes->validator->username' => 'regular',
+        'attributes->username' => 'regular',
     ]);
 
     (new CacheUsernames())->handle();
@@ -64,11 +68,33 @@ it('should cache the known wallet name if doesnt have validator name', function 
     ], 200));
 
     $wallet = Wallet::factory()->create([
-        'address'                         => 'AagJoLEnpXYkxYdYkmdDSNMLjjBkLJ6T67',
-        'attributes->validator->username' => null,
+        'address'              => 'AagJoLEnpXYkxYdYkmdDSNMLjjBkLJ6T67',
+        'attributes->username' => null,
     ]);
 
     (new CacheUsernames())->handle();
 
     expect(Cache::tags('wallet')->get(md5("username_by_address/$wallet->address")))->toBe('Hot Wallet');
+});
+
+it('should forget wallets with resigned usernames', function () {
+    $cache  = new WalletCache();
+    $wallet = Wallet::factory()->create([
+        'address'              => 'AagJoLEnpXYkxYdYkmdDSNMLjjBkLJ6T67',
+        'attributes->username' => 'joeblogs',
+    ]);
+
+    (new CacheUsernames())->handle();
+
+    expect($cache->getUsernameByAddress($wallet->address))->toBe('joeblogs');
+    expect($cache->getUsernameByPublicKey($wallet->public_key))->toBe('joeblogs');
+
+    Transaction::factory()->usernameResignation()->create([
+        'sender_public_key' => $wallet->public_key,
+    ]);
+
+    (new CacheUsernames())->handle();
+
+    expect($cache->getUsernameByAddress($wallet->address))->toBeNull();
+    expect($cache->getUsernameByPublicKey($wallet->public_key))->toBeNull();
 });
