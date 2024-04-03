baaef0f6948f1b39248647dfd3f2f125a5f02763
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-03 14:54:36 +0100

    update dark/dim address & button colors

--- resources/views/components/navbar/arkconnect.blade.php
+++ resources/views/components/navbar/arkconnect.blade.php
@@ -1,6 +1,6 @@
 <div
     x-data="Wallet()"
-    class="flex flex-col py-3 px-6 md:py-0 md:px-0 dark:border-transparent dark:text-theme-secondary-200 dark:bg-theme-dark-900"
+    class="flex flex-col py-3 px-6 md:py-0 md:px-0 dark:border-transparent dark:text-theme-dark-200 dark:bg-theme-dark-900"
     :class="{
         'border-t bg-theme-secondary-200 border-theme-secondary-300 md:border-0 md:bg-transparent': isConnected,
     }"
@@ -30,7 +30,7 @@
             </div>
         </div>
 
-        <div class="flex space-x-4 md:hidden">
+        <div class="flex space-x-4 md:hidden dark:text-theme-dark-300">
             <a
                 x-on:click="copy"
                 data-tippy-content="@lang('tooltips.copied')"
