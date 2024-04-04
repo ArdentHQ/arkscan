dc1fb4fe310f5320308db19d30867865ed7c5402
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-04 20:25:28 +0100

    remove unused round balance method

--- tests/Unit/ViewModels/RoundViewModelTest.php
+++ tests/Unit/ViewModels/RoundViewModelTest.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 use App\Models\Round;
 use App\ViewModels\RoundViewModel;
-use function Spatie\Snapshots\assertMatchesSnapshot;
 
-beforeEach(function () {
-    $this->subject = new RoundViewModel(Round::factory()->create(['balance' => '500000000000']));
-});
+it('should instantiate', function () {
+    $round = Round::factory()->create();
+    $viewModel = new RoundViewModel($round);
 
-it('should get the balance', function () {
-    expect($this->subject->balance())->toBeFloat();
-
-    assertMatchesSnapshot($this->subject->balance());
+    expect($viewModel->model())->toBe($round);
 });
