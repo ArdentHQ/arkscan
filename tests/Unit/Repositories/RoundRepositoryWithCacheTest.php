cdb65fd7e86621a265e3bc14306c849fe255f4e0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-09 15:06:28 +0100

    fix: repository tests (#832)

--- tests/Unit/Repositories/RoundRepositoryWithCacheTest.php
+++ tests/Unit/Repositories/RoundRepositoryWithCacheTest.php
@@ -4,53 +4,52 @@ declare(strict_types=1);
 
 // @TODO: assert that cache has been called
 
+use App\Facades\Network;
 use App\Facades\Rounds;
 use App\Models\Block;
 use App\Models\Round;
 use App\Models\Wallet;
 use App\Repositories\RoundRepository;
 use App\Repositories\RoundRepositoryWithCache;
+use function Tests\createRoundEntry;
 
 beforeEach(function () {
     $this->subject = new RoundRepositoryWithCache(new RoundRepository());
 
-    Wallet::factory(51)->create()->each(function ($wallet) {
-        Round::factory()->create([
-            'round'      => '112168',
-            'public_key' => $wallet->public_key,
-        ]);
-    });
+    $wallets = Wallet::factory(Network::validatorCount())->create();
+
+    createRoundEntry(112168, (112168 - 1) * Network::validatorCount(), $wallets);
 });
 
 it('should get all validators for the given round', function () {
-    expect($this->subject->byRound(112168))->toHaveCount(51);
+    expect($this->subject->byRound(112168)->validators)->toHaveCount(Network::validatorCount());
 });
 
 it('should get the current round', function () {
-    expect($this->subject->current())->toBe(Round::max('round'));
+    expect($this->subject->current()->round)->toBe(Round::max('round'));
 });
 
 it('should get the slot data for the current round', function () {
-    Block::factory()->create(['height' => 5720518]);
+    Block::factory()->create(['height' => 5944852]);
 
     $validators = $this->subject->validators();
 
-    expect($validators)->toHaveCount(51);
-    expect(Rounds::current())->toBe(112168);
+    expect($validators)->toHaveCount(Network::validatorCount());
+    expect(Rounds::current()->round)->toBe(112168);
 
     $wallet = $validators->first();
 
     expect($validators->firstWhere(fn ($validator) => $validator['publicKey'] === $wallet['publicKey'])['block'])->toBeNull();
 
     Block::factory()->create([
-        'height'               => 5720519,
+        'height'               => 5944853,
         'generator_public_key' => $wallet['publicKey'],
     ]);
 
     $validators = $this->subject->validators();
 
-    expect($validators)->toHaveCount(51);
-    expect(Rounds::current())->toBe(112168);
+    expect($validators)->toHaveCount(Network::validatorCount());
+    expect(Rounds::current()->round)->toBe(112168);
 
     $wallet = $validators->first();
 
