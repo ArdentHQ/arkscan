96bc772795fef90e662950fcc3d5021f75d5715b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-09 21:10:08 +0100

    add state factory

---
+++ database/factories/StateFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\State;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+final class StateFactory extends Factory
+{
+    protected $model = State::class;
+
+    public function definition()
+    {
+        return [
+            'id'     => 1,
+            'height' => $this->faker->numberBetween(1, 10000),
+            'supply' => $this->faker->numberBetween(1, 1000000) * 1e8,
+        ];
+    }
+}
