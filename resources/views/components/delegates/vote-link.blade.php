e98e9dd4da07256440048d6fc78807f0f7a32df6
alexbarnsley <alexbarnsley@users.noreply.github.com>
2024-04-18 13:56:37 +0000

    style: resolve style guide violations

--- resources/views/components/delegates/vote-link.blade.php
+++ resources/views/components/delegates/vote-link.blade.php
@@ -17,7 +17,7 @@
     >
         <x-slot
             name="button"
-            class="text-sm font-semibold transition-default hover:underline"
+            class="text-sm font-semibold hover:underline transition-default"
             ::class="{
                 'text-theme-primary-600 hover:text-theme-primary-700 dark:text-theme-dark-blue-400 dark:hover:text-theme-dark-blue-500': votingFor !== delegate,
                 'text-theme-danger-400 hover:text-theme-danger-500': votingFor === delegate,
@@ -32,22 +32,22 @@
             </div>
         </x-slot>
 
-        <div class="rounded-t-xl overflow-hidden">
-            <div class="flex bg-theme-secondary-200 dark:bg-theme-dark-950 leading-4.25 text-sm py-2 px-6 font-semibold">
+        <div class="overflow-hidden rounded-t-xl">
+            <div class="flex py-2 px-6 text-sm font-semibold bg-theme-secondary-200 leading-4.25 dark:bg-theme-dark-950">
                 @lang('general.vote_with')
             </div>
 
             <div class="flex flex-col py-3 px-6">
                 <x-ark-external-link
                     :url="$model->voteUrl()"
-                    class="flex items-center space-x-2 font-semibold leading-5 py-3 link"
+                    class="flex items-center py-3 space-x-2 font-semibold leading-5 link"
                     icon-class="inline relative -top-1 flex-shrink-0 mt-1 ml-0.5 text-theme-secondary-500 dark:text-theme-dark-500"
                     :text="trans('brands.arkvault')"
                 />
 
                 <button
                     type="button"
-                    class="flex items-center space-x-2 font-semibold leading-5 py-3 link"
+                    class="flex items-center py-3 space-x-2 font-semibold leading-5 link"
                     @click="performVote('{{ $model->address() }}')"
                 >
                     @lang('brands.arkconnect')
--- resources/views/components/tables/rows/mobile/encapsulated/delegates/voting-for.blade.php
+++ resources/views/components/tables/rows/mobile/encapsulated/delegates/voting-for.blade.php
@@ -2,7 +2,7 @@
 
 <div
     x-show="votingFor === '{{ $model->address() }}'"
-    class="flex items-center space-x-2 bg-theme-secondary-200 dark:bg-theme-dark-800 dark:text-theme-dark-200 p-3 -mx-3 mb-1"
+    class="flex items-center p-3 -mx-3 mb-1 space-x-2 bg-theme-secondary-200 dark:bg-theme-dark-800 dark:text-theme-dark-200"
 >
     <div>
         <x-ark-icon
