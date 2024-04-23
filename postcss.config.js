02f6150bb56ec03ed7404a26694690f4d7f1547d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-23 20:04:11 +0100

    scrub

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,11 @@
 import postcssImport from 'postcss-import';
 import postcssFocusVisible from 'postcss-focus-visible';
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default {
     plugins: [
         postcssImport,
         tailwindcss,
         postcssFocusVisible,
-        // autoprefixer,
     ],
 };
