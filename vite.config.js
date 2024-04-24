fb52004b0559f21235abafba470a27408c4fe225
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-24 09:58:21 +0100

    handle ssl in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+import fs from 'fs';
 import { defineConfig, loadEnv } from "vite";
-import path from "path";
+import { homedir } from "os";
+import { resolve } from "path";
 import laravel from "laravel-vite-plugin";
 
 export default ({ mode }) => defineConfig({
@@ -12,14 +14,57 @@ export default ({ mode }) => defineConfig({
     ],
     resolve: {
         alias: {
-            "@ui": path.resolve(
+            "@ui": resolve(
                 __dirname,
                 "vendor/arkecosystem/foundation/resources/assets/"
             ),
         },
     },
-    server: {
+    server: detectServerConfig(mode) || {
         host: loadEnv(mode, process.cwd()).VITE_HOST ?? 'localhost',
         port: 3000,
-   },
+    },
 });
+
+function getValetHome() {
+    let valetPath = resolve(homedir(), '.config/valet');
+    if (fs.existsSync(valetPath)) {
+        return valetPath;
+    }
+
+    valetPath = resolve(homedir(), '.valet');
+    if (fs.existsSync(valetPath)) {
+        return valetPath;
+    }
+
+    return null;
+}
+
+// Variation of https://freek.dev/2276-making-vite-and-valet-play-nice-together
+function detectServerConfig(mode) {
+    const valetPath = getValetHome();
+    if (!valetPath) {
+        return;
+    }
+
+    const host = loadEnv(mode, process.cwd()).VITE_HOST ?? 'localhost';
+    let keyPath = resolve(valetPath, `Certificates/${host}.key`);
+    let certificatePath = resolve(valetPath, `Certificates/${host}.crt`);
+
+    if (!fs.existsSync(keyPath)) {
+        return;
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return;
+    }
+
+    return {
+        host,
+        port: 3000,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    };
+}
