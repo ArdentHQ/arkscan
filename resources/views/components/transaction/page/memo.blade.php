5030a2053e3cbbab70cc8b47303e32b4fb62f26d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-04-24 18:58:41 +0100

    refactor: memo break words

--- resources/views/components/transaction/page/memo.blade.php
+++ resources/views/components/transaction/page/memo.blade.php
@@ -4,15 +4,15 @@
 
 <x-general.page-section.container
     :title="trans('pages.transaction.memo')"
-    wrapper-class="font-normal leading-7"
+    wrapper-class="font-normal leading-7 max-w-full"
 >
     @if (! $vendorField)
         <span class="text-sm font-semibold sm:text-base sm:font-normal text-theme-secondary-500 dark:text-theme-dark-500">
             @lang('general.na')
         </span>
     @else
-        <span class="text-base text-theme-secondary-900 dark:text-theme-dark-50">
+        <div class="text-base text-theme-secondary-900 dark:text-theme-dark-50 break-words">
             {{ $vendorField }}
-        </span>
+        </div>
     @endif
 </x-general.page-section.container>
