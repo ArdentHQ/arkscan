61e0d831973b02de1b086f6ebcba730f8be7b8c2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-05-08 11:10:54 +0100

    refactor: use arkscan nethash for arkvault url (#887)

--- app/Services/ArkVaultUrlBuilder.php
+++ app/Services/ArkVaultUrlBuilder.php
@@ -5,17 +5,13 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Facades\Network;
-use Ardenthq\UrlBuilder\Enums\Networks;
 use Ardenthq\UrlBuilder\UrlBuilder;
 
 final class ArkVaultUrlBuilder
 {
     public static function get(): UrlBuilder
     {
-        $urlBuilder = new UrlBuilder(config('arkscan.vault_url'));
-
-        $urlBuilder->setNetwork(Network::alias() === 'mainnet' ? Networks::ARKMainnet : Networks::ARKDevnet);
-
-        return $urlBuilder;
+        return (new UrlBuilder(config('arkscan.vault_url')))
+            ->setNetwork(Network::nethash());
     }
 }
--- tests/Unit/Services/ArkVaultUrlBuilderTest.php
+++ tests/Unit/Services/ArkVaultUrlBuilderTest.php
@@ -15,7 +15,7 @@ it('uses devnet network if explorer is set to development', function () {
 
     $builder = ArkVaultUrlBuilder::get();
 
-    expect($builder->nethash())->toBe('2a44f340d76ffc3df204c5f38cd355b7496c9065a1ade2ef92071436bd72e867');
+    expect($builder->nethash())->toBe(config('arkscan.networks.development.nethash'));
 });
 
 it('uses mainnet network if explorer is set to production', function () {
@@ -23,5 +23,5 @@ it('uses mainnet network if explorer is set to production', function () {
 
     $builder = ArkVaultUrlBuilder::get();
 
-    expect($builder->nethash())->toBe('6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988');
+    expect($builder->nethash())->toBe(config('arkscan.networks.production.nethash'));
 });
