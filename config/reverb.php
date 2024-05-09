a1ae7fabe18db724bf47fc9a911ef95057c1a0f7
alexbarnsley <alexbarnsley@users.noreply.github.com>
2024-05-09 14:25:32 +0000

    style: resolve style guide violations

--- config/reverb.php
+++ config/reverb.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
 
     /*
@@ -29,28 +31,28 @@ return [
     'servers' => [
 
         'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 8080),
+            'host'     => env('REVERB_SERVER_HOST', '0.0.0.0'),
+            'port'     => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
-            'options' => [
+            'options'  => [
                 'tls' => env('REVERB_CERT_FILE') ? [
                     'local_cert' => env('REVERB_CERT_FILE'),
                 ] : [],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
-            'scaling' => [
+            'scaling'          => [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
                 'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
-                'server' => [
-                    'url' => env('REDIS_URL'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'port' => env('REDIS_PORT', '6379'),
+                'server'  => [
+                    'url'      => env('REDIS_URL'),
+                    'host'     => env('REDIS_HOST', '127.0.0.1'),
+                    'port'     => env('REDIS_PORT', '6379'),
                     'username' => env('REDIS_USERNAME'),
                     'password' => env('REDIS_PASSWORD'),
                     'database' => env('REDIS_DB', '0'),
                 ],
             ],
-            'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
+            'pulse_ingest_interval'     => env('REVERB_PULSE_INGEST_INTERVAL', 15),
             'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
         ],
 
@@ -73,17 +75,17 @@ return [
 
         'apps' => [
             [
-                'key' => env('REVERB_APP_KEY'),
-                'secret' => env('REVERB_APP_SECRET'),
-                'app_id' => env('REVERB_APP_ID'),
+                'key'     => env('REVERB_APP_KEY'),
+                'secret'  => env('REVERB_APP_SECRET'),
+                'app_id'  => env('REVERB_APP_ID'),
                 'options' => [
-                    'host' => env('REVERB_HOST'),
-                    'port' => env('REVERB_PORT', 443),
+                    'host'   => env('REVERB_HOST'),
+                    'port'   => env('REVERB_PORT', 443),
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
-                'allowed_origins' => explode(',', env('REVERB_ALLOWED_ORIGINS', '*')),
-                'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
+                'allowed_origins'  => explode(',', env('REVERB_ALLOWED_ORIGINS', '*')),
+                'ping_interval'    => env('REVERB_APP_PING_INTERVAL', 60),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
             ],
         ],
