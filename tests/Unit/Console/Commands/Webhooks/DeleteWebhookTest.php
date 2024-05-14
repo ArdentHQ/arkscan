48248f26fdf0a0ac084ea7103535f17778208bae
alexbarnsley <alexbarnsley@users.noreply.github.com>
2024-05-14 15:35:24 +0000

    style: resolve style guide violations

--- tests/Unit/Console/Commands/Webhooks/DeleteWebhookTest.php
+++ tests/Unit/Console/Commands/Webhooks/DeleteWebhookTest.php
@@ -55,7 +55,7 @@ it('should override host', function () {
     $webhook = Webhook::factory()->create();
 
     Artisan::call('ark:webhook:delete', [
-        '--host' => '1.2.3.4',
+        '--host'  => '1.2.3.4',
         '--token' => $webhook->token,
     ]);
 
