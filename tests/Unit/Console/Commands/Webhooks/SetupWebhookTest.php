dbaceb5a2d266ff022e21a8b0a655975f76d0eaf
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-05-14 16:33:59 +0100

    test

--- tests/Unit/Console/Commands/Webhooks/SetupWebhookTest.php
+++ tests/Unit/Console/Commands/Webhooks/SetupWebhookTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\Webhook;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Http;
 
@@ -15,12 +16,22 @@ it('should execute the command', function () {
 
     Artisan::call('ark:webhook:setup', [
         '--host' => '1.2.3.4',
+        '--event' => 'test.event',
     ]);
 
     $output = Artisan::output();
 
     expect($output)->toContain('ID: responseId');
     expect($output)->toContain('Token: random-token');
+
+    $webhook = Webhook::first();
+
+    expect($webhook)->not->toBeNull();
+    expect($webhook->id)->toBe('responseId');
+    expect($webhook->token)->toBe('random-token');
+    expect($webhook->host)->toBe('1.2.3.4');
+    expect($webhook->port)->toBe(4004);
+    expect($webhook->event)->toBe('test.event');
 });
 
 it('should require host', function () {
