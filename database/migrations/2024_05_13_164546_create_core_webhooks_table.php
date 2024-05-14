ef429e533b37f4ac748e3290c8b1b94871b701af
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-05-14 12:49:13 +0100

    migration

---
+++ database/migrations/2024_05_13_164546_create_core_webhooks_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('core_webhooks', function (Blueprint $table) {
+            $table->string('token')->primary();
+            $table->string('host');
+            $table->integer('port');
+            $table->string('event');
+            $table->timestamps();
+        });
+    }
+};
