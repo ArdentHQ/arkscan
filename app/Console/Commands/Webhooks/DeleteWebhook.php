4f2b621064a19a197446b0726ec47828185c4680
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-05-16 16:36:26 +0100

    refactor: webhook management commands (#900)

---
+++ app/Console/Commands/Webhooks/DeleteWebhook.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands\Webhooks;
+
+use App\Models\Webhook;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+
+final class DeleteWebhook extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ark:webhook:delete {--host=} {--port=} {--token=} {--id=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string|null
+     */
+    protected $description = 'Delete ARK webhook';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        /** @var string|null $token */
+        $token = $this->option('token');
+
+        /** @var string|null $id */
+        $id = $this->option('id');
+
+        if ($token === null && $id === null) {
+            $this->error('Missing [token] or [id] argument.');
+
+            return Command::FAILURE;
+        }
+
+        $webhook = Webhook::find($id);
+        if ($webhook === null) {
+            $webhook = Webhook::firstWhere('token', $token);
+        }
+
+        if ($webhook === null) {
+            $this->error('Webhook does not exist.');
+
+            return Command::FAILURE;
+        }
+
+        /** @var string|null $coreHost */
+        $coreHost = $this->option('host');
+
+        /** @var string|null $corePort */
+        $corePort = $this->option('port');
+
+        if ($coreHost === null) {
+            $coreHost = $webhook->host;
+        }
+
+        if ($corePort === null) {
+            $corePort = $webhook->port;
+        }
+
+        try {
+            $response = Http::delete(sprintf(
+                'http://%s:%d/api/webhooks/%s',
+                $coreHost,
+                $corePort,
+                $webhook->id
+            ));
+
+            $data = json_decode($response->body(), true);
+            if ($data !== null || str_starts_with((string) $response->status(), '2') === false) {
+                $this->info(serialize($data));
+                $this->error(sprintf(
+                    'There was a problem removing the webhook: %s',
+                    Arr::get($data, 'message', 'Unknown')
+                ));
+
+                return Command::FAILURE;
+            }
+
+            $webhook->delete();
+
+            return Command::SUCCESS;
+        } catch (\Exception $e) {
+            $this->error('Could not connect to core webhooks endpoint: '.$e->getMessage());
+
+            return Command::FAILURE;
+        }
+    }
+}
---
+++ app/Models/Webhook.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Models\Concerns\ForgingStats\CanBeSorted;
+use App\Models\Concerns\SearchesCaseInsensitive;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $token
+ * @property string $host
+ * @property int $port
+ * @property string $event
+ */
+final class Webhook extends Model
+{
+    use CanBeSorted;
+    use HasFactory;
+    use SearchesCaseInsensitive;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    public $keyType = 'string';
+
+    /**
+     * The column name of the primary key.
+     *
+     * @var string
+     */
+    public $primaryKey = 'id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'core_webhooks';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'token',
+        'event',
+        'host',
+        'port',
+    ];
+}
