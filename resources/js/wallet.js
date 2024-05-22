c040c1cb54558a19ec3bd4b69082a47cf8b185b8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-05-22 10:54:24 +0100

    refactor: arkconnect handle delegate property naming change

--- resources/js/wallet.js
+++ resources/js/wallet.js
@@ -306,6 +306,22 @@ const Wallet = (network, xData = {}) => {
             window.clipboard(false).copy(this.address);
         },
 
+        get version() {
+            if (! this.hasExtension) {
+                return null;
+            }
+
+            return this.extension().version() || '1.0.0';
+        },
+
+        get delegateAddressKey() {
+            if (['1.8.0', '1.0.0', null].includes(this.version)) {
+                return 'delegateAddress';
+            }
+
+            return 'address';
+        },
+
         async performVote(address) {
             if (!this.hasExtension) {
                 return;
@@ -318,18 +334,18 @@ const Wallet = (network, xData = {}) => {
                 if (votingForAddress) {
                     voteData.unvote = {
                         amount: 0,
-                        delegateAddress: votingForAddress,
+                        [this.delegateAddressKey]: votingForAddress,
                     };
                 }
 
                 voteData.vote = {
                     amount: 0,
-                    delegateAddress: address,
+                    [this.delegateAddressKey]: address,
                 };
             } else {
                 voteData.unvote = {
                     amount: 0,
-                    delegateAddress: address,
+                    [this.delegateAddressKey]: address,
                 };
             }
 
