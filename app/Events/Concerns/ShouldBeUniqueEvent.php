0547661401211091e506481c6812c4f41453bec5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-05-28 19:47:29 +0100

    add concern for event

---
+++ app/Events/Concerns/ShouldBeUniqueEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events\Concerns;
+
+use Illuminate\Support\Facades\Cache;
+
+trait ShouldBeUniqueEvent
+{
+    public const UNIQUE_KEY = 'webhooks:event';
+
+    abstract protected function uniqueTimeout(): int;
+
+    final public function broadcastWhen(): bool
+    {
+        $lock = Cache::lock(
+            $this->uniqueKey(),
+            $this->uniqueTimeout()
+        );
+
+        // dump(
+        //     // $lock,
+        //     // $lock->get(),
+        //     $this->uniqueKey(),
+        //     now()->format('Y-m-d H:i:s'),
+        //     // debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 8),
+        // );
+
+        if ($lock->get() === false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function uniqueKey(): string
+    {
+        return sprintf(
+            '%s:%s',
+            static::UNIQUE_KEY,
+            $this->channelName()
+        );
+    }
+}
