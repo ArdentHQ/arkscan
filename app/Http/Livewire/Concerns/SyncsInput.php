84eb930db64e33171fec0cd5f610f38e4ddf795a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-04 20:10:17 +0100

    custom syncs inputs implementation

---
+++ app/Http/Livewire/Concerns/SyncsInput.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Livewire\Concerns;
+
+use Livewire\Features\SupportLifecycleHooks\SupportLifecycleHooks;
+
+trait SyncsInput
+{
+    private function syncInput(string $property, mixed $value): void
+    {
+        $hook = new SupportLifecycleHooks();
+
+        $hook->setComponent($this);
+        $updatedHook = $hook->update($property, $property, $value);
+
+        /** @phpstan-ignore-next-line */
+        $this->{$property} = $value;
+
+        $updatedHook();
+    }
+}
