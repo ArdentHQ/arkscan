542b24eb36f4898a809c21e5f8a782e62b5554f9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-12 17:39:33 +0100

    remove value casting on sync input

--- app/Http/Livewire/Concerns/SyncsInput.php
+++ app/Http/Livewire/Concerns/SyncsInput.php
@@ -4,19 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Livewire\Concerns;
 
-use Livewire\Attribute;
+use Livewire\Features\SupportAttributes\Attribute;
 use Livewire\Features\SupportAttributes\AttributeLevel;
 use Livewire\Features\SupportLifecycleHooks\SupportLifecycleHooks;
-use ReflectionClass;
-use ReflectionNamedType;
 
 trait SyncsInput
 {
     public function syncInput(string $property, mixed $value): void
     {
         $hook = new SupportLifecycleHooks();
-
-        $value = $this->castValueToProperty($property, $value);
 
         $hook->setComponent($this);
         $updatedHook = $hook->update($property, $property, $value);
@@ -38,30 +34,5 @@ trait SyncsInput
             ->filter(fn ($attribute) => $attribute->getLevel() === AttributeLevel::PROPERTY)
             ->keyBy('getName')
             ->get($property);
-    }
-
-    private function castValueToProperty(string $property, mixed $value): mixed
-    {
-        $reflection = new ReflectionClass($this);
-        if (! $reflection->hasProperty($property)) {
-            return $value;
-        }
-
-        $classProperty = $reflection->getProperty($property);
-        $type          = $classProperty->getType();
-
-        if ($type === null) {
-            return $value;
-        }
-
-        if (! is_a($type, ReflectionNamedType::class)) {
-            return $value;
-        }
-
-        if ($type->getName() === 'bool') {
-            $value = in_array($value, [true, 'true', 1], true);
-        }
-
-        return $value;
     }
 }
