4c16c160ad3bc5b5d2559b30b49e9968dd7d4037
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-20 14:37:51 +0100

    test

--- tests/Unit/Services/Monitor/ValidatorTrackerTest.php
+++ tests/Unit/Services/Monitor/ValidatorTrackerTest.php
@@ -4,55 +4,34 @@ declare(strict_types=1);
 
 use App\Facades\Network;
 use App\Facades\Rounds;
-use App\Models\Block;
-use App\Models\Wallet;
 use App\Services\Monitor\ValidatorTracker;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Cache;
-use function Spatie\Snapshots\assertMatchesSnapshot;
 use function Tests\createPartialRound;
 use function Tests\createRealisticRound;
 use function Tests\createRoundEntry;
 use function Tests\getRoundValidators;
 
-beforeEach(function () {
-    $this->activeValidators = require dirname(dirname(dirname(__DIR__))).'/fixtures/forgers.php';
-    $this->expected         = require dirname(dirname(dirname(__DIR__))).'/fixtures/validator-tracker.php';
-});
-
 it('should calculate the forging order', function () {
-    $this->travelTo(new Carbon('2021-01-01 00:04:00'));
+    $this->travelTo(Carbon::parse('2024-02-01 14:00:00Z'));
 
-    $wallets = Wallet::factory(51)->create();
-    $wallets->each(function ($wallet) {
-        Cache::tags(['validators'])->put($wallet->public_key, $wallet);
-        Cache::put('lastBlock:'.$wallet->public_key, []);
-    });
+    $this->freezeTime();
 
-    createRoundEntry(112168, 112168 * Network::validatorCount(), $wallets);
+    [$validators, $round, $height] = createRealisticRound([
+        array_fill(0, 53, true),
+        array_fill(0, 53, true),
+    ], $this);
 
-    // Start height for round 112168
-    Block::factory()->create([
-        'height'    => 5720517,
-        'timestamp' => 113620816,
-    ]);
+    $roundHeight = Rounds::current()->round_height;
 
-    Block::factory()->create([
-        'height'    => 5720529,
-        'timestamp' => 113620904,
-    ]);
-
-    $order = ValidatorTracker::execute($this->activeValidators->pluck('public_key')->toArray(), 5720517);
+    $order = ValidatorTracker::execute($validators->pluck('public_key')->toArray(), $roundHeight);
 
     expect($order)->toHaveCount(Network::validatorCount());
 
-    $publicKeys = $this->activeValidators->pluck('public_key');
+    $publicKeys = $validators->pluck('public_key');
 
     foreach ($order as $validator) {
         expect($publicKeys)->toContain($validator['publicKey']);
     }
-
-    assertMatchesSnapshot($order);
 });
 
 it('should handle no missed block', function () {
