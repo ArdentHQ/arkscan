b5a5c51445944c18a7417928518acbfce5cffb1a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-25 21:56:23 +0100

    dispatch cache blocks job on webhook

--- app/Http/Controllers/Concerns/HandlesBlockWebhooks.php
+++ app/Http/Controllers/Concerns/HandlesBlockWebhooks.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Concerns;
 
 use App\Events\NewBlock;
+use App\Jobs\CacheBlocks;
 
 trait HandlesBlockWebhooks
 {
     private function handleBlockApplied(): void
     {
         NewBlock::dispatch();
+
+        // We'll run the job instead of duplicating the logic as this is the only purpose for the job.
+        CacheBlocks::dispatch();
     }
 
     private function handleGeneratorBlockApplied(): void
