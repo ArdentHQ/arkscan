e4b08ffff4e86220865cd2ed4eb0c5244f30a949
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-25 21:56:35 +0100

    tidy up transaction webhook checks

--- app/Http/Controllers/Concerns/HandlesTransactionWebhooks.php
+++ app/Http/Controllers/Concerns/HandlesTransactionWebhooks.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Concerns;
 
 use App\Events\NewTransaction;
+use App\Events\Statistics\TransactionDetails;
 use App\Events\Statistics\UniqueAddresses;
 use App\Services\Addresses\Aggregates\LatestWalletAggregate;
+use App\Services\Cache\TransactionCache;
+use App\Services\Transactions\Aggregates\LargestTransactionAggregate;
 
 trait HandlesTransactionWebhooks
 {
@@ -14,9 +17,8 @@ trait HandlesTransactionWebhooks
     {
         NewTransaction::dispatch();
 
-        if ((new LatestWalletAggregate())->aggregate() !== null) {
-            UniqueAddresses::dispatch();
-        }
+        $this->checkLatestWallet();
+        $this->checkLargestTransaction();
     }
 
     private function handleSenderTransactionApplied(): void
@@ -28,5 +30,32 @@ trait HandlesTransactionWebhooks
     {
         // Recipient Address since we can't easily get the public key
         NewTransaction::dispatch(request()->input('data.recipientId'));
+    }
+
+    private function checkLatestWallet(): void
+    {
+        $latestWallet = (new LatestWalletAggregate())->aggregate();
+        if ($latestWallet === null) {
+            return;
+        }
+
+        UniqueAddresses::dispatch();
+    }
+
+    private function checkLargestTransaction(): void
+    {
+        $cache = new TransactionCache();
+        $largestTransaction = (new LargestTransactionAggregate())->aggregate();
+        if ($largestTransaction === null) {
+            return;
+        }
+
+        if ($cache->getLargestIdByAmount() === $largestTransaction->id) {
+            return;
+        }
+
+        $cache->setLargestIdByAmount($largestTransaction->id);
+
+        TransactionDetails::dispatch();
     }
 }
