19a0a7d84e32bb8bb9f8808002924a75fd61ab01
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-25 14:12:00 +0100

    refactor: prevent exchange exceptions (#918)

--- tests/Unit/Console/Commands/FetchExchangesDetailsTest.php
+++ tests/Unit/Console/Commands/FetchExchangesDetailsTest.php
@@ -2,31 +2,112 @@
 
 declare(strict_types=1);
 
-use App\Jobs\FetchExchangeDetails;
+use App\Contracts\MarketDataProvider;
 use App\Models\Exchange;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Bus;
+use App\Services\MarketDataProviders\CoinGecko;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
 
 beforeEach(function () {
-    Artisan::call('migrate:fresh');
+    Exchange::truncate();
 
-    Bus::fake(FetchExchangeDetails::class);
+    $this->freezeTime();
+
+    $this->travelTo(Carbon::parse('2024-05-14 12:22:49'));
+
+    $this->app->singleton(
+        MarketDataProvider::class,
+        fn () => new CoinGecko()
+    );
 });
 
-it('calls the job for fetching the exchange details for exchanges with coingecko id', function () {
-    Exchange::factory()->create([
-        'coingecko_id' => 'binance',
+it('should update exchange details for coingecko exchanges once per hour', function () {
+    Http::fake([
+        'https://api.coingecko.com/api/v3/exchanges/binance/tickers?coin_ids=ark' => Http::response([
+            'tickers' => [
+                [
+                    'converted_last' => [
+                        'usd' => 123,
+                    ],
+                    'converted_volume' => [
+                        'usd' => 456,
+                    ],
+                ],
+            ],
+        ], 200),
     ]);
 
-    Exchange::factory()->create([
+    $coingeckoExchange = Exchange::factory()->create([
+        'coingecko_id' => 'binance',
+        'volume'       => null,
+        'price'        => null,
+    ]);
+
+    $genericExchange = Exchange::factory()->create([
         'coingecko_id' => null,
+        'volume'       => null,
+        'price'        => null,
     ]);
 
     $this->artisan('exchanges:fetch-details');
 
-    Bus::assertDispatchedTimes(FetchExchangeDetails::class, 1);
+    $this->travel(59)->minutes();
 
-    Bus::assertDispatched(FetchExchangeDetails::class, function ($job) {
-        return $job->exchange->coingecko_id === 'binance';
-    });
+    expect($coingeckoExchange->fresh()->price)->toBeNull();
+    expect($coingeckoExchange->fresh()->volume)->toBeNull();
+
+    expect($genericExchange->fresh()->price)->toBeNull();
+    expect($genericExchange->fresh()->volume)->toBeNull();
+
+    $this->travel(2)->minutes();
+
+    $this->artisan('exchanges:fetch-details');
+
+    expect($coingeckoExchange->fresh()->price)->toBe('123');
+    expect($coingeckoExchange->fresh()->volume)->toBe('456');
+
+    expect($genericExchange->fresh()->price)->toBeNull();
+    expect($genericExchange->fresh()->volume)->toBeNull();
+});
+
+it('should do nothing if there is a coingecko error', function () {
+    Http::fake([
+        'https://api.coingecko.com/api/v3/exchanges/binance/tickers?coin_ids=ark' => Http::response([
+            'status' => [
+                'error_code' => 1234,
+            ],
+        ], 500),
+    ]);
+
+    $coingeckoExchange = Exchange::factory()->create([
+        'coingecko_id' => 'binance',
+        'volume'       => null,
+        'price'        => null,
+    ]);
+
+    $genericExchange = Exchange::factory()->create([
+        'coingecko_id' => null,
+        'volume'       => null,
+        'price'        => null,
+    ]);
+
+    $this->artisan('exchanges:fetch-details');
+
+    $this->travel(59)->minutes();
+
+    expect($coingeckoExchange->fresh()->price)->toBeNull();
+    expect($coingeckoExchange->fresh()->volume)->toBeNull();
+
+    expect($genericExchange->fresh()->price)->toBeNull();
+    expect($genericExchange->fresh()->volume)->toBeNull();
+
+    $this->travel(2)->minutes();
+
+    $this->artisan('exchanges:fetch-details');
+
+    expect($coingeckoExchange->fresh()->price)->toBeNull();
+    expect($coingeckoExchange->fresh()->volume)->toBeNull();
+
+    expect($genericExchange->fresh()->price)->toBeNull();
+    expect($genericExchange->fresh()->volume)->toBeNull();
 });
