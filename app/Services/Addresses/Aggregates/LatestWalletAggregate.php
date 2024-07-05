cfda467136131a56c11f21701503b03bf36a4ad8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-07-05 13:51:25 +0100

    refactor: latest wallet handle multipayment (#932)

--- app/Services/Addresses/Aggregates/LatestWalletAggregate.php
+++ app/Services/Addresses/Aggregates/LatestWalletAggregate.php
@@ -20,21 +20,73 @@ final class LatestWalletAggregate
 
         $newestQuery = Wallet::query()
             ->select('wallets.address', DB::raw('MIN(transactions.timestamp) as timestamp'))
-            ->leftJoin('transactions', function ($join) {
-                $join->on('wallets.address', '=', 'transactions.recipient_id');
-            })
+            ->leftJoin('transactions', 'wallets.address', '=', 'transactions.recipient_id')
             ->whereNotNull('transactions.sender_public_key')
             ->whereNotNull('transactions.recipient_id')
+            ->where('transactions.type', '!=', 6)
             ->groupBy('wallets.address')
             ->orderBy('timestamp', 'desc')
             ->limit(1);
 
+        $newestMultipaymentQuery = Wallet::query()
+            ->select([
+                'address',
+                'timestamp' => DB::raw('least(timestamp, standard_timestamp) as timestamp'),
+            ])
+            ->from(function ($query) {
+                $query->select([
+                    'wallets.address',
+                    DB::raw('MIN(transactions.timestamp) as timestamp'),
+                    'standard_timestamp' => function ($query) {
+                        $query->from('wallets', 'w')
+                            ->select(DB::raw('MIN(t.timestamp) as timestamp'))
+                            ->leftJoin('transactions as t', 'w.address', '=', 't.recipient_id')
+                            ->whereColumn('t.recipient_id', '=', 'wallets.address')
+                            ->where('t.type', '!=', 6)
+                            ->groupBy('w.address')
+                            ->orderBy('timestamp', 'desc')
+                            ->limit(1);
+                    },
+                ])
+                ->from('wallets')
+                ->join(DB::raw(
+                    '(select
+                        "t"."id",
+                        "t"."timestamp",
+                        jsonb_array_elements("t".asset->\'payments\')->>\'recipientId\' as multipayment_recipient
+                    from
+                        transactions "t") "transactions"'
+                ), 'multipayment_recipient', '=', 'wallets.address', 'left')
+                ->whereNotNull('transactions.multipayment_recipient')
+                ->groupBy('wallets.address')
+                ->orderBy('timestamp', 'desc')
+                ->limit(1);
+            }, 'data');
+
         $lastRun = Cache::get($cacheKey, null);
         if ($lastRun !== null) {
             $newestQuery->whereRaw(sprintf('timestamp + %d > ?', Network::epoch()->timestamp), [$lastRun->timestamp]);
+            $newestMultipaymentQuery->whereRaw(sprintf('timestamp + %d > ?', Network::epoch()->timestamp), [$lastRun->timestamp]);
         }
 
-        $newest = $newestQuery->first();
+        $newest = null;
+
+        /** @var ?Wallet $newestStandard */
+        $newestStandard = $newestQuery->first();
+
+        /** @var ?Wallet $newestMultipayment */
+        $newestMultipayment = $newestMultipaymentQuery->first();
+
+        if ($newestStandard !== null) {
+            $newest = $newestStandard;
+
+            if ($newestMultipayment !== null && $newestMultipayment->timestamp > $newestStandard->timestamp) {
+                $newest = $newestMultipayment;
+            }
+        } elseif ($newestMultipayment !== null) {
+            $newest = $newestMultipayment;
+        }
+
         if ($newest === null) {
             return null;
         }
--- tests/Unit/Services/Addresses/Aggregates/LatestWalletAggregateTest.php
+++ tests/Unit/Services/Addresses/Aggregates/LatestWalletAggregateTest.php
@@ -26,7 +26,12 @@ it('should refresh the latest wallet - A > B', function () {
         'timestamp'         => 0,
     ]);
 
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
     $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
 
     Transaction::factory()->transfer()->create([
         'sender_public_key' => $walletA->public_key,
@@ -63,7 +68,12 @@ it('should refresh the latest wallet - A > B > C', function () {
         'timestamp'         => 0,
     ]);
 
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
     $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
 
     Transaction::factory()->transfer()->create([
         'sender_public_key' => $walletA->public_key,
@@ -82,6 +92,7 @@ it('should refresh the latest wallet - A > B > C', function () {
     ]);
 
     $newestTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 14:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 14:24:45');
 
     Transaction::factory()->transfer()->create([
         'sender_public_key' => $walletB->public_key,
@@ -118,7 +129,12 @@ it('should refresh the latest wallet - A > B > existing C', function () {
         'timestamp'         => 0,
     ]);
 
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
     $existingTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 12:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 12:24:45');
 
     Transaction::factory()->transfer()->create([
         'sender_public_key' => $walletC->public_key,
@@ -127,6 +143,7 @@ it('should refresh the latest wallet - A > B > existing C', function () {
     ]);
 
     $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
 
     Transaction::factory()->transfer()->create([
         'sender_public_key' => $walletA->public_key,
@@ -145,6 +162,7 @@ it('should refresh the latest wallet - A > B > existing C', function () {
     ]);
 
     $newestTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 14:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 14:24:45');
 
     Transaction::factory()->transfer()->create([
         'sender_public_key' => $walletB->public_key,
@@ -180,9 +198,14 @@ it('should refresh the latest wallet - A > multipayment B', function () {
         'timestamp'         => 0,
     ]);
 
-    $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
 
-    Transaction::factory()->transfer()->create([
+    $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
+
+    Transaction::factory()->multiPayment()->create([
         'sender_public_key' => $walletA->public_key,
         'recipient_id'      => $walletA->address,
         'timestamp'         => $genesisTimestamp,
@@ -206,4 +229,271 @@ it('should refresh the latest wallet - A > multipayment B', function () {
         'timestamp' => $genesisTimestamp,
         'value'     => '01 Jan 2021',
     ]);
-})->skip('This test is not yet implemented.');
+});
+
+it('should refresh the latest wallet - A > multipayment B or multipayment C', function () {
+    $this->travelTo('2021-01-01 11:24:44');
+
+    $cache     = new StatisticsCache();
+    $aggregate = new LatestWalletAggregate();
+
+    expect($cache->getNewestAddress())->toBeNull();
+
+    $walletA  = Wallet::factory()->create(['address' => 'wallet-a']);
+    $walletB  = Wallet::factory()->create(['address' => 'wallet-b']);
+    $walletC  = Wallet::factory()->create(['address' => 'wallet-c']);
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => 0,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
+    $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
+
+    Transaction::factory()->multiPayment()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => $genesisTimestamp,
+
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => $walletB->address,
+                    'amount'      => 1 * 1e8,
+                ],
+                [
+                    'recipientId' => $walletC->address,
+                    'amount'      => 1 * 1e8,
+                ],
+            ],
+        ],
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect(in_array($result->address, [$walletB->address, $walletC->address], true))->toBeTrue();
+
+    $newestAddress = $cache->getNewestAddress();
+    expect(in_array($newestAddress['address'], [$walletB->address, $walletC->address], true))->toBeTrue();
+    expect($newestAddress['timestamp'])->toBe($genesisTimestamp);
+    expect($newestAddress['value'])->toBe('01 Jan 2021');
+});
+
+it('should refresh the latest wallet - multipayment over standard', function () {
+    $this->travelTo('2021-01-01 11:24:44');
+
+    $cache     = new StatisticsCache();
+    $aggregate = new LatestWalletAggregate();
+
+    expect($cache->getNewestAddress())->toBeNull();
+
+    $walletA  = Wallet::factory()->create(['address' => 'wallet-a']);
+    $walletB  = Wallet::factory()->create(['address' => 'wallet-b']);
+    $walletC  = Wallet::factory()->create(['address' => 'wallet-c']);
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => 0,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
+    $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletC->address,
+        'timestamp'         => $genesisTimestamp,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletC->address);
+
+    $multipaymentTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 14:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 14:24:45');
+
+    Transaction::factory()->multiPayment()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => $multipaymentTimestamp,
+
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => $walletB->address,
+                    'amount'      => 1 * 1e8,
+                ],
+            ],
+        ],
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletB->address);
+
+    expect($cache->getNewestAddress())->toBe([
+        'address'   => $walletB->address,
+        'timestamp' => $multipaymentTimestamp,
+        'value'     => '01 Jan 2021',
+    ]);
+});
+
+it('should refresh the latest wallet - standard over multipayment', function () {
+    $this->travelTo('2021-01-01 11:24:44');
+
+    $cache     = new StatisticsCache();
+    $aggregate = new LatestWalletAggregate();
+
+    expect($cache->getNewestAddress())->toBeNull();
+
+    $walletA  = Wallet::factory()->create(['address' => 'wallet-a']);
+    $walletB  = Wallet::factory()->create(['address' => 'wallet-b']);
+    $walletC  = Wallet::factory()->create(['address' => 'wallet-c']);
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => 0,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
+    $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
+
+    Transaction::factory()->multiPayment()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => $genesisTimestamp,
+
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => $walletB->address,
+                    'amount'      => 1 * 1e8,
+                ],
+            ],
+        ],
+    ]);
+
+    $standardTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 14:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 14:24:45');
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletC->address,
+        'timestamp'         => $standardTimestamp,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletC->address);
+
+    expect($cache->getNewestAddress())->toBe([
+        'address'   => $walletC->address,
+        'timestamp' => $standardTimestamp,
+        'value'     => '01 Jan 2021',
+    ]);
+});
+
+it('should not take an old wallet with its first multipayment as the newest', function () {
+    $this->travelTo('2021-01-01 11:24:44');
+
+    $cache     = new StatisticsCache();
+    $aggregate = new LatestWalletAggregate();
+
+    expect($cache->getNewestAddress())->toBeNull();
+
+    $walletA  = Wallet::factory()->create(['address' => 'wallet-a']);
+    $walletB  = Wallet::factory()->create(['address' => 'wallet-b']);
+    $walletC  = Wallet::factory()->create(['address' => 'wallet-c']);
+    $walletD  = Wallet::factory()->create(['address' => 'wallet-d']);
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => 0,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletA->address);
+
+    $genesisTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:24:45');
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletC->address,
+        'timestamp'         => $genesisTimestamp,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletC->address);
+
+    expect($cache->getNewestAddress())->toBe([
+        'address'   => $walletC->address,
+        'timestamp' => $genesisTimestamp,
+        'value'     => '01 Jan 2021',
+    ]);
+
+    $nextTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 13:25:44')->unix())->unix();
+    $this->travelTo('2021-01-01 13:25:45');
+
+    Transaction::factory()->transfer()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletB->address,
+        'timestamp'         => $nextTimestamp,
+    ]);
+
+    $result = $aggregate->aggregate();
+    expect($result)->not->toBeNull();
+    expect($result->address)->toBe($walletB->address);
+
+    expect($cache->getNewestAddress())->toBe([
+        'address'   => $walletB->address,
+        'timestamp' => $nextTimestamp,
+        'value'     => '01 Jan 2021',
+    ]);
+
+    $multipaymentTimestamp = Timestamp::fromUnix(Carbon::parse('2021-01-01 14:24:44')->unix())->unix();
+    $this->travelTo('2021-01-01 14:24:45');
+
+    Transaction::factory()->multiPayment()->create([
+        'sender_public_key' => $walletA->public_key,
+        'recipient_id'      => $walletA->address,
+        'timestamp'         => $multipaymentTimestamp,
+
+        'asset' => [
+            'payments' => [
+                [
+                    'recipientId' => $walletC->address,
+                    'amount'      => 1 * 1e8,
+                ],
+            ],
+        ],
+    ]);
+
+    $result = $aggregate->aggregate();
+    // expect($result)->toBeNull();
+
+    expect($cache->getNewestAddress())->toBe([
+        'address'   => $walletB->address,
+        'timestamp' => $nextTimestamp,
+        'value'     => '01 Jan 2021',
+    ]);
+});
