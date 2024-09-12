62bc9af7b8a5c5b2be7e699466b97af760b8887d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-09-12 23:38:42 +0100

    update resources

--- resources/views/components/tables/desktop/delegates/monitor.blade.php
+++ resources/views/components/tables/desktop/delegates/monitor.blade.php
@@ -1,126 +1,86 @@
 @props([
     'delegates',
     'round',
+    'overflowDelegates',
     'noResultsMessage' => null,
 ])
 
-<x-tables.encapsulated-table
-    id="delegate-monitor"
-    x-data="TableSorting('delegate-monitor', 'delegates.monitor', 'header-favorite', 'desc', 'header-order', 'asc')"
-    wire:key="{{ Helpers::generateId('delegate-monitor', $round) }}"
-    class="hidden w-full md:block delegate-monitor"
-    sticky
->
-    <thead>
-        <tr>
-            <x-tables.headers.desktop.text
-                width="20"
-                sorting-id="header-favorite"
-                hide-sorting
-            />
+<div>
+    <x-tables.encapsulated-table
+        id="delegate-monitor"
+        x-data="TableSorting('delegate-monitor', 'delegates.monitor', 'header-favorite', 'desc', 'header-order', 'asc')"
+        wire:key="{{ Helpers::generateId('delegate-monitor', $round) }}"
+        class="hidden w-full md:block delegate-monitor"
+        :with-bottom-border="count($overflowDelegates) === 0"
+    >
+        <thead>
+            <tr>
+                <x-tables.headers.desktop.text
+                    width="20"
+                    sorting-id="header-favorite"
+                    hide-sorting
+                />
 
-            <x-tables.headers.desktop.text
-                name="tables.delegate-monitor.order"
-                width="60"
-                sorting-id="header-order"
-                hide-sorting
-            />
+                <x-tables.headers.desktop.text
+                    name="tables.delegate-monitor.order"
+                    width="60"
+                    sorting-id="header-order"
+                    hide-sorting
+                />
 
-            <x-tables.headers.desktop.address
-                name="tables.delegate-monitor.delegate"
-                width="190"
-            />
+                <x-tables.headers.desktop.address
+                    name="tables.delegate-monitor.delegate"
+                    width="190"
+                />
 
-            <x-tables.headers.desktop.status
-                name="tables.delegate-monitor.status"
-                breakpoint="md-lg"
-                responsive
-            />
-
-            <x-tables.headers.desktop.status
-                name="tables.delegate-monitor.status_time_to_forge"
-                class="md-lg:hidden"
-                breakpoint="md"
-                responsive
-            />
-
-            <x-tables.headers.desktop.text
-                name="tables.delegate-monitor.time_to_forge"
-                class="whitespace-nowrap"
-                breakpoint="md-lg"
-                responsive
-            />
-
-            <x-tables.headers.desktop.number
-                name="tables.delegate-monitor.block_height"
-                class="whitespace-nowrap"
-            />
-        </tr>
-    </thead>
-
-    <tbody>
-        @foreach($delegates as $delegate)
-            <x-ark-tables.row
-                x-data="Delegate('{{ $delegate->publicKey() }}')"
-                wire:key="delegate-{{ $delegate->order() }}-{{ $delegate->wallet()->address() }}-{{ $delegate->roundNumber() }}-{{ microtime(true) }}"
-                ::class="{
-                    'delegate-monitor-favorite': isFavorite === true,
-                }"
-            >
-                <x-ark-tables.cell ::data-value="isFavorite ? 1 : 0">
-                    <x-delegates.favorite-toggle :model="$delegate" />
-                </x-ark-tables.cell>
-
-                <x-ark-tables.cell data-value="{{ $delegate->order() }}">
-                    <span class="text-sm font-semibold leading-4.25">
-                        {{ $delegate->order() }}
-                    </span>
-                </x-ark-tables.cell>
-
-                <x-ark-tables.cell>
-                    <div class="flex items-center space-x-2">
-                        <x-tables.rows.desktop.encapsulated.address
-                            :model="$delegate->wallet()"
-                            without-clipboard
-                            :delegate-name-class="Arr::toCssClasses([
-                                'md-lg:w-auto',
-                                'md:w-[200px]' => ! $delegate->keepsMissing(),
-                            ])"
-                        />
-
-                        <x-delegates.missed-warning :delegate="$delegate->wallet()" />
-                    </div>
-                </x-ark-tables.cell>
-
-                <x-ark-tables.cell
+                <x-tables.headers.desktop.status
+                    name="tables.delegate-monitor.status"
                     breakpoint="md-lg"
                     responsive
-                >
-                    <x-tables.rows.desktop.encapsulated.delegates.monitor.forging-status :model="$delegate" />
-                </x-ark-tables.cell>
+                    class="w-[374px]"
+                />
 
-                <x-ark-tables.cell
+                <x-tables.headers.desktop.status
+                    name="tables.delegate-monitor.status_time_to_forge"
                     class="md-lg:hidden"
                     breakpoint="md"
                     responsive
-                >
-                    <x-tables.rows.desktop.encapsulated.delegates.monitor.forging-status
-                        :model="$delegate"
-                        with-time
-                    />
-                </x-ark-tables.cell>
+                />
 
-                <x-ark-tables.cell
+                <x-tables.headers.desktop.text
+                    name="tables.delegate-monitor.time_to_forge"
+                    class="whitespace-nowrap"
                     breakpoint="md-lg"
                     responsive
-                >
-                    <x-tables.rows.desktop.encapsulated.delegates.monitor.time-to-forge :model="$delegate" />
-                </x-ark-tables.cell>
+                />
 
-                <x-ark-tables.cell class="text-right">
-                    <x-tables.rows.desktop.encapsulated.delegates.monitor.block-height :model="$delegate" />
-                </x-ark-tables.cell>
-            </x-ark-tables.row>
-        @endforeach
-    </tbody>
-</x-ark-tables.table>
+                <x-tables.headers.desktop.number
+                    name="tables.delegate-monitor.block_height"
+                    class="whitespace-nowrap"
+                />
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($delegates as $delegate)
+                <x-tables.rows.desktop.delegates.monitor-row :delegate="$delegate" />
+            @endforeach
+        </tbody>
+    </x-ark-tables.table>
+
+    @if (count($overflowDelegates) > 0)
+        <x-tables.encapsulated-table
+            x-data="{}"
+            wire:key="{{ Helpers::generateId('delegate-monitor', $round) }}-overflow-{{ microtime(true) }}"
+            class="hidden w-full md:block delegate-monitor"
+            :with-header="false"
+            :rounded="false"
+        >
+            <tbody>
+                @foreach($overflowDelegates as $delegate)
+                    <x-tables.rows.desktop.delegates.monitor-row :delegate="$delegate" />
+                @endforeach
+            </tbody>
+        </x-ark-tables.table>
+    @endif
+</div>
--- resources/views/livewire/delegates/monitor.blade.php
+++ resources/views/livewire/delegates/monitor.blade.php
@@ -1,44 +1,59 @@
 <div
-    id="delegate-monitor-list"
-    class="w-full"
     wire:init="monitorIsReady"
     @if (config('broadcasting.default') !== 'reverb')
         @if ($this->isReady && $this->hasDelegates)
-            wire:poll.1s="pollDelegates"
+            wire:poll.1s="pollData"
         @elseif ($this->isReady)
-            wire:poll.8s="pollDelegates"
+            wire:poll.8s="pollData"
         @endif
     @endif
 >
-    <x-skeletons.delegates.monitor>
-        <x-tables.desktop.delegates.monitor
-            :delegates="$delegates"
-            :round="$round"
+    <div class="px-6 pb-6 md:px-10 md:mx-auto md:max-w-7xl">
+        <x-delegates.monitor.data-boxes
+            :statistics="$statistics"
+            :height="$height"
         />
+    </div>
 
+    <x-general.mobile-divider />
+
+    <div class="px-6 pt-6 pb-8 md:px-10 md:pt-0 md:mx-auto md:max-w-7xl">
         <div
-            class="md:hidden"
-            x-data="{
-                hasFavorites: false,
-                toggleFavorites() {
-                    $nextTick(() => {
-                        this.hasFavorites = $root.querySelectorAll(`[data-favorite='1']`).length > 0;
-                    });
-                },
-            }"
-            x-init="toggleFavorites"
+            id="delegate-monitor-list"
+            class="w-full"
         >
-            <div
-                x-show="hasFavorites"
-                class="pb-3 font-semibold text-theme-secondary-700 dark:text-theme-dark-200"
-            >
-                @lang('tables.delegate-monitor.my_favorites')
-            </div>
+            <x-skeletons.delegates.monitor>
+                <x-tables.desktop.delegates.monitor
+                    :delegates="$delegates"
+                    :round="$round"
+                    :overflow-delegates="$overflowDelegates"
+                />
 
-            <x-tables.mobile.delegates.monitor
-                :delegates="$delegates"
-                :round="$round"
-            />
+                <div
+                    class="md:hidden"
+                    x-data="{
+                        hasFavorites: false,
+                        toggleFavorites() {
+                            $nextTick(() => {
+                                this.hasFavorites = $root.querySelectorAll(`[data-favorite='1']`).length > 0;
+                            });
+                        },
+                    }"
+                    x-init="toggleFavorites"
+                >
+                    <div
+                        x-show="hasFavorites"
+                        class="pb-3 font-semibold text-theme-secondary-700 dark:text-theme-dark-200"
+                    >
+                        @lang('tables.delegate-monitor.my_favorites')
+                    </div>
+
+                    <x-tables.mobile.delegates.monitor
+                        :delegates="$delegates"
+                        :round="$round"
+                    />
+                </div>
+            </x-skeletons.delegates.monitor>
         </div>
-    </x-skeletons.delegates.monitor>
+    </div>
 </div>
