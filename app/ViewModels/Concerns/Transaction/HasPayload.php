efb87c0d1f9fa1ba5eee9b26d0d69921c3d93b99
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-10-18 08:15:53 +0100

    refactor: default evm contract formatting (#975)

--- app/ViewModels/Concerns/Transaction/HasPayload.php
+++ app/ViewModels/Concerns/Transaction/HasPayload.php
@@ -40,6 +40,38 @@ trait HasPayload
 
     public function formattedPayload(): ?string
     {
-        return $this->rawPayload();
+        $payload = $this->rawPayload();
+        if ($payload === null) {
+            return null;
+        }
+
+        $methodId = substr($payload, 0, 8);
+
+        $functionName = null;
+        if (app('translator')->has('contracts.'.$methodId)) {
+            $functionName = trans('contracts.'.$methodId);
+        }
+
+        return trim(view('components.transaction.code-block.formatted-contract', [
+            'function'  => $functionName,
+            'methodId'  => $methodId,
+            'arguments' => $this->payloadArguments(),
+        ])->render());
+    }
+
+    private function payloadArguments(): ?array
+    {
+        $payload = $this->rawPayload();
+        if ($payload === null) {
+            return [];
+        }
+
+        $argumentsPayload   = substr($payload, 8);
+        $separatedArguments = trim(chunk_split($argumentsPayload, 64, ' '));
+        if (strlen($separatedArguments) === 0) {
+            return [];
+        }
+
+        return explode(' ', $separatedArguments);
     }
 }
