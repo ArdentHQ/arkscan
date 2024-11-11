2d15711ee069ae9b9d1ddf5754aba67e9ef249e5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-11-11 21:12:50 +0000

    remove scopes from aggregates

--- app/Services/Transactions/Aggregates/Type/VoteCombinationAggregate.php
+++ app/Services/Transactions/Aggregates/Type/VoteCombinationAggregate.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace App\Services\Transactions\Aggregates\Type;
 
-use App\Models\Scopes\VoteCombinationScope;
 use App\Models\Transaction;
 
 final class VoteCombinationAggregate
 {
     public function aggregate(): int
     {
-        return Transaction::withScope(VoteCombinationScope::class)->count();
+        return Transaction::count(); // TODO: add transaction type scope - https://app.clickup.com/t/86dur8fj6
     }
 }
