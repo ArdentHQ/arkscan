791aaa67bad16680d1bcf200187f695b6982f964
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-11-11 23:56:22 +0000

    refactor: cache fees command

--- app/Services/Transactions/Aggregates/Fees/Historical/AllAggregate.php
+++ app/Services/Transactions/Aggregates/Fees/Historical/AllAggregate.php
@@ -8,6 +8,7 @@ use App\Facades\Network;
 use App\Models\Transaction;
 use App\Services\Transactions\Aggregates\Concerns\HasPlaceholders;
 use App\Services\Transactions\Aggregates\Concerns\HasQueries;
+use ArkEcosystem\Crypto\Utils\UnitConverter;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
@@ -19,15 +20,16 @@ final class AllAggregate
     public function aggregate(): Collection
     {
         $select = [
-            'SUM(fee) as fee',
+            'SUM(gas_price * COALESCE(receipts.gas_used, 0)) as fee',
             sprintf("to_char(to_timestamp(%d+timestamp) AT TIME ZONE 'UTC', '%s') as formatted_date", Network::epoch()->timestamp, 'YYYY-MM'),
         ];
 
         return Transaction::query()
             ->select(DB::raw(implode(', ', $select)))
+            ->join('receipts', 'transactions.id', '=', 'receipts.id')
             ->orderBy('formatted_date')
             ->groupBy('formatted_date')
             ->pluck('fee', 'formatted_date')
-            ->mapWithKeys(fn ($fee, $month) => [$month => $fee->toFloat()]);
+            ->mapWithKeys(fn ($fee, $month) => [$month => UnitConverter::formatUnits($fee, 'gwei')]);
     }
 }
--- app/Services/Transactions/Aggregates/Fees/LastFeeAggregate.php
+++ app/Services/Transactions/Aggregates/Fees/LastFeeAggregate.php
@@ -6,12 +6,11 @@ namespace App\Services\Transactions\Aggregates\Fees;
 
 use App\Models\Transaction;
 use App\Services\Transactions\Aggregates\Concerns\HasQueries;
+use ArkEcosystem\Crypto\Utils\UnitConverter;
 
 final class LastFeeAggregate
 {
     use HasQueries;
-
-    private string $type;
 
     private int $limit = 20;
 
@@ -31,20 +30,19 @@ final class LastFeeAggregate
 
     public function aggregate(): array
     {
-        $scope = $this->getScopeByType($this->type);
-
+        // TODO: add transaction type scope - https://app.clickup.com/t/86dur8fj6
         $fees = Transaction::query()
-            ->select('fee')
-            ->withScope($scope)
+            ->selectRaw('gas_price * COALESCE(receipts.gas_used, 0) as fee')
+            ->join('receipts', 'transactions.id', '=', 'receipts.id')
             ->orderByDesc('timestamp')
             ->limit($this->limit)
             ->pluck('fee')
-            ->map(fn ($fee) => $fee->toFloat());
+            ->map(fn ($fee) => UnitConverter::formatUnits((string) $fee));
 
         return [
             'minimum' => $fees->min() ?? 0, // @phpstan-ignore-line
-            'average' => $fees->avg() ?? 0,
-            'maximum' => $fees->max() ?? 0, // @phpstan-ignore-line
+            'average' => $fees->max() ?? 0,
+            'maximum' => $fees->avg() ?? 0, // @phpstan-ignore-line
         ];
     }
 }
