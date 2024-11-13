34c302ef44c735d8b1b4c41474d712577b5583f5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-11-13 02:33:46 +0000

    payload signature enum

---
+++ app/Enums/PayloadSignature.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum PayloadSignature: string
+{
+    case TRANSFER = 'a9059cbb';
+
+    case VALIDATOR_REGISTRATION = '602a9eee';
+
+    case VOTE = '6dd7d8ea';
+
+    case UNVOTE = '3174b689';
+
+    case VALIDATOR_RESIGNATION = 'b85f5da2';
+}
