f7bcf143195192519c43323903b2566eee67628a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-11-20 14:45:36 +0000

    refactor: update resigned status on validators page

--- resources/views/components/tables/rows/desktop/encapsulated/validators/validator-status.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/validators/validator-status.blade.php
@@ -3,9 +3,9 @@
 <x-general.badge :attributes="$attributes->class('encapsulated-badge')">
     @if ($model->isActive())
         @lang('general.validators.forging-status.active')
-    @elseif ($model->isStandby())
-        @lang('general.validators.forging-status.standby')
-    @else
+    @elseif ($model->isResigned())
         @lang('general.validators.forging-status.resigned')
+    @else
+        @lang('general.validators.forging-status.standby')
     @endif
 </x-general.badge>
