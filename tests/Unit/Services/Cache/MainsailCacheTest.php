32a3b27676ab3ecf851af5582a4e78dc45090c8c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-11-20 00:20:13 +0000

    test

---
+++ tests/Unit/Services/Cache/MainsailCacheTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Services\Cache\MainsailCache;
+
+it('should get & set fees', function () {
+    $cache = new MainsailCache();
+
+    expect($cache->getFees())->toEqual([]);
+
+    $cache->setFees([
+        'min' => '5',
+        'max' => '5',
+        'avg' => '5',
+    ]);
+
+    expect($cache->getFees())->toEqual([
+        'min' => '5',
+        'max' => '5',
+        'avg' => '5',
+    ]);
+});
