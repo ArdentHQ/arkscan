0aeb9fc2a5b79ae086699b0b22e9a343e82183a6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-11-21 17:29:51 +0000

    refactor: update transaction id status styling

--- resources/views/components/tables/rows/desktop/encapsulated/transaction-id.blade.php
+++ resources/views/components/tables/rows/desktop/encapsulated/transaction-id.blade.php
@@ -6,12 +6,14 @@
 @php ($hasFailedStatus = $model->hasFailedStatus())
 
 <div class="flex flex-col md:space-y-1 xl:space-y-0">
-    <div @class(['flex space-x-2 items-center bg-theme-danger-50 py-0.5 px-1.5 rounded' => $hasFailedStatus])>
+    <div @class([
+        'flex space-x-2 items-center bg-theme-danger-50 dark:bg-transparent border border-transparent dark:border-[#AA6868] py-0.5 px-1.5 rounded' => $hasFailedStatus,
+    ])>
         <a
             href="{{ $model->url() }}"
             @class([
                 'mx-auto text-sm font-semibold whitespace-nowrap link leading-4.25',
-                '!text-theme-danger-700' => $hasFailedStatus,
+                '!text-theme-danger-700 dark:!text-[#F39B9B]' => $hasFailedStatus,
             ])
         >
             <x-truncate-middle>{{ $model->id() }}</x-truncate-middle>
@@ -19,9 +21,9 @@
 
         @if ($hasFailedStatus)
             <x-ark-icon
-                name="cross"
-                size="w-2.5 h-2.5"
-                class="text-theme-danger-700"
+                name="cross-small"
+                size="w-3 h-3"
+                class="text-theme-danger-700 dark:text-[#F39B9B]"
             />
         @endif
     </div>
