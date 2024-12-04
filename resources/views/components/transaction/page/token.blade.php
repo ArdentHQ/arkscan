736edab262b4ff0b0b70d134015465e0baf868e7
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-04 12:33:04 +0000

    remove value for tokens

--- resources/views/components/transaction/page/token.blade.php
+++ resources/views/components/transaction/page/token.blade.php
@@ -17,25 +17,4 @@
     >
         <x-transaction.amount :transaction="$transaction" />
     </x-transaction.page.section-detail.row>
-
-    @if (Network::canBeExchanged())
-        @if (ExplorerNumberFormatter::isFiat(Settings::currency()) && ExchangeRate::convertNumerical($transaction->amountWithFee(), $transaction->model()->timestamp) < 0.01)
-            <x-transaction.page.section-detail.row
-                :title="trans('pages.transaction.header.value')"
-                :value="'<'.ExplorerNumberFormatter::currency(0.01, Settings::currency())"
-                :transaction="$transaction"
-                :tooltip="$transaction->totalFiat(true)"
-            />
-        @else
-            <x-transaction.page.section-detail.row
-                :title="trans('pages.transaction.header.value')"
-                :transaction="$transaction"
-            >
-                <livewire:fiat-value
-                    :amount="$transaction->amountWithFee()"
-                    :timestamp="$transaction->model()->timestamp"
-                />
-            </x-transaction.page.section-detail.row>
-        @endif
-    @endif
 </x-general.page-section.container>
