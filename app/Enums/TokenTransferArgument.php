b83b40856fe360a6385c6c85688a353bcf5b7df5
alexbarnsley <alexbarnsley@users.noreply.github.com>
2024-12-04 13:42:51 +0000

    style: resolve style guide violations

--- app/Enums/TokenTransferArgument.php
+++ app/Enums/TokenTransferArgument.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-class TokenTransferArgument {
-    const RECIPIENT = 0;
+class TokenTransferArgument
+{
+    public const RECIPIENT = 0;
 
-    const AMOUNT = 1;
+    public const AMOUNT = 1;
 }
