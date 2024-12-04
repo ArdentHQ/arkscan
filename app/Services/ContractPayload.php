243cc211816151f1ecfa50af217ae3aa9b37eadd
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-04 03:23:43 +0000

    scrub

--- app/Services/ContractPayload.php
+++ app/Services/ContractPayload.php
@@ -27,17 +27,4 @@ final class ContractPayload
 
         return gmp_strval($value);
     }
-
-    // public static function decodeSignedInt(string $bytes, int $bits = 32, int $offset = 0): string
-    // {
-    //     $bytes = hex2bin($bytes);
-    //     $data  = substr($bytes, $offset, 32);
-    //     $hex   = bin2hex($data);
-    //     $value = gmp_import(hex2bin($hex), 1, GMP_MSW_FIRST | GMP_BIG_ENDIAN);
-    //     if (gmp_testbit($value, $bits - 1)) {
-    //         $value = gmp_sub($value, gmp_pow(2, $bits));
-    //     }
-
-    //     return gmp_strval($value);
-    // }
 }
