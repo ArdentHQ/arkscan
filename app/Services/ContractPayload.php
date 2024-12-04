8f7d302d022ddaa70b363b501699c31e478a25cf
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-04 03:31:53 +0000

    stan

--- app/Services/ContractPayload.php
+++ app/Services/ContractPayload.php
@@ -10,7 +10,11 @@ final class ContractPayload
 {
     public static function decodeAddress(string $bytes, int $offset = 0): string
     {
-        $bytes        = hex2bin($bytes);
+        $bytes = hex2bin($bytes);
+        if ($bytes === false) {
+            return '';
+        }
+
         $data         = substr($bytes, $offset, 32);
         $addressBytes = substr($data, 12, 20);
         $address      = Address::toChecksumAddress('0x'.bin2hex($addressBytes));
@@ -21,9 +25,12 @@ final class ContractPayload
     public static function decodeUnsignedInt(string $bytes, int $offset = 0): string
     {
         $bytes = hex2bin($bytes);
+        if ($bytes === false) {
+            return '';
+        }
+
         $data  = substr($bytes, $offset, 32);
-        $hex   = bin2hex($data);
-        $value = gmp_import(hex2bin($hex), 1, GMP_MSW_FIRST | GMP_BIG_ENDIAN);
+        $value = gmp_import($data, 1, GMP_MSW_FIRST | GMP_BIG_ENDIAN);
 
         return gmp_strval($value);
     }
