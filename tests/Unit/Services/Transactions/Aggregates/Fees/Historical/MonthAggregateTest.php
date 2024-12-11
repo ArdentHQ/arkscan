31013a6839897165d6553dca0a8abdc3bac3d2ec
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-11 09:50:53 +0000

    test: transaction historical fee aggregates (#1048)

--- tests/Unit/Services/Transactions/Aggregates/Fees/Historical/MonthAggregateTest.php
+++ tests/Unit/Services/Transactions/Aggregates/Fees/Historical/MonthAggregateTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\Receipt;
 use App\Models\Transaction;
 use App\Services\Transactions\Aggregates\Fees\Historical\MonthAggregate;
 use Carbon\Carbon;
@@ -12,14 +13,21 @@ it('should aggregate the fees for 30 days', function () {
     Carbon::setTestNow('2021-01-01 00:00:00');
 
     Transaction::factory(10)->create([
-        'fee'       => 1 * 1e18,
+        'gas_price' => 1,
         'timestamp' => Carbon::now()->subDays(30)->startOfDay()->getTimestampMs(),
     ]);
 
     Transaction::factory(10)->create([
-        'fee'       => 1 * 1e18,
+        'gas_price' => 1,
         'timestamp' => Carbon::now()->subMinutes(10)->getTimestampMs(),
     ]);
+
+    foreach (Transaction::all() as $transaction) {
+        Receipt::factory()->create([
+            'id'       => $transaction->id,
+            'gas_used' => 1e9,
+        ]);
+    }
 
     $result = (new MonthAggregate())->aggregate();
 
