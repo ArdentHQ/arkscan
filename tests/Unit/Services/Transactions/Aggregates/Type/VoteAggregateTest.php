82d2ea493f1be9ff72dbbefcd54d3fcbe5f32844
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-12 23:45:46 +0000

    test: transaction aggregates by type

--- tests/Unit/Services/Transactions/Aggregates/Type/VoteAggregateTest.php
+++ tests/Unit/Services/Transactions/Aggregates/Type/VoteAggregateTest.php
@@ -3,13 +3,16 @@
 declare(strict_types=1);
 
 use App\Models\Transaction;
+use App\Models\Wallet;
 use App\Services\Transactions\Aggregates\Type\VoteAggregate;
 
 it('should return count', function () {
     expect((new VoteAggregate())->aggregate())->toBe(0);
 
+    $wallet = Wallet::factory()->activeValidator()->create();
+
     Transaction::factory(10)
-        ->vote()
+        ->vote($wallet->address)
         ->create();
 
     expect((new VoteAggregate())->aggregate())->toBe(10);
