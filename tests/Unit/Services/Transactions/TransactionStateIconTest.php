2e6623dad4b0b549540d2a0c75dde69d3744e9d4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-16 12:28:41 +0000

    Update tests/Unit/Services/Transactions/TransactionStateIconTest.php

--- tests/Unit/Services/Transactions/TransactionStateIconTest.php
+++ tests/Unit/Services/Transactions/TransactionStateIconTest.php
@@ -11,9 +11,9 @@ it('should determine if the transaction is confirmed', function (int $transactio
     (new NetworkCache())->setHeight(fn () => $blockHeight);
 
     $transaction = Transaction::factory()->create([
-        'block_height'           => $transactionHeight,
-        'sender_public_key'      => Wallet::factory()->create(['address' => 'sender'])->public_key,
-        'recipient_address'      => Wallet::factory()->create(['address' => 'recipient'])->address,
+        'block_height'      => $transactionHeight,
+        'sender_public_key' => Wallet::factory()->create(['address' => 'sender'])->public_key,
+        'recipient_address' => Wallet::factory()->create(['address' => 'recipient'])->address,
     ]);
 
     expect((new TransactionStateIcon($transaction))->name())->toBe($icon);
