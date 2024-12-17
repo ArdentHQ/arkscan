44500fe3b9e9d9cda8ddc8d4d511ae36fb7e7b25
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-17 21:56:19 +0000

    Update TransactionDirectionIconTest.php

--- tests/Unit/Services/Transactions/TransactionDirectionIconTest.php
+++ tests/Unit/Services/Transactions/TransactionDirectionIconTest.php
@@ -9,8 +9,8 @@ use App\Services\Transactions\TransactionDirectionIcon;
 it('should determine if the transaction is sent', function () {
     $sender      = Wallet::factory()->create();
     $transaction = Transaction::factory()->create([
-        'sender_public_key'      => $sender->public_key,
-        'recipient_address'      => Wallet::factory()->create()->address,
+        'sender_public_key' => $sender->public_key,
+        'recipient_address' => Wallet::factory()->create()->address,
     ]);
 
     expect((new TransactionDirectionIcon($transaction))->name($sender->address))->toBe('sent');
@@ -18,8 +18,8 @@ it('should determine if the transaction is sent', function () {
 
 it('should determine if the transaction is received', function () {
     $transaction = Transaction::factory()->create([
-        'sender_public_key'      => Wallet::factory()->create()->public_key,
-        'recipient_address'      => $recipient = Wallet::factory()->create()->address,
+        'sender_public_key' => Wallet::factory()->create()->public_key,
+        'recipient_address' => $recipient = Wallet::factory()->create()->address,
     ]);
 
     expect((new TransactionDirectionIcon($transaction))->name($recipient))->toBe('received');
