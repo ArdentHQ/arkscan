ee2f877f279fd6df4052b60cc2dc0fd15a1550d2
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-12-18 09:43:27 -0600

    Update 2023_08_07_183334_add_missed_height_to_forging_stats_table.php

--- database/migrations/2023_08_07_183334_add_missed_height_to_forging_stats_table.php
+++ database/migrations/2023_08_07_183334_add_missed_height_to_forging_stats_table.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Models\ForgingStats;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
