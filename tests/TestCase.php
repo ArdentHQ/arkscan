24ec4808ec794f477abd5d43c46eb8f2a2036080
alfonsobries <alfonsobries@users.noreply.github.com>
2024-12-18 16:53:06 +0000

    style: resolve style guide violations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -62,7 +62,7 @@ abstract class TestCase extends BaseTestCase
         if (! RefreshDatabaseState::$migrated) {
             $this->artisan('migrate:fresh', [
                 '--database' => 'pgsql',
-                '--path' => 'database/migrations',
+                '--path'     => 'database/migrations',
             ]);
 
             $this->artisan('migrate:fresh', [
