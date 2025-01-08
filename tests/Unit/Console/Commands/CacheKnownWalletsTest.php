481b9c5d0714f92d0075ff7e1f3cfcc8e78f86d2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-01-08 15:27:47 +0000

    test

--- tests/Unit/Console/Commands/CacheKnownWalletsTest.php
+++ tests/Unit/Console/Commands/CacheKnownWalletsTest.php
@@ -10,28 +10,6 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
 
-it('should cache the known wallet name if defined', function () {
-    $knownWalletsUrl = 'https://knownwallets.com/known-wallets.json';
-
-    Config::set('arkscan.networks.development.knownWallets', $knownWalletsUrl);
-
-    Http::fake(Http::response([
-        [
-            'type'    => 'team',
-            'name'    => 'Hot Wallet',
-            'address' => '0xC5a19e23E99bdFb7aae4301A009763AdC01c1b5B',
-        ],
-    ], 200));
-
-    $knownWallet = Wallet::factory()->create([
-        'address' => '0xC5a19e23E99bdFb7aae4301A009763AdC01c1b5B',
-    ]);
-
-    (new CacheKnownWallets())->handle();
-
-    expect(Cache::tags('wallet')->get(md5("name_by_address/$knownWallet->address")))->toBe('Hot Wallet');
-});
-
 it('should cache the username for the public key', function () {
     $wallet = Wallet::factory()->create([
         'attributes->username' => 'testuser',
