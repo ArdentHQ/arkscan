96be90856aa8135dcd560cc75146fe91b3b1054a
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-01-08 03:35:40 -0600

    fix: transaction view model voted tests (#1115)

--- app/ViewModels/Concerns/Transaction/InteractsWithVotes.php
+++ app/ViewModels/Concerns/Transaction/InteractsWithVotes.php
@@ -17,16 +17,24 @@ trait InteractsWithVotes
         }
 
         $payload = $this->rawPayload();
+
+        // @codeCoverageIgnoreStart
+        // It is possible to reach this point if `rawPayload` returns null, which
+        // is theoretically only possible if the `stream_get_contents` function
+        // called in `app/ViewModels/Concerns/Transaction/HasPayload.php@rawPayload`
+        // returns false, something I have not been able to replicate in tests.
         if ($payload === null) {
             return null;
         }
-
+        // @codeCoverageIgnoreEnd
         $method = (new AbiDecoder())->decodeFunctionData($payload);
 
-        if (count($method['args']) === 0) {
-            return null;
-        }
-
+        // We don't need to check for the presence of the `args` key or the length
+        // of the array, as checking if the method is a vote ensures
+        // that it will attempt to extract an address from the input.
+        // We used to check the length of the arguments, but in the hypothetical case
+        // where it cannot extract the address, it would throw an exception beforehand,
+        // making the validation unnecessary.
         /** @var string $address */
         $address = $method['args'][0];
 
