037156299d3425d5ac377cb094453014ba37b098
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-01-13 18:32:02 +0000

    tidy

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Services\Cache\PriceCache;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
