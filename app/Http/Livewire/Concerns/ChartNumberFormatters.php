e65927db8a5028865eaa8f4f3600d847ffca68cc
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-01-20 14:25:42 +0000

    style: resolve style guide violations

--- app/Http/Livewire/Concerns/ChartNumberFormatters.php
+++ app/Http/Livewire/Concerns/ChartNumberFormatters.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Livewire\Concerns;
 
 use App\Facades\Network;
-use App\Services\BigNumber;
 use App\Services\NumberFormatter;
 
 trait ChartNumberFormatters
