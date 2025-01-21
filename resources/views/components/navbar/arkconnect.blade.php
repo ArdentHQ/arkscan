f42b0649661ed8c1b591249b269db087c882adfc
alfonsobries <17262776+alfonsobries@users.noreply.github.com>
2025-01-21 16:23:30 +0000

    style: resolve style guide violations

--- resources/views/components/navbar/arkconnect.blade.php
+++ resources/views/components/navbar/arkconnect.blade.php
@@ -7,7 +7,7 @@
     <div x-show="hasExtension && isSupported">
         <button
             x-show="! isConnected"
-            class="py-1.5 px-4 whitespace-nowrap button-secondary w-full"
+            class="py-1.5 px-4 w-full whitespace-nowrap button-secondary"
             @click="connect"
             :disabled="! hasExtension"
             disabled
