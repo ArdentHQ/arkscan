7f1427bf48559b69872c77b1645621a4673f42f6
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-01-28 15:01:32 +0000

    style: resolve style guide violations

--- tests/Unit/Console/Commands/CacheTransactionsTokenNameTest.php
+++ tests/Unit/Console/Commands/CacheTransactionsTokenNameTest.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
-use App\Console\Commands\CacheNetworkAggregates;
 use App\Console\Commands\CacheTransactionsTokenName;
 use App\Models\Receipt;
 use App\Models\Transaction;
-use App\Services\Cache\NetworkCache;
 use App\Services\Cache\TokenTransferCache;
 use Illuminate\Support\Facades\Http;
-
 use function Tests\faker;
 
 const TOKEN_RESULT = '0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000064441524b32300000000000000000000000000000000000000000000000000000';
@@ -24,7 +21,7 @@ it('should execute the command', function () {
     $contractAddress = faker()->wallet['address'];
 
     Receipt::factory()->create([
-        'id' => $transaction->id,
+        'id'                        => $transaction->id,
         'deployed_contract_address' => $contractAddress,
     ]);
 
@@ -43,7 +40,7 @@ it('should not re-cache token name', function () {
     $transaction = Transaction::factory()->contractDeployment()->create();
 
     Receipt::factory()->create([
-        'id' => $transaction->id,
+        'id'                        => $transaction->id,
         'deployed_contract_address' => $contractAddress,
     ]);
 
