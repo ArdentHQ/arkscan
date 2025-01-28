85b6a58f353abcfb0a45c91697f76aca36569113
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-01-28 17:00:40 +0000

    test

---
+++ tests/Unit/Services/Cache/TokenTransferCacheTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Services\Cache\TokenTransferCache;
+
+beforeEach(fn () => $this->subject = new TokenTransferCache());
+
+it('should get and set token name', function () {
+    expect($this->subject->getTokenName('address'))->toBeNull();
+
+    $this->subject->setTokenName('address', 'test-name');
+
+    expect($this->subject->getTokenName('address'))->toBe('test-name');
+});
+
+it('should determine if cache has token name', function () {
+    expect($this->subject->hasTokenName('address'))->toBeFalse();
+
+    $this->subject->setTokenName('address', 'test-name');
+
+    expect($this->subject->hasTokenName('address'))->toBeTrue();
+});
