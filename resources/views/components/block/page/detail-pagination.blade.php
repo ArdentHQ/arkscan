58f0e7b455802e353a586781f9b83a8eaf0e684b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-06 14:10:00 +0000

    swap previous and next functionality

--- resources/views/components/block/page/detail-pagination.blade.php
+++ resources/views/components/block/page/detail-pagination.blade.php
@@ -8,14 +8,14 @@
 <div {{ $attributes->class('px-3 sm:px-6 md:px-10 md:mx-auto md:max-w-7xl group last:mb-8 dark:text-theme-dark-200 mt-2') }}>
     <div class="flex space-x-2 sm:justify-end">
         <a
-            @if ($nextUrl !== null)
-                href="{{ $nextUrl }}"
+            @if ($previousUrl !== null)
+                href="{{ $previousUrl }}"
             @endif
 
             @class([
                 'py-1.5 px-4 font-semibold flex-1 sm:flex-none',
-                'button-secondary' => $nextUrl !== null,
-                'button-generic bg-theme-secondary-200 text-theme-secondary-500 dark:bg-theme-secondary-800 dark:text-theme-secondary-700 select-none' => $nextUrl === null,
+                'button-secondary' => $previousUrl !== null,
+                'button-generic bg-theme-secondary-200 text-theme-secondary-500 dark:bg-theme-secondary-800 dark:text-theme-secondary-700 select-none' => $previousUrl === null,
             ])
         >
             <div class="flex justify-center items-center space-x-2">
@@ -29,14 +29,14 @@
         </a>
 
         <a
-            @if ($previousUrl !== null)
-                href="{{ $previousUrl }}"
+            @if ($nextUrl !== null)
+                href="{{ $nextUrl }}"
             @endif
 
             @class([
                 'py-1.5 px-4 font-semibold flex-1 sm:flex-none',
-                'button-secondary' => $previousUrl !== null,
-                'button-generic bg-theme-secondary-200 text-theme-secondary-500 dark:bg-theme-secondary-800 dark:text-theme-secondary-700 select-none' => $previousUrl === null,
+                'button-secondary' => $nextUrl !== null,
+                'button-generic bg-theme-secondary-200 text-theme-secondary-500 dark:bg-theme-secondary-800 dark:text-theme-secondary-700 select-none' => $nextUrl === null,
             ])
         >
             <div class="flex justify-center items-center space-x-2">
