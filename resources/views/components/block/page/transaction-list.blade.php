bd442d6cafd10ca992eafcc553bc09254dc25b2f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-06 14:23:55 +0000

    feat: block detail pagination (#1175)

--- resources/views/components/block/page/transaction-list.blade.php
+++ resources/views/components/block/page/transaction-list.blade.php
@@ -3,6 +3,7 @@
 <x-general.page-section.container
     :title="trans('pages.block.transactions')"
     wrapper-class="flex flex-col flex-1 whitespace-nowrap"
+    no-bottom-padding
     no-border
 >
     <livewire:block-transactions-table :block="$block" />
