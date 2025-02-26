7dbfbcab116ca30bb30cfc119e875fe97d848c72
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-26 03:38:19 +0000

    test

--- tests/Feature/Http/Livewire/Stats/GasTrackerTest.php
+++ tests/Feature/Http/Livewire/Stats/GasTrackerTest.php
@@ -3,15 +3,16 @@
 declare(strict_types=1);
 
 use App\Http\Livewire\Stats\GasTracker;
+use App\Services\BigNumber;
 use App\Services\Cache\MainsailCache;
 use Livewire\Livewire;
 
 it('should render', function () {
     Livewire::test(GasTracker::class)
         ->assertViewHas([
-            'lowFee'     => ['amount' => 0.0, 'duration' => 8],
-            'averageFee' => ['amount' => 0.0, 'duration' => 8],
-            'highFee'    => ['amount' => 0.0, 'duration' => 8],
+            'lowFee'     => ['amount' => BigNumber::zero(), 'duration' => 8],
+            'averageFee' => ['amount' => BigNumber::zero(), 'duration' => 8],
+            'highFee'    => ['amount' => BigNumber::zero(), 'duration' => 8],
         ]);
 });
 
@@ -24,28 +25,22 @@ it('should render with cached data', function () {
 
     Livewire::test(GasTracker::class)
         ->assertViewHas([
-            'lowFee'     => ['amount' => 1.5, 'duration' => 8],
-            'averageFee' => ['amount' => 2.5, 'duration' => 8],
-            'highFee'    => ['amount' => 3.5, 'duration' => 8],
-        ]);
-});
-
-it('should show gas details', function ($title, $amount) {
-    (new MainsailCache())->setFees([
-        'min' => '1500000000',
-        'avg' => '2500000000',
-        'max' => '3500000000',
-    ]);
-
-    Livewire::test(GasTracker::class)
+            'lowFee'     => ['amount' => BigNumber::new(1.5), 'duration' => 8],
+            'averageFee' => ['amount' => BigNumber::new(2.5), 'duration' => 8],
+            'highFee'    => ['amount' => BigNumber::new(3.5), 'duration' => 8],
+        ])
         ->assertSeeInOrder([
-            $title,
+            'Low',
             '~ 8 secs',
-            $amount,
+            '1.5',
+            'Gwei',
+            'Average',
+            '~ 8 secs',
+            '2.5',
+            'Gwei',
+            'High',
+            '~ 8 secs',
+            '3.5',
             'Gwei',
         ]);
-})->with([
-    'low' => ['Low', '1.5'],
-    'avg' => ['Average', '2.5'],
-    'max' => ['High', '3.5'],
-]);
+});
