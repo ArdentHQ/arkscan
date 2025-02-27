d488085578ff0770ccd477e929ea53db9675a73a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-27 18:01:01 +0000

    test

--- tests/Unit/Services/Blockchain/CustomNetworkTest.php
+++ tests/Unit/Services/Blockchain/CustomNetworkTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 use App\Services\Blockchain\CustomNetwork;
 
-it('should have an address prefix and epoch', function () {
+it('should have a wif and epoch', function () {
     $subject = new CustomNetwork(['base58Prefix' => 23, 'epoch' => '2017-03-21T13:00:00.000Z']);
 
-    expect($subject->pubKeyHash())->toBe(17);
+    expect($subject->getAddressByte())->toBe('17');
     expect($subject->epoch())->toBe('2017-03-21T13:00:00.000Z');
 });
