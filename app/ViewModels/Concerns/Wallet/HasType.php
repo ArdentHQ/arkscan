e2da475246a7be3ba8991d8598fb26a62c356b9c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-05-19 14:04:30 +0100

    refactor: wallet second signature icon

--- app/ViewModels/Concerns/Wallet/HasType.php
+++ app/ViewModels/Concerns/Wallet/HasType.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace App\ViewModels\Concerns\Wallet;
 
 use App\Facades\Network;
+use Illuminate\Support\Arr;
 
 trait HasType
 {
+    public function hasSecondSignature(): bool
+    {
+        return Arr::has($this->wallet->attributes, 'secondPublicKey');
+    }
+
     public function isKnown(): bool
     {
         return ! is_null($this->findWalletByKnown());
