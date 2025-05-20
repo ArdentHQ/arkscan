3c47d90a77d1aaebc6acfa976d8804f875a2c95d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-05-20 13:56:03 +0100

    refactor: improvements to multipayment total amount

--- app/Models/Scopes/MultiPaymentTotalAmountScope.php
+++ app/Models/Scopes/MultiPaymentTotalAmountScope.php
@@ -20,12 +20,43 @@ final class MultiPaymentTotalAmountScope implements Scope
         $builder->joinSubLateral(function ($query) {
             $query->select('recipient_amount', 'is_multipayment')
                 ->from(function ($query) {
-                    $query->selectRaw('CAST(encode(SUBSTRING(transactions.data, 69, 32), \'hex\') AS int) as tx_count')
-                        ->selectRaw('CASE WHEN transactions.to = ? AND encode(SUBSTRING(data FROM 1 FOR 4), \'hex\') = ? THEN TRUE ELSE FALSE END as is_multipayment', [Network::knownContract('multipayment'), ContractMethod::multiPayment()]);
+
+                    $query->selectRaw('(\'0x\' || encode(substring(transactions.data, 69, 32), \'hex\'))::numeric::int as tx_count')
+                        ->selectRaw(
+                            'CASE WHEN transactions.to = ? AND encode(SUBSTRING(data FROM 1 FOR 4), \'hex\') = ? THEN
+                                TRUE
+                            ELSE
+                                FALSE
+                            END as is_multipayment',
+                            [Network::knownContract('multipayment'), ContractMethod::multiPayment()]
+                        )
+                        ->from('receipts')
+                        ->where('receipts.transaction_hash', '=', DB::raw('transactions.hash'))
+                        ->where('receipts.status', 1)
+                        ;
+
                 }, 'd')
                 ->joinSubLateral(function ($query) {
-                    $query->selectRaw('SUM(CASE WHEN is_multipayment THEN (\'0x\' || encode(substring(transactions.data, 69 + ((tx_count + 1) * 32) + (n * 32), 32), \'hex\'))::float::numeric ELSE 0 END) as recipient_amount')
-                        ->from(DB::raw('generate_series(1, CASE WHEN is_multipayment THEN CAST(encode(SUBSTRING(transactions.data, 69 + ((tx_count + 1) * 32), 32), \'hex\') AS int) ELSE 0 END) n'));
+                    $query->selectRaw(
+                        'SUM(
+                            CASE WHEN is_multipayment THEN
+                                (\'0x\' || encode(substring(transactions.data, 69 + ((tx_count + 1) * 32) + (n * 32), 32), \'hex\'))::float::numeric
+                            ELSE
+                                0
+                            END
+                        ) as recipient_amount'
+                    )
+                    ->from(DB::raw(
+                        'generate_series(
+                            1,
+                            CASE WHEN is_multipayment THEN
+                                (\'0x\' || encode(substring(transactions.data, 69 + ((tx_count + 1) * 32), 32), \'hex\'))::float::numeric::int
+                            ELSE
+                                0
+                            END
+                        ) n'
+                    ));
+
                 }, 'recipient_amounts_nested', 'is_multipayment', '=', DB::raw('true'), 'left outer');
         }, 'recipient_amounts', 'is_multipayment', '=', DB::raw('true'), 'left outer');
     }
