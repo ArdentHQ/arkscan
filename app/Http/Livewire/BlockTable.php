fb59c61cd758ba7fc87226da03cb8b90e8298f63
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-06-18 00:49:33 +0100

    refactor: block pagination handle snapshot

--- app/Http/Livewire/BlockTable.php
+++ app/Http/Livewire/BlockTable.php
@@ -52,16 +52,26 @@ final class BlockTable extends Component
         /** @var Block $lastBlock */
         $lastBlock = Block::withScope(OrderByTimestampScope::class)->first();
 
+        /** @var Block $firstBlock */
+        $firstBlock = Block::withScope(OrderByTimestampScope::class, 'asc')->first();
+
         $lastBlockHeight = $lastBlock->number->toNumber();
-        $heightTo        = $lastBlockHeight - ($this->perPage * ($this->getPage() - 1));
-        $heightFrom      = $heightTo - $this->perPage;
+        $blockCount = $lastBlockHeight;
+
+        $firstBlockHeight = $firstBlock->number->toNumber();
+        if ($firstBlockHeight > 1) {
+            $blockCount -= $firstBlockHeight - 1; // Adjust for the first block if it's not the genesis block
+        }
+
+        $heightTo   = $lastBlockHeight - ($this->perPage * ($this->getPage() - 1));
+        $heightFrom = $heightTo - $this->perPage;
 
         $blocks = Block::withScope(OrderByTimestampScope::class)
             ->where('number', '<=', $heightTo)
             ->where('number', '>', $heightFrom)
             ->get();
 
-        return new LengthAwarePaginator($blocks, $lastBlockHeight, $this->perPage, $this->getPage(), [
+        return new LengthAwarePaginator($blocks, $blockCount, $this->perPage, $this->getPage(), [
             'path'     => route('blocks'),
             'pageName' => 'page',
         ]);
--- app/Models/Scopes/OrderByTimestampScope.php
+++ app/Models/Scopes/OrderByTimestampScope.php
@@ -10,8 +10,13 @@ use Illuminate\Database\Eloquent\Scope;
 
 final class OrderByTimestampScope implements Scope
 {
+    public function __construct(private string $direction = 'desc')
+    {
+        //
+    }
+
     public function apply(Builder $builder, Model $model)
     {
-        $builder->orderBy('timestamp', 'desc');
+        $builder->orderBy('timestamp', $this->direction);
     }
 }
