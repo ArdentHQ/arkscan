6cb7a40373b511ffb6c262d34e5de11450d49a1b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-07-01 17:32:16 +0100

    refactor: voted for improvement

--- app/ViewModels/Concerns/Transaction/InteractsWithVotes.php
+++ app/ViewModels/Concerns/Transaction/InteractsWithVotes.php
@@ -4,44 +4,25 @@ declare(strict_types=1);
 
 namespace App\ViewModels\Concerns\Transaction;
 
-use App\Facades\Wallets;
 use App\ViewModels\WalletViewModel;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 
 trait InteractsWithVotes
 {
     public function voted(): ?WalletViewModel
     {
-        if (! $this->isVote()) {
+        if ($this->transaction->votedFor === null) {
             return null;
         }
 
-        /** @var array<int, string> */
-        $votes = Arr::get($this->transaction->asset ?? [], 'votes');
-
-        /** @var string */
-        $publicKey = collect($votes)
-            ->filter(fn ($vote) => Str::startsWith($vote, '+'))
-            ->first();
-
-        return new WalletViewModel(Wallets::findByPublicKey(substr($publicKey, 1)));
+        return new WalletViewModel($this->transaction->votedFor);
     }
 
     public function unvoted(): ?WalletViewModel
     {
-        if (! $this->isUnvote()) {
+        if ($this->transaction->unvotedFor === null) {
             return null;
         }
 
-        /** @var array<int, string> */
-        $votes = Arr::get($this->transaction->asset ?? [], 'votes');
-
-        /** @var string */
-        $publicKey = collect($votes)
-            ->filter(fn ($vote) => Str::startsWith($vote, '-'))
-            ->first();
-
-        return new WalletViewModel(Wallets::findByPublicKey(substr($publicKey, 1)));
+        return new WalletViewModel($this->transaction->unvotedFor);
     }
 }
