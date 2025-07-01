03275e023905ff1895fd24c6671bba3c56bc415d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-07-01 17:32:16 +0100

    refactor: voted for improvement

--- app/Http/Livewire/WalletTransactionTable.php
+++ app/Http/Livewire/WalletTransactionTable.php
@@ -141,6 +141,7 @@ final class WalletTransactionTable extends TabbedTableComponent
     {
         return Transaction::query()
             ->withTypeFilter($this->filter)
+            ->with('votedFor', 'unvotedFor')
             ->where(function ($query) {
                 $query->where(fn ($query) => $query->when($this->filter['outgoing'], fn ($query) => $query->where('sender_public_key', $this->publicKey)))
                     ->orWhere(fn ($query) => $query->when($this->filter['incoming'], fn ($query) => $query->where('recipient_id', $this->address)))
