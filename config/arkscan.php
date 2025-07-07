539240a8c34f03a45af25c05397ac707623e816f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-07-07 13:33:54 +0100

    revert per page value

--- config/arkscan.php
+++ config/arkscan.php
@@ -83,7 +83,7 @@ return [
     ],
 
     'pagination' => [
-        'per_page' => env('ARKSCAN_PAGINATION_PER_PAGE', 6),
+        'per_page' => env('ARKSCAN_PAGINATION_PER_PAGE', 25),
     ],
 
     'statistics'                        => [
