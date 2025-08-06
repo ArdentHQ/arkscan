f5d429a6c9d28e60f37e99601729e904333eb04e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-08-06 16:49:46 +0100

    chore: remove largest transaction todo

--- app/Services/Transactions/Aggregates/LargestTransactionAggregate.php
+++ app/Services/Transactions/Aggregates/LargestTransactionAggregate.php
@@ -10,7 +10,6 @@ final class LargestTransactionAggregate
 {
     public function aggregate(): ?Transaction
     {
-        // TODO: add transaction type for transfer - https://app.clickup.com/t/86dvxzh7f
         return Transaction::orderBy('value', 'desc')->limit(1)->first();
     }
 }
