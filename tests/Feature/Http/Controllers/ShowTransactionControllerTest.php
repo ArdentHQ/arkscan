03911d1dd14cce4b3dc7ef165e703514f2a892fe
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-08-26 11:52:30 +0100

    test

--- tests/Feature/Http/Controllers/ShowTransactionControllerTest.php
+++ tests/Feature/Http/Controllers/ShowTransactionControllerTest.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use App\Models\MultiPayment;
 use App\Models\Transaction;
 use App\Models\Wallet;
+use App\Services\BigNumber;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 it('should render the page without any errors', function ($type, $args) {
     $this->withoutExceptionHandling();
@@ -129,5 +132,83 @@ it('should show 0 if no corresponding validator registration', function () {
             trans('pages.transaction.header.unlocked_amount'),
             ' 0 DARK',
             trans('pages.transaction.legacy_registration_tooltip'),
+        ]);
+});
+
+it('should render the page for basic transaction types without any errors', function ($transactionType) {
+    $transaction = Transaction::factory()
+        ->{$transactionType}()
+        ->withReceipt()
+        ->create();
+
+    $this
+        ->get(route('transaction', $transaction->hash))
+        ->assertOk()
+        ->assertSee($transaction->hash);
+})->with([
+    'transfer',
+    'unvote',
+    'validatorRegistration',
+    'validatorUpdate',
+    'validatorResignation',
+    'usernameRegistration',
+    'usernameResignation',
+    'contractDeployment',
+]);
+
+it('should render the page for votes without any errors', function () {
+    $validator = Wallet::factory()->create();
+
+    $transaction = Transaction::factory()
+        ->vote($validator->address)
+        ->withReceipt()
+        ->create();
+
+    $this
+        ->get(route('transaction', $transaction->hash))
+        ->assertOk()
+        ->assertSee($transaction->hash)
+        ->assertSee($validator->address);
+});
+
+it('should render the page for multipayments without any errors', function () {
+    $recipient1 = Wallet::factory()->create();
+    $recipient2 = Wallet::factory()->create();
+
+    $transaction = Transaction::factory()
+        ->multiPayment([
+            $recipient1->address,
+            $recipient2->address,
+        ], [
+            BigNumber::new(123 * 1e18),
+            BigNumber::new(456 * 1e18),
+        ])
+        ->withReceipt()
+        ->create();
+
+    MultiPayment::factory()
+        ->count(2)
+        ->state(new Sequence(
+            [
+                'to'     => $recipient1->address,
+                'amount' => BigNumber::new(123 * 1e18),
+            ],
+            [
+                'to'     => $recipient2->address,
+                'amount' => BigNumber::new(456 * 1e18),
+            ],
+        ))
+        ->create([
+            'from' => $transaction->from,
+            'hash' => $transaction->hash,
+        ]);
+
+    $this
+        ->get(route('transaction', $transaction->hash))
+        ->assertOk()
+        ->assertSee($transaction->hash)
+        ->assertSeeInOrder([
+            '123.00 DARK',
+            '456.00 DARK',
         ]);
 });
