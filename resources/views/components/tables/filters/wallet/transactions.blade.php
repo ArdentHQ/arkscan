ef1e4c25e824537e22b5994c36ee96be94f4c38e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-09-05 10:58:54 +0100

    refactor: update dropdown styling (#1266)

--- resources/views/components/tables/filters/wallet/transactions.blade.php
+++ resources/views/components/tables/filters/wallet/transactions.blade.php
@@ -7,7 +7,6 @@
         <x-tables.filters.includes.checkbox
             name="select-all"
             :label="trans('tables.filters.transactions.select_all')"
-            class="pb-4 mb-4 border-b md:pb-2 md:mb-1 border-theme-secondary-300 dark:border-theme-dark-800"
             model="selectAllFilters.transactions"
         />
 
