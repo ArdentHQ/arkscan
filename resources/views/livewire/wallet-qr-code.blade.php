75c4fe7ab19ac1efe243d584a71dac9e8c0b79a9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-09-09 11:27:05 +0100

    scrub

--- resources/views/livewire/wallet-qr-code.blade.php
+++ resources/views/livewire/wallet-qr-code.blade.php
@@ -4,10 +4,6 @@
             @lang('pages.wallet.qrcode.title')
         </div>
 
-        {{-- close-button-class="absolute top-0 right-0 p-0 mt-4 mr-6 w-6 h-6 bg-transparent rounded-none sm:rounded dark:bg-transparent dark:shadow-none button button-secondary text-theme-secondary-700 dim:bg-transparent dim:shadow-none sm:mt-[0.875rem] dark:text-theme-dark-200 hover:dark:text-theme-dark-50 hover:dark:bg-theme-dark-blue-600"
-        title-class="text-lg font-semibold text-left mb-[0.875rem] dark:text-theme-dark-50"
-        padding-class="py-4 px-6 sm:pb-4 sm:pt-[0.875rem]"
-        buttons-style="flex flex-col-reverse sm:flex-row sm:justify-end !mt-4 sm:!mt-6 sm:space-x-3 border-t border-theme-secondary-300 dark:border-theme-dark-700 px-6 -mx-6 pt-4" --}}
         <div>
             <button
                 type="button"
