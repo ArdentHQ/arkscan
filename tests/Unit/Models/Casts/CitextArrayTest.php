d92a05c80835b9233a710c9bcb1966dc943415c4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-09-10 21:57:38 +0100

    test

--- tests/Unit/Models/Casts/CitextArrayTest.php
+++ tests/Unit/Models/Casts/CitextArrayTest.php
@@ -20,3 +20,19 @@ it('should not convert to array if already array', function () {
 
     expect($value)->toEqual(['123', '456']);
 });
+
+it('should convert to string from array', function () {
+    $transaction = Transaction::factory()->create();
+
+    $value = (new CitextArray())->set($transaction, 'multi_payment_recipients', ['123', '456'], []);
+
+    expect($value)->toEqual('{123,456}');
+});
+
+it('should not convert to string if already string', function () {
+    $transaction = Transaction::factory()->create();
+
+    $value = (new CitextArray())->set($transaction, 'multi_payment_recipients', '{123,456}', []);
+
+    expect($value)->toEqual('{123,456}');
+});
