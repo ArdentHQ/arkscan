59369876812b0087a76b18dbec2fc1731439e5fe
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-09-24 13:17:55 +0100

    refactor: setup dusk tests & fix missed warning (#1280)

---
+++ app/Testing/DuskServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Testing;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\Browser;
+use PHPUnit\Framework\Assert as PHPUnit;
+
+class DuskServiceProvider extends ServiceProvider
+{
+    /**
+     * Register Dusk's browser macros.
+     */
+    public function boot(): void
+    {
+        Browser::macro('assertEquals', function ($selector, $text, $ignoreCase = false) {
+            $element = $this->resolver->findOrFail($selector);
+
+            PHPUnit::assertEquals(
+                $ignoreCase ? strtolower($element->getText()) : $element->getText(),
+                $ignoreCase ? strtolower($text) : $text,
+            );
+
+            return $this;
+        });
+    }
+}
--- resources/inertia/Components/Validator/Monitor/MissedWarning.tsx
+++ resources/inertia/Components/Validator/Monitor/MissedWarning.tsx
@@ -6,20 +6,22 @@ import { useTranslation } from "react-i18next";
 export default function MissedWarning({ validator }: { validator: IValidator}) {
     const { t } = useTranslation();
 
-    if (! validator.wallet.keepsMissing) {
+    if (validator.wallet.keepsMissing === false) {
         return null;
     }
 
     return (
-        <Tippy content={
-            t('pages.validator-monitor.missed_blocks_tooltip', {
-                blocks: validator.wallet.blocksSinceLastForged,
-                time: validator.wallet.blocksSinceLastForged,
-            })
-        }>
-            <div className="text-theme-warning-900">
-                <AlertTriangleIcon className="w-4 h-4" />
-            </div>
-        </Tippy>
+        <div data-testid={`missed-warning-${validator.wallet.address}`}>
+            <Tippy content={
+                t('pages.validator-monitor.missed_blocks_tooltip', {
+                    blocks: validator.wallet.blocksSinceLastForged,
+                    time: validator.wallet.durationSinceLastForged,
+                })
+            }>
+                <div className="text-theme-warning-900">
+                    <AlertTriangleIcon className="w-4 h-4" />
+                </div>
+            </Tippy>
+        </div>
     )
 }
