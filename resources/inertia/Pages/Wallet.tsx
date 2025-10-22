05843c3b74ed255fe0d2227ea77ce23a8e5baa8f
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-10-22 11:08:39 -0600

    wip

--- resources/inertia/Pages/Wallet.tsx
+++ resources/inertia/Pages/Wallet.tsx
@@ -1,41 +1,46 @@
 import { Head } from "@inertiajs/react";
-import { INetwork, Currencies, ISettings, IConfigProductivity, IConfigArkConnect } from '@/types';
-import { IWallet } from '@/types/generated';
+import { INetwork, Currencies, ISettings, IConfigProductivity, IConfigArkConnect } from "@/types";
+import { IWallet } from "@/types/generated";
 import { usePageMetadata } from "@/Components/General/Metadata";
 import ConfigProvider from "@/Providers/Config/ConfigProvider";
 import Overview from "@/Components/Wallet/Overview/Overview";
 
 export default function Wallet({
-    arkconnect,
-    currencies,
-    network,
-    productivity,
-    settings,
-    wallet,
+	arkconnect,
+	currencies,
+	network,
+	productivity,
+	settings,
+	wallet,
 }: {
-    arkconnect: IConfigArkConnect;
-    currencies: Currencies;
-    network: INetwork;
-    productivity: IConfigProductivity;
-    settings: ISettings;
-    wallet: IWallet;
+	arkconnect: IConfigArkConnect;
+	currencies: Currencies;
+	network: INetwork;
+	productivity: IConfigProductivity;
+	settings: ISettings;
+	wallet: IWallet;
 }) {
-    const metadata = usePageMetadata({ page: "wallet", detail: {
-        name: network.name,
-        address: wallet.address,
-    } });
+	const metadata = usePageMetadata({
+		page: "wallet",
+		detail: {
+			name: network.name,
+			address: wallet.address,
+		},
+	});
 
-    return (<>
-        <Head>{metadata}</Head>
+	return (
+		<>
+			<Head>{metadata}</Head>
 
-        <ConfigProvider
-            arkconnect={arkconnect}
-            currencies={currencies}
-            productivity={productivity}
-            network={network}
-            settings={settings}
-        >
-            <Overview wallet={wallet} />
-        </ConfigProvider>
-    </>);
+			<ConfigProvider
+				arkconnect={arkconnect}
+				currencies={currencies}
+				productivity={productivity}
+				network={network}
+				settings={settings}
+			>
+				<Overview wallet={wallet} />
+			</ConfigProvider>
+		</>
+	);
 }
