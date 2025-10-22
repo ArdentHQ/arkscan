6eafcc719fefe46d06c80888e4721043e0a8e92c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-10-22 22:33:02 +0100

    tidy i18n load method

--- resources/inertia/i18n.ts
+++ resources/inertia/i18n.ts
@@ -4,20 +4,17 @@ import defaultTranslations from "../lang/php_en.json";
 import uiTranslations from "../lang/ui/php_en.json";
 
 export default async function loadI18n() {
-    const resources: Record<string, any> = {
-        en: {
-            'translation': JSON.parse(JSON.stringify(defaultTranslations).replace(/:([A-Za-z0-9_]+)/g, "{{$1}}")),
-            'ui': JSON.parse(JSON.stringify(uiTranslations).replace(/:([A-Za-z0-9_]+)/g, "{{$1}}")),
-        },
-    };
-
     i18n
-        .use(initReactI18next) // passes i18n down to react-i18next
+        .use(initReactI18next)
         .init({
-            resources,
-            lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-            // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-            // if you're using a language detector, do not define the lng option
+            resources: {
+                en: {
+                    'translation': JSON.parse(JSON.stringify(defaultTranslations).replace(/:([A-Za-z0-9_]+)/g, "{{$1}}")),
+                    'ui': JSON.parse(JSON.stringify(uiTranslations).replace(/:([A-Za-z0-9_]+)/g, "{{$1}}")),
+                },
+            },
+
+            lng: "en",
 
             interpolation: {
                 escapeValue: false,
