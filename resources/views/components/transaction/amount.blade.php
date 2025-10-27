78e309573f122a156c0b39778ef4ae695c037ea4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-10-27 15:26:58 +0000

    refactor: update amount currency output

--- resources/views/components/transaction/amount.blade.php
+++ resources/views/components/transaction/amount.blade.php
@@ -2,10 +2,12 @@
     'amount',
     'smallAmount' => 0.0001,
     'hideTooltip' => false,
+    'withoutCurrency' => false,
 ])
 
 <x-general.amount-small
     :amount="$amount"
     :small-amount="$smallAmount"
     :hide-tooltip="$hideTooltip"
+    :without-currency="$withoutCurrency"
 />
