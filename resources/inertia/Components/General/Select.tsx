84067d96b0b3c577942155cec59731d4ae47e125
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-10-27 12:27:30 -0600

    animate dropdown

--- resources/inertia/Components/General/Select.tsx
+++ resources/inertia/Components/General/Select.tsx
@@ -55,7 +55,7 @@ const SelectContent = ({ children, className, ...props }: React.ComponentProps<t
             <SelectPrimitive.Content
                 className={twMerge(
                     "z-50 overflow-hidden rounded-xl border border-white bg-white px-1 py-[0.125rem] shadow-lg dark:border-theme-dark-700 dark:bg-theme-dark-900",
-                    "data-[state=open]:animate-select-in data-[state=closed]:animate-select-out",
+                    "data-[state=open]:animate-opacity-in data-[state=closed]:animate-select-out",
                     className,
                 )}
                 position="popper"
--- tailwind.config.js
+++ tailwind.config.js
@@ -91,6 +91,8 @@ module.exports = {
                 "move-bg": "move-bg 15s infinite linear",
                 "move-bg-start-right":
                     "move-bg-start-right 15s infinite linear",
+                "opacity-in": "opacity-in 100ms ease-out",
+                "opacity-out": "opacity-out 75ms ease-in",
             },
 
             keyframes: {
@@ -101,6 +103,14 @@ module.exports = {
                 "move-bg-start-right": {
                     "0%": { backgroundPosition: "calc(100%)" },
                     "100%": { backgroundPosition: "calc(100% + 500px)" },
+                },
+                "opacity-in": {
+                    "0%": { opacity: "0" },
+                    "100%": { opacity: "1" },
+                },
+                "opacity-out": {
+                    "0%": { opacity: "1" },
+                    "100%": { opacity: "0" },
                 },
             },
 
