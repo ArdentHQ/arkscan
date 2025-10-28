9a70521ee3c6c0f7985775aa595147352b4742b8
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-10-28 10:58:18 -0600

    add multiselect tags

--- resources/inertia/Components/Tables/Desktop/Wallet/ExportTransactionsModal/TransactionTypesSelect.tsx
+++ resources/inertia/Components/Tables/Desktop/Wallet/ExportTransactionsModal/TransactionTypesSelect.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import MultiSelect from "@/Components/General/MultiSelect";
+import MultiSelectTags from "@/Components/General/MultiSelectTags";
 
 interface TransactionTypesSelectProps {
     value: string[];
@@ -9,11 +10,13 @@ interface TransactionTypesSelectProps {
 export default function TransactionTypesSelect({ value, onValueChange }: TransactionTypesSelectProps) {
     const { t } = useTranslation();
 
+    const allTypeValues = ["transfers", "votes", "multipayments", "others"];
+    const totalTypes = allTypeValues.length;
+
     const getSelectedTypesText = () => {
         const count = value.length;
         if (count === 0) return null;
 
-        const totalTypes = 4; // transfers, votes, multipayments, others
         if (count === totalTypes) {
             return `(${count}) ${t("general.all")} ${t("pages.wallet.export-transactions-modal.types_x_selected.plural")}`;
         }
@@ -26,32 +29,49 @@ export default function TransactionTypesSelect({ value, onValueChange }: Transac
         return `(${count}) ${typeLabel}`;
     };
 
-    const allTypeValues = ["transfers", "votes", "multipayments", "others"];
+    const handleRemoveType = (typeValue: string) => {
+        onValueChange(value.filter((v) => v !== typeValue));
+    };
+
+    // Only show tags if not all types are selected
+    const showTags = value.length > 0 && value.length < totalTypes;
 
     return (
-        <MultiSelect value={value} onValueChange={onValueChange}>
-            <MultiSelect.Trigger placeholder={t("pages.wallet.export-transactions-modal.types_placeholder")}>
-                {getSelectedTypesText()}
-            </MultiSelect.Trigger>
+        <>
+            <MultiSelect value={value} onValueChange={onValueChange}>
+                <MultiSelect.Trigger placeholder={t("pages.wallet.export-transactions-modal.types_placeholder")}>
+                    {getSelectedTypesText()}
+                </MultiSelect.Trigger>
 
-            <MultiSelect.Content className="w-full sm:w-100">
-                <MultiSelect.AllItem allValues={allTypeValues}>
-                    {t("general.select_all")} {t("pages.wallet.export-transactions-modal.types")}
-                </MultiSelect.AllItem>
+                <MultiSelect.Content className="w-full sm:w-100">
+                    <MultiSelect.AllItem allValues={allTypeValues}>
+                        {t("general.select_all")} {t("pages.wallet.export-transactions-modal.types")}
+                    </MultiSelect.AllItem>
 
-                <MultiSelect.Item value="transfers">
-                    {t("pages.wallet.export-transactions-modal.types-options.transfers")}
-                </MultiSelect.Item>
-                <MultiSelect.Item value="votes">
-                    {t("pages.wallet.export-transactions-modal.types-options.votes")}
-                </MultiSelect.Item>
-                <MultiSelect.Item value="multipayments">
-                    {t("pages.wallet.export-transactions-modal.types-options.multipayments")}
-                </MultiSelect.Item>
-                <MultiSelect.Item value="others">
-                    {t("pages.wallet.export-transactions-modal.types-options.others")}
-                </MultiSelect.Item>
-            </MultiSelect.Content>
-        </MultiSelect>
+                    <MultiSelect.Item value="transfers">
+                        {t("pages.wallet.export-transactions-modal.types-options.transfers")}
+                    </MultiSelect.Item>
+                    <MultiSelect.Item value="votes">
+                        {t("pages.wallet.export-transactions-modal.types-options.votes")}
+                    </MultiSelect.Item>
+                    <MultiSelect.Item value="multipayments">
+                        {t("pages.wallet.export-transactions-modal.types-options.multipayments")}
+                    </MultiSelect.Item>
+                    <MultiSelect.Item value="others">
+                        {t("pages.wallet.export-transactions-modal.types-options.others")}
+                    </MultiSelect.Item>
+                </MultiSelect.Content>
+            </MultiSelect>
+
+            {showTags && (
+                <MultiSelectTags>
+                    {value.map((typeValue) => (
+                        <MultiSelectTags.Tag key={typeValue} value={typeValue} onRemove={handleRemoveType}>
+                            {t(`pages.wallet.export-transactions-modal.types-options.${typeValue}`)}
+                        </MultiSelectTags.Tag>
+                    ))}
+                </MultiSelectTags>
+            )}
+        </>
     );
 }
