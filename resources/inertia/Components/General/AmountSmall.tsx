32ac8c7cffc151c335d1e2227813cd2b4fae7ec6
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-11-07 11:25:42 -0600

    hide currency on transactions

--- resources/inertia/Components/General/AmountSmall.tsx
+++ resources/inertia/Components/General/AmountSmall.tsx
@@ -2,36 +2,52 @@ import { currencyWithDecimals } from "@/utils/number-formatter";
 import Tooltip from "./Tooltip";
 import useConfig from "@/hooks/use-config";
 
-function AmountSmallWithoutTooltip({ amount, smallAmount }: { amount: number; smallAmount: number }) {
+function AmountSmallWithoutTooltip({
+    amount,
+    smallAmount,
+    hideCurrency = false,
+}: {
+    amount: number;
+    smallAmount: number;
+    hideCurrency?: boolean;
+}) {
     const { network } = useConfig();
 
     return (
         <>
             {amount < smallAmount ? (
                 <>
-                    &lt;{smallAmount} {network!.currency}
+                    &lt;{smallAmount} {hideCurrency ? "" : network!.currency}
                 </>
             ) : (
-                <>{currencyWithDecimals(amount, network!.currency, 2)}</>
+                <>{currencyWithDecimals(amount, network!.currency, 2, hideCurrency)}</>
             )}
         </>
     );
 }
 
-function AmountSmallWithTooltip({ amount, smallAmount }: { amount: number; smallAmount: number }) {
+function AmountSmallWithTooltip({
+    amount,
+    smallAmount,
+    hideCurrency = false,
+}: {
+    amount: number;
+    smallAmount: number;
+    hideCurrency?: boolean;
+}) {
     const { network } = useConfig();
 
     return (
         <>
             {amount < smallAmount ? (
-                <Tooltip content={currencyWithDecimals(amount, network!.currency, 18)}>
+                <Tooltip content={currencyWithDecimals(amount, network!.currency, 18, hideCurrency)}>
                     <span>
-                        &lt;{smallAmount} {network!.currency}
+                        &lt;{smallAmount} {hideCurrency ? "" : network!.currency}
                     </span>
                 </Tooltip>
             ) : (
-                <Tooltip content={currencyWithDecimals(amount, network!.currency, 18)}>
-                    <span>{currencyWithDecimals(amount, network!.currency, 2)}</span>
+                <Tooltip content={currencyWithDecimals(amount, network!.currency, 18, hideCurrency)}>
+                    <span>{currencyWithDecimals(amount, network!.currency, 2, hideCurrency)}</span>
                 </Tooltip>
             )}
         </>
@@ -42,10 +58,12 @@ export default function AmountSmall({
     amount,
     smallAmount = 0.0001,
     hideTooltip = false,
+    hideCurrency = false,
 }: {
     amount: number;
     smallAmount?: number;
     hideTooltip?: boolean;
+    hideCurrency?: boolean;
 }) {
     const { network } = useConfig();
 
@@ -56,9 +74,13 @@ export default function AmountSmall({
             ) : (
                 <>
                     {hideTooltip ? (
-                        <AmountSmallWithoutTooltip amount={amount} smallAmount={smallAmount} />
+                        <AmountSmallWithoutTooltip
+                            amount={amount}
+                            smallAmount={smallAmount}
+                            hideCurrency={hideCurrency}
+                        />
                     ) : (
-                        <AmountSmallWithTooltip amount={amount} smallAmount={smallAmount} />
+                        <AmountSmallWithTooltip amount={amount} smallAmount={smallAmount} hideCurrency={hideCurrency} />
                     )}
                 </>
             )}
