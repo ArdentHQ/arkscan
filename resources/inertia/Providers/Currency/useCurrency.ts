dec96f08247287f3d4c9ebc2a69ecd16d3b8ce04
alfonsobries <17262776+alfonsobries@users.noreply.github.com>
2025-11-14 16:08:18 +0000

    style: resolve style guide violations

--- resources/inertia/Providers/Currency/useCurrency.ts
+++ resources/inertia/Providers/Currency/useCurrency.ts
@@ -3,7 +3,7 @@ import CurrencyContext from "./CurrencyContext";
 
 export default function useCurrency() {
     const context = useContext(CurrencyContext);
-    
+
     if (!context) {
         throw new Error("useCurrency must be used within a CurrencyProvider");
     }
