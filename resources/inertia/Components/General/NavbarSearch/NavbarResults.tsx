dc1133e47f68cac48dcdc6a19649ac5583ee7d12
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-11-19 10:15:35 -0600

    results

--- resources/inertia/Components/General/NavbarSearch/NavbarResults.tsx
+++ resources/inertia/Components/General/NavbarSearch/NavbarResults.tsx
@@ -1,31 +1,32 @@
-import * as Popover from "@radix-ui/react-popover";
+import classNames from "classnames";
 import { useTranslation } from "react-i18next";
 
-export default function NavbarResults({
-    onOpenChange,
-    query,
-}: {
-    onOpenChange: (open: boolean) => void;
-    query: string;
-}) {
+export default function NavbarResults({ query }: { query: string }) {
     const { t } = useTranslation();
 
+    const open = query.length > 0;
+
+    const hasResults = false;
+
     return (
-        <Popover.Root open={!!query} onOpenChange={onOpenChange}>
-            <Popover.Portal>
-                <Popover.Content
-                    side="bottom"
-                    align="end"
-                    sideOffset={8}
-                    className={`/* Animaciones como x-transition */ data-[state=open]:animate-in data-[state=open]:fade-in data-[state=open]:zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=closed]:zoom-out-95 z-10 w-[560px] rounded-xl border border-transparent bg-white shadow-lg duration-100 dark:border-theme-dark-800 dark:bg-theme-dark-900 dark:text-theme-dark-200`}
-                >
-                    <div className="custom-scroll flex max-h-[410px] flex-col space-y-1 divide-y divide-dashed divide-theme-secondary-300 overflow-y-auto whitespace-nowrap px-6 py-3 text-sm font-semibold dark:divide-theme-dark-800">
-                        <p className="text-center text-theme-secondary-900 dark:text-theme-dark-50">
-                            We could not find anything matching your search criteria, please try again!
-                        </p>
-                    </div>
-                </Popover.Content>
-            </Popover.Portal>
-        </Popover.Root>
+        <div
+            className={classNames(
+                "absolute right-0 top-9 z-10 mt-2 origin-top-right rounded-xl border border-transparent bg-white py-1 shadow-lg transition-all duration-150 dark:border-theme-dark-800 dark:bg-theme-dark-900 dark:text-theme-dark-200",
+                {
+                    "pointer-events-auto scale-100 opacity-100": open,
+                    "pointer-events-none scale-95 opacity-0": !open,
+                    "w-[560px]": !hasResults,
+                    "w-[628px]": hasResults,
+                },
+            )}
+        >
+            {open && (
+                <div className="custom-scroll flex max-h-[410px] flex-col space-y-1 divide-y divide-dashed divide-theme-secondary-300 overflow-y-auto whitespace-nowrap px-6 py-3 text-sm font-semibold dark:divide-theme-dark-800">
+                    <p className="text-center text-theme-secondary-900 dark:text-theme-dark-50">
+                        {t("general.search.no_results")}
+                    </p>
+                </div>
+            )}
+        </div>
     );
 }
