283a7ae6fa261e434776697612cf2c87eba45d96
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-11-25 08:16:49 -0600

    clean css classes

--- resources/inertia/Components/General/NetworkDropdown/NetworkDropdown.tsx
+++ resources/inertia/Components/General/NetworkDropdown/NetworkDropdown.tsx
@@ -14,14 +14,16 @@ export default function NetworkDropdown() {
         <DropdownProvider>
             <Dropdown
                 useDefaultButtonClasses={false}
-                buttonClass="flex items-center justify-center focus:outline-none dropdown-button transition-default
-p-2 space-x-1.5 h-8 text-sm font-semibold rounded
-px-3
-bg-white hover:bg-theme-secondary-200
-border border-theme-secondary-300
-text-theme-secondary-700 hover:text-theme-secondary-900
-dark:bg-theme-dark-900 dark:border-theme-dark-700 dark:text-theme-dark-200 dark:hover:bg-theme-dark-700
-dim:bg-theme-dark-900 dim:hover:bg-theme-dark-700"
+                buttonClass={classNames(
+                    "dropdown-button",
+                    "flex items-center justify-center h-8 px-3 p-2 space-x-1.5",
+                    "text-sm font-semibold",
+                    "rounded border border-theme-secondary-300",
+                    "bg-white text-theme-secondary-700 hover:bg-theme-secondary-200 hover:text-theme-secondary-900",
+                    "dark:bg-theme-dark-900 dark:border-theme-dark-700 dark:text-theme-dark-200 dark:hover:bg-theme-dark-700",
+                    "dim:bg-theme-dark-900 dim:hover:bg-theme-dark-700",
+                    "transition-default focus:outline-none",
+                )}
                 button={({ isOpen }) => (
                     <>
                         <span>{isProduction ? t("general.navbar.mainnet") : t("general.navbar.testnet")}</span>
