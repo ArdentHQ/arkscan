6c8d2320647e157f53cf35169be3d4b43d25ed6c
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-11-25 09:02:25 -0600

    add component

--- resources/inertia/Components/General/Navbar/Navbar.tsx
+++ resources/inertia/Components/General/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import NavbarTop from "./NavbarTop";
+import NavbarDesktop from "./NavbarDesktop";
 
 export default function Navbar() {
     return (
         <div id="navbar" className="z-30 pb-13 sm:pb-16 md:sticky md:top-0 md:pb-0">
             <NavbarTop />
 
-            {/* @TODO: add desktop navigation (https://app.clickup.com/t/86dyee7q0) */}
+            <NavbarDesktop />
 
             {/* @TODO: add mobile navigation (https://app.clickup.com/t/86dyeejm5) */}
         </div>
---
+++ resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
@@ -0,0 +1,3 @@
+export default function NavbarDesktop() {
+    return <div>{/*  */}</div>;
+}
