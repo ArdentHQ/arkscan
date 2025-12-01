8747b8bac73b3c552b068c524b58faac869fb9b4
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-12-01 12:47:20 -0600

    Update CacheAddressStatistics.php

--- app/Console/Commands/CacheAddressStatistics.php
+++ app/Console/Commands/CacheAddressStatistics.php
@@ -93,7 +93,7 @@ final class CacheAddressStatistics extends Command
 
     private function cacheMostTransactions(StatisticsCache $cache): void
     {
-        /** @var array{address?: string, tx_count?: int} $mostActive */
+        
         $mostActive = DB::connection('explorer')->query()
             ->fromSub(function ($query) {
                 $query->select('from as address')
@@ -124,9 +124,10 @@ final class CacheAddressStatistics extends Command
             ->groupBy('address')
             ->orderByDesc('tx_count')
             ->limit(1)
-            ->first();
-
-        if ($mostActive) {
+            ->first();  
+    
+        /** @var object{address?: string, tx_count?: int} $mostActive */
+        if ($mostActive?->address !== null) {
             $newValue = [
                 'address' => $mostActive->address,
                 'value'   => (int) $mostActive->tx_count,
