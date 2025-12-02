4b39eecfb1f77d8983238c7c2e5d3f248ed64b63
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-12-02 11:30:58 -0600

    add page header for validators

--- resources/inertia/Pages/Validators/Validators.tsx
+++ resources/inertia/Pages/Validators/Validators.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { usePageMetadata } from "@/Components/General/Metadata";
 import Layout from "@/Layout";
 import { PageProps } from "@inertiajs/core";
+import PageHeader from "@/Components/PageHeader/PageHeader";
 
 export default function Validators({ network }: PageProps) {
     const { t } = useTranslation();
@@ -18,34 +19,7 @@ export default function Validators({ network }: PageProps) {
             <Head>{metadata}</Head>
 
             <Layout>
-                <p>General validators page</p>
-                {/* <PageHeader
-                    title={t("pages.validator-monitor.title")}
-                    subtitle={t("pages.validator-monitor.subtitle")}
-                />
-
-                <HeaderStats height={height} statistics={validatorData?.statistics} />
-
-                <ValidatorFavoritesProvider>
-                    <MissedBlocksTrackerProvider
-                        validators={[
-                            ...(validatorData?.validators ?? []),
-                            ...(validatorData?.overflowValidators ?? []),
-                        ]}
-                    >
-                        <MonitorTableWrapper
-                            validators={validatorData?.validators}
-                            overflowValidators={validatorData?.overflowValidators}
-                            rowCount={rowCount}
-                        />
-
-                        <MobileDivider />
-
-                        <div className="px-6 pb-8 pt-6 md:mx-auto md:max-w-7xl md:px-10 md:pt-0">
-                            <MonitorMobileTableWrapper validators={validatorData?.validators} rowCount={rowCount} />
-                        </div>
-                    </MissedBlocksTrackerProvider>
-                </ValidatorFavoritesProvider> */}
+                <PageHeader title={t("pages.validators.title")} subtitle={t("pages.validators.subtitle")} />
             </Layout>
         </>
     );
