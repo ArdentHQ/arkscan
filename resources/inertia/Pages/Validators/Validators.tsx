4b83ae3961e212e404bcbc281084477ec3ed0931
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-12-03 13:47:13 +0000

    refactor: validators tabs handling & improvements

--- resources/inertia/Pages/Validators/Validators.tsx
+++ resources/inertia/Pages/Validators/Validators.tsx
@@ -1,9 +1,65 @@
-import { Head } from "@inertiajs/react";
+import { Head, router } from "@inertiajs/react";
 import { useTranslation } from "react-i18next";
 import { usePageMetadata } from "@/Components/General/Metadata";
 import Layout from "@/Layout";
 import { PageProps } from "@inertiajs/core";
 import PageHeader from "@/Components/PageHeader/PageHeader";
+import PageHandlerProvider from "@/Providers/PageHandler/PageHandlerProvider";
+import TabsProvider from "@/Providers/Tabs/TabsProvider";
+import { ITab } from "@/Providers/Tabs/types";
+import { useEffect, useRef } from "react";
+import { useTabs } from "@/Providers/Tabs/TabsContext";
+import { usePageHandler } from "@/Providers/PageHandler/PageHandlerContext";
+import { useTabPolling } from "@/hooks/use-tab-polling";
+
+const ValidatorsTabsWrapper = () => {
+    return (
+        <TabsProvider
+            defaultSelected="validators"
+            tabs={[
+                { text: "Validators", value: "validators" },
+                { text: "Missed Blocks", value: "missed-blocks" },
+                { text: "Recent Votes", value: "recent-votes" },
+            ]}
+        >
+            <ValidatorsTabs />
+        </TabsProvider>
+    );
+};
+
+const ValidatorsTabs = () => {
+    const { currentTab } = useTabs();
+
+    useTabPolling((tab: string, callback?: CallableFunction) => {
+        let pollParameters: string[] = [];
+        if (tab === "validators") {
+            pollParameters = ["validators"];
+        } else if (tab === "missed-blocks") {
+            pollParameters = ["missed-blocks"];
+        } else if (tab === "recent-votes") {
+            pollParameters = ["recent-votes"];
+        }
+
+        router.reload({
+            only: pollParameters,
+            onSuccess: () => {
+                if (callback) {
+                    callback();
+                }
+            },
+        });
+    });
+
+    return (
+        <>
+            {currentTab === "validators" && <>{/*  */}</>}
+
+            {currentTab === "missed-blocks" && <>{/*  */}</>}
+
+            {currentTab === "recent-votes" && <>{/*  */}</>}
+        </>
+    );
+};
 
 export default function Validators({ network }: PageProps) {
     const { t } = useTranslation();
@@ -20,6 +76,10 @@ export default function Validators({ network }: PageProps) {
 
             <Layout>
                 <PageHeader title={t("pages.validators.title")} subtitle={t("pages.validators.subtitle")} />
+
+                <PageHandlerProvider>
+                    <ValidatorsTabsWrapper />
+                </PageHandlerProvider>
             </Layout>
         </>
     );
