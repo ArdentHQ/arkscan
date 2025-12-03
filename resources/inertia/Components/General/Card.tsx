70ad9657ff73d1fd1c1acb0146f56764c315b8ec
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-12-03 12:56:24 +0000

    style: resolve style guide violations

--- resources/inertia/Components/General/Card.tsx
+++ resources/inertia/Components/General/Card.tsx
@@ -9,8 +9,7 @@ export default function Card({
     return (
         <div
             className={classNames({
-                "rounded px-4 py-3 ring-1 ring-inset ring-theme-secondary-300 dark:ring-theme-dark-700 md:rounded-xl md:px-6 md:py-4":
-                    true,
+                "rounded px-4 py-3 ring-1 ring-inset ring-theme-secondary-300 dark:ring-theme-dark-700 md:rounded-xl md:px-6 md:py-4": true,
                 [className]: true,
             })}
         >
