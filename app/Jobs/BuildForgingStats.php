0439ea0748e3d32a7e12ef06235027c42b1786d8
alfonsobries <17262776+alfonsobries@users.noreply.github.com>
2025-12-03 14:35:41 +0000

    style: resolve style guide violations

--- app/Jobs/BuildForgingStats.php
+++ app/Jobs/BuildForgingStats.php
@@ -23,12 +23,12 @@ final class BuildForgingStats implements ShouldQueue
     use Queueable;
     use SerializesModels;
 
+    public const DEFAULT_RANGE_SECONDS = 24 * 60 * 60 * 30; // 30 days
+
     /**
      * @var int
      */
     public $tries = 1;
-
-    public const DEFAULT_RANGE_SECONDS = 24 * 60 * 60 * 30; // 30 days
 
     public function __construct(public int $height, public float $numberOfDays)
     {
