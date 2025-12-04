abf5192bef22e0302eb75a7a303f232d69262261
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-12-04 04:06:10 +0000

    refactor: add validators header cards  (#1350)

---
+++ resources/inertia/Pages/Validators.contracts.ts
@@ -0,0 +1,34 @@
+import { IValidator } from "../types";
+
+export interface IValidatorData {
+    statistics: IMonitorStatistics;
+    overflowValidators: IValidator[];
+    validators: IValidator[];
+}
+
+export interface IMonitorStatistics {
+    performances?: {
+        forging?: string | number;
+        missed?: string | number;
+        missing?: string | number;
+    };
+    blockCount?: number;
+    nextValidator?: {
+        address?: string;
+        attributes?: {
+            username?: string;
+        };
+    };
+}
+
+export interface IValidatorsStatistics {
+    voterCount: number;
+    totalVoted: number;
+    missedBlocks: number;
+    validatorsMissed: number;
+    votesPercentage: number;
+}
+
+export interface ValidatorsProps {
+    statistics: IValidatorsStatistics;
+}
