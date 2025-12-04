57786fd4fa33412efd0725c7770977af23efb8cc
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-12-04 16:42:03 +0000

    style: resolve style guide violations

--- resources/inertia/Components/PageHeader/Container.tsx
+++ resources/inertia/Components/PageHeader/Container.tsx
@@ -15,8 +15,7 @@ export default function PageHeaderContainer({
         <div className="flex flex-col px-6 pb-6 md:mx-auto md:max-w-7xl md:px-10">
             <div
                 className={classNames({
-                    "flex flex-col space-y-4 overflow-hidden font-semibold sm:flex-row sm:items-end sm:justify-between sm:space-y-0":
-                        true,
+                    "flex flex-col space-y-4 overflow-hidden font-semibold sm:flex-row sm:items-end sm:justify-between sm:space-y-0": true,
                     "sm:items-center sm:rounded-lg sm:border sm:border-theme-secondary-300 sm:dark:border-theme-dark-700":
                         breakpoint === "sm",
                     "md:items-center md:rounded-lg md:border md:border-theme-secondary-300 md:dark:border-theme-dark-700":
