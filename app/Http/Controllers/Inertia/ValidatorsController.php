a8dbbd16e452ee812a56a66cbfe7264b0e232d7a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-12-05 02:10:28 +0000

    chore: stanley

--- app/Http/Controllers/Inertia/ValidatorsController.php
+++ app/Http/Controllers/Inertia/ValidatorsController.php
@@ -64,23 +64,27 @@ final class ValidatorsController
         if (config('database.default') === 'sqlite') {
             return ForgingStats::with('validator')
                 ->orderByDesc('timestamp')
-                ->whereNotNull('missed_height');
+                ->whereNotNull('missed_height')
+                ->paginate($this->perPage(), page: $this->page())
+                ->through(fn (ForgingStats $voter) => ForgingStatsDTO::fromModel($voter));
         }
 
         // TODO: Re-implement sorting once the UI supports it - https://app.clickup.com/t/86dypp5jv
-        // $sortDirection = SortDirection::ASC;
-        // if ($this->getSortDirection('missed-blocks') === SortDirection::DESC) {
-        //     $sortDirection = SortDirection::DESC;
-        // }
+        //       Look at \App\Http\Livewire\Validators\Concerns\MissedBlocksTab for reference.
         $missedBlocksSortKey = 'height';
         $sortDirection       = SortDirection::DESC;
 
         return ForgingStats::query()
             ->with('validator')
+            // @phpstan-ignore-next-line
             ->when($missedBlocksSortKey === 'height', fn ($query) => $query->sortByHeight($sortDirection))
+            // @phpstan-ignore-next-line
             ->when($missedBlocksSortKey === 'age', fn ($query) => $query->sortByAge($sortDirection))
+            // @phpstan-ignore-next-line
             ->when($missedBlocksSortKey === 'name', fn ($query) => $query->sortByUsername($sortDirection))
+            // @phpstan-ignore-next-line
             ->when($missedBlocksSortKey === 'votes' || $missedBlocksSortKey === 'percentage_votes', fn ($query) => $query->sortByVoteCount($sortDirection))
+            // @phpstan-ignore-next-line
             ->when($missedBlocksSortKey === 'no_of_voters', fn ($query) => $query->sortByNumberOfVoters($sortDirection))
             ->whereNotNull('missed_height')
             ->paginate($this->perPage(), page: $this->page())
