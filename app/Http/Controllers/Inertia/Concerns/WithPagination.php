40e69b4872da78af932c2d2f1d6ee6d279182762
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-12-08 11:54:52 -0600

    Update WithPagination.php

--- app/Http/Controllers/Inertia/Concerns/WithPagination.php
+++ app/Http/Controllers/Inertia/Concerns/WithPagination.php
@@ -8,27 +8,13 @@ use Illuminate\Support\Str;
 
 trait WithPagination
 {
-    protected function page(string $name = 'default'): int
+    protected function page(): int
     {
-        $queryKey = $name === 'default' ? 'page' : "{$name}-page";
-
-        if (request()->has($queryKey)) {
-            return (int) request()->get($queryKey);
-        }
-
-        if ($queryKey !== 'page' && request()->has('page')) {
-            return (int) request()->get('page');
-        }
-
-        return (int) request()->get($queryKey, 1);
+        return (int) request()->get('page');
     }
 
     protected function perPage(string $name = 'default'): int
     {
-        if (request()->has('per-page')) {
-            return (int) request()->get('per-page');
-        }
-
         $constantName = Str::upper($name).'_PER_PAGE';  // e.g. VALIDATORS_PER_PAGE
 
         if (defined(static::class.'::'.$constantName)) {
