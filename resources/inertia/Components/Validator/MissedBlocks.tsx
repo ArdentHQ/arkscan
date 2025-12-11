96262dcf4c30eb1ef45d1725e850bb832ba07552
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-12-11 11:05:07 -0600

    fix missed blocks tag

--- resources/inertia/Components/Validator/MissedBlocks.tsx
+++ resources/inertia/Components/Validator/MissedBlocks.tsx
@@ -5,7 +5,8 @@ import classNames from "classnames";
 export default function MissedBlocks({ validator }: { validator: IValidator }) {
     return (
         <Badge
-            className={classNames("min-w-[30px] text-center", {
+            className="min-w-[30px] text-center"
+            colors={classNames({
                 "border-theme-success-100 bg-theme-success-100 text-theme-success-700 dark:border-theme-success-700 dark:text-theme-success-500":
                     validator.missedBlocksState === "success",
                 "border-theme-orange-light bg-theme-orange-light text-theme-orange-dark dim:text-theme-warning-400 dark:!border-theme-warning-600 dark:text-theme-warning-400":
