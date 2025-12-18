4537c63522beea2cd15905301ad7be3419c73f7e
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-12-18 08:23:46 -0600

    disable smooth scroll on dusk tests

--- resources/views/layouts/inertia.blade.php
+++ resources/views/layouts/inertia.blade.php
@@ -4,6 +4,7 @@
     @if (config('arkscan.arkconnect.enabled'))
         x-data="Wallet({{ json_encode(Network::toArray()) }})"
     @endif
+    class="env-{{ app()->environment() }}"
 >
     <x-ark-pages-includes-layout-head-inertia
         :default-name="trans('metatags.home.title')"
