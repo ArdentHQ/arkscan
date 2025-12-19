42a919bc0ea4de95f38b40863b5bd0cef914e2f8
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-12-19 10:09:03 -0600

    adjust stats grid

--- resources/inertia/Components/Transaction/HeaderStats.tsx
+++ resources/inertia/Components/Transaction/HeaderStats.tsx
@@ -11,34 +11,32 @@ export default function HeaderStats({
     volume,
     totalFees,
     averageFee,
-}: Pick<TransactionsProps, "transactionCount" | "volume" | "totalFees" | "averageFee">) {
+}: Pick<TransactionsProps["statistics"], "transactionCount" | "volume" | "totalFees" | "averageFee">) {
     const { t } = useTranslation();
     const { network } = useShareData();
 
     return (
-        <div className="flex flex-col space-y-2 px-6 pb-6 sm:space-y-3 md:mx-auto md:max-w-7xl md:px-10 xl:flex-row xl:space-x-3 xl:space-y-0">
-            <div className="grid w-full grid-cols-1 gap-2 sm:grid-cols-2 md:gap-3 xl:grid-cols-4">
-                <Card className="flex-1">
-                    <Detail title={t("pages.transactions.transactions_24h")}>
-                        <Number>{transactionCount}</Number>
-                    </Detail>
-                </Card>
-                <Card className="flex-1">
-                    <Detail title={t("pages.transactions.volume_24h")}>
-                        {currencyWithDecimals({ value: volume, currency: network!.currency })}
-                    </Detail>
-                </Card>
-                <Card className="flex-1">
-                    <Detail title={t("pages.transactions.total_fees_24h")}>
-                        {currencyWithDecimals({ value: totalFees, currency: network!.currency })}
-                    </Detail>
-                </Card>
-                <Card className="flex-1">
-                    <Detail title={t("pages.transactions.average_fee_24h")}>
-                        {currencyWithDecimals({ value: averageFee, currency: network!.currency })}
-                    </Detail>
-                </Card>
-            </div>
+        <div className="grid w-full flex-1 grid-cols-1 gap-2 px-6 pb-6 sm:grid-cols-2 md:mx-auto md:max-w-7xl md:gap-3 md:px-10 md:pb-3 xl:grid-cols-4">
+            <Card className="flex-1">
+                <Detail title={t("pages.transactions.transactions_24h")}>
+                    <Number>{transactionCount}</Number>
+                </Detail>
+            </Card>
+            <Card className="flex-1">
+                <Detail title={t("pages.transactions.volume_24h")}>
+                    {currencyWithDecimals({ value: volume, currency: network!.currency })}
+                </Detail>
+            </Card>
+            <Card className="flex-1">
+                <Detail title={t("pages.transactions.total_fees_24h")}>
+                    {currencyWithDecimals({ value: totalFees, currency: network!.currency })}
+                </Detail>
+            </Card>
+            <Card className="flex-1">
+                <Detail title={t("pages.transactions.average_fee_24h")}>
+                    {currencyWithDecimals({ value: averageFee, currency: network!.currency })}
+                </Detail>
+            </Card>
         </div>
     );
 }
