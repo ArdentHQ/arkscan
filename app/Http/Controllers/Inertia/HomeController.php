227c2fe37ebf0bcfc6606ba53bb8ffaf411320e5
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-05 08:32:39 -0600

    add empty home page

---
+++ app/Http/Controllers/Inertia/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Inertia;
+
+use Inertia\Inertia;
+use Inertia\Response;
+
+final class HomeController
+{
+    public function __invoke(): Response
+    {
+        return Inertia::render('Home/Index', []);
+    }
+
+}
