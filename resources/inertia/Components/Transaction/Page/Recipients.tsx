0c832e5e698471e6e1943758a603052424650ba7
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-12 11:13:52 -0600

    use table componentes

--- resources/inertia/Components/Transaction/Page/Recipients.tsx
+++ resources/inertia/Components/Transaction/Page/Recipients.tsx
@@ -4,6 +4,8 @@ import AmountSmall from "@/Components/General/AmountSmall";
 import { TransactionRecipient } from "@/Pages/Transaction.contracts";
 import { weiToArk } from "@/utils/UnitConverter";
 import TransactionAddress from "./Address";
+import TableHeader from "@/Components/Tables/Desktop/TableHeader";
+import TableCell from "@/Components/Tables/Desktop/TableCell";
 
 export default function TransactionRecipients({ recipients }: { recipients: TransactionRecipient[] }) {
     const { t } = useTranslation();
@@ -15,19 +17,19 @@ export default function TransactionRecipients({ recipients }: { recipients: Tran
                     <table>
                         <thead className="bg-theme-secondary-100 dark:bg-black">
                             <tr className="border-b-none text-sm font-semibold">
-                                <th className="w-full py-3 text-left">{t("general.address")}</th>
-                                <th className="py-3 text-right">{t("general.transaction.amount")}</th>
+                                <TableHeader name="general.address" className="w-full text-left" />
+                                <TableHeader name="general.transaction.amount" className="text-right" type="number" />
                             </tr>
                         </thead>
                         <tbody>
                             {recipients.map((recipient) => (
                                 <tr className="text-sm font-semibold" key={`${recipient.address}-${recipient.amount}`}>
-                                    <td className="py-3">
+                                    <TableCell>
                                         <TransactionAddress address={recipient.address} />
-                                    </td>
-                                    <td className="py-3 text-right">
+                                    </TableCell>
+                                    <TableCell className="text-right">
                                         <AmountSmall amount={Number(weiToArk(recipient.amount))} />
-                                    </td>
+                                    </TableCell>
                                 </tr>
                             ))}
                         </tbody>
