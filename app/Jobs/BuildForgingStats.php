faba06908010ae9df3da8e968a4abaea6006cf9e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-01-16 05:34:09 +0000

    no need to handle block timestamp ms adjustment

--- app/Jobs/BuildForgingStats.php
+++ app/Jobs/BuildForgingStats.php
@@ -149,6 +149,6 @@ final class BuildForgingStats implements ShouldQueue
 
     private function getTimestampForHeight(int $height): int
     {
-        return (int) (Block::where('number', $height)->firstOrFail()->timestamp / 1000);
+        return (int) (Block::where('number', $height)->firstOrFail()->timestamp);
     }
 }
