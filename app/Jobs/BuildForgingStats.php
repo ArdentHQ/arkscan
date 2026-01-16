681b2bd16a0264e0a6bd40ee97c59929e0104b40
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-01-16 06:18:08 +0000

    chore: stanley

--- app/Jobs/BuildForgingStats.php
+++ app/Jobs/BuildForgingStats.php
@@ -149,6 +149,6 @@ final class BuildForgingStats implements ShouldQueue
 
     private function getTimestampForHeight(int $height): int
     {
-        return (int) (Block::where('number', $height)->firstOrFail()->timestamp);
+        return Block::where('number', $height)->firstOrFail()->timestamp;
     }
 }
