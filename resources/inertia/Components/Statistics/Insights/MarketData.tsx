2d43b3ab336a2191e43f2a1b4958372fe9df6813
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-16 09:39:05 -0600

    wip

--- resources/inertia/Components/Statistics/Insights/MarketData.tsx
+++ resources/inertia/Components/Statistics/Insights/MarketData.tsx
@@ -124,7 +124,7 @@ export default function MarketDataInsights({
 
                 <InsightsContainer title={t("pages.statistics.insights.market_data.exchanges_volume")} fullWidth>
                     {(["today_volume", "atl", "ath"] as const).map((item) => (
-                        <div key={item} className="first:-mt-3 md:hidden">
+                        <div key={item} className="first:-mt-3 md:first:mt-0">
                             <div className="flex md:hidden">
                                 <div className="flex w-full flex-col justify-between space-y-3 pt-3 sm:flex-row sm:space-y-0">
                                     <div className="flex flex-col space-y-2">
