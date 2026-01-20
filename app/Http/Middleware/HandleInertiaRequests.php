c197caa3c2f6acbabaf21513417cf6534c9abf4d
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-20 13:38:29 -0600

    rollback flash

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -76,6 +76,13 @@ class HandleInertiaRequests extends Middleware
                 'navbarName'           => fn () => config('app.navbar_name'),
 
             ])->toArray(),
+            'flash' => fn () => flash()->message !== null
+                ? [
+                    'message' => flash()->message,
+                    'type'    => flash()->level,
+                ]
+                : null,
+            ...parent::share($request),
         ];
     }
 }
