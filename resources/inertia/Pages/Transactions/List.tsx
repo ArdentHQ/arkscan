22da2cf5a5345e66908950ec45e57b7ea3edef7c
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-21 10:08:16 -0600

    add new transaction event listener

--- resources/inertia/Pages/Transactions/List.tsx
+++ resources/inertia/Pages/Transactions/List.tsx
@@ -1,14 +1,26 @@
 import { useTranslation } from "react-i18next";
 import Layout from "@/Layout";
 import { PageProps } from "@inertiajs/core";
+import { router } from "@inertiajs/react";
 import PageHeader from "@/Components/PageHeader/PageHeader";
 import { TransactionsProps } from "../Transactions.contracts";
 import HeaderStats from "@/Components/Transaction/HeaderStats";
 import TransactionsTable from "@/Components/Transaction/TransactionsTable";
 import PageHandlerProvider from "@/Providers/PageHandler/PageHandlerProvider";
+import { useEffect } from "react";
+import useWebhooks from "@/Providers/Webhooks/useWebhooks";
 
 export default function TransactionsList({ network, statistics, filters, transactions }: PageProps<TransactionsProps>) {
     const { t } = useTranslation();
+    const { listen } = useWebhooks();
+
+    useEffect(() => {
+        return listen('transactions', "NewTransaction", () => {
+            router.reload({
+                only: ["transactions"],
+            });
+        });
+    }, []);
 
     return (
         <Layout>
