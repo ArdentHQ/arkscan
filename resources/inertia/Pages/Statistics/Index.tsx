a809e1fb2d0e4b9f487d886c16d5ccf7ed2616cd
alfonsobries <17262776+alfonsobries@users.noreply.github.com>
2026-01-23 17:00:41 +0000

    style: resolve style guide violations

--- resources/inertia/Pages/Statistics/Index.tsx
+++ resources/inertia/Pages/Statistics/Index.tsx
@@ -77,10 +77,10 @@ export default function StatisticsIndex({
             </div>
 
             <div className="px-6 pb-3 md:mx-auto md:max-w-7xl md:px-10">
-                <div className="dim:bg-theme-dim-800 dim:text-theme-dim-500 dark:bg-theme-dark-800 flex items-start space-x-2 rounded bg-theme-primary-100 px-6 py-6 text-sm leading-[17px] md:rounded-xl md:py-3">
+                <div className="dim:bg-theme-dim-800 dim:text-theme-dim-500 flex items-start space-x-2 rounded bg-theme-primary-100 px-6 py-6 text-sm leading-[17px] dark:bg-theme-dark-800 md:rounded-xl md:py-3">
                     <CircleInfoIcon className="dim:text-theme-dim-600 mt-0.5 h-4 w-4 shrink-0 text-theme-primary-700 dark:text-theme-dark-blue-400" />
 
-                    <span className="text-theme-primary-900 dark:text-theme-dark-50 leading-[21px] md:leading-[17px]">
+                    <span className="leading-[21px] text-theme-primary-900 dark:text-theme-dark-50 md:leading-[17px]">
                         {t("pages.statistics.mainsail_notice", {
                             blockHeight: snapshotBlockHeightLabel,
                         })}
