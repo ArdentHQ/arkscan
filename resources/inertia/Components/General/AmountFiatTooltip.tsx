5c7e77bbb87485cab32b9a80f0b7b82e115e2b3d
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-23 12:55:00 -0600

    Show fiat tooltip only if network can be exchanged

--- resources/inertia/Components/General/AmountFiatTooltip.tsx
+++ resources/inertia/Components/General/AmountFiatTooltip.tsx
@@ -124,7 +124,7 @@ export default function AmountFiatTooltip({
                 </Tooltip>
             )}
 
-            {fiat && (
+            {fiat && network.canBeExchanged && (
                 <Tooltip content={fiat}>
                     <AmountOutput
                         transaction={transaction}
@@ -137,7 +137,7 @@ export default function AmountFiatTooltip({
                 </Tooltip>
             )}
 
-            {!fiat && (
+            {!fiat || !network.canBeExchanged && (
                 <AmountOutput
                     transaction={transaction}
                     isSent={isSent}
