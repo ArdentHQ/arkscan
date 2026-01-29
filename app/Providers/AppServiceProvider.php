9a3404ca25d56778cef4160a5996b1a65e9f7c41
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-01-29 10:53:17 -0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,7 @@ use App\Facades\Network;
 use App\Services\BigNumber;
 use App\Services\GasTracker;
 use App\Services\Monitor\MissedBlocksCalculator;
-use App\Testing\FakeZendesk;
 use ARKEcosystem\Foundation\DataBags\DataBag;
-use Huddle\Zendesk\Facades\Zendesk;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
@@ -49,11 +47,6 @@ final class AppServiceProvider extends ServiceProvider
             GasTrackerContract::class,
             fn () => new (GasTracker::class)()
         );
-
-        // Mock Zendesk in Dusk tests
-        if ($this->app->environment('dusk')) {
-            Zendesk::swap(new FakeZendesk());
-        }
     }
 
     /**
