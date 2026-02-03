59f4ef7051c4b636cbe36ebbbc6d44330dd007b0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-02-03 01:17:58 +0000

    test resolutions

--- tests/Browser/Pages/Resources/CompatibleWalletsTest.php
+++ tests/Browser/Pages/Resources/CompatibleWalletsTest.php
@@ -5,18 +5,22 @@ declare(strict_types=1);
 use Facebook\WebDriver\WebDriverBy;
 use Laravel\Dusk\Browser;
 
-it('should have the correct url for learn more links', function () {
-    $this->browse(function (Browser $browser) {
+it('should have the correct url for learn more links', function ($resolution) {
+    $this->browse(function (Browser $browser) use ($resolution) {
+        $browser->resize($resolution['width'], $resolution['height']);
+
         $browser->visitRoute('compatible-wallets')
             ->assertSee('Compatible Wallets');
 
         expect($browser->driver->findElements(WebDriverBy::xpath('//a[@href="'.config('arkscan.urls.public.arkvault').'"]')))->toHaveCount(1);
         expect($browser->driver->findElements(WebDriverBy::xpath('//a[@href="'.config('arkscan.urls.public.arkconnect').'"]')))->toHaveCount(1);
     });
-});
+})->with('resolutions');
 
-it('should submit modal', function () {
-    $this->browse(function (Browser $browser) {
+it('should submit modal', function ($resolution) {
+    $this->browse(function (Browser $browser) use ($resolution) {
+        $browser->resize($resolution['width'], $resolution['height']);
+
         $browser->visitRoute('compatible-wallets')
             ->assertSee('Compatible Wallets')
             ->clickLink('Submit Wallet', 'button')
@@ -28,4 +32,13 @@ it('should submit modal', function () {
             ->click('button[type="submit"]')
             ->waitForText(trans('pages.compatible-wallets.submit-modal.success_toast'));
     });
-});
+})->with('resolutions');
+
+dataset('resolutions', [
+    'desktop' => [['width' => 1280, 'height' => 1024]],
+    'lg'      => [['width' => 1024, 'height' => 768]],
+    'md-lg'   => [['width' => 960, 'height' => 667]],
+    'md'      => [['width' => 768, 'height' => 1024]],
+    'sm'      => [['width' => 640, 'height' => 960]],
+    'xs'      => [['width' => 370, 'height' => 844]],
+]);
