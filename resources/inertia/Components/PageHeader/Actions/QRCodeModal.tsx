32426e6ccf1705e4d8705c01f02a340622fbd0e6
alfonsobries <17262776+alfonsobries@users.noreply.github.com>
2026-02-04 14:02:58 +0000

    style: resolve style guide violations

--- resources/inertia/Components/PageHeader/Actions/QRCodeModal.tsx
+++ resources/inertia/Components/PageHeader/Actions/QRCodeModal.tsx
@@ -70,7 +70,7 @@ function ArkVaultButton({
 
                     <ExternalLink
                         url={walletUri}
-                        className="button-secondary mt-2 w-full group"
+                        className="button-secondary group mt-2 w-full"
                         iconClass="inline transition-default relative -top-1 w-3 h-3 flex-shrink-0 mt-1 ml-0.5 text-theme-primary-400 dim:text-theme-dark-300 dark:text-theme-dark-500 group-hover:text-white dim:group-hover:text-white"
                     >
                         {t("brands.arkvault")}
@@ -82,7 +82,7 @@ function ArkVaultButton({
                 <div>
                     <ExternalLink
                         url={walletUri}
-                        className="button-secondary mt-2 w-full group"
+                        className="button-secondary group mt-2 w-full"
                         iconClass="inline relative -top-1 w-3 h-3 flex-shrink-0 mt-1 ml-0.5 w-3 h-3 text-theme-primary-400 dim:text-theme-dark-300 dark:text-theme-dark-500 group-hover:text-white dim:group-hover:text-white"
                     >
                         {t("brands.arkvault")}
