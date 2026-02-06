3feb63301b504afdac1332ed21f69b9658f21469
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-02-06 11:51:30 -0600

    wip

--- app/Actions/CacheNetworkHeight.php
+++ app/Actions/CacheNetworkHeight.php
@@ -6,19 +6,11 @@ namespace App\Actions;
 
 use App\Models\State;
 use App\Services\Cache\NetworkCache;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 final class CacheNetworkHeight
 {
     public static function execute(): int
     {
-        return (new NetworkCache())->setHeight(function (): int {
-            try {
-                return State::latest()->block_number->toNumber();
-            } catch (ModelNotFoundException) {
-                return 0;
-            }
-        });
+        return (new NetworkCache())->setHeight(fn (): int => State::latest()?->block_number->toNumber() ?? 0);
     }
 }
-
