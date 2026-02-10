79b8a0afab4b7c6f316002e3c8cb286d4d432a9f
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-02-10 10:33:30 -0600

    fix mobile table row props

--- resources/inertia/Components/Tables/Mobile/Row.tsx
+++ resources/inertia/Components/Tables/Mobile/Row.tsx
@@ -10,6 +10,7 @@ export default function MobileTableRow({
     contentClass = "",
     className = "",
     children,
+    ...props
 }: React.PropsWithChildren<{
     header: React.ReactNode;
     expandable?: boolean;
@@ -17,6 +18,7 @@ export default function MobileTableRow({
     expandDisabled?: boolean;
     contentClass?: string;
     className?: string;
+    
 }>) {
     const [isExpanded, setIsExpanded] = useState(expandable ? false : true);
 
@@ -26,6 +28,7 @@ export default function MobileTableRow({
                 "rounded border border-theme-secondary-300 text-sm dark:border-theme-dark-700": true,
                 [className]: true,
             })}
+            {...props}
         >
             <div
                 className={classNames({
