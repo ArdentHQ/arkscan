2f2a097c9cef0d524b5c50907b5a4327d682a14f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-02-10 01:31:02 +0000

    nullable recipient username

--- resources/inertia/Components/General/Addressing/AddressingGeneric.tsx
+++ resources/inertia/Components/General/Addressing/AddressingGeneric.tsx
@@ -19,7 +19,7 @@ export function AddressingGeneric({
     sender: string;
     senderUsername?: string;
     recipient: string;
-    recipientUsername?: string;
+    recipientUsername?: string | null;
     contractAddress: string;
     withTruncate?: boolean;
     disableTooltip?: boolean;
