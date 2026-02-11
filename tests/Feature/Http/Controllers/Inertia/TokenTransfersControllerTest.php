bfcbb2ca2d8e3c65764fba7e901829dc9741c6e9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-02-11 11:29:17 +0000

    test

--- tests/Feature/Http/Controllers/Inertia/TokenTransfersControllerTest.php
+++ tests/Feature/Http/Controllers/Inertia/TokenTransfersControllerTest.php
@@ -54,7 +54,7 @@ it('should provide the no results message if no transfers exist', function () {
     );
 });
 
-it('should not include contract deployment transactions', function () {
+it('should include contract deployment transactions', function () {
     TokenTransfer::factory(3)->create();
 
     $contractDeployment = Transaction::factory()->contractDeployment()->create();
