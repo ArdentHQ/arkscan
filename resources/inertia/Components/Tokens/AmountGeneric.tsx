58e40c04f298ae7e3c580cdab696742c6d1514f6
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-02-18 11:16:14 -0600

    style: use absolute import for Tooltip

--- resources/inertia/Components/Tokens/AmountGeneric.tsx
+++ resources/inertia/Components/Tokens/AmountGeneric.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import AmountSmall from "../General/AmountSmall";
 import { formatCompact, networkCurrency } from "@/utils/number-formatter";
-import Tooltip from "../General/Tooltip";
+import Tooltip from "@/Components/General/Tooltip";
 
 export default function AmountGeneric({
     amount,
