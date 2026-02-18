b10bb6ce5a13902d2e1af306aec4fdc433f20355
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-02-18 05:44:39 -0500

    fix: horizontal padding to error pages & image (#1495)

--- resources/inertia/Pages/Error/Show.tsx
+++ resources/inertia/Pages/Error/Show.tsx
@@ -34,7 +34,7 @@ export default function ErrorShow({ error, status }: { error?: string; status: n
     }
 
     return (
-        <Layout>
+        <Layout className="mx:px-10 px-6 py-8">
             <div className="text-center">
                 <div className="mx-auto max-w-error-image">
                     {createElement(image, { className: "light-dark-icon h-full w-full" })}
