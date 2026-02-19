57436e46c392505d130a24a55c12f4164991cf60
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-02-19 04:56:58 +0000

    bignumber toFloat helper

---
+++ resources/inertia/utils/big-number.ts
@@ -0,0 +1,12 @@
+import BigNumber from 'bignumber.js';
+
+export default BigNumber;
+
+export function toFloat(value: BigNumber.Value, divisor: number = 1e18, scale?: number): number {
+    let result = new BigNumber(value).dividedBy(divisor);
+    if (scale !== undefined) {
+        result = result.precision(scale, BigNumber.ROUND_DOWN);
+    }
+
+    return result.toNumber();
+}
