752035de72c8d8da622b2e98c1c5d5720ad92900
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-02-23 15:53:47 +0000

    style: resolve style guide violations

--- app/ViewModels/Concerns/Transaction/HasMethod.php
+++ app/ViewModels/Concerns/Transaction/HasMethod.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\ViewModels\Concerns\Transaction;
 
-use App\Services\Transactions\TransactionMethod;
-
 trait HasMethod
 {
     public function isTransfer(): bool
