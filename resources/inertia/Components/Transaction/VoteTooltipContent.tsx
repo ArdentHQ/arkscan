18898a03cc95390dbf202cfd4467e4f814cf47a1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-02-25 19:17:42 +0000

    fix: vote tooltip content

---
+++ resources/inertia/Components/Transaction/VoteTooltipContent.tsx
@@ -0,0 +1,23 @@
+export default function VoteTooltipContent({
+    variant,
+    validator,
+}: {
+    variant: "vote" | "voting";
+    validator: string;
+}) {
+    return (
+        <span className="break-words font-semibold text-theme-secondary-500">
+            {variant === "vote" ? (
+                <>
+                    <span>Vote:</span>
+                    <span className="ml-1 text-white">{validator}</span>
+                </>
+            ) : (
+                <>
+                    <span>Voting for </span>
+                    <span className="text-white">{validator}</span>
+                </>
+            )}
+        </span>
+    );
+}
