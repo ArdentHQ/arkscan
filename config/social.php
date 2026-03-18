68213d91afc5425bdbdc0d3baeca33ce34c17a9e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-03-18 17:05:51 +0000

    refactor: footer social link accessibility

--- config/social.php
+++ config/social.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 return [
    'networks' => [
         'twitter' => [
-            'icon' => 'brands.x',
-            'url'  => 'https://x.com/Ardent_HQ',
+            'icon'  => 'brands.x',
+            'url'   => 'https://x.com/Ardent_HQ',
+            'label' => 'X (Twitter)',
         ],
 
-        'github'  => [
-            'icon' => 'brands.solid.github',
-            'url'  => 'https://github.com/ArdentHQ',
+        'github' => [
+            'icon'  => 'brands.solid.github',
+            'url'   => 'https://github.com/ArdentHQ',
+            'label' => 'GitHub',
         ],
    ],
 ];
