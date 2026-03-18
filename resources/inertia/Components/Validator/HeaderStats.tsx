3c194986a06a5baf2f048304d12e360e77f28fe9
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-03-18 19:43:19 +0000

    style: resolve style guide violations

--- resources/inertia/Components/Validator/HeaderStats.tsx
+++ resources/inertia/Components/Validator/HeaderStats.tsx
@@ -35,11 +35,19 @@ function ExploreHeaderStat() {
 
                     <img src={headerBgDark} className="hidden max-w-none dim:sm:hidden dark:sm:block" alt="" />
 
-                    <img src={headerBgMobileDark} className="hidden max-w-none dim:hidden dark:block dark:sm:hidden" alt="" />
+                    <img
+                        src={headerBgMobileDark}
+                        className="hidden max-w-none dim:hidden dark:block dark:sm:hidden"
+                        alt=""
+                    />
 
                     <img src={headerBgDim} className="hidden max-w-none dark:hidden dim:sm:block" alt="" />
 
-                    <img src={headerBgMobileDim} className="hidden max-w-none dim:block dark:hidden dim:sm:hidden" alt="" />
+                    <img
+                        src={headerBgMobileDim}
+                        className="hidden max-w-none dim:block dark:hidden dim:sm:hidden"
+                        alt=""
+                    />
                 </>
             }
         >
