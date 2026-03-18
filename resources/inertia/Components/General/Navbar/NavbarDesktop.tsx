5a825ae264b245d7d17170ae8e04ee655f538b77
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-18 08:52:17 -0600

    fix: reduce spacing between nav dropdowns

--- resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
+++ resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
@@ -58,7 +58,7 @@ export default function NavbarDesktop({ navigation }: { navigation: Navigation }
                                     <div
                                         key={index}
                                         className={classNames("relative h-full", {
-                                            "ml-3": navItem.children,
+                                            "ml-1": navItem.children,
                                         })}
                                     >
                                         {navItem.children ? (
