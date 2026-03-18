cfcba714d448330d2d688fe0139f604ef0df5182
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-18 08:29:24 -0600

    fix: tighten dropdown spacing

--- resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
+++ resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
@@ -59,7 +59,7 @@ export default function NavbarDesktop({ navigation }: { navigation: Navigation }
                                         {navItem.children ? (
                                             <DropdownProvider>
                                                 <Dropdown
-                                                    wrapperClass="relative h-full mr-4"
+                                                    wrapperClass="relative h-full mr-3"
                                                     useDefaultButtonClasses={false}
                                                     buttonClass="inline-flex h-full px-2 focus-visible:ring-inset"
                                                     dropdownClasses="w-auto"
