ec6d9884d7711b4e9568b5146579267f3c928078
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-18 08:43:12 -0600

    fix: remove right margin from last nav item

--- resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
+++ resources/inertia/Components/General/Navbar/NavbarDesktop.tsx
@@ -53,13 +53,18 @@ export default function NavbarDesktop({ navigation }: { navigation: Navigation }
                 <div className="flex items-center space-x-3">
                     <div className="flex justify-end">
                         <div className="flex flex-1 items-center justify-end sm:items-stretch sm:justify-between">
-                            <div className="-ml-4 -mr-3 hidden h-[3.25rem] items-center sm:h-16 md:flex">
+                            <div className="-mx-4 hidden h-[3.25rem] items-center sm:h-16 md:flex">
                                 {navigation.map((navItem, index) => (
-                                    <div key={index} className="relative h-full">
+                                    <div
+                                        key={index}
+                                        className={classNames("relative h-full", {
+                                            "mr-3": navItem.children && index < navigation.length - 1,
+                                        })}
+                                    >
                                         {navItem.children ? (
                                             <DropdownProvider>
                                                 <Dropdown
-                                                    wrapperClass="relative h-full mr-3"
+                                                    wrapperClass="relative h-full"
                                                     useDefaultButtonClasses={false}
                                                     buttonClass="inline-flex h-full px-2 focus-visible:ring-inset"
                                                     dropdownClasses="w-auto"
