3f1dfc0a689cc6eedb359004ef70f172eba15b0c
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-20 12:08:15 -0600

    style: fix copy icon color in light mode on addresses mobile

--- resources/inertia/Components/Tables/Mobile/Bookmarks/Addresses.tsx
+++ resources/inertia/Components/Tables/Mobile/Bookmarks/Addresses.tsx
@@ -32,7 +32,7 @@ function Row({ wallet }: { wallet: IWallet }) {
                         <Clipboard
                             value={wallet.address}
                             noStyling
-                            className="transition-default flex items-center text-theme-secondary-700 hover:text-theme-primary-700 dark:text-theme-dark-300 dark:hover:text-theme-dark-50"
+                            className="transition-default flex items-center text-theme-primary-400 hover:text-theme-primary-700 dark:text-theme-dark-300 dark:hover:text-theme-dark-50"
                             tooltipContent={t("pages.wallet.address_copied")}
                             checkmarksClass=""
                         />
