9c99c51f3d666693d5fa73da8e67cb15b3095c20
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-20 12:45:53 -0600

    style: use light blue icon on hover+select

--- resources/inertia/Components/General/BookmarkButton.tsx
+++ resources/inertia/Components/General/BookmarkButton.tsx
@@ -38,7 +38,7 @@ export default function BookmarkButton({
                     className={classNames(
                         "transition-default h-4 w-4",
                         isBookmarked
-                            ? "fill-theme-primary-600 text-theme-primary-600 group-hover/bookmark:fill-none group-hover/bookmark:text-white dark:fill-theme-dark-blue-500 dark:text-theme-dark-blue-500 dark:group-hover/bookmark:fill-none dark:group-hover/bookmark:text-white"
+                            ? "fill-theme-primary-600 text-theme-primary-600 group-hover/bookmark:fill-none group-hover/bookmark:text-theme-primary-300 dark:fill-theme-dark-blue-500 dark:text-theme-dark-blue-500 dark:group-hover/bookmark:fill-none dark:group-hover/bookmark:text-theme-primary-300"
                             : "text-theme-secondary-700 group-hover/bookmark:text-white dark:text-theme-dark-200 dark:group-hover/bookmark:text-white",
                     )}
                 />
