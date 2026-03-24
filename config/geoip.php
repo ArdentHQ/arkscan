0b4c26e4004cd670022da2a7606631017968a5fc
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-24 09:39:15 -0600

    style: fix formatting

--- config/geoip.php
+++ config/geoip.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
 
     /*
@@ -50,37 +52,37 @@ return [
     'services' => [
 
         'maxmind_database' => [
-            'class' => \Torann\GeoIP\Services\MaxMindDatabase::class,
+            'class'         => Torann\GeoIP\Services\MaxMindDatabase::class,
             'database_path' => storage_path('app/geoip.mmdb'),
-            'update_url' => sprintf('https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz', env('MAXMIND_LICENSE_KEY')),
-            'locales' => ['en'],
+            'update_url'    => sprintf('https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz', env('MAXMIND_LICENSE_KEY')),
+            'locales'       => ['en'],
         ],
 
         'maxmind_api' => [
-            'class' => \Torann\GeoIP\Services\MaxMindWebService::class,
-            'user_id' => env('MAXMIND_USER_ID'),
+            'class'       => Torann\GeoIP\Services\MaxMindWebService::class,
+            'user_id'     => env('MAXMIND_USER_ID'),
             'license_key' => env('MAXMIND_LICENSE_KEY'),
-            'locales' => ['en'],
+            'locales'     => ['en'],
         ],
 
         'ipgeolocation' => [
-            'class' => \Torann\GeoIP\Services\IPGeoLocation::class,
-            'secure' => true,
-            'key' => env('IPGEOLOCATION_KEY'),
+            'class'          => Torann\GeoIP\Services\IPGeoLocation::class,
+            'secure'         => true,
+            'key'            => env('IPGEOLOCATION_KEY'),
             'continent_path' => storage_path('app/continents.json'),
-            'lang' => 'en',
+            'lang'           => 'en',
         ],
 
         'ipdata' => [
-            'class' => \Torann\GeoIP\Services\IPData::class,
-            'key' => env('IPDATA_API_KEY'),
+            'class'  => Torann\GeoIP\Services\IPData::class,
+            'key'    => env('IPDATA_API_KEY'),
             'secure' => true,
         ],
 
         'ipfinder' => [
-            'class' => \Torann\GeoIP\Services\IPFinder::class,
-            'key' => env('IPFINDER_API_KEY'),
-            'secure' => true,
+            'class'   => Torann\GeoIP\Services\IPFinder::class,
+            'key'     => env('IPFINDER_API_KEY'),
+            'secure'  => true,
             'locales' => ['en'],
         ],
 
@@ -137,19 +139,19 @@ return [
     */
 
     'default_location' => [
-        'ip' => '127.0.0.0',
-        'iso_code' => 'US',
-        'country' => 'United States',
-        'city' => 'New Haven',
-        'state' => 'CT',
-        'state_name' => 'Connecticut',
+        'ip'          => '127.0.0.0',
+        'iso_code'    => 'US',
+        'country'     => 'United States',
+        'city'        => 'New Haven',
+        'state'       => 'CT',
+        'state_name'  => 'Connecticut',
         'postal_code' => '06510',
-        'lat' => 41.31,
-        'lon' => -72.92,
-        'timezone' => 'America/New_York',
-        'continent' => 'NA',
-        'default' => true,
-        'currency' => 'USD',
+        'lat'         => 41.31,
+        'lon'         => -72.92,
+        'timezone'    => 'America/New_York',
+        'continent'   => 'NA',
+        'default'     => true,
+        'currency'    => 'USD',
     ],
 
 ];
