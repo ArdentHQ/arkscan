199ee42a6e8e96f75e21942851578bf8b86eca76
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-26 13:40:36 -0600

    refactor: remove redundant name from renderWithMeta

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ namespace App\Providers;
 use App\Contracts\MarketDataProvider;
 use App\Contracts\Services\GasTracker as GasTrackerContract;
 use App\Contracts\Services\Monitor\MissedBlocksCalculator as MissedBlocksCalculatorContract;
-use App\Facades\Network;
 use App\Services\BigNumber;
 use App\Services\GasTracker;
 use App\Services\Monitor\MissedBlocksCalculator;
@@ -103,11 +102,7 @@ final class AppServiceProvider extends ServiceProvider
         InertiaResponseFactory::macro('renderWithMeta', function (string $component, string $pageName, array $props = [], array $detail = []) {
             /** @var InertiaResponseFactory $this */
             return $this->render($component, $props)
-                ->withMeta($pageName, [
-                    'name' => Network::currency(),
-
-                    ...$detail,
-                ]);
+                ->withMeta($pageName, $detail);
         });
     }
 
