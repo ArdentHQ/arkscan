dfccc8de971034295bf6e3e26461d5b14d4794da
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-27 08:37:51 -0600

    feat: add meta image

--- resources/lang/en/metatags.php
+++ resources/lang/en/metatags.php
@@ -139,5 +139,6 @@ return [
     'peers-map'       => [
         'title'       => 'ARK Node Map | :name Blockchain Explorer',
         'description' => 'Geographic distribution of peers making up the :name blockchain network.',
+        'image'       => 'metadata/peers.png',
     ],
 ];
