249214520be1f850d6fcb1edd335ee8d06593123
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-03-27 07:58:56 -0600

    refactor: adjust active peers count color

--- resources/inertia/Pages/PeersMap/Index.tsx
+++ resources/inertia/Pages/PeersMap/Index.tsx
@@ -22,14 +22,14 @@ export default function PeersMapIndex({ peers }: PageProps<PeersMapProps>) {
             <div className="px-6 md:mx-auto md:max-w-7xl md:px-10">
                 <div className="rounded border border-theme-secondary-300 p-6 dark:border-theme-dark-700 md:rounded-xl">
                     <div className="mb-4 flex items-center">
-                        <span className="inline-flex items-center gap-2 rounded-full border-2 border-white bg-gray-200 px-3 py-1.5 text-sm dark:border-theme-dark-900 dark:bg-theme-dark-950">
-                            <span className="h-2.5 w-2.5 rounded-full bg-blue-600 dark:bg-theme-dark-blue-600" />
+                        <span className="inline-flex items-center gap-2 rounded-lg px-3 py-2 border-2 bg-theme-secondary-200 border-white text-sm dark:border-theme-dark-900 dark:bg-theme-dark-950">
+                            <span className="h-2 w-2 outline-2 rounded-full bg-theme-primary-600 outline-white outline dark:bg-theme-dark-blue-600 dark:outline-theme-dark-900" />
 
-                            <span className="text-gray-700 dark:text-theme-dark-200">
+                            <span className="font-semibold text-theme-secondary-700 dark:text-theme-dark-200 text-sm">
                                 {t("pages.peers-map.active_peers")}
                             </span>
 
-                            <span className="font-semibold text-gray-900 dark:text-theme-dark-50">
+                            <span className="font-semibold text-theme-secondary-900 dark:text-theme-dark-50 text-sm">
                                 {peers.length}
                             </span>
                         </span>
