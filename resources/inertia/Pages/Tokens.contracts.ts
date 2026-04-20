81dfb5e990fbf2bc06a3750218ce846e5bbd1090
alexbarnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-04-20 19:24:06 +0000

    style: resolve style guide violations

--- resources/inertia/Pages/Tokens.contracts.ts
+++ resources/inertia/Pages/Tokens.contracts.ts
@@ -2,7 +2,6 @@ import { IPaginatedResponse } from "@/types";
 import { ITokenAction } from "@/types/generated";
 import { PageProps } from "@inertiajs/core";
 
-export interface TokenActionsProps
-    extends PageProps<{
-        transfers?: IPaginatedResponse<ITokenAction>;
-    }> {}
+export interface TokenActionsProps extends PageProps<{
+    transfers?: IPaginatedResponse<ITokenAction>;
+}> {}
--- resources/inertia/Pages/Transactions.contracts.ts
+++ resources/inertia/Pages/Transactions.contracts.ts
@@ -2,22 +2,21 @@ import { IPaginatedResponse } from "@/types";
 import { ITransaction } from "@/types/generated";
 import { PageProps } from "@inertiajs/core";
 
-export interface TransactionsProps
-    extends PageProps<{
-        statistics: {
-            transactionCount: number;
-            volume: number;
-            totalFees: number;
-            averageFee: number;
-        };
-        transactions?: IPaginatedResponse<ITransaction>;
-        filters: {
-            transfers: boolean;
-            multipayments: boolean;
-            votes: boolean;
-            validator: boolean;
-            username: boolean;
-            contract_deployment: boolean;
-            others: boolean;
-        };
-    }> {}
+export interface TransactionsProps extends PageProps<{
+    statistics: {
+        transactionCount: number;
+        volume: number;
+        totalFees: number;
+        averageFee: number;
+    };
+    transactions?: IPaginatedResponse<ITransaction>;
+    filters: {
+        transfers: boolean;
+        multipayments: boolean;
+        votes: boolean;
+        validator: boolean;
+        username: boolean;
+        contract_deployment: boolean;
+        others: boolean;
+    };
+}> {}
