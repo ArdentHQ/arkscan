72b292bff90cc01bccea6eac98fae5efcd1c8c10
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-04-22 13:56:13 +0100

    chore: major update of js dependencies & node 24 in workflows (#1584)

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,12 +4,15 @@ module.exports = {
     testEnvironment: 'node',
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/resources/inertia/$1',
+        '^@inertiajs/react$': '<rootDir>/node_modules/@inertiajs/react/dist/index.js',
+        '^@inertiajs/core$': '<rootDir>/node_modules/@inertiajs/core/dist/index.js',
     },
     transform: {
-        '^.+\\.tsx?$': ['ts-jest', {
+        '^.+\\.[tj]sx?$': ['ts-jest', {
             tsconfig: './tsconfig.test.json',
         }],
     },
+    transformIgnorePatterns: [],
     testMatch: ['<rootDir>/resources/inertia/**/__tests__/**/*.test.ts'],
     testPathIgnorePatterns: ['/node_modules/', '/vendor/'],
 };
