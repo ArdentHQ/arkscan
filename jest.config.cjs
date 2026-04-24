fa17e08d4abd904699368459f119564e19c2ec98
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-04-24 07:59:25 -0600

    chore: simplify jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,17 +4,12 @@ module.exports = {
     testEnvironment: 'node',
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/resources/inertia/$1',
-        '^@inertiajs/react$': '<rootDir>/node_modules/@inertiajs/react/dist/index.js',
-        '^@inertiajs/core$': '<rootDir>/node_modules/@inertiajs/core/dist/index.js',
     },
     transform: {
-        '^.+\\.[jt]sx?$': ['ts-jest', {
+        '^.+\\.tsx?$': ['ts-jest', {
             tsconfig: './tsconfig.test.json',
         }],
     },
-    transformIgnorePatterns: [
-        'node_modules/(?!(@inertiajs|es-toolkit)/)',
-    ],
     testMatch: ['<rootDir>/resources/inertia/**/__tests__/**/*.test.ts'],
     testPathIgnorePatterns: ['/node_modules/', '/vendor/'],
 };
