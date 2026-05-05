8e75114fe9b76cee00d1286ecf43743d55dcfcaf
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-05-05 07:11:42 -0600

    fix: align bookmark button height with siblings

--- resources/inertia/Components/General/BookmarkButton.tsx
+++ resources/inertia/Components/General/BookmarkButton.tsx
@@ -44,7 +44,7 @@ export default function BookmarkButton({
                 className={classNames(
                     "group/bookmark transition-default flex items-center justify-center focus-visible:ring-inset",
                     variant === "button" && [
-                        "rounded border p-2",
+                        "rounded border p-[7px]",
                         bookmarked
                             ? "border-theme-primary-400 bg-theme-primary-100 hover:border-theme-primary-700 hover:bg-theme-primary-700 dim:border-theme-dark-600 dim:bg-theme-dark-800 dark:border-theme-dark-blue-800 dark:bg-theme-dark-blue-900 dark:hover:border-theme-primary-700 dark:hover:bg-theme-primary-700"
                             : "border-theme-secondary-300 hover:border-theme-primary-700 hover:bg-theme-primary-700 dark:border-theme-dark-700 dark:bg-theme-dark-900 dark:hover:border-theme-primary-700 dark:hover:bg-theme-primary-700 bg-white",
