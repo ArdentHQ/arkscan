8a781f0d6de7691ecf0a319c1925150b4343991b
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-05-11 07:39:15 -0600

    chore: make tab layout full width on mobile (#1596)

--- resources/inertia/Components/Tabs/Wrapper.tsx
+++ resources/inertia/Components/Tabs/Wrapper.tsx
@@ -25,7 +25,7 @@ export default function Wrapper({ tabs, ariaLabel }: { tabs: ITab[]; ariaLabel?:
     }, []);
 
     return (
-        <div className="relative px-0 sm:mb-3 sm:px-6 md:mx-auto md:max-w-7xl md:px-10">
+        <div className="relative px-0 md:mb-3 md:px-10 lg:mx-auto lg:max-w-7xl">
             {showOverflowIndicators && (
                 <>
                     <div className="to-theme-secondary-200/0 dark:to-theme-dark-950/0 from-theme-secondary-200 dark:from-theme-dark-950 pointer-events-none absolute top-0 left-0 z-20 h-12 h-full w-12 bg-gradient-to-r"></div>
@@ -35,10 +35,10 @@ export default function Wrapper({ tabs, ariaLabel }: { tabs: ITab[]; ariaLabel?:
 
             <div
                 ref={containerRef}
-                className="no-scrollbar bg-theme-secondary-200 dark:bg-theme-dark-950 mb-6 w-screen overflow-scroll px-6 py-2 sm:mb-4 sm:w-auto sm:bg-transparent! sm:px-0 sm:py-0 md:mb-0"
+                className="no-scrollbar bg-theme-secondary-200 dark:bg-theme-dark-950 mb-6 w-screen overflow-scroll px-6 py-2 md:mb-4 md:w-auto md:bg-transparent! md:px-0 md:py-0 lg:mb-0"
             >
-                <div className="bg-theme-secondary-200 dark:bg-theme-dark-950 relative z-10 inline-flex items-center justify-between rounded-xl sm:p-1">
-                    <div role="tablist" aria-label={ariaLabel} className="flex space-x-1 px-0! pr-6 sm:pr-0">
+                <div className="bg-theme-secondary-200 dark:bg-theme-dark-950 relative z-10 inline-flex items-center justify-between rounded-xl md:p-1">
+                    <div role="tablist" aria-label={ariaLabel} className="flex space-x-1 px-0! pr-6 md:pr-0">
                         {tabs.map((tab) => (
                             <Tab key={tab.value} text={tab.text} value={tab.value} />
                         ))}
